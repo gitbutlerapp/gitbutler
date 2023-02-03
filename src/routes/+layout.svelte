bd558f4c2004e255f204ee8d2d2dae3832bcfd9d
Kiril Videlov <kiril@videlov.com>
2023-02-03 10:13:32 +0100

    make the app window draggable again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,10 @@
     };
 </script>
 
-<header data-tauri-drag-region class="ml-16 h-8">
+<header data-tauri-drag-region class="h-6">
+</header>
+
+<main class="p-2">
     <nav class="flex flex-row m-2">
         <ul class="flex-1 flex flex-row gap-2 overflow-x-scroll">
             {#each $projects as project}
@@ -53,8 +56,5 @@
             new project
         </button>
     </nav>
-</header>
-
-<main class="p-2">
     <slot />
 </main>
