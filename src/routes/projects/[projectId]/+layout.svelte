4a48ec5160a6356ca8027ed6995deebc706b2ddd
Kiril Videlov <kiril@videlov.com>
2023-02-14 09:13:56 +0100

    Improve project layout navigation
    - Add link to last session in the project layout
    - Change "Session" link to an anchor if there is a last session
    
    [src/routes/projects/[projectId]/+layout.svelte]
    - Add a `sessions` variable to the layout
    - Add a link to the last session in the layout
    - Change the "Session" link to an anchor if there is a last session

--- src/routes/projects/[projectId]/+layout.svelte
+++ src/routes/projects/[projectId]/+layout.svelte
@@ -3,12 +3,20 @@
     export let data: LayoutData;
 
     $: project = data.project;
+    $: sessions = data.sessions;
+    $: lastSessionId = $sessions[$sessions.length - 1]?.id;
 </script>
 
 <div class="p-3 flex flex-row space-x-3 text-zinc-500 text-lg select-none">
     <div>Week</div>
     <a href="/projects/{$project?.id}" class="hover:text-zinc-300">Day</a>
-    <div title="go to current session">Session</div>
+    {#if lastSessionId}
+        <a
+            href="/projects/{$project?.id}/sessions/{lastSessionId}"
+            class="hover:text-zinc-300"
+            title="go to current session">Session</a
+        >
+    {/if}
 </div>
 
 <slot />
