24398abde25083e24b101c0713f072fc647de0aa
Scott Chacon <schacon@gmail.com>
2023-02-16 10:59:33 +0100

    again!

--- scripts/release.sh
+++ scripts/release.sh
@@ -163,10 +163,9 @@ info "moving artifacts..."
 
 echo "pwd: $(pwd)"
 
-pushd "gitbutler-client"
 find .
 
-BUNDLE_DIR="./src-tauri/target/release/bundle"
+BUNDLE_DIR="./gitbutler-client/src-tauri/target/release/bundle"
 MACOS_DMG="$(find "$BUNDLE_DIR/dmg" -depth 1 -type f -name "*.dmg")"
 MACOS_UPDATER="$(find "$BUNDLE_DIR/macos" -depth 1 -type f -name "*.tar.gz")"
 MACOS_UPDATER_SIG="$(find "$BUNDLE_DIR/macos" -depth 1 -type f -name "*.tar.gz.sig")"
