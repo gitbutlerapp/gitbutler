f92a2370bde6d1685127543f02d8a3c21c42220e
Kiril Videlov <kiril@videlov.com>
2023-02-20 12:34:07 +0100

    Add date-fns for better date calculations
    - Add date-fns package to project
    - Update dependencies in pnpm-lock.yaml
    - Replace manual date calculations with date-fns functions
    - Add functions to Week class for getting the next/previous week and the nth day of the week
    
    [pnpm-lock.yaml]
    - Add `date-fns` dependency to `pnpm-lock.yaml`
    - Update `@codemirror/state`, `@codemirror/view`, `@tauri-apps/api`, `idb-keyval`, `mm-jsr`, and `seti-icons` dependencies in `pnpm-lock.yaml`
    - Add `date-fns` package to `pnpm-lock.yaml`
    [src/lib/week.ts]
    - Replace manual date calculations with `date-fns` functions
    - Make `Week.from` use `weekStartsOn` to set the start of the week
    - Add `Week.next`, `Week.previous` and `Week.nThDay` functions
    [package.json]
    - Add date-fns package

--- src/lib/week.ts
+++ src/lib/week.ts
@@ -1,33 +1,20 @@
+import { startOfWeek, endOfWeek, addWeeks, subWeeks, addDays } from 'date-fns';
+
 export type Week = {
     start: Date;
     end: Date;
 };
 export namespace Week {
     export const from = (date: Date): Week => {
-        const start = new Date(date);
-        start.setHours(0, 0, 0, 0);
-        start.setDate(start.getDate() - start.getDay() + 1); // Start on Monday
-        const end = new Date(start);
-        end.setDate(end.getDate() + 7);
-        return { start, end };
+        return { start: startOfWeek(date, { weekStartsOn: 1 }), end: endOfWeek(date) };
     };
     export const next = (week: Week): Week => {
-        const start = new Date(week.start);
-        start.setDate(start.getDate() + 7);
-        const end = new Date(week.end);
-        end.setDate(end.getDate() + 7);
-        return { start, end };
+        return { start: addWeeks(week.start, 1), end: addWeeks(week.end, 1) };
     };
     export const previous = (week: Week): Week => {
-        const start = new Date(week.start);
-        start.setDate(start.getDate() - 7);
-        const end = new Date(week.end);
-        end.setDate(end.getDate() - 7);
-        return { start, end };
+        return { start: subWeeks(week.start, 1), end: subWeeks(week.end, 1) };
     };
     export const nThDay = (week: Week, n: number): Date => {
-        const date = new Date(week.start);
-        date.setDate(date.getDate() + n);
-        return date;
+        return addDays(week.start, n)
     };
 }
