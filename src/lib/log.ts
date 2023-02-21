51aa80cfd9aa0295033cf556aeceaeba19c3cbe8
Nikita Galaiko <nikita@galaiko.rocks>
2023-02-21 11:23:31 +0100

    better logging

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -2,11 +2,20 @@ export { attachConsole as setup } from "tauri-plugin-log-api";
 
 import * as log from "tauri-plugin-log-api";
 
-export const debug = (...args: any[]) =>
-    log.debug(args.map((argument) => JSON.stringify(argument)).join(" "));
+const toString = (value: any) => {
+    if (value instanceof Error) {
+        return value.message;
+    } else if (typeof value === "object") {
+        return JSON.stringify(value);
+    } else {
+        return value.toString();
+    }
+};
 
-export const info = (...args: any[]) =>
-    log.info(args.map((argument) => JSON.stringify(argument)).join(" "));
+export const debug = (...args: any[]) =>
+    log.debug(args.map(toString).join(" "));
+
+export const info = (...args: any[]) => log.info(args.map(toString).join(" "));
 
 export const error = (...args: any[]) =>
-    log.error(args.map((argument) => JSON.stringify(argument)).join(" "));
+    log.error(args.map(toString).join(" "));
