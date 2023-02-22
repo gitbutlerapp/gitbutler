c8f2412b9eea4c85b30bb33f08e8e6095ed8647c
Kiril Videlov <kiril@videlov.com>
2023-02-22 22:45:58 +0100

    🤦 Fix performance & completions issues
    Linear GB-43
    
    - Enable performance tracking for PostHog
    - Add a console log statement
    
    [src/hooks.client.ts]
    - Change PostHog initialization to include capture_performance setting
    - Add a console log statement

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,13 @@
-import posthog from "posthog-js"
+import posthog from "posthog-js";
 import { PUBLIC_POSTHOG_API_KEY } from "$env/static/public";
 
 async function setupPostHog() {
-    console.log("Initializing PostHog")
-    posthog.init(PUBLIC_POSTHOG_API_KEY, { api_host: 'https://eu.posthog.com' })
+    console.log("Initializing PostHog");
+    posthog.init(PUBLIC_POSTHOG_API_KEY, {
+        api_host: "https://eu.posthog.com",
+        capture_performance: false,
+    });
+    console.log("HERE");
 }
 
 // Initialize the database *once*
