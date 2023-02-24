05f699b22e43a631ad3c4c90ffc13045e59dd7a3
Scott Chacon <schacon@gmail.com>
2023-02-24 10:35:57 +0100

    wip: new home screen, blank slate and project list

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,32 +16,24 @@
     user.subscribe(posthog.identify);
 </script>
 
-<header
-    data-tauri-drag-region
-    class="sticky top-0 z-50 flex flex-row items-center h-8 overflow-hidden border-b select-none  text-zinc-400 border-zinc-700 bg-zinc-900 "
->
-    <div class="ml-24">
-        <BackForwardButtons />
-    </div>
-    <div class="ml-6"><Breadcrumbs /></div>
-    <div class="flex-grow" />
-    <a href="/users/" class="mr-4 font-medium hover:text-zinc-200"
-        >{$user ? $user.email : "Login"}</a
+<div class="flex flex-col h-0 min-h-full bg-zinc-800 text-zinc-400">
+    <header
+        data-tauri-drag-region
+        class="sticky top-0 z-50 flex flex-row items-center h-8 overflow-hidden border-b select-none  text-zinc-400 border-zinc-700 bg-zinc-900 "
     >
-</header>
-
-<div class="h-0 min-h-full bg-zinc-800 text-zinc-400">
-    <!-- <div class="flex-1"> -->
-    <slot />
-    <!-- </div> -->
-
-    <!-- <div
-        class="flex items-center flex-shrink-0 h-6 border-t select-none border-zinc-700 bg-zinc-900 "
-    >
-        <div class="flex flex-row items-center ml-4 space-x-2">
-            <div class="w-2 h-2 bg-green-700 rounded-full" />
-            <div>Up to date</div>
+        <div class="ml-24">
+            <BackForwardButtons />
         </div>
+        <div class="ml-6"><Breadcrumbs /></div>
+        <div class="flex-grow" />
+        <a href="/users/" class="mr-4 font-medium hover:text-zinc-200"
+            >{$user ? $user.email : "Login"}</a
+        >
+    </header>
+
+    <div class="flex-grow bg-zinc-800 text-zinc-400">
+        <!-- <div class="flex-1"> -->
+        <slot />
+        <!-- </div> -->
     </div>
-    <div id="foo" class="h-8" /> -->
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,36 +24,185 @@
     };
 </script>
 
-<div class="select-none p-16">
-    <div class="mb-6">
-        <h1 class="text-4xl text-zinc-200 mb-2">GitButler</h1>
-        <h2 class="text-2xl">Your Personal VCS Assistant</h2>
-    </div>
-
-    <div class="flex flex-row">
-        <div class="w-1/2">
-            <div class="text-xl text-zinc-200 mb-1">Start</div>
-            <div class="flex items-center space-x-2">
-                <div class="w-4 h-4"><FaFolderOpen /></div>
-                <button
-                    on:click={onAddLocalRepositoryClick}
-                    class="hover:text-zinc-200">Add a local repository...</button
-                >
-            </div>
-        </div>
-        <div class="w-1/2">
-            <div class="text-xl text-zinc-200 mb-1">Recent</div>
-            <div class="flex flex-col space-y-1">
-                {#each $projects as project}
-                    <div class="space-x-2">
-                        <a
-                            class="hover:text-zinc-200"
-                            href="/projects/{project.id}/">{project.title}</a
-                        >
-                        <span class="text-zinc-500">{project.path}</span>
+<div class="w-full h-full p-8">
+    <div class="flex flex-col h-full">
+        {#if $projects.length == 0}
+            <div class="h-fill grid grid-cols-2 gap-4 items-center h-full">
+                <!-- right box, welcome text -->
+                <div class="flex flex-col space-y-4 content-center p-4">
+                    <div class="text-xl text-zinc-300 p-0 m-0">
+                        <div class="font-bold">Welcome to GitButler.</div>
+                        <div class="text-lg text-zinc-300 mb-1">
+                            More than just version control.
+                        </div>
                     </div>
-                {/each}
+                    <div class="">
+                        GitButler is a tool to help you manage all the local
+                        work you do on your code projects.
+                    </div>
+                    <div class="">
+                        Think of us as a <strong>code concierge</strong>, a
+                        smart assistant for all the coding related tasks you
+                        need to do every day.
+                    </div>
+                    <ul class="text-zinc-400 pt-2 pb-4 space-y-4">
+                        <li class="flex flex-row space-x-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-8 h-8 flex-none"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+                                />
+                            </svg>
+
+                            <span class="text-zinc-200"
+                                >Automatically recording everything you do in
+                                any of your butlered projects.</span
+                            >
+                        </li>
+                        <li class="flex flex-row space-x-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-8 h-8 flex-none"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                                />
+                            </svg>
+
+                            <span class="text-zinc-200"
+                                >Simplifying all your Git work, including
+                                committing, branching and pushing, to be easy
+                                and intuitive.
+                            </span>
+                        </li>
+                        <li class="flex flex-row space-x-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-8 h-8 flex-none"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                            </svg>
+
+                            <span class="text-zinc-200"
+                                >Helping you not just search for strings or past
+                                commits, but find useful context in the story of
+                                your code.
+                            </span>
+                        </li>
+                    </ul>
+                    <div class="pt-6">
+                        <a
+                            target="_blank"
+                            href="https://help.gitbutler.com"
+                            class="text-base font-semibold leading-7 text-white bg-zinc-700 px-4 py-3 rounded-lg mt-4"
+                        >
+                            Learn more <span aria-hidden="true">→</span></a
+                        >
+                    </div>
+                </div>
+                <!-- left box, add a new project -->
+                <div class="text-center">
+                    <svg
+                        class="mx-auto h-12 w-12 text-gray-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            vector-effect="non-scaling-stroke"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                        />
+                    </svg>
+                    <h3 class="mt-2 text-lg font-semibold text-zinc-300">
+                        No projects
+                    </h3>
+                    <p class="mt-1 text-gray-500">
+                        Get started by tracking a project you're working on.
+                    </p>
+                    <div class="mt-6">
+                        <button
+                            on:click={onAddLocalRepositoryClick}
+                            type="button"
+                            class="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                        >
+                            Start Tracking a Project
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
+        {:else}
+            <div class="select-none p-8">
+                <div class="flex flex-col">
+                    <div class="flex flex-row justify-between">
+                        <div class="text-xl text-zinc-300 mb-1">
+                            My Projects
+                            <div class="text-lg text-zinc-500 mb-1">
+                                All the projects that I am currently helping you
+                                with.
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                on:click={onAddLocalRepositoryClick}
+                                type="button"
+                                class="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            >
+                                Track a New Project
+                            </button>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2 gap-4 mt-6">
+                        {#each $projects as project}
+                            <div
+                                class="flex flex-col space-x-2 bg-zinc-700 p-4 rounded-lg"
+                            >
+                                <a
+                                    class="hover:text-zinc-200 text-zinc-300 text-lg"
+                                    href="/projects/{project.id}/"
+                                    >{project.title}</a
+                                >
+                                <div class="text-zinc-300 font-mono">
+                                    {project.path}
+                                </div>
+                            </div>
+                        {/each}
+                    </div>
+                </div>
+            </div>
+
+            <div class="absolute bottom-0 left-0 w-full">
+                <div
+                    class="flex items-center flex-shrink-0 p-4 h-18 border-t select-none border-zinc-700 bg-zinc-900"
+                >
+                    <div class="text-sm text-zinc-300">Timeline</div>
+                </div>
+            </div>
+        {/if}
     </div>
 </div>
