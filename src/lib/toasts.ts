6e50d2f30de5406e4938825f837885d245090fb6
Nikita Galaiko <nikita@galaiko.rocks>
2023-02-24 09:30:44 +0100

    connect update user api

---
+++ src/lib/toasts.ts
@@ -0,0 +1,15 @@
+import toast, {
+    type ToastOptions,
+    type ToastPosition,
+} from "svelte-french-toast";
+
+const defaultOptions = {
+    position: "bottom-center" as ToastPosition,
+    style: "border-radius: 200px; background: #333; color: #fff;",
+};
+
+export const error = (msg: string, options: ToastOptions = {}) =>
+    toast.error(msg, { ...defaultOptions, ...options });
+
+export const success = (msg: string, options: ToastOptions = {}) =>
+    toast.success(msg, { ...defaultOptions, ...options });
