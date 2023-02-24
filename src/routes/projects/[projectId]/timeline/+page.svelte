d9dc79a4b4c91d5c0326c8740c09f60ee40d5215
Kiril Videlov <kiril@videlov.com>
2023-02-24 21:46:09 +0100

    Found this nice way of doing async derived stores

--- src/routes/projects/[projectId]/timeline/+page.svelte
+++ src/routes/projects/[projectId]/timeline/+page.svelte
@@ -2,9 +2,11 @@
 	import { themeIcons } from 'seti-icons';
 	import type { PageData } from './$types';
 	import { derived } from 'svelte/store';
+	import { asyncDerived } from '@square/svelte-store';
 	import type { Session } from '$lib/sessions';
 	import { startOfDay } from 'date-fns';
 	import { list as listDeltas } from '$lib/deltas';
+	import type { Delta } from '$lib/deltas';
 
 	export let data: PageData;
 	const { project, sessions } = data;
@@ -48,17 +50,36 @@
 		return svg;
 	}
 
-	$: dateSessions = derived([sessions], ([sessions]) => {
-		const dateSessions: Record<number, Session[]> = {};
-		sessions.forEach((session) => {
-			const date = startOfDay(new Date(session.meta.startTimestampMs));
+	type UISession = { session: Session; deltas: Record<string, Delta[]> };
+
+	$: dateSessions = asyncDerived([sessions], async ([sessions]) => {
+		const deltas = await Promise.all(
+			sessions.map((session) => {
+				return listDeltas({
+					projectId: $project?.id,
+					sessionId: session.id
+				});
+			})
+		);
+
+		const uiSessions = sessions
+			.map((session, i) => {
+				return { session, deltas: deltas[i] } as UISession;
+			})
+			.filter((uiSession) => {
+				return Object.keys(uiSession.deltas).length > 0;
+			});
+
+		const dateSessions: Record<number, UISession[]> = {};
+		uiSessions.forEach((uiSession) => {
+			const date = startOfDay(new Date(uiSession.session.meta.startTimestampMs));
 			if (dateSessions[date.getTime()]) {
-				dateSessions[date.getTime()]?.push(session);
+				dateSessions[date.getTime()]?.push(uiSession);
 			} else {
-				dateSessions[date.getTime()] = [session];
+				dateSessions[date.getTime()] = [uiSession];
 			}
 		});
-		// TODO: maybe sort?
+
 		return dateSessions;
 	});
 </script>
@@ -67,43 +88,48 @@
 	<div class="m-6 overflow-x-hidden w-full">
 		All sessions
 		<div class="flex flex-row border overflow-x-auto space-x-12 px-4 py-12">
-			{#each Object.entries($dateSessions) as [dateMilliseconds, sessions]}
-				<!-- Day -->
-				<div class="bg-zinc-600 py-1 min-w-full overflow-hidden">
-					<div>{formatDate(new Date(+dateMilliseconds))}</div>
-					<div class="flex space-x-2 ">
-						{#each sessions as session}
-							<!-- Session -->
-							<div>
-								<div class="text-sm rounded borded bg-orange-500 text-zinc-200">
-									{formatTime(new Date(session.meta.startTimestampMs))}
-									-
-									{formatTime(new Date(session.meta.lastTimestampMs))}
-								</div>
-								<div title="Session duration">
-									{Math.round(
-										(session.meta.lastTimestampMs - session.meta.startTimestampMs) / 1000 / 60
-									)} min
-								</div>
-								<div title="Session files">
-									{#await listDeltas( { projectId: $project?.id, sessionId: session.id } ) then deltas}
-										{#each Object.keys(deltas) as delta}
+			{#if $dateSessions === undefined}
+				<span>Loading...</span>
+			{:else}
+				{#each Object.entries($dateSessions) as [dateMilliseconds, uiSessions]}
+					<!-- Day -->
+					<div class="bg-zinc-600 py-1 min-w-full overflow-hidden">
+						<div>{formatDate(new Date(+dateMilliseconds))}</div>
+						<div class="flex space-x-2 ">
+							{#each uiSessions as uiSession}
+								<!-- Session -->
+								<div>
+									<div class="text-sm rounded borded bg-orange-500 text-zinc-200">
+										{formatTime(new Date(uiSession.session.meta.startTimestampMs))}
+										-
+										{formatTime(new Date(uiSession.session.meta.lastTimestampMs))}
+									</div>
+									<div title="Session duration">
+										{Math.round(
+											(uiSession.session.meta.lastTimestampMs -
+												uiSession.session.meta.startTimestampMs) /
+												1000 /
+												60
+										)} min
+									</div>
+									<div title="Session files">
+										{#each Object.keys(uiSession.deltas) as filePath}
 											<div class="flex flex-row w-32 items-center">
 												<div class="w-6 h-6 text-white fill-blue-400">
-													{@html pathToIconSvg(delta)}
+													{@html pathToIconSvg(filePath)}
 												</div>
 												<div class="text-white w-24 truncate">
-													{pathToName(delta)}
+													{pathToName(filePath)}
 												</div>
 											</div>
 										{/each}
-									{/await}
+									</div>
 								</div>
-							</div>
-						{/each}
+							{/each}
+						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			{/if}
 		</div>
 	</div>
 </div>
