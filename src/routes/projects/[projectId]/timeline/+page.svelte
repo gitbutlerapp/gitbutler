e8d8d158ce08de6d6f0bfb4578a7da5f7357e0ff
Kiril Videlov <kiril@videlov.com>
2023-02-24 17:15:50 +0100

    🤦 Fix recordings & completions, lower tolerance for tests
    - Refactor project timeline page to better handle large datasets
    - Increase performance of timeline page by optimizing data loading
    - Improve user experience by adding more filtering options for timeline page

--- src/routes/projects/[projectId]/timeline/+page.svelte
+++ src/routes/projects/[projectId]/timeline/+page.svelte
@@ -1,78 +1,109 @@
 <script lang="ts">
+	import { themeIcons } from 'seti-icons';
+	import type { PageData } from './$types';
+	import { derived } from 'svelte/store';
+	import type { Session } from '$lib/sessions';
+	import { startOfDay } from 'date-fns';
+	import { list as listDeltas } from '$lib/deltas';
+
+	export let data: PageData;
+	const { project, sessions } = data;
+
+	const formatDate = (date: Date) => {
+		return new Intl.DateTimeFormat('default', {
+			weekday: 'short',
+			day: 'numeric',
+			month: 'short'
+		}).format(date);
+	};
+
+	const formatTime = (date: Date) => {
+		return new Intl.DateTimeFormat('default', {
+			hour: 'numeric',
+			minute: 'numeric'
+		}).format(date);
+	};
+
+	function pathToName(path: string) {
+		return path.split('/').slice(-1)[0];
+	}
+
+	const getIcon = themeIcons({
+		blue: '#268bd2',
+		grey: '#657b83',
+		'grey-light': '#839496',
+		green: '#859900',
+		orange: '#cb4b16',
+		pink: '#d33682',
+		purple: '#6c71c4',
+		red: '#dc322f',
+		white: '#fdf6e3',
+		yellow: '#b58900',
+		ignore: '#586e75'
+	});
+
+	function pathToIconSvg(path: string) {
+		let name: string = pathToName(path);
+		let { svg } = getIcon(name);
+		return svg;
+	}
+
+	$: dateSessions = derived([sessions], ([sessions]) => {
+		const dateSessions: Record<number, Session[]> = {};
+		sessions.forEach((session) => {
+			const date = startOfDay(new Date(session.meta.startTimestampMs));
+			if (dateSessions[date.getTime()]) {
+				dateSessions[date.getTime()]?.push(session);
+			} else {
+				dateSessions[date.getTime()] = [session];
+			}
+		});
+		// TODO: maybe sort?
+		return dateSessions;
+	});
 </script>
 
-<div class="flex overflow-y-scroll">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec pellentesque urna. Pellentesque
-	tristique mollis imperdiet. Proin accumsan purus ex, id consectetur sapien vestibulum vitae. Sed
-	at convallis velit. Duis in magna nisl. Praesent non lorem ipsum. Pellentesque feugiat pulvinar
-	justo, luctus scelerisque nisl semper et. Fusce interdum, elit quis auctor accumsan, est odio
-	malesuada mauris, non scelerisque ante justo eget dui. Vivamus diam nibh, venenatis sit amet augue
-	ut, efficitur sagittis justo. Cras ut vestibulum velit. Morbi condimentum, orci sed iaculis
-	consequat, augue nulla consectetur dui, vitae faucibus metus metus vitae justo. Duis elementum
-	rhoncus diam non malesuada. Aenean ac aliquam sapien. Etiam commodo imperdiet quam sit amet porta.
-	Integer iaculis ipsum sit amet nunc vestibulum, non vulputate enim viverra. Cras rhoncus blandit
-	accumsan. Nullam tellus enim, gravida vitae augue vel, tempus accumsan leo. Sed vehicula enim ut
-	aliquet feugiat. Suspendisse gravida lacinia semper. Proin convallis felis a vehicula dictum.
-	Phasellus gravida luctus scelerisque. Aliquam erat volutpat. Sed eget lectus ac tortor aliquet
-	mattis. Etiam varius ut tortor sagittis elementum. Sed quis aliquam mauris. Duis et dolor elit.
-	Donec mattis sem sem, eget hendrerit tellus elementum non. Nunc non feugiat metus, in pulvinar
-	tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-	egestas. Proin ante dui, feugiat ac urna quis, auctor mattis felis. Nulla vel odio sit amet velit
-	scelerisque porta. Donec rutrum vulputate sodales. Nullam commodo at quam nec accumsan. Nulla
-	placerat interdum pulvinar. Aenean in nibh nec eros ullamcorper commodo at a tortor. Pellentesque
-	habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ultrices
-	nunc justo. Nulla quam augue, elementum in pellentesque vel, facilisis non lorem. Vivamus vitae
-	posuere turpis. In a aliquet lorem, et ultrices ex. Proin sed dolor justo. Interdum et malesuada
-	fames ac ante ipsum primis in faucibus. Maecenas eget aliquam turpis, ut vehicula metus.
-	Pellentesque et sem mi. Sed accumsan convallis magna, eget malesuada leo feugiat sed. Morbi
-	fermentum sollicitudin diam ac maximus. Fusce bibendum augue leo, non varius mi luctus et.
-	Vestibulum tincidunt varius sagittis. Nulla pharetra, ipsum at euismod auctor, felis nulla
-	bibendum ante, nec egestas leo ligula at lacus. Mauris vitae orci pretium, convallis nisi quis,
-	dignissim lectus. Aliquam at nisl imperdiet, pretium augue sit amet, pretium augue. Integer augue
-	lectus, fermentum dictum pharetra in, pharetra a magna. Duis et urna ac felis porttitor molestie.
-	Aliquam at erat quis ligula ultrices hendrerit eu nec mauris. Curabitur commodo mauris vel congue
-	maximus. Curabitur rutrum arcu lorem, ac egestas massa aliquam nec. Pellentesque imperdiet, neque
-	blandit volutpat ultricies, arcu justo varius libero, id suscipit velit eros eu neque.
-	Pellentesque porttitor porta dui, quis tincidunt velit ornare at. Quisque blandit nibh tempus,
-	consectetur sem sed, interdum risus. Donec nec maximus nisi. Aenean nec cursus nibh, et egestas
-	risus. Nunc ac augue nec libero eleifend laoreet et vitae mauris. Nullam et iaculis ex. Maecenas
-	ut augue dolor. Donec mollis et risus a congue. Maecenas ut blandit lectus, vehicula suscipit
-	augue. Etiam euismod, arcu et sodales ultrices, purus dolor dictum elit, non posuere purus erat id
-	mauris. Sed congue facilisis pharetra. Suspendisse vel pellentesque ante. Pellentesque habitant
-	morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus eu quam hendrerit,
-	malesuada est et, pulvinar dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-	posuere cubilia curae; Quisque egestas velit eget pulvinar mattis. Nulla convallis felis libero,
-	sed posuere enim faucibus ac. Fusce mattis consequat convallis. Aliquam egestas ac quam nec
-	pellentesque. In pellentesque ultricies lectus, ut aliquam dolor tristique id. Fusce ex turpis,
-	sollicitudin sit amet nunc eget, semper aliquam nunc. Vivamus eget nulla vitae urna bibendum
-	dictum eget non tellus. Sed nec ultricies orci. Nunc sodales, mi a ultricies porta, sapien metus
-	lacinia tellus, vitae rhoncus mi sem vel sem. Proin non commodo velit. In ut fermentum ante.
-	Vivamus finibus nisl in eros molestie vehicula. Aliquam ac convallis dui. Vestibulum tincidunt
-	dictum nulla, eu maximus est porttitor ac. Duis quam mauris, imperdiet vitae velit convallis,
-	fermentum dapibus orci. Mauris rutrum mollis felis, id egestas augue. Praesent sit amet sagittis
-	est. Proin diam eros, bibendum a rhoncus vitae, faucibus sed ipsum. Fusce pellentesque neque sit
-	amet iaculis faucibus. Morbi viverra ante at magna fringilla dictum. Duis hendrerit eros sit amet
-	mauris consectetur molestie. Vestibulum porta neque urna, ut pharetra dolor lacinia vel. Aenean
-	ornare ullamcorper nulla id molestie. Fusce faucibus interdum libero, a accumsan massa lacinia id.
-	Vestibulum pulvinar sapien nibh, et hendrerit turpis condimentum sed. Pellentesque egestas, nibh
-	ac pulvinar consectetur, dolor tortor rhoncus sem, a blandit purus libero ut turpis. Curabitur
-	accumsan quam id arcu malesuada, ut aliquam eros elementum. Ut maximus dui vel tempor vulputate.
-	Donec lacinia libero at pretium blandit. Praesent tempor lobortis turpis, ac dapibus lectus
-	blandit sit amet. Morbi non sagittis nulla. In in auctor ligula. Nunc eget quam placerat tortor
-	varius eleifend non nec nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-	per inceptos himenaeos. Maecenas pharetra ut leo et hendrerit. Aliquam erat volutpat. Nunc
-	suscipit scelerisque tristique. Morbi efficitur odio in urna sodales pretium. Aenean leo sem,
-	pulvinar vel vulputate id, pellentesque in nisi. Suspendisse id lorem ornare, venenatis tortor et,
-	dignissim nisl. Ut volutpat vehicula dui, et semper nulla viverra ac. Mauris facilisis blandit
-	leo, ut sagittis ante tincidunt id. Curabitur diam est, euismod iaculis massa in, porttitor
-	dignissim augue. Maecenas a molestie elit. Aliquam imperdiet scelerisque vehicula. Nullam massa
-	sem, varius ac lorem in, venenatis vulputate nulla. Suspendisse sem mauris, dignissim sed quam
-	nec, hendrerit malesuada risus. Sed sagittis suscipit lorem, eget venenatis orci eleifend maximus.
-	In arcu enim, pulvinar eget finibus a, sagittis ultrices massa. Duis ac ante commodo, feugiat nibh
-	vel, mattis sapien. Donec ut est venenatis, mattis enim at, commodo tellus. Curabitur eros nulla,
-	malesuada ac ultrices non, condimentum vitae nibh. Donec ut convallis neque. Aenean sed mi velit.
-	Nullam dignissim, tellus sit amet consequat facilisis, sem ex porttitor sapien, quis semper nisl
-	purus non tortor. Donec cursus massa in mi mattis rutrum. Vestibulum aliquam, enim sed mollis
-	vulputate, turpis justo sollicitudin lorem, quis posuere enim turpis eget mauris. Duis tellus
-	urna, consectetur eu tincidunt vel, venenatis id lacus.
+<div class="flex">
+	<div class="m-6 overflow-x-hidden w-full">
+		All sessions
+		<div class="flex flex-row border overflow-x-auto space-x-12 px-4 py-12">
+			{#each Object.entries($dateSessions) as [dateMilliseconds, sessions]}
+				<!-- Day -->
+				<div class="bg-zinc-600 py-1 min-w-full overflow-hidden">
+					<div>{formatDate(new Date(+dateMilliseconds))}</div>
+					<div class="flex space-x-2 ">
+						{#each sessions as session}
+							<!-- Session -->
+							<div>
+								<div class="text-sm rounded borded bg-orange-500 text-zinc-200">
+									{formatTime(new Date(session.meta.startTimestampMs))}
+									-
+									{formatTime(new Date(session.meta.lastTimestampMs))}
+								</div>
+								<div title="Session duration">
+									{Math.round(
+										(session.meta.lastTimestampMs - session.meta.startTimestampMs) / 1000 / 60
+									)} min
+								</div>
+								<div title="Session files">
+									{#await listDeltas( { projectId: $project?.id, sessionId: session.id } ) then deltas}
+										{#each Object.keys(deltas) as delta}
+											<div class="flex flex-row w-32 items-center">
+												<div class="w-6 h-6 text-white fill-blue-400">
+													{@html pathToIconSvg(delta)}
+												</div>
+												<div class="text-white w-24 truncate">
+													{pathToName(delta)}
+												</div>
+											</div>
+										{/each}
+									{/await}
+								</div>
+							</div>
+						{/each}
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
