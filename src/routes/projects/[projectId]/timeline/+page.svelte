d596251f8783c907fc1e1586f1e19b10f3741142
Kiril Videlov <kiril@videlov.com>
2023-02-27 13:08:40 +0100

    add the session overview to unified timeline

--- src/routes/projects/[projectId]/timeline/+page.svelte
+++ src/routes/projects/[projectId]/timeline/+page.svelte
@@ -8,6 +8,9 @@
 	import { list as listDeltas } from '$lib/deltas';
 	import type { Delta } from '$lib/deltas';
 	import { toHumanBranchName } from '$lib/branch';
+	import { add, format, differenceInSeconds, addSeconds } from 'date-fns';
+	import { Slider } from 'fluent-svelte';
+	import { CodeViewer } from '$lib/components';
 
 	export let data: PageData;
 	const { project, sessions } = data;
@@ -84,10 +87,18 @@
 		return dateSessions;
 	});
 
-	type Selection = { sessionIdx: number; dateMilliseconds: number };
+	type Selection = {
+		sessionIdx: number;
+		dateMilliseconds: number;
+		start: Date;
+		end: Date;
+		deltas: Record<string, Delta[]>;
+	};
 	let selection = {
-		sessionIdx: 3,
-		dateMilliseconds: 1677106800000
+		// sessionIdx: 3,
+		// dateMilliseconds: 1677106800000,
+		// start: new Date(1677106800000),
+		// deltas: {} as Record<string, Delta[]>,
 	} as Selection;
 
 	const resetSelection = () => {
@@ -105,6 +116,39 @@
 	}
 
 	let animatingOut = false;
+
+	// $: start = new Date($session.meta.startTimestampMs);
+
+	// $: end = $session.hash ? addSeconds(new Date($session.meta.lastTimestampMs), 10) : time; // For some reason, some deltas are stamped a few seconds after the session end.
+	// Also, if the session is current, the end time moves.
+
+	// $: tickSizeMs = Math.floor((end.getTime() - start.getTime()) / 63); // how many ms each column represents
+	let selectedFileIdx = 0;
+	let value = 0; // TODO: rename
+
+	const timeStampToCol = (deltaTimestamp: Date, start: Date, end: Date) => {
+		if (deltaTimestamp < start || deltaTimestamp > end) {
+			console.error(
+				`Delta timestamp out of session range. Delta timestamp: ${deltaTimestamp}, Session start: ${start}, Session end: ${end}`
+			);
+		}
+		// there are 88 columns
+		// start is column 17
+		const totalDiff = differenceInSeconds(end, start);
+		const eventDiff = differenceInSeconds(deltaTimestamp, start);
+		const rat = eventDiff / totalDiff;
+		const col = Math.floor(rat * 63 + 17);
+		return col;
+	};
+
+	const colToTimestamp = (col: number, start: Date, end: Date) => {
+		const totalDiff = differenceInSeconds(end, start);
+		const colDiff = col - 17;
+		const rat = colDiff / 63;
+		const eventDiff = totalDiff * rat;
+		const timestamp = addSeconds(start, eventDiff);
+		return timestamp;
+	};
 </script>
 
 <div class="h-full">
@@ -121,7 +165,7 @@
 							class="bg-zinc-800/50 rounded-xl border border-zinc-700 
                                 flex flex-col h-full
                                 {selection.dateMilliseconds == +dateMilliseconds
-								? 'min-w-full overflow-x-hidden'
+								? 'min-w-full overflow-hidden'
 								: ''}
                                 "
 						>
@@ -147,7 +191,10 @@
 													on:click={() => {
 														selection = {
 															sessionIdx: i,
-															dateMilliseconds: +dateMilliseconds
+															dateMilliseconds: +dateMilliseconds,
+															start: new Date(uiSession.session.meta.startTimestampMs),
+															end: new Date(uiSession.session.meta.lastTimestampMs), // TODO: maybe use last delta timestamp?
+															deltas: uiSession.deltas
 														};
 														scrollExpandedIntoView(dateMilliseconds);
 													}}
@@ -203,6 +250,166 @@
 										class="flex-grow bg-orange-400/1 border-t border-l border-r rounded-t border-orange-400/50"
 									>
 										<button on:click={resetSelection}>Close</button>
+
+										<div
+											class="flex flex-col flex-none max-w-full select-none border-b border-zinc-700 h-full overflow-auto"
+										>
+											<div class="flex flex-col flex-none max-w-full mb-40">
+												<!-- sticky header -->
+												<div
+													class="overflow-hidden sticky top-0 z-30 bg-zinc-800 flex-none shadow shadow-zinc-700 ring-1 ring-zinc-700 ring-opacity-5 mb-1"
+												>
+													<div class="grid-cols-11 -mr-px  border-zinc-700  grid">
+														<div />
+														<div class="col-span-2 flex items-center justify-center py-2">
+															<span>{format(selection.start, 'hh:mm')}</span>
+														</div>
+														<div class="col-span-2 flex items-center justify-center py-2">
+															<span
+																>{format(
+																	add(selection.start, {
+																		seconds:
+																			differenceInSeconds(selection.end, selection.start) * 0.25
+																	}),
+																	'hh:mm'
+																)}</span
+															>
+														</div>
+														<div class="col-span-2 flex items-center justify-center py-2">
+															<span
+																>{format(
+																	add(selection.start, {
+																		seconds:
+																			differenceInSeconds(selection.end, selection.start) * 0.5
+																	}),
+																	'hh:mm'
+																)}</span
+															>
+														</div>
+														<div class="col-span-2 flex items-center justify-center py-2">
+															<span
+																>{format(
+																	add(selection.start, {
+																		seconds:
+																			differenceInSeconds(selection.end, selection.start) * 0.75
+																	}),
+																	'hh:mm'
+																)}</span
+															>
+														</div>
+														<div class="col-span-2 flex items-center justify-center py-2">
+															<span>{format(selection.end, 'hh:mm')}</span>
+														</div>
+													</div>
+													<!-- needle -->
+													<div class="grid grid-cols-11">
+														<div class="col-span-2 flex items-center justify-center" />
+														<div class="-mx-1 col-span-8 flex items-center justify-center">
+															<Slider min={17} max={80} step={1} bind:value>
+																<svelte:fragment slot="tooltip" let:value>
+																	{format(
+																		colToTimestamp(value, selection.start, selection.end),
+																		'hh:mm'
+																	)}
+																</svelte:fragment>
+															</Slider>
+														</div>
+														<div class="col-span-1 flex items-center justify-center" />
+													</div>
+												</div>
+												<div class="flex flex-auto mb-1">
+													<div class="grid flex-auto grid-cols-1 grid-rows-1">
+														<!-- file names list -->
+														<div
+															class="bg-col-start-1 col-end-2 row-start-1 grid divide-y divide-zinc-700/20"
+															style="grid-template-rows: repeat({Object.keys(selection.deltas)
+																.length}, minmax(2rem, 1fr));"
+														>
+															<!-- <div class="row-end-1 h-7" /> -->
+
+															{#each Object.keys(selection.deltas) as filePath, i}
+																<div class="flex {i == selectedFileIdx ? 'bg-zinc-500/70' : ''}">
+																	<button
+																		class="z-20 flex justify-end items-center overflow-hidden sticky left-0 w-1/6 leading-5 
+                                                                        {selectedFileIdx == i
+																			? 'text-zinc-200 cursor-default'
+																			: 'text-zinc-400 hover:text-zinc-200 cursor-pointer'}"
+																		on:click={() => (selectedFileIdx = i)}
+																		title={filePath}
+																	>
+																		{filePath.split('/').pop()}
+																	</button>
+																</div>
+															{/each}
+														</div>
+
+														<!-- col selection -->
+														<div
+															class="col-start-1 col-end-2 row-start-1 grid"
+															style="grid-template-columns: repeat(88, minmax(0, 1fr));"
+														>
+															<div class="bg-orange-400/40 " style=" grid-column: {value};" />
+														</div>
+														<!-- time vertical lines -->
+														<div
+															class="col-start-1 col-end-2 row-start-1 grid-rows-1 divide-x divide-zinc-700/50 grid grid-cols-11"
+														>
+															<div class="col-span-2 row-span-full" />
+															<div class="col-span-2 row-span-full" />
+															<div class="col-span-2 row-span-full" />
+															<div class="col-span-2 row-span-full" />
+															<div class="col-span-2 row-span-full" />
+															<div class="col-span-2 row-span-full" />
+														</div>
+
+														<!-- actual entries  -->
+														<ol
+															class="col-start-1 col-end-2 row-start-1 grid"
+															style="
+                        grid-template-columns: repeat(88, minmax(0, 1fr));
+                        grid-template-rows: repeat({Object.keys(selection.deltas)
+																.length}, minmax(0px, 1fr)) auto;"
+														>
+															{#each Object.entries(selection.deltas) as [filePath, fileDeltas], idx}
+																{#each fileDeltas as delta}
+																	<li
+																		class="relative flex items-center bg-zinc-300 hover:bg-zinc-100 rounded m-0.5 cursor-pointer"
+																		style="
+                                grid-row: {idx + 1} / span 1;
+                                grid-column: {timeStampToCol(
+																			new Date(delta.timestampMs),
+																			selection.start,
+																			selection.end
+																		)} / span 1;"
+																	>
+																		<button
+																			class="z-20 h-full flex flex-col w-full items-center justify-center"
+																			on:click={() => {
+																				value = timeStampToCol(
+																					new Date(delta.timestampMs),
+																					selection.start,
+																					selection.end
+																				);
+																				selectedFileIdx = idx;
+																			}}
+																		/>
+																	</li>
+																{/each}
+															{/each}
+														</ol>
+													</div>
+												</div>
+												<!-- <div class="grid grid-cols-11 mt-6">
+				<div class="col-span-2" />
+				<div class="col-span-8 p-1 bg-zinc-500/70 rounded select-text">
+					{#if $doc}
+						<CodeViewer value={$doc} />
+					{/if}
+				</div>
+				<div class="" />
+			</div> -->
+											</div>
+										</div>
 									</div>
 									<div class="">
 										<div
