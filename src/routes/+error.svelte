34f868fb9505fc47036f812441728f5ed6070446
Nikita Galaiko <nikita@galaiko.rocks>
2023-02-27 14:41:25 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { log } from '$lib';
+
+	$: if ($page.error) {
+		log.error($page.url, $page.error);
+	} else {
+		log.error($page.url, $page.status);
+	}
+
+	$: message = $page.error
+		? $page.error.message
+		: $page.status === 404
+		? 'Page not found'
+		: 'Something went wrong';
+</script>
+
+<div class="flex flex-1 h-full">
+	<h1 class="m-auto text-xl">
+		{message} :(
+	</h1>
+</div>
