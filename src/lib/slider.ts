ff1bd5f02f446b882d7cd773860bda9e617e8de3
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-08 15:15:01 +0100

    pnpm format

--- src/lib/slider.ts
+++ src/lib/slider.ts
@@ -1,47 +1,47 @@
 export default (node: HTMLElement) => {
-    const onDown = getOnDown(node);
+	const onDown = getOnDown(node);
 
-    node.addEventListener('touchstart', onDown);
-    node.addEventListener('mousedown', onDown);
-    return {
-        destroy() {
-            node.removeEventListener('touchstart', onDown);
-            node.removeEventListener('mousedown', onDown);
-        }
-    };
+	node.addEventListener('touchstart', onDown);
+	node.addEventListener('mousedown', onDown);
+	return {
+		destroy() {
+			node.removeEventListener('touchstart', onDown);
+			node.removeEventListener('mousedown', onDown);
+		}
+	};
 };
 
 const getOnDown = (node: HTMLElement) => {
-    const onMove = getOnMove(node);
+	const onMove = getOnMove(node);
 
-    return (e: Event) => {
-        e.preventDefault();
-        node.dispatchEvent(new CustomEvent('dragstart'));
+	return (e: Event) => {
+		e.preventDefault();
+		node.dispatchEvent(new CustomEvent('dragstart'));
 
-        const moveevent = 'touches' in e ? 'touchmove' : 'mousemove';
-        const upevent = 'touches' in e ? 'touchend' : 'mouseup';
+		const moveevent = 'touches' in e ? 'touchmove' : 'mousemove';
+		const upevent = 'touches' in e ? 'touchend' : 'mouseup';
 
-        const onUp = (e: Event) => {
-            e.stopPropagation();
+		const onUp = (e: Event) => {
+			e.stopPropagation();
 
-            document.removeEventListener(moveevent, onMove);
-            document.removeEventListener(upevent, onUp);
+			document.removeEventListener(moveevent, onMove);
+			document.removeEventListener(upevent, onUp);
 
-            node.dispatchEvent(new CustomEvent('dragend'));
-        };
+			node.dispatchEvent(new CustomEvent('dragend'));
+		};
 
-        document.addEventListener(moveevent, onMove);
-        document.addEventListener(upevent, onUp);
-    };
+		document.addEventListener(moveevent, onMove);
+		document.addEventListener(upevent, onUp);
+	};
 };
 
 const getOnMove = (node: HTMLElement) => {
-    const track = node.parentNode as HTMLElement;
+	const track = node.parentNode as HTMLElement;
 
-    return (e: TouchEvent | MouseEvent) => {
-        const { left, width } = track.getBoundingClientRect();
-        const clickOffset = 'touches' in e ? e.touches[0].clientX : e.clientX;
-        const clickPos = Math.min(Math.max((clickOffset - left) / width, 0), 1) || 0;
-        node.dispatchEvent(new CustomEvent('drag', { detail: clickPos }));
-    };
+	return (e: TouchEvent | MouseEvent) => {
+		const { left, width } = track.getBoundingClientRect();
+		const clickOffset = 'touches' in e ? e.touches[0].clientX : e.clientX;
+		const clickPos = Math.min(Math.max((clickOffset - left) / width, 0), 1) || 0;
+		node.dispatchEvent(new CustomEvent('drag', { detail: clickPos }));
+	};
 };
