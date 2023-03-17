d4d1a8e3fb06622b61e58a28cde9be1e7b71e8b1
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-17 10:19:58 +0100

    return highlighted result entries from search

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,18 +1,17 @@
 import { invoke } from '@tauri-apps/api';
 
 export type SearchResult = {
-	projectId: string;
-	sessionId: string;
-	filePath: string;
-	// index of the delta in the session.
-	index: number;
-	timestampMsGte?: number;
-	timestampMsLt?: number;
+    projectId: string;
+    sessionId: string;
+    filePath: string;
+    // index of the delta in the session.
+    index: number;
+    highlighted: string[]; // contains the highlighted text
 };
 
 export const search = (params: {
-	projectId: string;
-	query: string;
-	limit?: number;
-	offset?: number;
+    projectId: string;
+    query: string;
+    limit?: number;
+    offset?: number;
 }) => invoke<SearchResult[]>('search', params);
