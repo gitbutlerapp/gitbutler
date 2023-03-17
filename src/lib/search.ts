1ebf8492212278ab16387a33a6edb31a79963f80
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-17 14:43:21 +0100

    clean results on query update

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,17 +1,17 @@
 import { invoke } from '@tauri-apps/api';
 
 export type SearchResult = {
-    projectId: string;
-    sessionId: string;
-    filePath: string;
-    // index of the delta in the session.
-    index: number;
-    highlighted: string[]; // contains the highlighted text
+	projectId: string;
+	sessionId: string;
+	filePath: string;
+	// index of the delta in the session.
+	index: number;
+	highlighted: string[]; // contains the highlighted text
 };
 
 export const search = (params: {
-    projectId: string;
-    query: string;
-    limit?: number;
-    offset?: number;
+	projectId: string;
+	query: string;
+	limit?: number;
+	offset?: number;
 }) => invoke<SearchResult[]>('search', params);
