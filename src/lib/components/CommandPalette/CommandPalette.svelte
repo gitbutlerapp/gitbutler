5fa0b9e68ecccbb123cb5806e5314873ef71b67e
Kiril Videlov <kiril@videlov.com>
2023-03-23 14:19:13 +0100

    command palette refactor

--- src/lib/components/CommandPalette/CommandPalette.svelte
+++ src/lib/components/CommandPalette/CommandPalette.svelte
@@ -1,315 +1,81 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { getContext } from 'svelte';
-	import type { Readable } from 'svelte/store';
-	import { currentProject } from '$lib/current_project';
-	import { IconCircleCancel } from '$lib/components/icons';
-	import type { Project } from '$lib/projects';
-	import tinykeys from 'tinykeys';
-	import type { CommandGroup, Command } from './commands';
-	import { Action, previousCommand, nextCommand, firstVisibleCommand } from './commands';
 	import type { ComponentType } from 'svelte';
-	import { default as RewindCommand } from './RewindCommand.svelte';
-	import { default as HelpCommand } from './HelpCommand.svelte';
-	import { invoke } from '@tauri-apps/api';
 
-	const matchFiles = (params: { projectId: string; matchPattern: string }) =>
-		invoke<Array<string>>('git_match_paths', params);
+	import { onDestroy, onMount } from 'svelte';
+	import tinykeys, { type KeyBindingMap } from 'tinykeys';
+	import CmdK from './CmdK.svelte';
+	import Commit from './Commit.svelte';
+	import Replay from './Replay.svelte';
+	import Branch from './Branch.svelte';
 
-	const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
-		let timeout: ReturnType<typeof setTimeout>;
-		return (...args: any[]) => {
-			clearTimeout(timeout);
-			timeout = setTimeout(() => fn(...args), delay);
-		};
-	};
+	let dialog: ComponentType | undefined;
 
-	let matchFilesQuery = '';
-	const updateMatchFilesQuery = debounce(async () => {
-		matchFilesQuery = userInput;
-	}, 100);
+	function isEventTargetInputOrTextArea(target: any) {
+		if (target === null) return false;
 
-	let matchingFiles: Array<string> = [];
-	$: if (matchFilesQuery) {
-		matchFiles({ projectId: $currentProject?.id || '', matchPattern: matchFilesQuery }).then(
-			(files) => {
-				matchingFiles = files;
-			}
-		);
-	} else {
-		matchingFiles = [];
+		const targetElementName = target.tagName.toLowerCase();
+		return ['input', 'textarea'].includes(targetElementName);
 	}
 
-	$: scopeToProject = $currentProject ? true : false;
-
-	let showingCommandPalette = false;
-	let dialog: HTMLDialogElement;
-	let userInput: string;
-
-	let projects: Readable<any> = getContext('projects');
-
-	let selection: [number, number] = [0, 0];
-	// if the group or the command are no longer visible, select the first visible group and first visible command
-	$: if (
-		!commandGroups[selection[0]]?.visible ||
-		!commandGroups[selection[0]].commands[selection[1]]?.visible
-	) {
-		selection = firstVisibleCommand(commandGroups);
+	function hotkeys(target: Window | HTMLElement, bindings: KeyBindingMap, disableOnInputs = true) {
+		const wrappedBindings = disableOnInputs
+			? Object.fromEntries(
+					Object.entries(bindings).map(([key, handler]) => [
+						key,
+						(event: KeyboardEvent) => {
+							if (!isEventTargetInputOrTextArea(event.target)) {
+								handler(event);
+							}
+						}
+					])
+			  )
+			: bindings;
+		return tinykeys(target, wrappedBindings);
 	}
-	$: selectedCommand = commandGroups[selection[0]].commands[selection[1]];
-	$: {
-		const element = document.getElementById(`${selection[0]}-${selection[1]}`);
-		if (element) {
-			// TODO: this works, but it's not standard
-			element.scrollIntoViewIfNeeded(false);
-		}
-	}
-
-	let componentOfTriggeredCommand: ComponentType | undefined;
-	let triggeredCommand: Command | undefined;
-
-	$: commandGroups = [
-		{
-			name: 'Go to project',
-			visible: !scopeToProject,
-			commands: $projects.map((project: Project) => {
-				return {
-					title: project.title,
-					description: 'Repository',
-					selected: false,
-					action: {
-						href: `/projects/${project.id}/`
-					},
-					visible: project.title.toLowerCase().includes(userInput?.toLowerCase())
-				};
-			})
-		},
-		{
-			name: 'Actions',
-			visible: scopeToProject,
-			commands: [
-				{
-					title: 'Replay History',
-					description: 'Command',
-					selected: false,
-					action: {
-						component: RewindCommand
-					},
-					visible: 'replay'.includes(userInput?.toLowerCase())
-				},
-				{
-					title: 'Help',
-					description: 'Command',
-					selected: false,
-					action: {
-						component: HelpCommand
-					},
-					visible: 'help'.includes(userInput?.toLowerCase())
-				}
-			]
-		},
-		{
-			name: 'Files',
-			visible: scopeToProject,
-			description: !userInput
-				? 'type part of a file name'
-				: matchingFiles.length === 0
-				? `no files containing '${userInput}'`
-				: '',
-			commands: matchingFiles.map((file) => {
-				return {
-					title: file,
-					description: 'File',
-					selected: false,
-					action: {
-						href: `/`
-					},
-					visible: true
-				};
-			})
-		}
-	] as CommandGroup[];
-
-	const resetState = () => {
-		userInput = '';
-		scopeToProject = $currentProject ? true : false;
-		selection = [0, 0];
-		componentOfTriggeredCommand = undefined;
-		triggeredCommand = undefined;
-		matchingFiles = [];
-	};
-
-	const triggerCommand = () => {
-		if (
-			!commandGroups[selection[0]].visible ||
-			!commandGroups[selection[0]].commands[selection[1]].visible
-		) {
-			return;
-		}
-		if (Action.isLink(selectedCommand.action)) {
-			toggleCommandPalette();
-			goto(selectedCommand.action.href);
-		} else if (Action.isActionInPalette(selectedCommand.action)) {
-			userInput = '';
-			componentOfTriggeredCommand = selectedCommand.action.component;
-			triggeredCommand = selectedCommand;
-		}
-	};
-
-	const toggleCommandPalette = () => {
-		if (dialog && dialog.open) {
-			dialog.close();
-			showingCommandPalette = false;
-		} else {
-			resetState();
-			dialog.showModal();
-			showingCommandPalette = true;
-		}
-	};
 
 	let unsubscribeKeyboardHandler: () => void;
-
+	let unsubscribeKeyboardHandlerDisabledOnInput: () => void;
 	onMount(() => {
-		toggleCommandPalette(); // developmnet only
-		unsubscribeKeyboardHandler = tinykeys(window, {
-			'Meta+k': () => {
-				toggleCommandPalette();
-			},
-			Backspace: () => {
-				if (!userInput) {
-					if (triggeredCommand) {
-						// Untrigger command
-						componentOfTriggeredCommand = undefined;
-						triggeredCommand = undefined;
-					} else {
-						scopeToProject = false;
-					}
+		unsubscribeKeyboardHandler = hotkeys(
+			window,
+			{
+				'Meta+k': () => {
+					dialog === CmdK ? (dialog = undefined) : (dialog = CmdK);
 				}
 			},
-			Enter: () => {
-				triggerCommand();
+			false // works even when an input is focused
+		);
+		unsubscribeKeyboardHandlerDisabledOnInput = hotkeys(
+			window,
+			{
+				c: () => {
+					dialog === Commit ? (dialog = undefined) : (dialog = Commit);
+				},
+				r: () => {
+					dialog === Replay ? (dialog = undefined) : (dialog = Replay);
+				},
+				b: () => {
+					dialog === Branch ? (dialog = undefined) : (dialog = Branch);
+				}
 			},
-			ArrowDown: () => {
-				selection = nextCommand(commandGroups, selection);
-			},
-			ArrowUp: () => {
-				selection = previousCommand(commandGroups, selection);
-			},
-			'Control+n': () => {
-				selection = nextCommand(commandGroups, selection);
-			},
-			'Control+p': () => {
-				selection = previousCommand(commandGroups, selection);
-			}
-		});
+			true // disabled when an input is focused
+		);
 	});
 
 	onDestroy(() => {
 		unsubscribeKeyboardHandler?.();
+		unsubscribeKeyboardHandlerDisabledOnInput?.();
 	});
+
+	const onDialogClose = () => {
+		dialog = undefined;
+	};
+
+	const onNewDialog = (e: CustomEvent) => {
+		dialog = e.detail;
+	};
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<dialog
-	class="rounded-lg 
-    border border-zinc-400/40
-    bg-zinc-900/70 p-0 backdrop-blur-xl
-    "
-	bind:this={dialog}
-	on:click|self={() => toggleCommandPalette()}
->
-	<div class="flex h-[640px] w-[640px] flex-col rounded text-zinc-400" on:click|stopPropagation>
-		<!-- Search input area -->
-		<div class="flex items-center border-b border-zinc-400/20 py-2">
-			<div class="ml-4 mr-2 flex flex-grow items-center">
-				<!-- Project scope -->
-				{#if scopeToProject}
-					<div class="mr-1 flex items-center">
-						<span class="font-semibold text-zinc-300">{$currentProject?.title}</span>
-						<span class="ml-1 text-lg">/</span>
-					</div>
-				{/if}
-				<!-- Selected command -->
-				{#if scopeToProject && triggeredCommand}
-					<div class="mr-1 flex items-center">
-						<span class="font-semibold text-zinc-300">{triggeredCommand?.title}</span>
-						<span class="ml-1 text-lg">/</span>
-					</div>
-				{/if}
-				<!-- Search input -->
-				<div class="mr-1 flex-grow">
-					<!-- svelte-ignore a11y-autofocus -->
-					<input
-						class="w-full bg-transparent text-zinc-300 focus:outline-none"
-						bind:value={userInput}
-						on:input={updateMatchFilesQuery}
-						type="text"
-						autofocus
-						placeholder={!scopeToProject
-							? 'Search for repositories'
-							: !componentOfTriggeredCommand
-							? 'Search for commands, files and code changes...'
-							: ''}
-					/>
-				</div>
-				<button on:click={toggleCommandPalette} class="rounded p-2 hover:bg-zinc-600">
-					<IconCircleCancel class="fill-zinc-400" />
-				</button>
-			</div>
-		</div>
-		<!-- Main part -->
-		<div class="flex-auto overflow-y-auto">
-			{#if componentOfTriggeredCommand}
-				<svelte:component this={componentOfTriggeredCommand} {userInput} />
-			{:else}
-				{#each commandGroups as group, groupIdx}
-					{#if group.visible}
-						<div class="mx-2 cursor-default select-none">
-							<p
-								class="mx-2 cursor-default select-none py-2 text-sm font-semibold text-zinc-300/80"
-							>
-								<span>{group.name}</span>
-								{#if group.description}
-									<span class="ml-2 font-light italic text-zinc-300/60">({group.description})</span>
-								{/if}
-							</p>
-							<ul class="">
-								{#each group.commands as command, commandIdx}
-									{#if command.visible}
-										{#if Action.isLink(command.action)}
-											<a
-												on:mouseover={() => (selection = [groupIdx, commandIdx])}
-												on:focus={() => (selection = [groupIdx, commandIdx])}
-												id={`${groupIdx}-${commandIdx}`}
-												href={command.action.href}
-												class="{selection[0] === groupIdx && selection[1] === commandIdx
-													? 'bg-zinc-700/70'
-													: ''} flex cursor-default items-center rounded-lg p-2 px-2 outline-none"
-											>
-												<span class="flex-grow">{command.title}</span>
-												<span>{command.description}</span>
-											</a>
-										{:else if Action.isActionInPalette(command.action)}
-											<div
-												on:mouseover={() => (selection = [groupIdx, commandIdx])}
-												on:focus={() => (selection = [groupIdx, commandIdx])}
-												on:click={triggerCommand}
-												class="{selection[0] === groupIdx && selection[1] === commandIdx
-													? 'bg-zinc-700/70'
-													: ''} flex cursor-default items-center rounded-lg p-2 px-2 outline-none"
-											>
-												<span class="flex-grow">{command.title}</span>
-												<span>{command.description}</span>
-											</div>
-										{/if}
-									{/if}
-								{/each}
-							</ul>
-						</div>
-					{/if}
-				{/each}
-			{/if}
-		</div>
-	</div>
-</dialog>
+{#if dialog}
+	<svelte:component this={dialog} on:close={onDialogClose} on:newdialog={onNewDialog} />
+{/if}
---
+++ src/lib/components/CommandPalette/Commit.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+	import BaseDialog from './BaseDialog.svelte';
+	import { shortPath } from '$lib/paths';
+	import { invoke } from '@tauri-apps/api';
+	import { currentProject } from '$lib/current_project';
+	import { onMount } from 'svelte';
+	import toast from 'svelte-french-toast';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let commitMessage = '';
+
+	let changedFiles: Record<string, string> = {};
+
+	const listFiles = (params: { projectId: string }) =>
+		invoke<Record<string, string>>('git_status', params);
+
+	const commit = (params: {
+		projectId: string;
+		message: string;
+		files: Array<string>;
+		push: boolean;
+	}) => invoke<boolean>('git_commit', params);
+
+	onMount(() => {
+		listFiles({ projectId: $currentProject?.id || '' }).then((files) => {
+			changedFiles = files;
+		});
+	});
+
+	function doCommit() {
+		// get checked files
+		let changedFiles: Array<string> = [];
+		let doc = document.getElementsByClassName('file-checkbox');
+		Array.from(doc).forEach((c: any) => {
+			if (c.checked) {
+				changedFiles.push(c.dataset['file']);
+			}
+		});
+		if ($currentProject) {
+			commit({
+				projectId: $currentProject.id,
+				message: commitMessage,
+				files: changedFiles,
+				push: false
+			}).then((result) => {
+				toast.success('Commit successful!', {
+					icon: '🎉'
+				});
+				commitMessage = '';
+				dispatch('close');
+			});
+		}
+	}
+</script>
+
+<BaseDialog on:close>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="flex flex-col rounded text-zinc-400" on:click|stopPropagation>
+		<div class="mb-4 w-full border-b border-zinc-700 p-4 text-lg text-white">
+			Commit Your Changes
+		</div>
+		<div
+			class="relative mx-auto transform overflow-hidden p-2 text-left transition-all sm:w-full sm:max-w-sm"
+		>
+			{#if Object.entries(changedFiles).length > 0}
+				<div>
+					<div class="">
+						<h3 class="text-base font-semibold text-zinc-200" id="modal-title">Commit Message</h3>
+						<div class="mt-2">
+							<div class="mt-2">
+								<textarea
+									rows="4"
+									name="message"
+									placeholder="Description of changes"
+									id="commit-message"
+									bind:value={commitMessage}
+									class="block w-full rounded-md border-0 p-4 text-zinc-200 ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:py-1.5 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="mt-5 sm:mt-6">
+					<button
+						type="button"
+						on:click={doCommit}
+						class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+						>Commit Your Changes</button
+					>
+				</div>
+				<div class="mt-4 py-4 text-zinc-200">
+					<h3 class="text-base font-semibold text-zinc-200" id="modal-title">Changed Files</h3>
+					{#each Object.entries(changedFiles) as file}
+						<div class="flex flex-row space-x-2">
+							<div>
+								<input type="checkbox" class="file-checkbox" data-file={file[0]} checked />
+							</div>
+							<div>
+								{file[1]}
+							</div>
+							<div class="font-mono">
+								{shortPath(file[0])}
+							</div>
+						</div>
+					{/each}
+				</div>
+			{:else}
+				<div class="mx-auto text-center text-white">No changes to commit</div>
+			{/if}
+		</div>
+	</div>
+</BaseDialog>
