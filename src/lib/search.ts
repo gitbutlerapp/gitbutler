b10518b14f8c527034e2a064ddde556be44a4ef9
Ian Donahue <idonahue@users.noreply.github.com>
2023-03-23 16:22:43 +0100

    Scroll for search results are now on the right of screen
    This commit includes refactoring the code format to use consistent indentation, and updates the CSS classes to improve UI element styling. The changes were made in the src/lib/search.ts file and the src/routes/projects/[projectId]/search/+page.svelte file.
    
    - Adjusted the indentation in src/lib/search.ts to use tabs consistently
    - Changed the figure class from "mx-14 flex h-full flex-col gap-2" to "search-results flex h-full flex-col gap-2" in +page.svelte
    - Updated the figcaption and ul elements to have consistent margins and padding in +page.svelte

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,18 +1,18 @@
 import { invoke } from '@tauri-apps/api';
 
 export type SearchResult = {
-    projectId: string;
-    sessionId: string;
-    filePath: string;
-    // index of the delta in the session.
-    index: number;
-    // contains the highlighted text snippets.
-    highlighted: string[];
+	projectId: string;
+	sessionId: string;
+	filePath: string;
+	// index of the delta in the session.
+	index: number;
+	// contains the highlighted text snippets.
+	highlighted: string[];
 };
 
 export const search = (params: {
-    projectId: string;
-    query: string;
-    limit?: number;
-    offset?: number;
+	projectId: string;
+	query: string;
+	limit?: number;
+	offset?: number;
 }) => invoke<{ total: number; page: SearchResult[] }>('search', params);
