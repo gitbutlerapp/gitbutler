ba4d581a2b641261a6c07bdfecfa02a9ca3896d0
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-24 14:47:10 +0100

    fix some linter issues

--- src/lib/components/icons/IconChevronLeft.svelte
+++ src/lib/components/icons/IconChevronLeft.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	let className = '';
+	export { className as class };
+</script>
+
 <svg
-	{...$$restProps}
+	class={className}
 	xmlns="http://www.w3.org/2000/svg"
 	width="24"
 	height="24"
