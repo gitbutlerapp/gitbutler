615f0092a6d3250b910a73b5a6b48470f15f7498
Kiril Videlov <kiril@videlov.com>
2023-03-27 17:02:11 +0200

    add Storybook dev dependency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,20 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:storybook/recommended'
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3'
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
