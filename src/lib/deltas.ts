3653ae6a313dd916524ff5a1fcefa4e4a5662f47
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-27 17:15:29 +0200

    cache date on the client

--- src/lib/deltas.ts
+++ src/lib/deltas.ts
@@ -9,49 +9,73 @@ export type OperationInsert = { insert: [number, string] };
 export type Operation = OperationDelete | OperationInsert;
 
 export namespace Operation {
-	export const isDelete = (operation: Operation): operation is OperationDelete =>
-		'delete' in operation;
+    export const isDelete = (operation: Operation): operation is OperationDelete =>
+        'delete' in operation;
 
-	export const isInsert = (operation: Operation): operation is OperationInsert =>
-		'insert' in operation;
+    export const isInsert = (operation: Operation): operation is OperationInsert =>
+        'insert' in operation;
 }
 
 export type Delta = { timestampMs: number; operations: Operation[] };
 
 export type DeltasEvent = {
-	deltas: Delta[];
-	filePath: string;
+    deltas: Delta[];
+    filePath: string;
 };
 
-export const list = (params: { projectId: string; sessionId: string; paths?: string[] }) =>
-	invoke<Record<string, Delta[]>>('list_deltas', params);
+const cache: Record<string, Record<string, Promise<Record<string, Delta[]>>>> = {};
+
+export const list = async (params: { projectId: string; sessionId: string; paths?: string[] }) => {
+    const sessionCache = cache[params.projectId] || {};
+    if (params.sessionId in sessionCache) {
+        return sessionCache[params.sessionId].then((deltas) =>
+            Object.fromEntries(
+                Object.entries(deltas).filter(([path]) =>
+                    params.paths ? params.paths.includes(path) : true
+                )
+            )
+        );
+    }
+
+    const promise = invoke<Record<string, Delta[]>>('list_deltas', {
+        projectId: params.projectId,
+        sessionId: params.sessionId
+    });
+    sessionCache[params.sessionId] = promise;
+    cache[params.projectId] = sessionCache;
+    return promise.then((deltas) =>
+        Object.fromEntries(
+            Object.entries(deltas).filter(([path]) => (params.paths ? params.paths.includes(path) : true))
+        )
+    );
+};
 
 export const subscribe = (
-	params: { projectId: string; sessionId: string },
-	callback: (filepath: string, deltas: Delta[]) => void
+    params: { projectId: string; sessionId: string },
+    callback: (filepath: string, deltas: Delta[]) => void
 ) => {
-	log.info(`Subscribing to deltas for ${params.projectId}, ${params.sessionId}`);
-	return appWindow.listen<DeltasEvent>(
-		`project://${params.projectId}/sessions/${params.sessionId}/deltas`,
-		(event) => {
-			log.info(
-				`Received deltas for ${params.projectId}, ${params.sessionId}, ${event.payload.filePath}`
-			);
-			callback(event.payload.filePath, event.payload.deltas);
-		}
-	);
+    log.info(`Subscribing to deltas for ${params.projectId}, ${params.sessionId}`);
+    return appWindow.listen<DeltasEvent>(
+        `project://${params.projectId}/sessions/${params.sessionId}/deltas`,
+        (event) => {
+            log.info(
+                `Received deltas for ${params.projectId}, ${params.sessionId}, ${event.payload.filePath}`
+            );
+            callback(event.payload.filePath, event.payload.deltas);
+        }
+    );
 };
 
 export default async (params: { projectId: string; sessionId: string }) => {
-	const init = await list(params);
+    const init = await list(params);
 
-	const store = writable<Record<string, Delta[]>>(init);
-	subscribe(params, (filepath, newDeltas) =>
-		store.update((deltas) => ({
-			...deltas,
-			[filepath]: newDeltas
-		}))
-	);
+    const store = writable<Record<string, Delta[]>>(init);
+    subscribe(params, (filepath, newDeltas) =>
+        store.update((deltas) => ({
+            ...deltas,
+            [filepath]: newDeltas
+        }))
+    );
 
-	return store as Readable<Record<string, Delta[]>>;
+    return store as Readable<Record<string, Delta[]>>;
 };
