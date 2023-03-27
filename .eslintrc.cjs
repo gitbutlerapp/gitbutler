14ced0d5441f7712e93ca9d271f6d6f51ca56913
Kiril Videlov <kiril@videlov.com>
2023-03-27 17:24:18 +0200

    Revert "add Storybook dev dependency"
    This reverts commit 615f0092a6d3250b910a73b5a6b48470f15f7498.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,10 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-		'plugin:storybook/recommended'
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3'
-		}
-	],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
