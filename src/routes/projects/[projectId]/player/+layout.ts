e955f4a786faeac4fdabb276c13af1199c5df02f
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-27 15:47:32 +0200

    format

--- src/routes/projects/[projectId]/player/+layout.ts
+++ src/routes/projects/[projectId]/player/+layout.ts
@@ -4,11 +4,11 @@ import { readable, type Readable } from 'svelte/store';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ params }) => {
-    const sessions: Readable<Session[]> = building
-        ? readable<Session[]>([])
-        : await import('$lib/sessions').then((m) => m.default({ projectId: params.projectId }));
-    return {
-        sessions,
-        projectId: params.projectId
-    };
+	const sessions: Readable<Session[]> = building
+		? readable<Session[]>([])
+		: await import('$lib/sessions').then((m) => m.default({ projectId: params.projectId }));
+	return {
+		sessions,
+		projectId: params.projectId
+	};
 };
