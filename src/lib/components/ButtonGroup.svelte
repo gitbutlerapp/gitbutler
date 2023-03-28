ed6c89d435c5b5a3214018336a1aefe6100b6414
Kiril Videlov <kiril@videlov.com>
2023-03-28 14:24:38 +0200

    wip

---
+++ src/lib/components/ButtonGroup.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let leftLabel: string;
+    export let leftAction: function;
+    export let wide = false;
+</script>
+{#if $$slots.middle}
+	<div>
+		<slot name="left" />
+		<slot name="middle" />
+		<slot name="right" />
+	</div>
+{:else}
+	<div>
+		<slot name="left" />
+		<slot name="right" />
+	</div>
+{/if}
