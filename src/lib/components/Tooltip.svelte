f948f0d24fab32b54c3a09f87b79ddccd6bb7ad4
Kiril Videlov <kiril@videlov.com>
2023-03-29 17:12:26 +0200

    implement a tooltip which always floats correctly

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { offset, flip, shift } from 'svelte-floating-ui/dom';
+	import { arrow } from 'svelte-floating-ui';
+	import { createFloatingActions } from 'svelte-floating-ui';
+	import { writable } from 'svelte/store';
+
+	let arrowRef = writable({} as HTMLElement);
+
+	export let label: string;
+
+	const [floatingRef, floatingContent, update] = createFloatingActions({
+		strategy: 'absolute',
+		placement: 'bottom',
+		middleware: [offset(6), flip(), shift(), arrow({ element: arrowRef })],
+		onComputed({ placement, middlewareData }) {
+			if (!middlewareData.arrow) return;
+			const { x, y } = middlewareData.arrow;
+			const staticSide = {
+				top: 'bottom',
+				right: 'left',
+				bottom: 'top',
+				left: 'right'
+			}[placement.split('-')[0]] as any;
+
+			if (!$arrowRef) return;
+			Object.assign($arrowRef.style, {
+				left: x != null ? `${x}px` : '',
+				top: y != null ? `${y}px` : '',
+				[staticSide]: '-4px'
+			});
+		}
+	});
+
+	let showTooltip = false;
+</script>
+
+<div
+	class="h-fit w-fit"
+	on:mouseenter={() => (showTooltip = true)}
+	on:mouseleave={() => (showTooltip = false)}
+	use:floatingRef
+>
+	<slot />
+</div>
+
+{#if showTooltip}
+	<div
+		role="tooltip"
+		class="
+            absolute
+            rounded-[5px]
+            bg-[#171717]
+            p-2
+            text-base
+            text-[#FAFAFA]
+            shadow-lg
+    "
+		use:floatingContent
+	>
+		{label}
+		<div
+			class="
+                absolute
+                h-3
+                w-3 rotate-45
+                rounded-sm
+                bg-[#171717]
+        "
+			bind:this={$arrowRef}
+		/>
+	</div>
+{/if}
