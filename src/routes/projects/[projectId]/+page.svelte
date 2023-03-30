6cb7456f15fbe209bbe74a5e68da808a638b5c64
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-30 10:19:49 +0200

    show staged / not staged on the project page

--- src/routes/projects/[projectId]/+page.svelte
+++ src/routes/projects/[projectId]/+page.svelte
@@ -284,7 +284,11 @@
 					<ul class="rounded border border-yellow-400 bg-yellow-500 p-4 font-mono text-yellow-900">
 						{#each $statuses as activity}
 							<li class="flex w-full gap-2">
-								<span class="font-semibold">{activity.status.slice(0, 1).toUpperCase()}</span>
+								<span
+									class:text-left={activity.staged}
+									class:text-right={!activity.staged}
+									class="w-[3ch] font-semibold">{activity.status.slice(0, 1).toUpperCase()}</span
+								>
 								<span class="truncate" use:collapsable={{ value: activity.path, separator: '/' }} />
 							</li>
 						{/each}
