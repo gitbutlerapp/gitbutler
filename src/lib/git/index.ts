16b3be927840291ee499dade71a525f027423892
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-30 10:59:56 +0200

    pnpm format

--- src/lib/git/index.ts
+++ src/lib/git/index.ts
@@ -4,10 +4,10 @@ export { default as statuses } from './statuses';
 export { default as activity } from './activity';
 
 export const commit = (params: { projectId: string; message: string; push: boolean }) =>
-    invoke<boolean>('git_commit', params);
+	invoke<boolean>('git_commit', params);
 
 export const stage = (params: { projectId: string; paths: Array<string> }) =>
-    invoke<void>('git_stage', params);
+	invoke<void>('git_stage', params);
 
 export const unstage = (params: { projectId: string; paths: Array<string> }) =>
-    invoke<void>('git_unstage', params);
+	invoke<void>('git_unstage', params);
