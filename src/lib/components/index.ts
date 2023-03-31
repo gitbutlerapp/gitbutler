7227ff8bb034d5354d2b128c862f842f5cd75257
Nikita Galaiko <nikita@galaiko.rocks>
2023-03-31 12:33:24 +0200

    extract home icon

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -4,7 +4,7 @@ export { default as Breadcrumbs } from './Breadcrumbs.svelte';
 export { default as CodeViewer } from './CodeViewer';
 export { default as CommandPalette } from './CommandPalette';
 export { default as Modal } from './Modal.svelte';
-export { default as Button } from './Button.svelte';
 export { default as ButtonGroup } from './ButtonGroup.svelte';
 export { default as Dialog } from './Dialog.svelte';
 export { default as Tooltip } from './Tooltip.svelte';
+export { default as Button } from './Button';
