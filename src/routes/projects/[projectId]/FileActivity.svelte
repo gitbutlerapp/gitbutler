259fcfd40db07161f1e80262d044a57890186a3f
Ian Donahue <idonahue@users.noreply.github.com>
2023-04-04 16:23:06 +0200

    Refactor project file UI and improve file activities display
    In this commit, the UI for the project files page is enhanced by restructuring the layout and improving the readability of the file activities. The file activity bars are redesigned to use a more visual and adaptable representation.
    
    Changes include:
    - Updated the layout and styling of `+page.svelte` for better readability.
    - Refactored the file activities display in `FileActivity.svelte` with more descriptive bar styles and heights for different activity levels.
    - Added styles to the file activity bars for a cleaner and more visually appealing look.

--- src/routes/projects/[projectId]/FileActivity.svelte
+++ src/routes/projects/[projectId]/FileActivity.svelte
@@ -16,24 +16,69 @@
 	});
 </script>
 
-<div class="font-mono text-zinc-400">
+<div class="file-activity font-mono text-zinc-400 w-[50%]">
 	{#each buckets as bucket}
 		{#if bucket < 1}
-			<span class="text-zinc-600">▁</span>
+			<span class="file-bar bar bar-0 text-zinc-600 "></span>
 		{:else if bucket < 2}
-			<span class="text-blue-200">▂</span>
+			<span class="file-bar bar bar-1 text-blue-500"></span>
 		{:else if bucket < 3}
-			<span class="text-blue-200">▃</span>
+			<span class="file-bar bar bar-2 text-blue-500"></span>
 		{:else if bucket < 4}
-			<span class="text-blue-200">▄</span>
+			<span class="file-bar bar bar-3 text-blue-500"></span>
 		{:else if bucket < 5}
-			<span class="text-blue-200">▅</span>
+			<span class="file-bar bar bar-4 text-blue-500"></span>
 		{:else if bucket < 6}
-			<span class="text-blue-200">▆</span>
-		{:else if bucket < 6}
-			<span class="text-blue-200">▇</span>
+			<span class="file-bar bar bar-5 text-blue-500"></span>
+		{:else if bucket < 7}
+			<span class="file-bar bar bar-6 text-blue-500"></span>
 		{:else}
-			<span class="text-blue-200">█</span>
+			<span class="file-bar bar bar-7 text-blue-500"></span>
 		{/if}
 	{/each}
 </div>
+
+
+<style>
+	.file-activity {
+		@apply h-full flex items-baseline gap-1 pt-1 pl-1 pr-1;
+		align-items: flex-end;
+		border-bottom: 1px solid rgb(55, 55, 55);
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	.file-bar {
+		width: auto;
+	}
+	.bar {
+		display: inline-block;
+		width: 100%;
+		height: 1rem;
+		background-color: #6959DF ;
+		border-radius: 2px 2px 0 0;
+
+	}
+	.bar-0 {
+		height: 0;
+	}
+	.bar-1 {
+		height: 14%;
+	}
+	.bar-2 {
+		height: 28%;
+	}
+	.bar-3 {
+		height: 42%;
+	}
+	.bar-4 {
+		height: 56%;
+	}
+	.bar-5 {
+		height: 70%;
+	}
+	.bar-6 {
+		height: 84%;
+	}
+	.bar-7 {
+		height: 100%;
+	}
+</style>
