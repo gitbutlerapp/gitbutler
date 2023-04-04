e98aefb9cb11bed481a41211a189963cb61ed627
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-04 12:09:11 +0200

    use button component in more places

--- src/routes/projects/[projectId]/+layout.svelte
+++ src/routes/projects/[projectId]/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import type { Project } from '$lib/projects';
+	import { Button, Tooltip } from '$lib/components';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 
@@ -72,34 +73,29 @@
 
 			<ul>
 				<li>
-					<a href="/projects/{$project?.id}/settings" class="text-zinc-400 hover:text-zinc-300">
-						<div
-							title="Project settings"
-							class="project-settings-button cursor-default rounded-md p-1 hover:bg-zinc-700 hover:text-zinc-200"
-						>
-							<div class="h-6 w-6 ">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="h-6 w-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-									/>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-									/>
-								</svg>
-							</div>
-						</div>
-					</a>
+					<Tooltip label="Project settings">
+						<Button filled={false} href="/projects/{$project?.id}/settings">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="h-6 w-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+								/>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+							</svg>
+						</Button>
+					</Tooltip>
 				</li>
 			</ul>
 		</nav>
@@ -113,13 +109,13 @@
 		<div class="flex h-8 flex-shrink-0 select-none items-center border-t border-zinc-700">
 			<div class="mx-4 flex w-full flex-row items-center justify-between space-x-2">
 				{#if $project?.api?.sync}
-					<a href="/projects/{$project?.id}/settings" class="text-zinc-400 hover:text-zinc-300">
+					<Button filled={false} href="/projects/{$project?.id}/settings">
 						<div class="flex flex-row items-center space-x-2 ">
 							<div class="h-2 w-2 rounded-full bg-green-700" />
 							<div>Backed up</div>
 						</div>
-					</a>
-					<a target="_blank" rel="noreferrer" href={projectUrl($project)} class="flex">
+					</Button>
+					<Button target="_blank" rel="noreferrer" filled={false} href={projectUrl($project)}>
 						<div class="leading-5">Open in GitButler Cloud</div>
 						<div class="icon ml-1 h-5 w-5">
 							<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
@@ -129,14 +125,12 @@
 								/></svg
 							>
 						</div>
-					</a>
+					</Button>
 				{:else}
-					<a href="/projects/{$project?.id}/settings" class="text-zinc-400 hover:text-zinc-300">
-						<div class="flex flex-row items-center space-x-2 ">
-							<div class="h-2 w-2 rounded-full bg-red-700" />
-							<div>Offline</div>
-						</div>
-					</a>
+					<Button filled={false} href="/projects/{$project?.id}/settings">
+						<div class="h-2 w-2 rounded-full bg-red-700" />
+						Offline
+					</Button>
 				{/if}
 			</div>
 		</div>
