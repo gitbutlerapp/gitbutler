e2a305d8fa64a3a0bae95afac864350bf676d228
Ian Donahue <idonahue@users.noreply.github.com>
2023-04-05 17:48:44 +0200

    The most basic updates to the terminal page experience

--- src/routes/projects/[projectId]/terminal/+page.svelte
+++ src/routes/projects/[projectId]/terminal/+page.svelte
@@ -3,6 +3,7 @@
 	import type { LayoutData } from '../$types';
 	import Terminal from '$lib/components/Terminal.svelte';
 	import * as terminals from '$lib/terminals';
+	import Button from '$lib/components/Button/Button.svelte';
 
 	export let data: LayoutData;
 	const { project, statuses } = data;
@@ -22,15 +23,20 @@
 </script>
 
 <!-- Actual terminal -->
-<div class="flex h-full w-full flex-row">
-	<div class="h-full w-80 p-2">
-		<div class="p-2 font-bold">Git Status</div>
+<div class="terminal-page flex flex h-full w-full flex-row">
+	<div class="main-content h-full w-2/3">
+		{#if terminalSession}
+			<Terminal session={terminalSession} bind:this={terminal} />
+		{/if}
+	</div>
+	<div class="right-panel h-full w-1/3  p-2">
+		<h2 class="p-2 pb-4 text-lg font-bold text-zinc-300">Git Status</h2>
 		{#if $statuses.length == 0}
 			<div class="rounded border border-green-400 bg-green-600 p-2 font-mono text-green-900">
 				No changes
 			</div>
 		{:else}
-			<ul class="rounded border border-yellow-400 bg-yellow-500 p-2 font-mono text-yellow-900">
+			<ul class="mx-2 rounded border border-yellow-400 bg-yellow-500 p-2 font-mono text-yellow-900">
 				{#each $statuses as activity}
 					<li class="flex w-full gap-2">
 						<span
@@ -44,14 +50,11 @@
 			</ul>
 		{/if}
 		<div class="mt-4 p-2 font-bold">Commands</div>
-		<ul class="px-2">
+		<ul class="px-2  ">
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<li class="cursor-pointer" on:click={() => runCommand('git push')}>Push Commit</li>
+			<Button role="primary" width="full-width" on:click={() => runCommand('git push')}
+				>Push Commit</Button
+			>
 		</ul>
-	</div>
-	<div class="h-full w-full">
-		{#if terminalSession}
-			<Terminal session={terminalSession} bind:this={terminal} />
-		{/if}
 	</div>
 </div>
