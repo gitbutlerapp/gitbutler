eee42bfd44afce566cc6fe27ad2b682241c4ce80
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-12 07:58:58 +0200

    pty: use different port for dev

--- src-tauri/src/pty/server.rs
+++ src-tauri/src/pty/server.rs
@@ -3,14 +3,13 @@ use crate::projects;
 use anyhow::{Context, Result};
 use tokio::net;
 
-const PTY_WS_ADDRESS: &str = "127.0.0.1:7703";
-
-pub async fn start_server(projects_store: projects::Storage) -> Result<()> {
-    let listener = net::TcpListener::bind(&PTY_WS_ADDRESS)
+pub async fn start_server(port: usize, projects_store: projects::Storage) -> Result<()> {
+    let pty_ws_address = format!("127.0.0.1:{}",  port);
+    let listener = net::TcpListener::bind(&pty_ws_address)
         .await
-        .with_context(|| format!("failed to bind to {}", PTY_WS_ADDRESS))?;
+        .with_context(|| format!("failed to bind to {}", pty_ws_address))?;
 
-    log::info!("pty-ws: listening on {}", PTY_WS_ADDRESS);
+    log::info!("pty-ws: listening on {}", pty_ws_address);
 
     while let Ok((stream, _)) = listener.accept().await {
         let projects_store = projects_store.clone();
