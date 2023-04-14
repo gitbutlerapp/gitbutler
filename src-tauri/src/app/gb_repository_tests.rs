0451829f7bff02d21c3874f1e3d765c641241c46
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-14 14:23:28 +0200

    cleanup gb_repository interface

--- src-tauri/src/app/gb_repository_tests.rs
+++ src-tauri/src/app/gb_repository_tests.rs
@@ -41,45 +41,6 @@ fn project_store(project: &projects::Project) -> Result<projects::Storage> {
 }
 
 #[test]
-fn test_get_current_session_writer_should_create_session() -> Result<()> {
-    let repository = test_repository()?;
-    let project = test_project(&repository)?;
-    let gb_repo_path = tempdir()?.path().to_str().unwrap().to_string();
-    let project_store = project_store(&project)?;
-    let gb_repo =
-        gb_repository::Repository::open(gb_repo_path, project.id.clone(), project_store.clone())?;
-
-    gb_repo.get_current_session_writer()?;
-
-    let current_session = gb_repo.get_current_session()?;
-    assert!(current_session.is_some());
-    let current_session = current_session.unwrap();
-
-    assert_eq!(
-        std::fs::read_to_string(gb_repo.session_path().join("meta/id"))?,
-        current_session.id
-    );
-    assert_eq!(
-        std::fs::read_to_string(gb_repo.session_path().join("meta/branch"))?,
-        current_session.meta.branch.unwrap()
-    );
-    assert_eq!(
-        std::fs::read_to_string(gb_repo.session_path().join("meta/commit"))?,
-        current_session.meta.commit.unwrap()
-    );
-    assert_eq!(
-        std::fs::read_to_string(gb_repo.session_path().join("meta/last"))?,
-        current_session.meta.last_timestamp_ms.to_string()
-    );
-    assert_eq!(
-        std::fs::read_to_string(gb_repo.session_path().join("meta/start"))?,
-        current_session.meta.start_timestamp_ms.to_string()
-    );
-
-    Ok(())
-}
-
-#[test]
 fn test_get_current_session_writer_should_use_existing_session() -> Result<()> {
     let repository = test_repository()?;
     let project = test_project(&repository)?;
@@ -88,15 +49,9 @@ fn test_get_current_session_writer_should_use_existing_session() -> Result<()> {
     let gb_repo =
         gb_repository::Repository::open(gb_repo_path, project.id.clone(), project_store.clone())?;
 
-    gb_repo.get_current_session_writer()?;
-
-    let current_session_1 = gb_repo.get_current_session()?;
-    assert!(current_session_1.is_some());
-
-    gb_repo.get_current_session_writer()?;
-
-    let current_session_2 = gb_repo.get_current_session()?;
-    assert_eq!(current_session_1, current_session_2);
+    let current_session_1 = gb_repo.get_or_create_current_session()?;
+    let current_session_2 = gb_repo.get_or_create_current_session()?;
+    assert_eq!(current_session_1.id, current_session_2.id);
 
     Ok(())
 }
