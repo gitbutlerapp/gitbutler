565d1667f451249261c20450846a13154be9a2b8
Kiril Videlov <kiril@videlov.com>
2023-04-17 12:41:42 +0200

    Project home: proportional histogram bars

--- src/routes/projects/[projectId]/FileActivity.svelte
+++ src/routes/projects/[projectId]/FileActivity.svelte
@@ -1,40 +1,28 @@
 <script lang="ts">
 	import type { Delta } from '$lib/deltas';
+	import { bucketByTimestamp } from './histogram';
 
 	export let deltas: Delta[];
+	export let largestBucketSize: number;
 
-	const timestamps = deltas.map((delta) => delta.timestampMs).sort((a, b) => a - b);
-
-	const totalBuckets = 18;
-	const range = timestamps[timestamps.length - 1] - timestamps[0];
-	const bucketSize = range / totalBuckets;
-	const buckets: number[] = Array.from({ length: totalBuckets }, () => 0);
-
-	timestamps.forEach((timestamp) => {
-		const bucketIndex = Math.floor((timestamp - timestamps[0]) / bucketSize);
-		buckets[bucketIndex] += 1;
-	});
+	$: buckets = bucketByTimestamp(
+		deltas.map((delta) => delta.timestampMs),
+		18
+	);
 </script>
 
 <div class="file-activity w-[50%] font-mono text-zinc-400">
 	{#each buckets as bucket}
-		{#if bucket < 1}
-			<span class="file-bar bar bar-0 text-zinc-600 " />
-		{:else if bucket < 2}
-			<span class="file-bar bar bar-1 text-blue-500" />
-		{:else if bucket < 3}
-			<span class="file-bar bar bar-2 text-blue-500" />
-		{:else if bucket < 4}
-			<span class="file-bar bar bar-3 text-blue-500" />
-		{:else if bucket < 5}
-			<span class="file-bar bar bar-4 text-blue-500" />
-		{:else if bucket < 6}
-			<span class="file-bar bar bar-5 text-blue-500" />
-		{:else if bucket < 7}
-			<span class="file-bar bar bar-6 text-blue-500" />
-		{:else}
-			<span class="file-bar bar bar-7 text-blue-500" />
-		{/if}
+		<span
+			class={`inline-block w-full rounded-t-sm`}
+			style="
+			height: {Math.round((bucket.length / largestBucketSize) * 100)}%;
+			background: #3b82f6;
+			background: linear-gradient(0deg, #3b82f6 0%, #eab308 {100 -
+				Math.round((bucket.length / largestBucketSize) * 100) +
+				100}%);
+			"
+		/>
 	{/each}
 </div>
 
@@ -44,41 +32,5 @@
 		align-items: flex-end;
 		border-bottom: 1px solid rgb(55, 55, 55);
 		background-color: rgba(0, 0, 0, 0.1);
-	}
-	.file-bar {
-		width: auto;
-	}
-	.bar {
-		@apply bg-blue-500;
-		display: inline-block;
-		width: 100%;
-		height: 100%;
-
-		/* background-color: #6959df;  Amazing purple color */
-		border-radius: 2px 2px 0 0;
-	}
-	.bar-0 {
-		height: 0;
-	}
-	.bar-1 {
-		height: 14%;
-	}
-	.bar-2 {
-		height: 28%;
-	}
-	.bar-3 {
-		height: 42%;
-	}
-	.bar-4 {
-		height: 56%;
-	}
-	.bar-5 {
-		height: 70%;
-	}
-	.bar-6 {
-		height: 84%;
-	}
-	.bar-7 {
-		height: 100%;
 	}
 </style>
---
+++ src/routes/projects/[projectId]/histogram.ts
@@ -0,0 +1,28 @@
+type Timestamp = number;
+
+export const bucketByTimestamp = (timestamps: Timestamp[], bucketCount: number): Timestamp[][] => {
+	// 1. Find the minimum and maximum timestamps
+	const min = Math.min(...timestamps);
+	const max = Math.max(...timestamps);
+
+	// 2. Calculate the range and bucket size
+	const range = max - min;
+	const bucketSize = range / bucketCount;
+
+	// 3. Create an empty array of buckets
+	const buckets: Timestamp[][] = new Array(bucketCount).fill(null).map(() => []);
+
+	// 4. Iterate through the timestamps, find the corresponding bucket, and push the timestamp into the bucket
+	for (const timestamp of timestamps) {
+		let bucketIndex = Math.floor((timestamp - min) / bucketSize);
+		if (bucketIndex === bucketCount) {
+			bucketIndex--; // Edge case: if the timestamp is equal to the max, assign it to the last bucket
+		}
+		if (!bucketIndex) {
+			bucketIndex = bucketCount - 1;
+		}
+		// console.log(bucketIndex)
+		buckets[bucketIndex].push(timestamp);
+	}
+	return buckets;
+};
