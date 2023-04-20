80c72dbf8a41c529078674a0481489c7410a1752
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-20 08:42:59 +0200

    avoid overflow when checking session ts

--- src-tauri/src/app/watcher/handlers/check_current_session.rs
+++ src-tauri/src/app/watcher/handlers/check_current_session.rs
@@ -2,17 +2,13 @@ use std::{sync, time};
 
 use anyhow::{Context, Result};
 
-use crate::app::gb_repository;
+use crate::{app::gb_repository, sessions};
 
 use super::events;
 
 pub struct Handler<'handler> {
     gb_repository: sync::Arc<sync::Mutex<&'handler gb_repository::Repository>>,
 }
-
-const FIVE_MINUTES: u128 = time::Duration::new(5 * 60, 0).as_millis();
-const ONE_HOUR: u128 = time::Duration::new(60 * 60, 0).as_millis();
-
 impl<'handler> Handler<'handler> {
     pub fn new(gb_repository: &'handler gb_repository::Repository) -> Self {
         Self {
@@ -20,33 +16,44 @@ impl<'handler> Handler<'handler> {
         }
     }
 
-    pub fn handle(&self, ts: time::SystemTime) -> Result<Vec<events::Event>> {
-        let current_session = self
+    pub fn handle(&self, now: time::SystemTime) -> Result<Vec<events::Event>> {
+        match self
             .gb_repository
             .lock()
             .unwrap()
             .get_current_session()
-            .context("failed to get current session")?;
-        if current_session.is_none() {
-            return Ok(vec![]);
+            .context("failed to get current session")?
+        {
+            None => Ok(vec![]),
+            Some(current_session) => {
+                if should_flush(now, &current_session) {
+                    Ok(vec![events::Event::FlushSession(current_session)])
+                } else {
+                    Ok(vec![])
+                }
+            }
         }
-        let current_session = current_session.unwrap();
-
-        let now = ts
-            .duration_since(time::SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_millis();
-
-        let elapsed_last = now - current_session.meta.last_timestamp_ms;
-        if elapsed_last < FIVE_MINUTES {
-            return Ok(vec![]);
-        }
-
-        let elapsed_start = now - current_session.meta.start_timestamp_ms;
-        if elapsed_start < ONE_HOUR {
-            return Ok(vec![]);
-        }
-
-        Ok(vec![events::Event::FlushSession(current_session)])
     }
+}
+
+pub(super) fn should_flush(now: time::SystemTime, session: &sessions::Session) -> bool {
+    println!("is_session_active {}", is_session_active(now, session));
+    println!("is_session_too_old {}", is_session_too_old(now, session));
+    !is_session_active(now, session) || is_session_too_old(now, session)
+}
+
+const ONE_HOUR: time::Duration = time::Duration::new(60 * 60, 0);
+
+fn is_session_too_old(now: time::SystemTime, session: &sessions::Session) -> bool {
+    let session_start = time::UNIX_EPOCH
+        + time::Duration::from_millis(session.meta.start_timestamp_ms.try_into().unwrap());
+    session_start + ONE_HOUR < now
+}
+
+const FIVE_MINUTES: time::Duration = time::Duration::new(5 * 60, 0);
+
+fn is_session_active(now: time::SystemTime, session: &sessions::Session) -> bool {
+    let session_last_update = time::UNIX_EPOCH
+        + time::Duration::from_millis(session.meta.last_timestamp_ms.try_into().unwrap());
+    session_last_update + FIVE_MINUTES > now
 }
