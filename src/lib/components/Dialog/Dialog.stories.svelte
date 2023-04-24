e72d738d882177a0750659ff6a864efd9de435a0
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-24 13:15:34 +0200

    dialog,button: resolve preventDefault conflict

--- src/lib/components/Dialog/Dialog.stories.svelte
+++ src/lib/components/Dialog/Dialog.stories.svelte
@@ -10,12 +10,12 @@
 <Meta title="GitButler/Dialog" component={Dialog} />
 
 <Story name="Dialog with title only">
-	<Button on:click={() => dialog.show()}>Open Dialog</Button>
+	<Button on:click={dialog.show}>Open Dialog</Button>
 	<Dialog bind:this={dialog} />
 </Story>
 
 <Story name="Dialog with content">
-	<Button on:click={() => dialog.show()}>Open Dialog</Button>
+	<Button on:click={dialog.show}>Open Dialog</Button>
 	<Dialog bind:this={dialog}>
 		<p class="w-[346px]">
 			GitButler offers support for generating Git commits automatically. To use this feature, you
