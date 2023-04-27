13da9d8f87e3e883e593957e6e6741e393f23ae0
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-27 10:43:50 +0200

    handle strip_prefix error

--- src-tauri/src/fs.rs
+++ src-tauri/src/fs.rs
@@ -1,9 +1,10 @@
 use std::path::{Path, PathBuf};
 
+use anyhow::Result;
 use walkdir::WalkDir;
 
 // Returns an ordered list of relative paths for files inside a directory recursively.
-pub fn list_files<P: AsRef<Path>>(dir_path: P) -> Result<Vec<PathBuf>, std::io::Error> {
+pub fn list_files<P: AsRef<Path>>(dir_path: P) -> Result<Vec<PathBuf>> {
     let mut files = vec![];
     let dir_path = dir_path.as_ref();
     if !dir_path.exists() {
@@ -13,7 +14,7 @@ pub fn list_files<P: AsRef<Path>>(dir_path: P) -> Result<Vec<PathBuf>, std::io::
         let entry = entry?;
         if entry.file_type().is_file() {
             let path = entry.path();
-            let path = path.strip_prefix(dir_path).unwrap();
+            let path = path.strip_prefix(dir_path)?;
             let path = path.to_path_buf();
             files.push(path);
         }
