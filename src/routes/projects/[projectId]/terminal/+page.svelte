3acc396bffb62bd5a93ebe3c6f1bd6ddc4242298
idonahue <donahue.ian@gmail.com>
2023-04-28 16:30:58 +0200

    pnpm install

--- src/routes/projects/[projectId]/terminal/+page.svelte
+++ src/routes/projects/[projectId]/terminal/+page.svelte
@@ -31,8 +31,8 @@
 	};
 </script>
 
-<div class="terminal-page flex h-full w-full flex-row flex-auto">
-	<div class="side-panel h-full w-[424px]  p-4 flex-auto">
+<div class="terminal-page flex h-full w-full flex-auto flex-row">
+	<div class="side-panel h-full w-[424px]  flex-auto p-4">
 		<h2 class="pb-4 text-lg font-bold text-zinc-300">Git Status</h2>
 		<Statuses statuses={$statuses} />
 		<div class="mt-4 font-bold">Commands</div>
