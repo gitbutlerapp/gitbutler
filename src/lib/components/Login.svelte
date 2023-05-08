b635b83b30c5a3d8994efb45577b94dd3647993b
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-08 08:28:54 +0200

    pnpm format

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -25,7 +25,7 @@
 
 <div>
 	{#if $user}
-		<button class="hover:text-red-400 text-zinc-400 hover:underline" on:click={() => user.delete()}
+		<button class="text-zinc-400 hover:text-red-400 hover:underline" on:click={() => user.delete()}
 			>Log out</button
 		>
 	{:else if $token !== null}
