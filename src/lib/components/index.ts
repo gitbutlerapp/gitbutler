0f95db77ebb757c28d225a67d1bdcd258d26b58a
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-15 09:37:39 +0200

    checkbox component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -10,3 +10,4 @@ export { default as Statuses } from './Statuses.svelte';
 export { default as Differ } from './Differ';
 export { default as DeltasViewer } from './DeltasViewer.svelte';
 export { default as DiffContext } from './DiffContext.svelte';
+export { default as Checkbox } from './Checkbox';
