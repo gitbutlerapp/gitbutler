6a0701be5ebb26c37a9943873f2483b6c50d9e84
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-17 14:01:48 +0200

    do not store duplicated file contents

--- src-tauri/src/database/migrations/V2__files.sql
+++ src-tauri/src/database/migrations/V2__files.sql
@@ -1,8 +1,13 @@
 CREATE TABLE `files` (
     `session_id` text NOT NULL,
     `file_path` text NOT NULL,
-    `content` blob NOT NULL,
+    `sha1` blob NOT NULL,
     PRIMARY KEY (`session_id`, `file_path`)
 );
 
 CREATE INDEX `files_session_id_index` ON `files` (`session_id`);
+
+CREATE TABLE `contents` (
+    `sha1` blob NOT NULL PRIMARY KEY,
+    `content` blob NOT NULL
+);
