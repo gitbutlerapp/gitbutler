5031e6ba12f23048669329ca85668ea193b2ad9d
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-19 13:59:01 +0200

    simplify project file change

--- src-tauri/src/watcher/handlers/project_file_change.rs
+++ src-tauri/src/watcher/handlers/project_file_change.rs
@@ -60,78 +60,6 @@ impl<'listener> Handler<'listener> {
         }
     }
 
-    fn get_latest_file_from_repository_head(
-        &self,
-        project_repository: &'listener project_repository::Repository,
-        path: &str,
-    ) -> Result<Option<String>> {
-        let head_reader = project_repository
-            .get_head_reader()
-            .context("failed to get head reader")?;
-
-        if !head_reader.exists(path) {
-            return Ok(None);
-        }
-
-        if head_reader.size(path)? > 100_000 {
-            log::warn!("{}: ignoring large file: {}", self.project_id, path);
-            return Ok(None);
-        }
-
-        match head_reader.read(path)? {
-            reader::Content::UTF8(content) => Ok(Some(content)),
-            reader::Content::Binary(_) => {
-                log::warn!("{}: ignoring non-utf8 file: {}", self.project_id, path);
-                return Ok(None);
-            }
-        }
-    }
-
-    // returns latest seen file content by gitbutler.
-    // None means the file was not seen at all.
-    fn get_latest_file_contents(
-        &self,
-        project_repository: &project_repository::Repository,
-        path: &std::path::Path,
-    ) -> Result<Option<String>> {
-        let path = path.to_str().unwrap();
-        match self.gb_repository.git_repository.head() {
-            Ok(head) => {
-                let head_commit = head.peel_to_commit()?;
-                let commit_reader = reader::CommitReader::from_commit(
-                    &self.gb_repository.git_repository,
-                    head_commit,
-                )
-                .context("failed to get commit reader")?;
-
-                let session_path = std::path::Path::new("wd").join(path);
-                if !commit_reader.exists(session_path.to_str().unwrap()) {
-                    return Ok(None);
-                }
-
-                if commit_reader.size(session_path.to_str().unwrap())? > 100_000 {
-                    log::warn!("{}: ignoring large file: {}", self.project_id, path);
-                    return Ok(None);
-                }
-
-                match commit_reader.read(session_path.to_str().unwrap())? {
-                    reader::Content::UTF8(content) => Ok(Some(content)),
-                    reader::Content::Binary(_) => {
-                        log::warn!("{}: ignoring non-utf8 file: {}", self.project_id, path);
-                        return Ok(None);
-                    }
-                }
-            }
-            Err(err) => {
-                if err.code() == git2::ErrorCode::UnbornBranch {
-                    self.get_latest_file_from_repository_head(project_repository, path)
-                } else {
-                    Err(err).context("failed to get head")?
-                }
-            }
-        }
-    }
-
     // returns deltas for the file that are already part of the current session (if any)
     fn get_current_deltas(&self, path: &std::path::Path) -> Result<Option<Vec<deltas::Delta>>> {
         let current_session = self.gb_repository.get_current_session()?;
@@ -184,22 +112,43 @@ impl<'listener> Handler<'listener> {
             None => return Ok(vec![]),
         };
 
-        let latest_file_content = self
-            .get_latest_file_contents(&project_repository, &path)
-            .with_context(|| "failed to get latest file content")?;
+        let current_session = self
+            .gb_repository
+            .get_or_create_current_session()
+            .context("failed to get or create current session")?;
+        let reader = sessions::Reader::open(&self.gb_repository, &current_session)
+            .context("failed to get session reader")?;
+
+        let latest_file_content = match reader.file(path) {
+            Ok(reader::Content::UTF8(content)) => content,
+            Ok(reader::Content::Binary(_)) => {
+                log::warn!(
+                    "{}: ignoring non-utf8 file: {}",
+                    self.project_id,
+                    path.display()
+                );
+                return Ok(vec![]);
+            }
+            Err(reader::Error::NotFound) => "".to_string(),
+            Err(err) => Err(err).context("failed to get file content")?,
+        };
+
+        // let latest_file_content = self
+        //     .get_latest_file_contents(&current_session, &project_repository, &path)
+        //     .with_context(|| "failed to get latest file content")?;
 
         let current_deltas = self
             .get_current_deltas(&path)
             .with_context(|| "failed to get current deltas")?;
 
-        let mut text_doc = match (latest_file_content, current_deltas) {
-            (Some(latest_contents), Some(deltas)) => {
-                deltas::Document::new(Some(&latest_contents), deltas)?
-            }
-            (Some(latest_contents), None) => deltas::Document::new(Some(&latest_contents), vec![])?,
-            (None, Some(deltas)) => deltas::Document::new(None, deltas)?,
-            (None, None) => deltas::Document::new(None, vec![])?,
-        };
+        println!("current_file_content: {:?}", current_file_content);
+        println!("latest_file_content: {:?}", latest_file_content);
+        println!("current_deltas: {:?}", current_deltas);
+
+        let mut text_doc = deltas::Document::new(
+            Some(&latest_file_content),
+            current_deltas.unwrap_or_default(),
+        )?;
 
         if !text_doc
             .update(&current_file_content)
@@ -213,10 +162,11 @@ impl<'listener> Handler<'listener> {
             return Ok(vec![]);
         }
 
-        let current_session = self.gb_repository.get_or_create_current_session()?;
         let writer = sessions::Writer::open(&self.gb_repository, &current_session)?;
 
         let deltas = text_doc.get_deltas();
+
+        println!("deltas: {:?}", deltas);
 
         writer
             .write_deltas(path, &deltas)
@@ -229,7 +179,7 @@ impl<'listener> Handler<'listener> {
             events::Event::File((
                 current_session.id.clone(),
                 path.to_path_buf(),
-                current_file_content,
+                latest_file_content,
             )),
             events::Event::Session(current_session.clone()),
             events::Event::Deltas((current_session.id, path.to_path_buf(), deltas)),
