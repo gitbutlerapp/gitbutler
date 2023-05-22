91a26865068183af17d529c28681c1d29737e55f
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-22 09:55:53 +0200

    add quick bookmark cmdk

--- src/lib/api/ipc/bookmarks.ts
+++ src/lib/api/ipc/bookmarks.ts
@@ -1,4 +1,5 @@
 import { invoke } from '$lib/ipc';
+import { nanoid } from 'nanoid';
 
 export type Bookmark = {
 	id: string;
@@ -9,4 +10,23 @@ export type Bookmark = {
 	deleted: boolean;
 };
 
+const newBookmark = (params: { projectId: string; note: string }) => ({
+	id: nanoid(),
+	projectId: params.projectId,
+	createdTimestampMs: Date.now(),
+	updatedTimestampMs: Date.now(),
+	note: params.note,
+	deleted: false
+});
+
+export { newBookmark as new };
+
 export const upsert = (params: { bookmark: Bookmark }) => invoke<void>('upsert_bookmark', params);
+
+export const list = (params: {
+	projectId: string;
+	range?: {
+		start: number;
+		end: number;
+	};
+}) => invoke<Bookmark[]>('list_bookmarks', params);
--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,1 +1,2 @@
 export { default as user } from './user';
+export { default as bookmarks } from './bookmarks';
