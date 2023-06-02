9d4820ee9abde6b6af37a1523a4e8114fc48a107
Nikita Galaiko <nikita@galaiko.rocks>
2023-06-02 13:28:33 +0200

    watcher/handler: log events

--- src-tauri/src/watcher/events.rs
+++ src-tauri/src/watcher/events.rs
@@ -28,20 +28,20 @@ impl Display for Event {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Event::Reindex => write!(f, "Reindex"),
-            Event::Tick(_) => write!(f, "Tick"),
-            Event::Flush(_) => write!(f, "Flush"),
-            Event::SessionFlushed(_) => write!(f, "SessionFlushed"),
+            Event::Tick(ts) => write!(f, "Tick({:?})", ts),
+            Event::Flush(session) => write!(f, "Flush({})", session.id),
+            Event::SessionFlushed(session) => write!(f, "SessionFlushed({})", session.id),
             Event::Fetch => write!(f, "Fetch"),
             Event::FileChange(_) => write!(f, "FileChange"),
             Event::GitFileChange(_) => write!(f, "GitFileChange"),
             Event::GitIndexChange => write!(f, "GitIndexChange"),
             Event::GitActivity => write!(f, "GitActivity"),
-            Event::GitHeadChange(_) => write!(f, "GitHeadChange"),
-            Event::ProjectFileChange(_) => write!(f, "ProjectFileChange"),
-            Event::Session(_) => write!(f, "Session"),
+            Event::GitHeadChange(head) => write!(f, "GitHeadChange({})", head),
+            Event::ProjectFileChange(path) => write!(f, "ProjectFileChange({})", path.display()),
+            Event::Session(session) => write!(f, "Session({})", session.id),
             Event::Bookmark(_) => write!(f, "Bookmark"),
-            Event::File(_) => write!(f, "File"),
-            Event::Deltas(_) => write!(f, "Deltas"),
+            Event::File((sid, path, _)) => write!(f, "File({},{})", sid, path.display()),
+            Event::Deltas((sid, path, deltas)) => write!(f, "Deltas({},{},{})", sid, path.display(), deltas.len()),
         }
     }
 }
--- src-tauri/src/watcher/handlers/mod.rs
+++ src-tauri/src/watcher/handlers/mod.rs
@@ -91,6 +91,7 @@ impl<'handler> Handler<'handler> {
     }
 
     pub fn handle(&self, event: events::Event) -> Result<Vec<events::Event>> {
+        log::info!("{}: handling event: {}", self.project_id, event);
         match event {
             events::Event::FileChange(path) => self
                 .file_change_handler
