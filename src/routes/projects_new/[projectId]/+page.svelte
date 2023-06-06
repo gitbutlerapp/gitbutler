7b64fad9edb2b2a00185cf3d7fd541e12ea9ae34
Kiril Videlov <kiril@videlov.com>
2023-06-06 13:05:37 +0200

    Revert "updates to data structure"
    This reverts commit 533cda662551ace88c506447c0186c97ecae87a4.

--- src/routes/projects_new/[projectId]/+page.svelte
+++ src/routes/projects_new/[projectId]/+page.svelte
@@ -5,7 +5,7 @@
 	let columnsData: BranchLane[] = [
 		{
 			id: 'c1',
-			name: 'implement new api',
+			name: 'TODO',
 			items: [
 				{ id: 1, name: 'item41' },
 				{ id: 2, name: 'item42' },
@@ -16,71 +16,20 @@
 				{ id: 7, name: 'item47' },
 				{ id: 8, name: 'item48' },
 				{ id: 9, name: 'item49' }
-			],
-			uncommittedFiles: [
-				{
-					filePath: 'src/foo.py',
-					hunks: [
-						{ id: 'a', updated: new Date('2011-04-11T10:20:30Z'), description: 'add api' },
-						{ id: 'b', updated: new Date('2011-04-11T10:25:30Z'), description: 'new user model' },
-						{ id: 'c', updated: new Date('2011-04-11T10:27:30Z'), description: 'refactor' }
-					]
-				},
-				{
-					filePath: 'src/bar.py',
-					hunks: [
-						{ id: 'd', updated: new Date('2011-04-11T10:10:30Z'), description: 'extract function' },
-						{ id: 'e', updated: new Date('2011-04-11T10:15:30Z'), description: 'things' }
-					]
-				},
-				{
-					filePath: 'src/baz.py',
-					hunks: [
-						{
-							id: 'f',
-							updated: new Date('2011-04-11T11:15:30Z'),
-							description: 'database migration'
-						}
-					]
-				}
 			]
 		},
 		{
 			id: 'c2',
-			name: 'bug fixes',
+			name: 'DOING',
 			items: [
 				{ id: 10, name: 'item50' },
 				{ id: 11, name: 'item51' }
-			],
-			uncommittedFiles: [
-				{
-					filePath: 'src/foo.py',
-					hunks: [
-						{
-							id: 'g',
-							updated: new Date('2011-04-11T10:20:30Z'),
-							description: 'correct broken things'
-						}
-					]
-				}
 			]
 		},
 		{
 			id: 'c3',
-			name: 'for later',
-			items: [{ id: 13, name: 'item52' }],
-			uncommittedFiles: [
-				{
-					filePath: '.env',
-					hunks: [
-						{
-							id: 'h',
-							updated: new Date('2011-04-10T12:20:30Z'),
-							description: 'API keys for local development'
-						}
-					]
-				}
-			]
+			name: 'DONE',
+			items: [{ id: 13, name: 'item52' }]
 		}
 	];
 	function handleBoardUpdated(newColumnsData: BranchLane[]) {
--- src/routes/projects_new/[projectId]/board.ts
+++ src/routes/projects_new/[projectId]/board.ts
@@ -3,20 +3,8 @@ export type FileCard = {
 	name: string;
 };
 
-export type Hunk = {
-	id: string;
-	updated: Date;
-	description: string;
-};
-
-export type File = {
-	filePath: string;
-	hunks: Hunk[];
-};
-
 export type BranchLane = {
 	id: string;
 	name: string;
 	items: FileCard[];
-	uncommittedFiles: File[];
 };
