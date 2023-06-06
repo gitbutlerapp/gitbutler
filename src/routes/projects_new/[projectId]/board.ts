38fbb3793af5f91f19a0fd85e110eb023754f963
Kiril Videlov <kiril@videlov.com>
2023-06-06 12:41:47 +0200

    updates to data structure

--- src/routes/projects_new/[projectId]/board.ts
+++ src/routes/projects_new/[projectId]/board.ts
@@ -3,8 +3,20 @@ export type FileCard = {
 	name: string;
 };
 
+export type Hunk = {
+	id: string;
+	updated: Date;
+	description: string;
+};
+
+export type File = {
+	filePath: string;
+	hunks: Hunk[];
+};
+
 export type BranchLane = {
 	id: string;
 	name: string;
 	items: FileCard[];
+	uncommittedFiles: File[];
 };
