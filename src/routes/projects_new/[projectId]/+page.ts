b0db9bfc288013b9a6d9fa3ad80406e3b1a6fa01
Kiril Videlov <kiril@videlov.com>
2023-06-13 19:28:29 +0200

    use testdata from file

--- src/routes/projects_new/[projectId]/+page.ts
+++ src/routes/projects_new/[projectId]/+page.ts
@@ -1,122 +1,32 @@
 import type { PageLoad } from './$types';
 import { wrapLoadWithSentry } from '@sentry/sveltekit';
-import type { Branch } from './types';
+import type { Branch, File } from './types';
 import { subSeconds, subMinutes, subHours } from 'date-fns';
+import { resolveResource } from '@tauri-apps/api/path';
+import { readTextFile } from '@tauri-apps/api/fs';
 
 export const load: PageLoad = async () => {
-	const branches: Branch[] = [
-		{
-			id: 'b1',
-			name: 'feature-1',
-			active: true,
-			kind: 'branch',
-			commits: [
-				{
-					id: 'c1',
-					description: 'First commit',
-					committedAt: subMinutes(new Date(), 3),
-					kind: 'commit',
-					files: [
-						{
-							id: 'f1',
-							path: 'src/foo.py',
-							kind: 'file',
-							hunks: [
-								{
-									id: 'h1',
-									name: 'foo-hunk-1',
-									kind: 'hunk',
-									modifiedAt: subMinutes(new Date(), 5),
-									filePath: 'src/foo.py'
-								},
-								{
-									id: 'h2',
-									name: 'foo-hunk-2',
-									kind: 'hunk',
-									modifiedAt: subSeconds(new Date(), 15),
-									filePath: 'src/foo.py'
-								}
-							]
-						},
-						{
-							id: 'f2',
-							path: 'src/bar.py',
-							kind: 'file',
-							hunks: [
-								{
-									id: 'h3',
-									name: 'bar-hunk-1',
-									kind: 'hunk',
-									modifiedAt: subHours(new Date(), 2),
-									filePath: 'src/bar.py'
-								}
-							]
-						}
-					]
-				}
-			]
-		},
-		{
-			id: 'b2',
-			name: 'bugfix',
-			active: true,
-			kind: 'branch',
-			commits: [
-				{
-					id: 'c2',
-					description: 'Second commit',
-					committedAt: subMinutes(new Date(), 10),
-					kind: 'commit',
-					files: [
-						{
-							id: 'f3',
-							path: 'src/foo.py',
-							kind: 'file',
-							hunks: [
-								{
-									id: 'h4',
-									name: 'foo-hunk-3',
-									kind: 'hunk',
-									modifiedAt: subMinutes(new Date(), 32),
-									filePath: 'src/foo.py'
-								}
-							]
-						}
-					]
-				}
-			]
-		},
-		{
-			id: 'b3',
-			name: 'stashed-things',
-			active: false,
-			kind: 'branch',
-			commits: [
-				{
-					id: 'c3',
-					description: 'Third commit',
-					committedAt: subMinutes(new Date(), 50),
-					kind: 'commit',
-					files: [
-						{
-							id: 'f4',
-							path: 'src/bar.py',
-							kind: 'file',
-							hunks: [
-								{
-									id: 'h5',
-									name: 'bar-hunk-2',
-									kind: 'hunk',
-									modifiedAt: subHours(new Date(), 1),
-									filePath: 'src/bar.py'
-								}
-							]
-						}
-					]
-				}
-			]
-		}
-	].map((column) => ({
+	const testdata = await resolveResource('../scripts/branch_testdata.json');
+	const test_branches = JSON.parse(await readTextFile(testdata));
+
+	// fix dates from the test data
+	test_branches.map((branch: Branch) =>
+		branch.commits.map((commit: any) => {
+			commit.committedAt = new Date(commit.committedAt);
+			commit.files = commit.files.map((file: File) => {
+				file.hunks = file.hunks.map((hunk: any) => {
+					hunk.modifiedAt = new Date(hunk.modifiedAt);
+					return hunk;
+				});
+				return file;
+			});
+
+			return commit;
+		})
+	);
+	let branches = test_branches as Branch[];
+
+	branches = branches.map((column) => ({
 		...column,
 		commits: column.commits.map((commit) => ({
 			...commit,
