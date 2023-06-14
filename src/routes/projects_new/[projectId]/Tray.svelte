7e7bb8aef478347181fe64844d05727d1acbfe3a
Mattias Granlund <mtsgrd@gmail.com>
2023-06-14 16:52:11 +0200

    Update tray on finalize event from board

--- src/routes/projects_new/[projectId]/Tray.svelte
+++ src/routes/projects_new/[projectId]/Tray.svelte
@@ -3,15 +3,15 @@
 	import { Checkbox } from '$lib/components';
 	import type { Branch } from './types';
 
-	export let columns: Branch[];
+	export let branches: Branch[];
 </script>
 
 <section class="flex h-full w-64 flex-col gap-y-2 border-r border-zinc-700 bg-[#2F2F33] p-4">
 	<div class="flex flex-col gap-y-2 py-2">
-		{#each columns as column (column.id)}
+		{#each branches as branch (branch.id)}
 			<div animate:flip={{ duration: 300 }} class="rounded border border-zinc-600 bg-zinc-700 p-2">
-				<Checkbox bind:checked={column.active} />
-				<span class="ml-2">{column.name}</span>
+				<Checkbox bind:checked={branch.active} />
+				<span class="ml-2">{branch.name}</span>
 			</div>
 		{/each}
 	</div>
