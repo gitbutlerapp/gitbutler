d0a83e97cf9f713c91db35a63c8c47ee5fa2cd97
Nikita Galaiko <nikita@galaiko.rocks>
2023-06-15 16:16:47 +0200

    small events refactor

--- src-tauri/src/watcher/events.rs
+++ src-tauri/src/watcher/events.rs
@@ -6,7 +6,6 @@ use crate::{bookmarks, deltas, sessions};
 pub enum Event {
     Tick(time::SystemTime),
     Flush(sessions::Session),
-    SessionFlushed(sessions::Session),
     Fetch,
 
     FileChange(path::PathBuf),
@@ -17,20 +16,20 @@ pub enum Event {
 
     ProjectFileChange(path::PathBuf),
 
-    Reindex,
     Session(sessions::Session),
+    SessionFile((String, path::PathBuf, String)),
+    SessionDelta((String, path::PathBuf, deltas::Delta)),
     Bookmark(bookmarks::Bookmark),
-    File((String, path::PathBuf, String)),
-    Deltas((String, path::PathBuf, Vec<deltas::Delta>)),
+
+    IndexAll,
 }
 
 impl Display for Event {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Event::Reindex => write!(f, "Reindex"),
+            Event::IndexAll => write!(f, "IndexAll"),
             Event::Tick(ts) => write!(f, "Tick({:?})", ts),
             Event::Flush(session) => write!(f, "Flush({})", session.id),
-            Event::SessionFlushed(session) => write!(f, "SessionFlushed({})", session.id),
             Event::Fetch => write!(f, "Fetch"),
             Event::FileChange(_) => write!(f, "FileChange"),
             Event::GitFileChange(_) => write!(f, "GitFileChange"),
@@ -40,9 +39,15 @@ impl Display for Event {
             Event::ProjectFileChange(path) => write!(f, "ProjectFileChange({})", path.display()),
             Event::Session(session) => write!(f, "Session({})", session.id),
             Event::Bookmark(_) => write!(f, "Bookmark"),
-            Event::File((sid, path, _)) => write!(f, "File({},{})", sid, path.display()),
-            Event::Deltas((sid, path, deltas)) => {
-                write!(f, "Deltas({},{},{})", sid, path.display(), deltas.len())
+            Event::SessionFile((sid, path, _)) => write!(f, "File({},{})", sid, path.display()),
+            Event::SessionDelta((sid, path, delta)) => {
+                write!(
+                    f,
+                    "Deltas({},{},{})",
+                    sid,
+                    path.display(),
+                    delta.timestamp_ms
+                )
             }
         }
     }
