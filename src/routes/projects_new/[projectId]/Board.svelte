f8366d7ee3a7dd2ee053ed9785ee93589679ab7d
Mattias Granlund <mtsgrd@gmail.com>
2023-06-17 16:06:44 +0100

    Create new branches through dedicated dropzone

--- src/routes/projects_new/[projectId]/Board.svelte
+++ src/routes/projects_new/[projectId]/Board.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-	import { flip } from 'svelte/animate';
 	import { dndzone } from 'svelte-dnd-action';
 	import Lane from './BranchLane.svelte';
 	import { Branch, File, Hunk } from './types';
 	import type { DndEvent } from 'svelte-dnd-action/typings';
 	import { createBranch, createFile } from './helpers';
 	import { createEventDispatcher } from 'svelte';
+	import NewBranchDropZone from './NewBranchDropZone.svelte';
 
 	export let branches: Branch[];
 
-	const flipDurationMs = 300;
 	const dispatch = createEventDispatcher();
+	const newBranchClass = 'new-branch-active';
 
 	function handleDndEvent(e: CustomEvent<DndEvent<Branch | File | Hunk>>) {
+		if (e.type == 'consider' && !e.detail.info.types?.includes('branch')) {
+			return; // No shadow element while considering drop.
+		}
 		const newItems = e.detail.items;
 		const branchItems = newItems.filter((item) => item instanceof Branch) as Branch[];
 
@@ -33,6 +36,10 @@
 		}
 	}
 
+	function handleNewBranch(e: CustomEvent<Branch[]>) {
+		branches.push(...e.detail);
+	}
+
 	function handleEmpty() {
 		const emptyIndex = branches.findIndex((item) => !item.files || item.files.length == 0);
 		if (emptyIndex != -1) {
@@ -42,18 +49,33 @@
 </script>
 
 <section
-	class="swimlane-container flex h-full w-full snap-x gap-x-4 overflow-x-scroll bg-zinc-900 p-4"
+	id="branch-lanes"
+	class="flex h-full snap-x gap-x-4 overflow-x-scroll bg-zinc-900 p-4"
 	use:dndzone={{
 		items: branches,
 		types: ['branch'],
-		receives: ['branch']
+		receives: ['branch', 'file', 'hunk'],
+		dropTargetClassMap: {
+			file: [newBranchClass],
+			hunk: [newBranchClass]
+		}
 	}}
 	on:consider={handleDndEvent}
 	on:finalize={handleDndEvent}
 >
 	{#each branches.filter((c) => c.active) as { id, name, files, description } (id)}
-		<div class="swimlane flex h-full w-96 snap-start scroll-ml-4 rounded-lg bg-zinc-900">
+		<div
+			id="branch-{id}"
+			class="branch flex h-full w-96 snap-start scroll-ml-4 rounded-lg bg-zinc-900"
+		>
 			<Lane {name} {description} bind:files on:empty={handleEmpty} />
 		</div>
 	{/each}
+	<NewBranchDropZone on:finalize={handleNewBranch} />
 </section>
+
+<style lang="postcss">
+	:global(#branch-lanes.new-branch-active [data-dnd-ignore]) {
+		@apply visible flex;
+	}
+</style>
