a1e6ee2df6e664450347b0dafb2c4e5117ae8ba3
Kiril Videlov <kiril@videlov.com>
2023-06-28 09:37:05 +0200

    this may be fixing freezing up when computer is ofline

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,28 +1,24 @@
 import posthog from 'posthog-js';
 import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
 import type { User } from '$lib/api';
-import * as log from '$lib/log';
 
 export default () => {
 	const instance = posthog.init(PUBLIC_POSTHOG_API_KEY, {
 		api_host: 'https://eu.posthog.com',
-		capture_performance: false
+		capture_performance: false,
+		request_batching: true,
+		on_xhr_error: () => {
+			// noop
+		}
 	});
-	log.info('posthog initialized');
 	return {
 		identify: (user: User | null) => {
 			if (user) {
-				log.info('posthog identify', {
-					id: user.id,
-					name: user.name,
-					email: user.email
-				});
 				instance?.identify(`user_${user.id}`, {
 					email: user.email,
 					name: user.name
 				});
 			} else {
-				log.info('posthog reset');
 				instance?.capture('log-out');
 				instance?.reset();
 			}
