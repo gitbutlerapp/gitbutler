494492992a7cc3941be486b17bc5be7c30187a14
Kiril Videlov <kiril@videlov.com>
2023-06-28 09:37:05 +0200

    this may be fixing freezing up when computer is ofline

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,19 +1,16 @@
 import { setUser } from '@sentry/sveltekit';
 import type { User } from '$lib/api';
-import * as log from '$lib/log';
 
 export default () => {
 	return {
 		identify: (user: User | null) => {
 			if (user) {
-				log.info(`sentry identify`);
 				setUser({
 					id: user.id.toString(),
 					email: user.email,
 					username: user.name
 				});
 			} else {
-				log.info(`sentry reset`);
 				setUser(null);
 			}
 		}
