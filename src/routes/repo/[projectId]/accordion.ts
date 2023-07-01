5f3aebd5192d59e33ba306d1dcb58e30f34c20b2
Mattias Granlund <mtsgrd@gmail.com>
2023-07-01 17:26:03 +0100

    Bugfix tray accordion

--- src/routes/repo/[projectId]/accordion.ts
+++ src/routes/repo/[projectId]/accordion.ts
@@ -1,29 +1,9 @@
 export function accordion(node: HTMLDivElement, isOpen: boolean) {
-	const initialHeight = node.offsetHeight;
 	node.style.height = isOpen ? 'auto' : '0';
 	node.style.overflow = 'hidden';
 	return {
 		update(isOpenInner: boolean) {
-			const animation = node.animate(
-				[
-					{
-						height: initialHeight + 'px',
-						overflow: 'visible'
-					},
-					{
-						height: 0,
-						overflow: 'hidden'
-					}
-				],
-				{ duration: 100, fill: 'both' }
-			);
-			animation.pause();
-			if (!isOpenInner) {
-				animation.play();
-			} else {
-				animation.reverse();
-				animation.play();
-			}
+			node.style.display = isOpenInner ? 'initial' : 'none';
 		}
 	};
 }
