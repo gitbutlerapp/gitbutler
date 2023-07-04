d9316c858d45b1b554ec9ccfa58f31911d87f8f6
Mattias Granlund <mtsgrd@gmail.com>
2023-07-04 18:15:32 +0100

    Update modal style to match design

--- src/lib/components/Modal/Modal.svelte
+++ src/lib/components/Modal/Modal.svelte
@@ -23,13 +23,30 @@
 
 <Overlay bind:this={modal} let:close on:close>
 	<div
-		class="flex flex-col gap-y-4 p-4"
+		class="flex cursor-default flex-col gap-y-4 py-4"
 		class:w-[680px]={width === 'default'}
 		class:w-[380px]={width === 'small'}
 		class:w-[980px]={width === 'large'}
 	>
+		{#if $$slots.title}
+			<div
+				class="flex w-full items-center justify-between border-b border-light-400 px-4 pb-4 dark:border-dark-500"
+			>
+				<div class="flex items-center gap-2">
+					<svelte:component this={icon} class="h-5 w-5" />
+					<h2 class="text-lg font-medium">
+						<slot name="title" />
+					</h2>
+				</div>
+				<Button height="small" kind="plain" on:click={close} icon={IconClose} />
+			</div>
+		{/if}
+
+		<!-- TODO: Remove this props based way once all other modals have been updated -->
 		{#if title}
-			<div class="flex w-full items-center justify-between">
+			<div
+				class="flex w-full items-center justify-between border-b border-light-400 px-4 pb-4 dark:border-dark-500"
+			>
 				<div class="flex items-center gap-2">
 					<svelte:component this={icon} class="h-5 w-5" />
 
@@ -42,11 +59,13 @@
 			</div>
 		{/if}
 
-		<main class="flex max-h-[500px] flex-auto overflow-auto">
+		<div class="flex max-h-[500px] flex-auto overflow-auto px-4">
 			<slot {item} />
-		</main>
+		</div>
 
-		<div class="shadowk flex w-full justify-end gap-4">
+		<div
+			class="flex w-full justify-end gap-4 border-t border-light-400 px-4 pt-4 dark:border-dark-500"
+		>
 			<slot name="controls" {item} {close}>
 				<Button kind="outlined" on:click={close}>Secondary action</Button>
 				<Button color="primary" on:click={close}>Primary action</Button>
