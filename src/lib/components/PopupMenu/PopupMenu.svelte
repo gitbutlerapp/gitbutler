767d15fb9aeaae2e603656b7a0954d6b8cb754de
idonahue <donahue.ian@gmail.com>
2023-07-11 17:59:08 +0200

    Greater contrast between contextual menu and background

--- src/lib/components/PopupMenu/PopupMenu.svelte
+++ src/lib/components/PopupMenu/PopupMenu.svelte
@@ -55,7 +55,7 @@
 			on:mouseleave={onDismiss}
 			on:blur={onDismiss}
 			style="position: absolute; top:{pos.y}px; left:{pos.x}px"
-			class="flex flex-col rounded border border-light-400 bg-white p-1 shadow dark:border-dark-500 dark:bg-dark-700"
+			class="flex flex-col rounded border border-light-400 bg-white p-1 drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)] dark:border-dark-500 dark:bg-dark-700"
 		>
 			<slot {item} />
 		</div>
