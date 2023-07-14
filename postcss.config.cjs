8a6314eb27095535a610b9fbe90223670117c657
Mattias Granlund <mtsgrd@gmail.com>
2023-07-14 09:44:37 +0200

    Use bundled tailwind nesting plugin
    See documentation at:
    https://tailwindcss.com/docs/using-with-preprocessors#nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,11 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const nested = require('tailwindcss/nesting');
 
 const config = {
 	plugins: [
 		//Makes it easier to define .dark theme classes
-		require('postcss-nested'),
+		nested,
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
