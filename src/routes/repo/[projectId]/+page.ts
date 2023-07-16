0c596d80385e87930be5fbfa22669d538aba3591
Kiril Videlov <kiril@videlov.com>
2023-07-16 13:07:01 +0200

    remove test that does nothing

--- src/routes/repo/[projectId]/+page.ts
+++ src/routes/repo/[projectId]/+page.ts
@@ -1,15 +1,9 @@
-import type { BranchData } from '$lib/vbranches';
 import type { PageLoadEvent } from './$types';
 import { invoke } from '$lib/ipc';
 import { api } from '$lib';
 
 async function getRemoteBranches(params: { projectId: string }) {
 	return invoke<Array<string>>('git_remote_branches', params);
-}
-
-function sortBranchData(branchData: BranchData[]): BranchData[] {
-	// sort remote_branches_data by date
-	return branchData.sort((a, b) => b.lastCommitTs - a.lastCommitTs);
 }
 
 export async function load({ parent, params }: PageLoadEvent) {
@@ -30,21 +24,4 @@ export async function load({ parent, params }: PageLoadEvent) {
 		remoteBranchStore,
 		targetBranchStore
 	};
-}
-
-if (import.meta.vitest) {
-	const { it, expect } = import.meta.vitest;
-	it('sorts by last commit timestamp', () => {
-		const bd: BranchData[] = [
-			{
-				sha: 'a',
-				lastCommitTs: 1
-			} as BranchData,
-			{
-				sha: 'b',
-				lastCommitTs: 2
-			} as BranchData
-		];
-		expect(sortBranchData(bd)[0].sha).toBe('b');
-	});
 }
