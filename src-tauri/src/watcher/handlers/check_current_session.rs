60dee0154dacdb5906d46afa4e4ccf782889b5a6
Nikita Galaiko <nikita@galaiko.rocks>
2023-07-21 08:32:24 +0200

    handlers: move tests into same file

--- src-tauri/src/watcher/handlers/check_current_session.rs
+++ src-tauri/src/watcher/handlers/check_current_session.rs
@@ -72,3 +72,36 @@ fn is_session_active(now: time::SystemTime, session: &sessions::Session) -> Resu
         time::UNIX_EPOCH + time::Duration::from_millis(session.meta.last_timestamp_ms.try_into()?);
     Ok(session_last_update + FIVE_MINUTES > now)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const FIVE_MINUTES: time::Duration = time::Duration::new(5 * 60, 0);
+    const ONE_HOUR: time::Duration = time::Duration::new(60 * 60, 0);
+
+    #[test]
+    fn test_should_flush() -> Result<()> {
+        let now = time::SystemTime::now();
+        let start = now;
+        let last = now;
+
+        let session = sessions::Session {
+            id: "session-id".to_string(),
+            hash: None,
+            meta: sessions::Meta {
+                start_timestamp_ms: start.duration_since(time::UNIX_EPOCH)?.as_millis(),
+                last_timestamp_ms: last.duration_since(time::UNIX_EPOCH)?.as_millis(),
+                branch: None,
+                commit: None,
+            },
+        };
+
+        assert!(!should_flush(now, &session)?);
+
+        assert!(should_flush(start + FIVE_MINUTES, &session)?);
+        assert!(should_flush(last + ONE_HOUR, &session)?);
+
+        Ok(())
+    }
+}
