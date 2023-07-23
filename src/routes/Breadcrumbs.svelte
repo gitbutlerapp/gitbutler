3c02a4a8e7f2c1a61c43ddb75fb3dc41b6df88fe
Mattias Granlund <mtsgrd@gmail.com>
2023-07-23 22:26:46 +0100

    Link to project correctly from breadcrumbs.

--- src/routes/Breadcrumbs.svelte
+++ src/routes/Breadcrumbs.svelte
@@ -10,11 +10,7 @@
 <div class="flex flex-row items-stretch gap-1 font-bold text-light-800 dark:text-dark-100">
 	<button class="p-1" on:click={() => goto('/')}><IconHome /></button>
 	{#if project}
-		<a
-			class="button flex items-center px-3"
-			href="/projects/{project.id}"
-			title="{project.title} home"
-		>
+		<a class="button flex items-center px-3" href="/repo/{project.id}" title="{project.title} home">
 			{project.title}
 		</a>
 	{/if}
