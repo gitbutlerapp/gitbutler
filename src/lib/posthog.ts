4541a92ab1b6720cce66092bd04a7d6318f41117
Nikita Galaiko <nikita@galaiko.rocks>
2023-07-26 08:43:32 +0200

    posthog: report app name & app version

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,6 +1,7 @@
 import posthog from 'posthog-js';
 import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
 import type { User } from '$lib/api';
+import { getVersion, getName } from '@tauri-apps/api/app';
 
 export default () => {
 	const instance = posthog.init(PUBLIC_POSTHOG_API_KEY, {
@@ -10,7 +11,14 @@ export default () => {
 		persistence: 'localStorage',
 		on_xhr_error: () => {
 			// noop
-		}
+		},
+		loaded: (instance) =>
+			Promise.all([getVersion(), getName()]).then(([version, name]) => {
+				instance.register_for_session({
+					appName: name,
+					appVersion: version
+				});
+			})
 	});
 	return {
 		identify: (user: User | null) => {
