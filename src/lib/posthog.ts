c44c3e3c2816e883326f917ae69c700d6face144
Nikita Galaiko <nikita@galaiko.rocks>
2023-07-26 08:58:33 +0200

    promised gitbutler client

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -3,34 +3,37 @@ import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
 import type { User } from '$lib/api';
 import { getVersion, getName } from '@tauri-apps/api/app';
 
-export default () => {
-	const instance = posthog.init(PUBLIC_POSTHOG_API_KEY, {
-		api_host: 'https://eu.posthog.com',
-		capture_performance: false,
-		request_batching: true,
-		persistence: 'localStorage',
-		on_xhr_error: () => {
-			// noop
-		},
-		loaded: (instance) =>
-			Promise.all([getVersion(), getName()]).then(([version, name]) => {
+export default async function () {
+	const [appName, appVersion] = await Promise.all([getName(), getVersion()]);
+	return new Promise((resolve, _reject) => {
+		posthog.init(PUBLIC_POSTHOG_API_KEY, {
+			api_host: 'https://eu.posthog.com',
+			capture_performance: false,
+			request_batching: true,
+			persistence: 'localStorage',
+			on_xhr_error: () => {
+				// noop
+			},
+			loaded: (instance) => {
+				console.log('Posthog loaded');
 				instance.register_for_session({
-					appName: name,
-					appVersion: version
+					appName,
+					appVersion
 				});
-			})
-	});
-	return {
-		identify: (user: User | null) => {
-			if (user) {
-				instance?.identify(`user_${user.id}`, {
-					email: user.email,
-					name: user.name
+				resolve({
+					identify: (user: User | null) => {
+						if (user) {
+							instance.identify(`user_${user.id}`, {
+								email: user.email,
+								name: user.name
+							});
+						} else {
+							instance.capture('log-out');
+							instance.reset();
+						}
+					}
 				});
-			} else {
-				instance?.capture('log-out');
-				instance?.reset();
 			}
-		}
-	};
-};
+		});
+	});
+}
