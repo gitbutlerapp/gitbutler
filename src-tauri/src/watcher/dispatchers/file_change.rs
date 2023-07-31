b126a3d7c9b2ed1a4c5dd1e0ecb24b38420c3664
Nikita Galaiko <nikita@galaiko.rocks>
2023-07-31 13:35:22 +0200

    only emit interesting file changes

--- src-tauri/src/watcher/dispatchers/file_change.rs
+++ src-tauri/src/watcher/dispatchers/file_change.rs
@@ -39,7 +39,38 @@ impl Dispatcher {
     }
 
     pub async fn run(&self, rtx: mpsc::UnboundedSender<events::Event>) -> Result<()> {
-        let (mut watcher, mut rx) = async_watcher()?;
+        let repo = git2::Repository::open(&self.project_path).with_context(|| {
+            format!(
+                "failed to open project repository: {}",
+                self.project_path.display()
+            )
+        })?;
+
+        let (tx, mut rx) = mpsc::channel(1);
+        let mut watcher = RecommendedWatcher::new(
+            move |res: notify::Result<notify::Event>| match res {
+                Ok(event) => {
+                    if !is_interesting_kind(&event.kind) {
+                        return;
+                    }
+                    for path in event
+                        .paths
+                        .into_iter()
+                        .filter(|file| is_interesting_file(&repo, file))
+                    {
+                        block_on(async {
+                            log::warn!("sending file change event: {}", path.display());
+                            if let Err(error) = tx.send(path).await {
+                                log::error!("failed to send file change event: {:#}", error);
+                            }
+                        });
+                    }
+                }
+                Err(error) => log::error!("file watcher error: {:#}", error),
+            },
+            Config::default(),
+        )?;
+
         watcher
             .watch(
                 std::path::Path::new(&self.project_path),
@@ -55,23 +86,29 @@ impl Dispatcher {
 
         log::info!("{}: file watcher started", self.project_id);
 
-        while let Some(event) = rx.recv().await {
-            for file_path in event.paths {
-                match file_path.strip_prefix(&self.project_path) {
-                    Ok(relative_file_path) => {
-                        if let Err(e) =
-                            rtx.send(events::Event::FileChange(relative_file_path.to_path_buf()))
-                        {
-                            log::error!(
-                                "{}: failed to send file change event: {:#}",
-                                self.project_id,
-                                e
-                            );
-                        }
+        while let Some(file_path) = rx.recv().await {
+            match file_path.strip_prefix(&self.project_path) {
+                Ok(relative_file_path) => {
+                    let event = if relative_file_path.starts_with(".git") {
+                        events::Event::GitFileChange(
+                            relative_file_path
+                                .strip_prefix(".git")
+                                .unwrap()
+                                .to_path_buf(),
+                        )
+                    } else {
+                        events::Event::ProjectFileChange(relative_file_path.to_path_buf())
+                    };
+                    if let Err(e) = rtx.send(event) {
+                        log::error!(
+                            "{}: failed to send file change event: {:#}",
+                            self.project_id,
+                            e
+                        );
                     }
-                    Err(err) => {
-                        log::error!("{}: failed to strip prefix: {:#}", self.project_id, err)
-                    }
+                }
+                Err(err) => {
+                    log::error!("{}: failed to strip prefix: {:#}", self.project_id, err)
                 }
             }
         }
@@ -82,29 +119,7 @@ impl Dispatcher {
     }
 }
 
-fn async_watcher() -> notify::Result<(RecommendedWatcher, mpsc::Receiver<notify::Event>)> {
-    let (tx, rx) = mpsc::channel(1);
-
-    let watcher = RecommendedWatcher::new(
-        move |res: notify::Result<notify::Event>| match res {
-            Ok(event) => {
-                if is_interesting_event(&event.kind) {
-                    block_on(async {
-                        if let Err(error) = tx.send(event).await {
-                            log::error!("failed to send file change event: {:#}", error);
-                        }
-                    });
-                }
-            }
-            Err(error) => log::error!("file watcher error: {:#}", error),
-        },
-        Config::default(),
-    )?;
-
-    Ok((watcher, rx))
-}
-
-fn is_interesting_event(kind: &notify::EventKind) -> bool {
+fn is_interesting_kind(kind: &notify::EventKind) -> bool {
     matches!(
         kind,
         notify::EventKind::Create(notify::event::CreateKind::File)
@@ -112,4 +127,15 @@ fn is_interesting_event(kind: &notify::EventKind) -> bool {
             | notify::EventKind::Modify(notify::event::ModifyKind::Name(_))
             | notify::EventKind::Remove(notify::event::RemoveKind::File)
     )
+}
+
+fn is_interesting_file(git_repo: &git2::Repository, file_path: &path::Path) -> bool {
+    if file_path.starts_with(git_repo.path()) {
+        file_path.ends_with("FETCH_HEAD")
+            || file_path.eq(path::Path::new("logs/HEAD"))
+            || file_path.eq(path::Path::new("HEAD"))
+            || file_path.eq(path::Path::new("index"))
+    } else {
+        !git_repo.is_path_ignored(file_path).unwrap_or(false)
+    }
 }
