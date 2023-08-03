67f67d83ad7be7d6b01ec2446f76ed5f2b517b29
Nikita Galaiko <nikita@galaiko.rocks>
2023-08-03 12:48:35 +0200

    store keys as plaintext

--- src-tauri/src/keys/mod.rs
+++ src-tauri/src/keys/mod.rs
@@ -2,5 +2,4 @@ pub mod commands;
 mod controller;
 mod key;
 
-pub use controller::{Controller, Error};
-pub use key::{PrivateKey, PublicKey};
+pub use controller::{Controller, Error, PublicKey, PrivateKey};
