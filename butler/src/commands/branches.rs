25ebce22706f6dc6de3ac373f1226df02f25d9fd
Nikita Galaiko <nikita@galaiko.rocks>
2023-08-03 09:16:52 +0200

    move all deps to lib.rs

--- butler/src/commands/branches.rs
+++ butler/src/commands/branches.rs
@@ -13,12 +13,9 @@ impl super::RunCommand for Branches {
     fn run(self) -> Result<()> {
         let app = App::new().context("Failed to create app")?;
 
-        let branches = virtual_branches::list_virtual_branches(
-            app.gb_repository(),
-            &app.project_repository(),
-            true,
-        )
-        .context("failed to list branches")?;
+        let branches =
+            virtual_branches::list_virtual_branches(app.gb_repository(), &app.project_repository())
+                .context("failed to list branches")?;
 
         for branch in branches {
             println!("{}", branch.id.red());
