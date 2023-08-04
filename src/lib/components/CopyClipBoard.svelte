de24f802000bbb71fe8310656d959c39db1ee09d
Scott Chacon <schacon@gmail.com>
2023-08-04 12:44:02 +0200

    add ssh key copy to clipboard to settings

---
+++ src/lib/components/CopyClipBoard.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let value;
+
+	let textarea;
+
+	onMount(() => {
+		textarea.select();
+		document.execCommand('copy');
+	});
+</script>
+
+<textarea bind:value bind:this={textarea} />
