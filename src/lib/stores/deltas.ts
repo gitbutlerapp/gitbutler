e266827bee84854127ba0da91353e989015d53f5
Mattias Granlund <mtsgrd@gmail.com>
2023-08-07 22:39:52 +0100

    Simplify svelte store related function params

--- src/lib/stores/deltas.ts
+++ src/lib/stores/deltas.ts
@@ -4,31 +4,31 @@ import { get, type Writable } from '@square/svelte-store';
 import type { Delta } from '$lib/api/ipc/deltas';
 
 export interface DeltasStore extends Writable<Loadable<Record<string, Delta[]>>> {
-	subscribeStream(params: { projectId: string; sessionId: string }): () => void;
+	subscribeStream(sessionId: string): () => void;
 }
 
 export function getDeltasStore(params: { projectId: string; sessionId: string }) {
-	const store = getDeltasStore2();
-	store.subscribeStream(params);
+	const store = getDeltasStore2(params.projectId);
+	store.subscribeStream(params.sessionId);
 	return store;
 }
-export function getDeltasStore2(): DeltasStore {
+export function getDeltasStore2(projectId: string): DeltasStore {
 	const store = writable<Record<string, Delta[]>>();
-	const subscribe = (innerParams: { projectId: string; sessionId: string }) => {
-		console.log('subscribing streams', innerParams);
-		deltas.list(innerParams).then((results) => {
+	const subscribe = (sessionId: string) => {
+		const combinedParams = { sessionId, projectId };
+		deltas.list(combinedParams).then((results) => {
 			if (Loaded.isValue(results)) {
 				store.set(results);
 			}
 		});
-		return deltas.subscribe(innerParams, ({ filePath, deltas: newDeltas }) => {
+		return deltas.subscribe(combinedParams, ({ filePath, deltas: newDeltas }) => {
 			const oldValue = get(store);
 			if (oldValue.isLoading) {
-				deltas.list(innerParams).then((result) => {
+				deltas.list(combinedParams).then((result) => {
 					if (Loaded.isValue(result)) store.set(result);
 				});
 			} else if (Loaded.isError(oldValue)) {
-				deltas.list(innerParams).then((result) => {
+				deltas.list(combinedParams).then((result) => {
 					if (Loaded.isValue(result)) store.set(result);
 				});
 			} else {
