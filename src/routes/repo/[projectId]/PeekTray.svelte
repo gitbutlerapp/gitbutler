f012210c72592c0df09c327e74409e0f2150b844
Mattias Granlund <mtsgrd@gmail.com>
2023-08-14 10:24:49 +0200

    Use resizer in peek tray component as well

--- src/routes/repo/[projectId]/PeekTray.svelte
+++ src/routes/repo/[projectId]/PeekTray.svelte
@@ -8,6 +8,7 @@
 	import type { Readable } from '@square/svelte-store';
 	import BaseBranchPeek from './BaseBranchPeek.svelte';
 	import RemoteBranchPeek from './RemoteBranchPeek.svelte';
+	import Resizer from '$lib/components/Resizer.svelte';
 
 	export let item: Readable<BranchData | Branch | BaseBranch | undefined> | undefined;
 	export let base: BaseBranch | undefined;
@@ -17,10 +18,7 @@
 	export let fullHeight = false;
 	export let disabled = false;
 
-	let dragging = false;
-	let hovering = false;
-	let container: HTMLDivElement;
-	let addMargin = 0;
+	let viewport: HTMLElement;
 
 	$: offsetLeft = expanded
 		? $userSettings.trayWidth
@@ -32,40 +30,6 @@
 
 	function toggleExpanded() {
 		expanded = !expanded;
-		dragging = false;
-		hovering = false;
-	}
-
-	function onMouseDown(e: MouseEvent) {
-		e.stopPropagation();
-		e.preventDefault();
-		dragging = true;
-		addMargin = container.offsetWidth + $userSettings.trayWidth - e.clientX;
-		document.addEventListener('mouseup', onMouseUp);
-		document.addEventListener('mousemove', onMouseMove);
-	}
-
-	function onMouseEnter() {
-		hovering = true;
-	}
-
-	function onMouseLeave() {
-		if (!dragging) {
-			hovering = false;
-		}
-	}
-
-	function onMouseMove(e: MouseEvent) {
-		userSettings.update((s) => ({
-			...s,
-			peekTrayWidth: e.clientX - $userSettings.trayWidth + addMargin
-		}));
-	}
-
-	function onMouseUp() {
-		dragging = false;
-		document.removeEventListener('mouseup', onMouseUp);
-		document.removeEventListener('mousemove', onMouseMove);
 	}
 
 	$: if (item == undefined || $item == undefined) expanded = false;
@@ -73,46 +37,48 @@
 </script>
 
 <div
-	bind:this={container}
 	class:h-full={fullHeight}
 	class:shadow-xl={expanded}
 	style:top={fullHeight ? 0 : `${offsetTop}px`}
 	style:width={`${$userSettings.peekTrayWidth}px`}
 	style:translate={`${offsetLeft}px`}
 	style:transition-property={!disabled ? (expanded ? 'top,translate,height' : 'translate') : 'none'}
-	class="absolute z-30 flex shrink-0 overflow-y-auto overflow-x-visible bg-white text-light-800 duration-200 ease-in-out dark:bg-dark-800 dark:text-dark-100"
+	class="absolute z-30 flex shrink-0 overflow-visible bg-white text-light-800 duration-200 ease-in-out dark:bg-dark-800 dark:text-dark-100"
 >
-	<div class="w-full flex-grow overflow-y-scroll" style:width={`${$userSettings.peekTrayWidth}px`}>
-		{#if $item instanceof BranchData}
-			<RemoteBranchPeek {branchController} {base} branch={$item} />
-		{:else if $item instanceof Branch}
-			<VirtualBranchPeek {branchController} {base} branch={$item} />
-		{:else if $item instanceof BaseBranch}
-			<BaseBranchPeek base={$item} {branchController} />
-		{:else}
-			Unknown instance
-		{/if}
+	<div class="flex w-full flex-grow" bind:this={viewport}>
+		<div
+			class="w-full flex-grow overflow-y-scroll"
+			style:width={`${$userSettings.peekTrayWidth}px`}
+		>
+			{#if $item instanceof BranchData}
+				<RemoteBranchPeek {branchController} {base} branch={$item} />
+			{:else if $item instanceof Branch}
+				<VirtualBranchPeek {branchController} {base} branch={$item} />
+			{:else if $item instanceof BaseBranch}
+				<BaseBranchPeek base={$item} {branchController} />
+			{:else}
+				Unknown instance
+			{/if}
+		</div>
+		<div
+			class="flex w-4 cursor-pointer items-center bg-light-50 text-light-600 dark:bg-dark-700"
+			role="button"
+			tabindex="0"
+			on:click={toggleExpanded}
+			on:keypress={toggleExpanded}
+		>
+			<IconChevronLeft />
+		</div>
+		<Resizer
+			{viewport}
+			direction="horizontal"
+			class="z-30"
+			on:width={(e) => {
+				userSettings.update((s) => ({
+					...s,
+					peekTrayWidth: e.detail
+				}));
+			}}
+		/>
 	</div>
-	<div
-		class="flex w-4 cursor-pointer items-center bg-light-50 text-light-600 dark:bg-dark-700"
-		role="button"
-		tabindex="0"
-		on:click={toggleExpanded}
-		on:keypress={toggleExpanded}
-		on:mouseenter={onMouseEnter}
-		on:mouseleave={onMouseLeave}
-	>
-		<IconChevronLeft />
-	</div>
-	<div
-		on:mousedown={onMouseDown}
-		on:mouseenter={onMouseEnter}
-		on:mouseleave={onMouseLeave}
-		tabindex="0"
-		role="slider"
-		aria-valuenow={$userSettings.peekTrayWidth}
-		class:bg-orange-300={hovering}
-		class:dark:bg-orange-700={hovering}
-		class="right-0 h-full w-0.5 shrink-0 cursor-ew-resize bg-light-50 text-light-600 dark:bg-dark-700"
-	/>
 </div>
