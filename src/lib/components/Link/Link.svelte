c4fb2e7653d80b3bb5d355a01572c943304535a7
Mattias Granlund <mtsgrd@gmail.com>
2023-08-17 23:31:12 +0100

    Fix truncation of github branch link

--- src/lib/components/Link/Link.svelte
+++ src/lib/components/Link/Link.svelte
@@ -2,6 +2,8 @@
 	import { onMount } from 'svelte';
 	import { IconExternalLink } from '../../icons';
 
+	let classes = '';
+	export { classes as class };
 	export let target: '_blank' | '_self' | '_parent' | '_top' | undefined = undefined;
 	export let rel: string | undefined = undefined;
 	export let role: 'basic' | 'primary' | 'destructive' = 'basic';
@@ -21,15 +23,15 @@
 	{href}
 	{target}
 	{rel}
-	class="link flex items-center {role}"
+	class="link flex items-center {role} {classes}"
 	bind:this={element}
 	class:disabled
 	on:click
 >
-	<div>
+	<div class="flex-grow truncate">
 		<slot />
 	</div>
-	<div>
+	<div class="shrink-0">
 		{#if isExternal}
 			<IconExternalLink class="h-4 w-4" />
 		{/if}
