56d652b04225a9ee7dec2bb49f891e78d618a185
Nikita Galaiko <nikita@galaiko.rocks>
2023-08-30 17:37:59 +0200

    wrap tree entry

--- src-tauri/src/git/tree.rs
+++ src-tauri/src/git/tree.rs
@@ -1,8 +1,6 @@
 use std::path;
 
-use git2::TreeEntry;
-
-use super::{Oid, Result};
+use super::{Oid, Repository, Result};
 
 pub struct Tree<'repo> {
     tree: git2::Tree<'repo>,
@@ -25,19 +23,52 @@ impl<'repo> Tree<'repo> {
         self.tree.id().into()
     }
 
-    pub fn get_path(&self, path: &path::Path) -> Result<git2::TreeEntry<'repo>> {
-        self.tree.get_path(path)
+    pub fn get_path(&self, path: &path::Path) -> Result<TreeEntry<'repo>> {
+        self.tree.get_path(path).map(Into::into)
     }
 
-    pub fn walk<C, T>(&self, mode: git2::TreeWalkMode, callback: C) -> Result<()>
+    pub fn walk<C, T>(&self, mode: git2::TreeWalkMode, mut callback: C) -> Result<()>
     where
-        C: FnMut(&str, &git2::TreeEntry) -> T,
+        C: FnMut(&str, &TreeEntry) -> T,
         T: Into<i32>,
     {
-        self.tree.walk(mode, callback)
+        self.tree
+            .walk(mode, |root, entry| callback(root, &entry.clone().into()))
     }
 
     pub fn get_name(&self, filename: &str) -> Option<TreeEntry> {
-        self.tree.get_name(filename)
+        self.tree.get_name(filename).map(Into::into)
+    }
+}
+
+pub struct TreeEntry<'repo> {
+    entry: git2::TreeEntry<'repo>,
+}
+
+impl<'repo> From<git2::TreeEntry<'repo>> for TreeEntry<'repo> {
+    fn from(entry: git2::TreeEntry<'repo>) -> Self {
+        TreeEntry { entry }
+    }
+}
+
+impl<'repo> TreeEntry<'repo> {
+    pub fn filemode(&self) -> i32 {
+        self.entry.filemode()
+    }
+
+    pub fn to_object(&self, repo: &'repo Repository) -> Result<git2::Object> {
+        self.entry.to_object(repo.into())
+    }
+
+    pub fn kind(&self) -> Option<git2::ObjectType> {
+        self.entry.kind()
+    }
+
+    pub fn id(&self) -> Oid {
+        self.entry.id().into()
+    }
+
+    pub fn name(&self) -> Option<&str> {
+        self.entry.name()
     }
 }
