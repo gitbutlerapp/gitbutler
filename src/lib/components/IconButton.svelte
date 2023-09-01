1b34c151cc1dc85ddb639472724330a910622d76
Mattias Granlund <mtsgrd@gmail.com>
2023-09-01 11:59:39 +0100

    Use an on hover "apply >" button in vbranch tray section

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -12,5 +12,6 @@
 	class="{className} text-light-600 hover:text-light-800 focus:text-light-800 disabled:cursor-not-allowed disabled:text-light-200 dark:text-dark-400 hover:dark:text-dark-100 focus:dark:text-dark-600 dark:disabled:text-dark-400"
 	{title}
 >
+	<slot />
 	<svelte:component this={icon} />
 </button>
