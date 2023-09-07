28abbda9dd96ca74ba625f5f2a5b8410950a62ed
Nikita Galaiko <nikita@galaiko.rocks>
2023-09-07 17:50:58 +0200

    upload debs to s3

--- scripts/release.sh
+++ scripts/release.sh
@@ -233,8 +233,6 @@ BUNDLE_DIR=$(readlink -f "$PWD/../target/release/bundle")
 RELEASE_DIR="$DIST/$OS/$ARCH"
 mkdir -p "$RELEASE_DIR"
 
-find $RELEASE_DIR -type f
-
 if [ "$OS" = "macos" ]; then
 	MACOS_DMG="$(find "$BUNDLE_DIR/dmg" -depth 1 -type f -name "*.dmg")"
 	MACOS_UPDATER="$(find "$BUNDLE_DIR/macos" -depth 1 -type f -name "*.tar.gz")"
@@ -252,15 +250,18 @@ elif [ "$OS" = "linux" ]; then
 	APPIMAGE="$(find $BUNDLE_DIR/appimage -name \*.AppImage)"
 	APPIMAGE_UPDATER="$(find $BUNDLE_DIR/appimage -name \*.AppImage.tar.gz)"
 	APPIMAGE_UPDATER_SIG="$(find $BUNDLE_DIR/appimage -name \*.AppImage.tar.gz.sig)"
+	DEB="$(find $BUNDLE_DIR/deb -name \*.deb)"
 
 	cp "$APPIMAGE" "$RELEASE_DIR"
 	cp "$APPIMAGE_UPDATER" "$RELEASE_DIR"
 	cp "$APPIMAGE_UPDATER_SIG" "$RELEASE_DIR"
+	cp "$DEB" "$RELEASE_DIR"
 
 	info "built:"
 	info "  - $RELEASE_DIR/$(basename "$APPIMAGE")"
 	info "  - $RELEASE_DIR/$(basename "$APPIMAGE_UPDATER")"
 	info "  - $RELEASE_DIR/$(basename "$APPIMAGE_UPDATER_SIG")"
+	info "  - $RELEASE_DIR/$(basename "$DEB")"
 fi
 
 info "done! bye!"
