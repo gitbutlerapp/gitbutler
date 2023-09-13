c353809869d1e9e19f62754b171491a04af48a54
Mattias Granlund <mtsgrd@gmail.com>
2023-09-13 21:31:58 +0200

    Upgrade to prettier npm -> v3

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -169,29 +169,7 @@ const config = {
 				900: '#18181b'
 			}
 		}
-	},
-	plugins: [
-		// Expose color palette as CSS variables (--color-xxx-yyy)
-		// https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
-		plugin(function ({ addBase, theme }) {
-			function extractColorVars(colorObj, colorGroup = '') {
-				return Object.keys(colorObj).reduce((vars, colorKey) => {
-					const value = colorObj[colorKey];
-
-					const newVars =
-						typeof value === 'string'
-							? { [`--color${colorGroup}-${colorKey}`]: value }
-							: extractColorVars(value, `-${colorKey}`);
-
-					return { ...vars, ...newVars };
-				}, {});
-			}
-
-			addBase({
-				':root': extractColorVars(theme('colors'))
-			});
-		})
-	]
+	}
 };
 
 module.exports = config;
