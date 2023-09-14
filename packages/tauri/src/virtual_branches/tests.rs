0526d11b56bcf6540facc122402db13c3270c785
Nikita Galaiko <nikita@galaiko.rocks>
2023-09-14 15:31:50 +0200

    fix tests

--- packages/tauri/src/virtual_branches/tests.rs
+++ packages/tauri/src/virtual_branches/tests.rs
@@ -2021,8 +2021,6 @@ fn test_detect_mergeable_branch() -> Result<()> {
         .find(|b| b.name == "refs/remotes/origin/remote_branch")
         .unwrap();
     assert!(!remote1.mergeable);
-    assert_eq!(remote1.ahead, 1);
-    assert_eq!(remote1.merge_conflicts.len(), 1);
     assert_eq!(remote1.merge_conflicts.first().unwrap(), "test.txt");
     assert_eq!(remote1.commits.len(), 1);
 
@@ -2031,7 +2029,6 @@ fn test_detect_mergeable_branch() -> Result<()> {
         .find(|b| b.name == "refs/remotes/origin/remote_branch2")
         .unwrap();
     assert!(remote2.mergeable);
-    assert_eq!(remote2.ahead, 2);
     assert_eq!(remote2.merge_conflicts.len(), 0);
     assert_eq!(remote2.commits.len(), 2);
 
