bd98ee0f367c37eca45710ced0511b722a404e48
Mattias Granlund <mtsgrd@gmail.com>
2023-10-06 16:19:50 +0200

    Add a handler for unhandled rejections
    - this change gives us better stack traces when reactive things break

--- packages/ui/src/hooks.client.ts
+++ packages/ui/src/hooks.client.ts
@@ -15,3 +15,16 @@ function myErrorHandler({ error, event }: { error: any; event: NavigationEvent }
 }
 
 export const handleError = handleErrorWithSentry(myErrorHandler);
+
+/**
+ * This is not an ideal way of handling unhandled errors, but it's what we have at the moment. The
+ * main reason for adding this is that it gives us better stack traces when promises throw errors
+ * in reactive statements.
+ *
+ * See: https://github.com/sveltejs/rfcs/pull/46
+ */
+const originalUnhandledHandler = window.onunhandledrejection;
+window.onunhandledrejection = (event: PromiseRejectionEvent) => {
+	console.log('Unhandled exception', event.reason);
+	originalUnhandledHandler?.bind(window)(event);
+};
