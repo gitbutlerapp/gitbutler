f535ba30d59bc60fc5d162a857b0f1a6475f4700
extrawurst <mail@rusticorn.com>
2023-10-09 15:11:13 +0200

    trigger `flush_session` if `GB_FLUSH` file triggers watcher git event
    * remove GB_FLUSH after processing it
    * add test for flushing and deleting flush file
    * add more unittests for cases:
     * no session, file -> no flush, file deleted
     * session, file -> flush, file deleted
     * session, no file -> no flush

--- packages/tauri/src/watcher/handlers/git_file_change.rs
+++ packages/tauri/src/watcher/handlers/git_file_change.rs
@@ -1,20 +1,32 @@
+use std::path;
+
 use anyhow::{Context, Result};
 use tauri::AppHandle;
 
-use crate::{analytics, events as app_events, project_repository, projects};
+use crate::{analytics, events as app_events, gb_repository, project_repository, projects, users};
 
 use super::events;
 
 #[derive(Clone)]
 pub struct Handler {
+    local_data_dir: path::PathBuf,
     projects: projects::Controller,
+    users: users::Controller,
 }
 
 impl TryFrom<&AppHandle> for Handler {
     type Error = anyhow::Error;
     fn try_from(value: &AppHandle) -> Result<Self, Self::Error> {
+        let local_data_dir = value
+            .path_resolver()
+            .app_local_data_dir()
+            .context("failed to get local data dir")?;
+        let project_store = projects::Controller::try_from(value)?;
+        let user_store = users::Controller::try_from(value)?;
         Ok(Self {
-            projects: projects::Controller::try_from(value)?,
+            projects: project_store,
+            local_data_dir,
+            users: user_store,
         })
     }
 }
@@ -40,6 +52,35 @@ impl Handler {
             "logs/HEAD" => Ok(vec![events::Event::Emit(app_events::Event::git_activity(
                 &project.id,
             ))]),
+            "GB_FLUSH" => {
+                let user = self.users.get_user()?;
+                let gb_repo = gb_repository::Repository::open(
+                    &self.local_data_dir,
+                    &project_repository,
+                    user.as_ref(),
+                )
+                .context("failed to open repository")?;
+
+                let file_path = project.path.join(".git/GB_FLUSH");
+
+                if file_path.exists() {
+                    if let Err(e) = std::fs::remove_file(&file_path) {
+                        tracing::error!(project_id, path = %file_path.display(), "GB_FLUSH file delete error: {}", e);
+                    }
+
+                    if let Some(current_session) = gb_repo
+                        .get_current_session()
+                        .context("failed to get current session")?
+                    {
+                        return Ok(vec![events::Event::Flush(
+                            project.id.clone(),
+                            current_session,
+                        )]);
+                    }
+                }
+
+                Ok(vec![])
+            }
             "HEAD" => {
                 let head_ref = project_repository
                     .get_head()
@@ -62,5 +103,110 @@ impl Handler {
             ))]),
             _ => Ok(vec![]),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+
+    use events::Event;
+    use pretty_assertions::assert_eq;
+
+    use crate::{
+        test_utils::{Case, Suite},
+        watcher::handlers,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_flush_session() -> Result<()> {
+        let suite = Suite::default();
+        let Case {
+            project,
+            gb_repository,
+            ..
+        } = suite.new_case();
+
+        assert!(gb_repository.get_current_session()?.is_none());
+        create_new_session_via_new_file(&project, &suite);
+        assert!(gb_repository.get_current_session()?.is_some());
+
+        let listener = Handler {
+            local_data_dir: suite.local_app_data,
+            projects: suite.projects,
+            users: suite.users,
+        };
+
+        let flush_file_path = project.path.join(".git/GB_FLUSH");
+        fs::write(flush_file_path.as_path(), "")?;
+
+        let result = listener.handle("GB_FLUSH", &project.id)?;
+
+        assert_eq!(result.len(), 1);
+        assert!(matches!(result[0], Event::Flush(_, _)));
+
+        assert!(!flush_file_path.exists(), "flush file deleted");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_do_not_flush_session_if_file_is_missing() -> Result<()> {
+        let suite = Suite::default();
+        let Case {
+            project,
+            gb_repository,
+            ..
+        } = suite.new_case();
+
+        assert!(gb_repository.get_current_session()?.is_none());
+        create_new_session_via_new_file(&project, &suite);
+        assert!(gb_repository.get_current_session()?.is_some());
+
+        let listener = Handler {
+            local_data_dir: suite.local_app_data,
+            projects: suite.projects,
+            users: suite.users,
+        };
+
+        let result = listener.handle("GB_FLUSH", &project.id)?;
+
+        assert_eq!(result.len(), 0);
+
+        Ok(())
+    }
+
+    fn create_new_session_via_new_file(project: &projects::Project, suite: &Suite) {
+        fs::write(project.path.join("test.txt"), "test").unwrap();
+        let file_change_listener =
+            handlers::project_file_change::Handler::from(&suite.local_app_data);
+        file_change_listener
+            .handle("test.txt", &project.id)
+            .unwrap();
+    }
+
+    #[test]
+    fn test_flush_deletes_flush_file_without_session_to_flush() -> Result<()> {
+        let suite = Suite::default();
+        let Case { project, .. } = suite.new_case();
+
+        let listener = Handler {
+            local_data_dir: suite.local_app_data,
+            projects: suite.projects,
+            users: suite.users,
+        };
+
+        let flush_file_path = project.path.join(".git/GB_FLUSH");
+        fs::write(flush_file_path.as_path(), "")?;
+
+        let result = listener.handle("GB_FLUSH", &project.id)?;
+
+        assert_eq!(result.len(), 0);
+
+        assert!(!flush_file_path.exists(), "flush file deleted");
+
+        Ok(())
     }
 }
