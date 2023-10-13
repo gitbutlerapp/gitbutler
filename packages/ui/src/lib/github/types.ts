1ae863800552717e03bb2f1c658768fda3c652a4
Kiril Videlov <kiril@videlov.com>
2023-10-13 15:09:10 +0200

    fix formatting

--- packages/ui/src/lib/github/types.ts
+++ packages/ui/src/lib/github/types.ts
@@ -3,9 +3,9 @@ import { Octokit } from '@octokit/rest';
 export type GitHubIntegrationContext = {
 	authToken: string;
 	owner: string;
-    repo: string;
+	repo: string;
 };
 
 export type PullRequest = {
 	html_url: string;
-}
+};
