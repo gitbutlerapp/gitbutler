b36f0a76c1574cdcf51eac87598233b8b105721e
extrawurst <mail@rusticorn.com>
2023-10-16 16:23:53 +0200

    also run clippy on tests

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo ------ cargo check
 cargo c --all-features
+
+echo ------ cargo clippy
 cargo clippy --all-features
-# cargo clippy -- --cfg test
+cargo clippy --tests
+
+echo ------ cargo test
 cargo t
+
+echo ------ cargo fmt
 cargo fmt --check
+
+echo ------ cargo sort
 cargo sort -c -w
