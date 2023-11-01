451f30bdf2d14e35e0a8f3fd3583ede91b217088
Mattias Granlund <mtsgrd@gmail.com>
2023-11-01 11:23:39 +0100

    Remove histoire since it's not useful

--- packages/ui/histoire.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'histoire';
-import { HstSvelte } from '@histoire/plugin-svelte';
-
-export default defineConfig({
-	plugins: [HstSvelte()],
-	setupFile: 'histoire.setup.ts'
-});
