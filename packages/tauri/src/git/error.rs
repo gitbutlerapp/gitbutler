8917e3b9baa4bb8f7c819afea0dfc83a09f67cb7
Nikita Galaiko <nikita@galaiko.rocks>
2023-11-06 15:24:08 +0100

    git: treat ssh key error as auth error

--- packages/tauri/src/git/error.rs
+++ packages/tauri/src/git/error.rs
@@ -6,8 +6,6 @@ pub enum Error {
     NotFound(Box<dyn std::error::Error + Send + Sync>),
     #[error("authentication failed")]
     AuthenticationFailed(Box<dyn std::error::Error + Send + Sync>),
-    #[error("ssh key error: {0}")]
-    SshKeyError(Box<dyn std::error::Error + Send + Sync>),
     #[error("sign error: {0}")]
     SignError(Box<dyn std::error::Error + Send + Sync>),
     #[error("remote url error: {0}")]
@@ -21,19 +19,20 @@ pub enum Error {
 impl From<git2::Error> for Error {
     fn from(err: git2::Error) -> Self {
         if err.class() == git2::ErrorClass::Ssh && err.code() == git2::ErrorCode::GenericError {
-            return Error::SshKeyError(err.into());
-        }
-        match err.code() {
-            git2::ErrorCode::NotFound => Error::NotFound(err.into()),
-            git2::ErrorCode::Auth => Error::AuthenticationFailed(err.into()),
-            _ => Error::Other(err.into()),
+            Error::AuthenticationFailed(err.into())
+        } else {
+            match err.code() {
+                git2::ErrorCode::NotFound => Error::NotFound(err.into()),
+                git2::ErrorCode::Auth => Error::AuthenticationFailed(err.into()),
+                _ => Error::Other(err.into()),
+            }
         }
     }
 }
 
 impl From<keys::SignError> for Error {
     fn from(err: keys::SignError) -> Self {
-        Error::SshKeyError(err.into())
+        Error::SignError(err.into())
     }
 }
 
