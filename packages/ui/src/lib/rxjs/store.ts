3da4d2e81b788221e2f732508a4ab0ee89de714f
Mattias Granlund <mtsgrd@gmail.com>
2023-11-13 12:58:36 +0100

    Make remote contributions code a bit more succinct

--- packages/ui/src/lib/rxjs/store.ts
+++ packages/ui/src/lib/rxjs/store.ts
@@ -1,15 +1,8 @@
-import type { Readable, Writable, Unsubscriber } from 'svelte/store';
+import type { Readable, Writable } from 'svelte/store';
 import { Observable } from 'rxjs';
 
 export function storeToObservable<T>(svelteStore: Writable<T> | Readable<T>): Observable<T> {
-	let unsubscribe: Unsubscriber;
-	const observable = new Observable<T>((subscriber) => {
-		unsubscribe = svelteStore.subscribe((val) => {
-			subscriber.next(val);
-		});
-		return () => {
-			unsubscribe();
-		};
+	return new Observable<T>((subscriber) => {
+		return svelteStore.subscribe((val) => subscriber.next(val));
 	});
-	return observable;
 }
