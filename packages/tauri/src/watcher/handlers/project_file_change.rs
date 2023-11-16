2ba8a9e547112241a4a4c8f0c8db2695fe1ccd5c
Kiril Videlov <kiril@videlov.com>
2023-11-16 15:53:15 +0100

    feat: add rate limiting to throttle events in the Handler's handle method

--- packages/tauri/src/watcher/handlers/project_file_change.rs
+++ packages/tauri/src/watcher/handlers/project_file_change.rs
@@ -1,15 +1,30 @@
 use crate::projects::ProjectId;
 use anyhow::Result;
-use std::vec;
+use governor::{
+    clock::QuantaClock,
+    state::{InMemoryState, NotKeyed},
+    Quota, RateLimiter,
+};
+use nonzero_ext::nonzero;
+use once_cell::sync::OnceCell;
+use std::{sync::Arc, vec};
 
 use super::events;
 
-#[derive(Clone, Default)]
-pub struct Handler {}
+#[derive(Clone)]
+pub struct Handler {
+    quota: Quota,
+    limit: Arc<OnceCell<RateLimiter<NotKeyed, InMemoryState, QuantaClock>>>,
+}
 
 impl Handler {
     pub fn new() -> Self {
-        Default::default()
+        let quota: Quota = Quota::per_second(nonzero!(1_u32)); // 1 per second at most.
+        let limit: OnceCell<RateLimiter<NotKeyed, InMemoryState, QuantaClock>> = OnceCell::new();
+        Handler {
+            quota: quota,
+            limit: limit.into(),
+        }
     }
 
     pub fn handle<P: AsRef<std::path::Path>>(
@@ -18,10 +33,15 @@ impl Handler {
         project_id: &ProjectId,
     ) -> Result<Vec<events::Event>> {
         let path = path.as_ref().to_path_buf();
-        Ok(vec![
-            events::Event::SessionProcessing(project_id.clone(), path),
-            // TODO: throttle this event to max 1 per 30ms
-            events::Event::VirtualBranch(project_id.clone()),
-        ])
+        let mut events = vec![events::Event::SessionProcessing(*project_id, path)];
+
+        let rate_limiter = self
+            .limit
+            .get_or_init(|| RateLimiter::direct(self.quota.clone()));
+
+        if rate_limiter.check().is_ok() {
+            events.push(events::Event::VirtualBranch(*project_id));
+        }
+        Ok(events)
     }
 }
