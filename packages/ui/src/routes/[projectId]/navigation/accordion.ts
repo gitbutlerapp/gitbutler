5fd83e2461823f34cabb3a4d13054bbcc4ae350e
Mattias Granlund <mtsgrd@gmail.com>
2023-11-22 21:28:53 +0100

    Force collapse stashed branches when empty

--- packages/ui/src/routes/[projectId]/navigation/accordion.ts
+++ packages/ui/src/routes/[projectId]/navigation/accordion.ts
@@ -4,10 +4,8 @@ export function accordion(node: HTMLDivElement, isOpen: boolean) {
 	if (!isOpen) {
 		node.classList.add(COLLAPSED_CLASS);
 	}
-	node.style.overflow = 'hidden';
 	return {
 		update(isOpenInner: boolean) {
-			node.style.display = isOpenInner ? 'initial' : 'none';
 			isOpenInner ? node.classList.remove(COLLAPSED_CLASS) : node.classList.add(COLLAPSED_CLASS);
 		}
 	};
