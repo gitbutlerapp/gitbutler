829390ee735b7e87c6f713d092141610cc78a5d4
Mattias Granlund <mtsgrd@gmail.com>
2023-11-22 13:52:08 +0100

    Fix lint errors

--- packages/ui/src/lib/gravatar/url.ts
+++ packages/ui/src/lib/gravatar/url.ts
@@ -1,5 +1,3 @@
-import md5 from 'blueimp-md5';
-
 export function gravatarUrl(id: string | undefined | null): URL | undefined {
 	if (id) return new URL(`https://www.gravatar.com/avatar/${id}?s=100&r=g&d=retro`);
 }
