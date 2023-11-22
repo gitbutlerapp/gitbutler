78d2944476f5efe14d651b7046a4f69fc5803b27
Nikita Galaiko <nikita@galaiko.rocks>
2023-11-22 13:38:44 +0100

    push default_target, not head

--- packages/tauri/src/watcher/handlers/push_project_to_gitbutler.rs
+++ packages/tauri/src/watcher/handlers/push_project_to_gitbutler.rs
@@ -5,6 +5,8 @@ use tauri::AppHandle;
 use tokio::sync::Mutex;
 
 use crate::{
+    gb_repository,
+    paths::DataDir,
     project_repository,
     projects::{self, CodePushState, ProjectId},
     users,
@@ -43,6 +45,7 @@ impl Handler {
 }
 
 pub struct HandlerInner {
+    local_data_dir: DataDir,
     project_store: projects::Controller,
     users: users::Controller,
 }
@@ -52,6 +55,7 @@ impl TryFrom<&AppHandle> for HandlerInner {
 
     fn try_from(value: &AppHandle) -> std::result::Result<Self, Self::Error> {
         Ok(Self {
+            local_data_dir: DataDir::try_from(value)?,
             project_store: projects::Controller::try_from(value)?,
             users: users::Controller::from(value),
         })
@@ -64,105 +68,104 @@ impl HandlerInner {
         project_id: &ProjectId,
         now: &time::SystemTime,
     ) -> Result<Vec<events::Event>> {
-        tracing::info!(
-            %project_id,
-            "push_project_to_gb::handle",
-        );
-
         let project = self
             .project_store
             .get(project_id)
             .context("failed to get project")?;
 
+        if !project.is_sync_enabled() || !project.has_code_url() {
+            return Ok(vec![]);
+        }
+
         let user = self.users.get_user()?;
         let project_repository =
             project_repository::Repository::open(&project).context("failed to open repository")?;
 
-        if project_repository.project().is_sync_enabled()
-            && project_repository.project().has_code_url()
-        {
-            let head_id = project_repository.get_head()?.peel_to_commit()?.id();
-            let gb_code_last_commit = project
-                .gitbutler_code_push_state
-                .as_ref()
-                .map(|state| &state.id)
-                .copied();
+        let gb_code_last_commit = project
+            .gitbutler_code_push_state
+            .as_ref()
+            .map(|state| &state.id)
+            .copied();
 
-            let ids = project_repository.l(
-                head_id,
-                project_repository::LogUntil::EveryNth {
-                    n: 1000,
-                    until_id: gb_code_last_commit,
-                },
-            )?;
+        let gb_repository = gb_repository::Repository::open(
+            &self.local_data_dir,
+            &project_repository,
+            user.as_ref(),
+        )?;
+        let default_target = gb_repository
+            .default_target()
+            .context("failed to open gb repo")?
+            .context("failed to get default target")?;
 
-            tracing::debug!(
-                %project_id,
-                batches=%ids.len(),
-                "batches collected",
-            );
+        let ids = project_repository.l(
+            default_target.sha,
+            project_repository::LogUntil::EveryNth {
+                n: 1000,
+                until_id: gb_code_last_commit,
+            },
+        )?;
 
-            let id_count = ids.len();
+        tracing::debug!(
+            %project_id,
+            batches=%ids.len(),
+            "batches collected",
+        );
 
-            for (idx, id) in ids.iter().enumerate().rev() {
-                let refspec = format!("+{}:refs/push-tmp/{}", id, project_id);
+        let id_count = ids.len();
 
-                project_repository
-                    .push_to_gitbutler_server(user.as_ref(), &[&refspec])
-                    .context("failed to push project to gitbutler")?;
+        for (idx, id) in ids.iter().enumerate().rev() {
+            let refspec = format!("+{}:refs/push-tmp/{}", id, project_id);
 
-                self.project_store
-                    .update(&projects::UpdateRequest {
-                        id: *project_id,
-                        gitbutler_code_push_state: Some(CodePushState {
-                            id: *id,
-                            timestamp: *now,
-                        }),
-                        ..Default::default()
-                    })
-                    .await
-                    .context("failed to update last push")?;
-
-                tracing::debug!(
-                    %project_id,
-                    "project batch pushed: {}/{}",id_count.saturating_sub(idx),id_count,
-                );
-            }
-
-            // push refs/{project_id}
             project_repository
-                .push_to_gitbutler_server(
-                    user.as_ref(),
-                    &[&format!("+{}:refs/{}", head_id, project_id)],
-                )
-                .context("failed to push project (head) to gitbutler")?;
+                .push_to_gitbutler_server(user.as_ref(), &[&refspec])
+                .context("failed to push project to gitbutler")?;
 
-            let refs = gb_refs(&project_repository)?;
-
-            let all_refs = refs
-                .iter()
-                .map(|r| format!("+{}:{}", r, r))
-                .collect::<Vec<_>>();
-
-            let all_refs = all_refs.iter().map(String::as_str).collect::<Vec<_>>();
-
-            // push all gitbutler refs
-            project_repository
-                .push_to_gitbutler_server(user.as_ref(), all_refs.as_slice())
-                .context("failed to push project (all refs) to gitbutler")?;
-
-            //TODO: remove push-tmp ref
+            self.project_store
+                .update(&projects::UpdateRequest {
+                    id: *project_id,
+                    gitbutler_code_push_state: Some(CodePushState {
+                        id: *id,
+                        timestamp: *now,
+                    }),
+                    ..Default::default()
+                })
+                .await
+                .context("failed to update last push")?;
 
             tracing::debug!(
                 %project_id,
-                "project fully pushed",
-            );
-        } else {
-            tracing::debug!(
-                %project_id,
-                "cannot push code to gb",
+                "project batch pushed: {}/{}",id_count.saturating_sub(idx),id_count,
             );
         }
+
+        // push refs/{project_id}
+        project_repository
+            .push_to_gitbutler_server(
+                user.as_ref(),
+                &[&format!("+{}:refs/{}", default_target.sha, project_id)],
+            )
+            .context("failed to push project (head) to gitbutler")?;
+
+        let refs = gb_refs(&project_repository)?;
+
+        let all_refs = refs
+            .iter()
+            .map(|r| format!("+{}:{}", r, r))
+            .collect::<Vec<_>>();
+
+        let all_refs = all_refs.iter().map(String::as_str).collect::<Vec<_>>();
+
+        // push all gitbutler refs
+        project_repository
+            .push_to_gitbutler_server(user.as_ref(), all_refs.as_slice())
+            .context("failed to push project (all refs) to gitbutler")?;
+
+        //TODO: remove push-tmp ref
+
+        tracing::debug!(
+            %project_id,
+            "project fully pushed",
+        );
 
         Ok(vec![])
     }
