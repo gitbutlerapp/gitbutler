27e7366d4928158f5a6e7970ac34fe21f4930ed0
Mattias Granlund <mtsgrd@gmail.com>
2023-11-25 13:19:20 +0100

    Add branch filtering to sidebar

---
+++ packages/ui/src/lib/components/SegmentControl/SegmentedControl.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { createEventDispatcher, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	import type { SegmentContext, SegmentItem } from './segment';
+
+	export let wide = false;
+	export let selectedIndex = 0;
+
+	let dispatch = createEventDispatcher<{ select: string }>();
+
+	let indexesIterator = -1;
+	let segments: SegmentItem[] = [];
+
+	let focusedSegmentIndex = writable(selectedIndex);
+	let selectedSegmentIndex = writable(selectedIndex);
+	let length = writable(0);
+
+	const context: SegmentContext = {
+		focusedSegmentIndex,
+		selectedSegmentIndex,
+		length,
+		setIndex: () => {
+			indexesIterator += 1;
+			return indexesIterator;
+		},
+		addSegment: ({ id, index, disabled }) => {
+			segments = [...segments, { id, index, disabled }];
+			length.set(segments.length);
+		},
+		setSelected: (segmentIndex) => {
+			if (segmentIndex >= 0 && segmentIndex < segments.length) {
+				$focusedSegmentIndex = segmentIndex;
+
+				if (!segments[segmentIndex].disabled) {
+					$selectedSegmentIndex = $focusedSegmentIndex;
+					dispatch('select', segments[segmentIndex].id);
+				}
+			}
+		}
+	};
+	setContext<SegmentContext>('SegmentedControl', context);
+</script>
+
+<div class="wrapper" class:wide>
+	<slot />
+</div>
+
+<style lang="postcss">
+	.wrapper {
+		display: inline-flex;
+		&.wide {
+			display: flex;
+		}
+	}
+</style>
--- packages/ui/src/routes/[projectId]/navigation/BranchFilter.svelte
+++ packages/ui/src/routes/[projectId]/navigation/BranchFilter.svelte
@@ -1,11 +1,45 @@
-<script lang="ts">
-	import Icon from '$lib/icons/Icon.svelte';
+<script lang="ts" context="module">
+	export type TypeFilter = 'all' | 'branch' | 'pr';
 </script>
 
-<div
-	class="flex w-full items-center gap-x-2 rounded border px-3 py-1"
-	style:border-color="var(--clr-theme-container-outline-light)"
->
-	<input type="text" class="w-full bg-transparent" />
-	<Icon name="filter" />
+<script lang="ts">
+	import Segment from '$lib/components/SegmentControl/Segment.svelte';
+	import SegmentedControl from '$lib/components/SegmentControl/SegmentedControl.svelte';
+	import TextBox from '$lib/components/TextBox.svelte';
+	import type { BehaviorSubject } from 'rxjs';
+
+	export let textFilter$: BehaviorSubject<string | undefined>;
+	export let typeFilter$: BehaviorSubject<TypeFilter>;
+
+	let options: { id: TypeFilter; name: string }[] = [
+		{ id: 'all', name: 'All' },
+		{ id: 'branch', name: 'Branch' },
+		{ id: 'pr', name: 'Pull request' }
+	];
+
+	function onSelect(id: string) {
+		typeFilter$.next(id as TypeFilter);
+	}
+</script>
+
+<div class="wrapper">
+	<TextBox icon="filter" on:input={(e) => textFilter$.next(e.detail)} />
+	<div class="filter-btns">
+		<SegmentedControl on:select={(e) => onSelect(e.detail)} wide selectedIndex={0}>
+			{#each options as option}
+				<Segment id={option.id}>{option.name}</Segment>
+			{/each}
+		</SegmentedControl>
+	</div>
 </div>
+
+<style lang="postcss">
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-8);
+	}
+	.filter-btns {
+		width: 100%;
+	}
+</style>
