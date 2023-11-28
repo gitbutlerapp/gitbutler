e402f8c088f146e2ec45af065e3b6e6aec74d77f
Mattias Granlund <mtsgrd@gmail.com>
2023-11-29 00:21:30 +0100

    🔧 fix: update getGithubContext to only emit distinct values when baseBranch remoteUrl changes

--- packages/ui/src/lib/stores/github.ts
+++ packages/ui/src/lib/stores/github.ts
@@ -1,13 +1,14 @@
 import type { User } from '$lib/backend/cloud';
 import type { GitHubIntegrationContext } from '$lib/github/types';
 import type { BaseBranch } from '$lib/vbranches/types';
-import { combineLatest, switchMap, type Observable, of, shareReplay } from 'rxjs';
+import { combineLatest, switchMap, type Observable, of, shareReplay, distinct } from 'rxjs';
 
 export function getGithubContext(
 	user$: Observable<User | undefined>,
 	baseBranch$: Observable<BaseBranch | null>
 ): Observable<GitHubIntegrationContext | undefined> {
-	return combineLatest([user$, baseBranch$]).pipe(
+	const distinctUrl$ = baseBranch$.pipe(distinct((ctx) => ctx?.remoteUrl));
+	return combineLatest([user$, distinctUrl$]).pipe(
 		switchMap(([user, baseBranch]) => {
 			const remoteUrl = baseBranch?.remoteUrl;
 			const authToken = user?.github_access_token;
