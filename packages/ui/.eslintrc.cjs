6cd9f9b2d9a3763a7659f1aca8a50ea6900bd077
Mattias Granlund <mtsgrd@gmail.com>
2023-11-28 23:28:15 +0100

    Make eslint fail lint on unused variables

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -36,7 +36,7 @@ module.exports = {
 		'@typescript-eslint/no-empty-function': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-unused-vars': [
-			'warn', // or "error"
+			'error', // or "error"
 			{
 				argsIgnorePattern: '^_',
 				varsIgnorePattern: '^_',
