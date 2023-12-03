ced7ff1f4bfd903fb4fa06cb0f61b48c6fdd96a3
Mattias Granlund <mtsgrd@gmail.com>
2023-12-03 02:05:16 +0100

    Fix commit mistake from settings UI update

--- packages/ui/src/routes/[projectId]/settings/Spacer.svelte
+++ packages/ui/src/routes/[projectId]/settings/Spacer.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
 </script>
 
-<div
-	class="h-[0.0625rem] shrink-0"
-	style="background-color: var(--clr-theme-container-outline-light)"
-/>
+<div class="h-[0.0625rem] shrink-0" style="background-color: var(--clr-theme-container-mid)" />
