36586967ac6036830b3144cb1e02d85b20eaa271
Mattias Granlund <mtsgrd@gmail.com>
2023-12-03 01:46:18 +0100

    Give global settings page a max width and facelift

--- packages/ui/src/routes/[projectId]/settings/Spacer.svelte
+++ packages/ui/src/routes/[projectId]/settings/Spacer.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	export let type: 'card' | 'surface' = 'surface';
 </script>
 
 <div
 	class="h-[0.0625rem] shrink-0"
-	style:background-color={type == 'surface'
-		? 'var(--bg-surface-highlight)}'
-		: 'var(--bg-card-highlight'}
+	style="background-color: var(--clr-theme-container-outline-light)"
 />
