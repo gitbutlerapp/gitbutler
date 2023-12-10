fd42da07746bc62152b4f8d359279ce58ef7e13b
Pavel Laptev <pawellaptew@gmail.com>
2023-12-10 16:42:02 +0100

    New branch dragzone updated

--- packages/ui/src/routes/[projectId]/board/NewBranchDropZone.svelte
+++ packages/ui/src/routes/[projectId]/board/NewBranchDropZone.svelte
@@ -44,13 +44,17 @@
 				width="20"
 				height="20"
 				viewBox="0 0 20 20"
-				fill="none"
+				fill="currentcolor"
 				xmlns="http://www.w3.org/2000/svg"
 			>
-				<path d="M10 20V0M0 10L20 10" stroke="currentcolor" stroke-width="1.5" />
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M10.75 10.75V20H9.25V10.75H0V9.25H9.25V0H10.75V9.25H20V10.75H10.75Z"
+				/>
 			</svg>
 
-			<span class="text-base-12 text-semibold">New virtual branch</span>
+			<span class="text-base-12 text-semibold" />
 		</div>
 	</button>
 </div>
@@ -59,22 +63,35 @@
 	.canvas-dropzone {
 		display: flex;
 		height: 100%;
+		width: 100%;
 	}
 
 	.new-virtual-branch {
-		color: var(--clr-theme-scale-ntrl-0);
+		color: color-mix(in srgb, var(--clr-theme-scale-ntrl-0) 30%, transparent);
 		width: 20rem;
 		height: 100%;
 		border-radius: var(--radius-m);
-		border: 1px dashed var(--clr-theme-container-outline-light);
+		border: 1px solid color-mix(in srgb, var(--clr-theme-container-outline-light) 40%, transparent);
+		background-color: transparent;
+		transition:
+			opacity var(--transition-medium),
+			background-color var(--transition-medium),
+			border-color var(--transition-medium),
+			color var(--transition-medium);
 
 		&:hover {
+			opacity: 0.8;
+			background-color: color-mix(in srgb, var(--clr-theme-container-sub) 60%, transparent);
+			border: 1px solid color-mix(in srgb, var(--clr-theme-container-sub) 60%, transparent);
+			color: var(--clr-theme-scale-ntrl-40);
+
 			& .new-virtual-branch__content {
-				opacity: 0.3;
+				opacity: 1;
 				transform: translateY(0);
 
 				& span {
-					opacity: 1;
+					opacity: 0.5;
+					transform: translateY(0);
 				}
 			}
 		}
@@ -86,15 +103,58 @@
 		align-items: center;
 		justify-content: center;
 		gap: var(--space-16);
-		opacity: 0.15;
-		transform: translateY(calc(var(--space-12)));
+		transform: translateY(calc(var(--space-20)));
 		transition:
-			transform 0.2s var(--transition-fast),
-			opacity 0.2s var(--transition-fast);
+			transform var(--transition-medium),
+			opacity var(--transition-medium);
 
 		& span {
 			opacity: 0;
-			transition: opacity 0.2s var(--transition-fast);
+			transition:
+				opacity var(--transition-medium),
+				transform var(--transition-medium);
+			transform: translateY(calc(var(--space-8) * -1));
+
+			&:after {
+				content: 'New Branch';
+			}
+		}
+
+		& svg {
+			transition: fill var(--transition-medium);
+		}
+	}
+
+	/* DRAGZONE MODIEFIERS */
+	.canvas-dropzone {
+		&:global(.new-dz-active > .new-virtual-branch) {
+			background-color: color-mix(in srgb, var(--clr-theme-pop-container-dark) 30%, transparent);
+			border: 1px solid color-mix(in srgb, var(--clr-theme-pop-container-dark) 30%, transparent);
+			color: var(--clr-theme-scale-pop-50);
+
+			& .new-virtual-branch__content {
+				& span {
+					&:after {
+						content: 'Drop to create new branch';
+					}
+				}
+			}
+		}
+
+		&:global(.new-dz-hover > .new-virtual-branch) {
+			background-color: color-mix(in srgb, var(--clr-theme-pop-container-dark) 60%, transparent);
+			border: 1px solid color-mix(in srgb, var(--clr-theme-pop-container-dark) 60%, transparent);
+			color: var(--clr-theme-scale-pop-50);
+
+			& .new-virtual-branch__content {
+				opacity: 1;
+				transform: translateY(0);
+
+				& span {
+					opacity: 0.6;
+					transform: translateY(0);
+				}
+			}
 		}
 	}
 </style>
