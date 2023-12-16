f8b86d1186bd0f8bce038f8096c0390c5183b69a
Mattias Granlund <mtsgrd@gmail.com>
2023-12-16 14:02:29 +0100

    Integrate github context object into PrService

--- packages/ui/src/routes/[projectId]/+layout.ts
+++ packages/ui/src/routes/[projectId]/+layout.ts
@@ -1,7 +1,6 @@
 import { getFetchNotifications } from '$lib/stores/fetches';
 import { getHeads } from '$lib/stores/head';
 import { BranchController } from '$lib/vbranches/branchController';
-import { getGithubContext } from '$lib/stores/github';
 import { BaseBranchService, VirtualBranchService } from '$lib/vbranches/branchStoresCache';
 import type { LayoutLoad } from './$types';
 import { PrService } from '$lib/github/service';
@@ -11,13 +10,12 @@ import { BranchService } from '$lib/branches/service';
 export const prerender = false;
 
 export const load: LayoutLoad = async ({ params, parent }) => {
-	const { user$, projectService } = await parent();
+	const { user$, projectService, userService } = await parent();
 	const projectId = params.projectId;
 	const project$ = projectService.getProject(projectId);
 	const fetches$ = getFetchNotifications(projectId);
 	const heads$ = getHeads(projectId);
 	const baseBranchService = new BaseBranchService(projectId, fetches$, heads$);
-	const githubContext$ = getGithubContext(user$, baseBranchService.base$);
 	const vbranchService = new VirtualBranchService(projectId);
 
 	const remoteBranchService = new RemoteBranchService(
@@ -33,7 +31,8 @@ export const load: LayoutLoad = async ({ params, parent }) => {
 		baseBranchService
 	);
 
-	const prService = new PrService(branchController, vbranchService, githubContext$);
+	const prService = new PrService(branchController, vbranchService, userService, baseBranchService);
+	const githubContext$ = prService.ctx$;
 	const branchService = new BranchService(vbranchService, remoteBranchService, prService);
 
 	return {
