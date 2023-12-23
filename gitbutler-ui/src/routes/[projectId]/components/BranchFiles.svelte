3983bdca58414cfe03ffa99e9d5e876e99c369e8
Mattias Granlund <mtsgrd@gmail.com>
2023-12-23 17:43:23 +0200

    Add select all/none to branch files list view

--- gitbutler-ui/src/routes/[projectId]/components/BranchFiles.svelte
+++ gitbutler-ui/src/routes/[projectId]/components/BranchFiles.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import { filesToFileTree, sortLikeFileTree } from '$lib/vbranches/filetree';
-	import type { Branch } from '$lib/vbranches/types';
+	import { filesToFileTree } from '$lib/vbranches/filetree';
+	import type { Branch, File } from '$lib/vbranches/types';
 	import type { Ownership } from '$lib/vbranches/ownership';
 	import type { Writable } from 'svelte/store';
 	import Badge from '$lib/components/Badge.svelte';
 	import SegmentedControl from '$lib/components/SegmentControl/SegmentedControl.svelte';
 	import Segment from '$lib/components/SegmentControl/Segment.svelte';
-	import FileListItem from './FileListItem.svelte';
 	import FileTree from './FileTree.svelte';
 	import Resizer from '$lib/components/Resizer.svelte';
 	import ScrollableContainer from '$lib/components/ScrollableContainer.svelte';
+	import Checkbox from '$lib/components/Checkbox.svelte';
+	import BranchFilesList from './BranchFilesList.svelte';
 
 	export let branch: Branch;
 	export let readonly: boolean;
@@ -29,8 +30,35 @@
 	let maxHeight: number | undefined;
 	let headerElement: HTMLDivElement;
 
-	function updateResizable() {
-		// todo
+	function isAllChecked(selectedOwnership: Ownership): boolean {
+		return branch.files.every((f) =>
+			f.hunks.every((h) => selectedOwnership.containsHunk(f.id, h.id))
+		);
+	}
+
+	$: checked = isAllChecked($selectedOwnership);
+
+	function isIndeterminate(selectedOwnership: Ownership): boolean {
+		if (branch.files.length <= 1) return false;
+
+		let file = branch.files[0];
+		let prev = selectedOwnership.containsHunk(file.id, ...file.hunkIds);
+		for (let i = 1; i < branch.files.length; i++) {
+			file = branch.files[i];
+			const contained = selectedOwnership.containsHunk(file.id, ...file.hunkIds);
+			if (contained != prev) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	$: indeterminate = isIndeterminate($selectedOwnership);
+
+	function selectAll(selectedOwnership: Writable<Ownership>, files: File[]) {
+		files.forEach((f) =>
+			selectedOwnership.update((ownership) => ownership.addHunk(f.id, ...f.hunks.map((h) => h.id)))
+		);
 	}
 </script>
 
@@ -46,9 +74,25 @@
 {/if}
 
 <div class="header" bind:this={headerElement}>
-	<div class="header-title text-base-13 text-semibold">
-		<span>Changes</span>
-		<Badge count={branch.files.length} />
+	<div class="header__left">
+		{#if showCheckboxes && selectedListMode == 'list'}
+			<Checkbox
+				small
+				{checked}
+				{indeterminate}
+				on:change={(e) => {
+					if (e.detail) {
+						selectAll(selectedOwnership, branch.files);
+					} else {
+						selectedOwnership.update((ownership) => ownership.clear());
+					}
+				}}
+			/>
+		{/if}
+		<div class="header__title text-base-13 text-semibold">
+			<span>Changes</span>
+			<Badge count={branch.files.length} />
+		</div>
 	</div>
 	<SegmentedControl bind:selected={selectedListMode} selectedIndex={0}>
 		<Segment id="list" icon="list-view" />
@@ -73,20 +117,13 @@
 			<div class="scroll-container">
 				<!-- TODO: This is an experiment in file sorting. Accept or reject! -->
 				{#if selectedListMode == 'list'}
-					{#each sortLikeFileTree(branch.files) as file (file.id)}
-						<FileListItem
-							{file}
-							{readonly}
-							branchId={branch.id}
-							{selectedOwnership}
-							showCheckbox={showCheckboxes}
-							on:click={() => {
-								if ($selectedFileId == file.id) $selectedFileId = undefined;
-								else $selectedFileId = file.id;
-							}}
-							selected={file.id == $selectedFileId}
-						/>
-					{/each}
+					<BranchFilesList
+						{branch}
+						{selectedOwnership}
+						{selectedFileId}
+						{showCheckboxes}
+						{readonly}
+					/>
 				{:else}
 					<FileTree
 						node={filesToFileTree(branch.files)}
@@ -109,7 +146,6 @@
 			viewport={rsViewport}
 			on:height={(e) => {
 				height = e.detail;
-				updateResizable();
 			}}
 		/>
 	{/if}
@@ -137,14 +173,18 @@
 		justify-content: space-between;
 		padding-top: var(--space-12);
 		padding-bottom: var(--space-12);
-		padding-left: var(--space-16);
+		padding-left: var(--space-20);
 		padding-right: var(--space-12);
 		border-color: var(--clr-theme-container-outline-light);
 	}
-	.header-title {
+	.header__title {
 		display: flex;
 		align-items: center;
 		gap: var(--space-4);
 		color: var(--clr-theme-scale-ntrl-0);
+	}
+	.header__left {
+		display: flex;
+		gap: var(--space-10);
 	}
 </style>
