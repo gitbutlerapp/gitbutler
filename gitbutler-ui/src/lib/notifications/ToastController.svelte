7a3ba7324d3167c6f9eb98520d208c07274ef001
Mattias Granlund <mtsgrd@gmail.com>
2024-01-04 10:18:20 +0100

    Add new type of toast along with controller
    - not hooked up to anything yet
    - we need to catch specific errors and provide copy (plus action)

---
+++ gitbutler-ui/src/lib/notifications/ToastController.svelte
@@ -0,0 +1,35 @@
+<script lang="ts" context="module">
+</script>
+
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	import InfoMessage from '../components/InfoMessage.svelte';
+	import { dismissToast, toastStore } from '$lib/notifications/toasts';
+</script>
+
+<div class="toast-controller">
+	{#each $toastStore as toast (toast.id)}
+		<div transition:slide={{ duration: 170 }}>
+			<InfoMessage
+				title={toast.title}
+				style={toast.style ?? 'neutral'}
+				secondary="Dismiss"
+				on:secondary={() => dismissToast(toast.id)}
+				shadow>{toast.message}</InfoMessage
+			>
+		</div>
+	{/each}
+</div>
+
+<style lang="postcss">
+	.toast-controller {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		bottom: var(--space-20);
+		right: var(--space-20);
+		gap: var(--space-8);
+		z-index: 50;
+	}
+</style>
