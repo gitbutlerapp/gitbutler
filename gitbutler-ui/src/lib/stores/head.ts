55ae30f62fe4152bdf752812d54b4428b170ec5f
Mattias Granlund <mtsgrd@gmail.com>
2024-01-05 12:06:15 +0100

    Fix duplicate list_remote_branches call
    - give head and fetches streams an initial value

--- gitbutler-ui/src/lib/stores/head.ts
+++ gitbutler-ui/src/lib/stores/head.ts
@@ -1,10 +1,10 @@
 import { getHead, subscribeToHead } from '$lib/backend/heads';
-import { Observable, concat, from } from 'rxjs';
+import { Observable, concat, from, shareReplay } from 'rxjs';
 
 export function getHeads(projectId: string): Observable<string> {
 	const head$ = from(getHead(projectId));
 	const stream$ = new Observable<string>((subscriber) =>
 		subscribeToHead(projectId, (head) => subscriber.next(head))
 	);
-	return concat(head$, stream$);
+	return concat(head$, stream$).pipe(shareReplay(1));
 }
