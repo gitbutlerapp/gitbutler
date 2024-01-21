61ad099feb06fb8900815bc481afcd36175634c0
Kiril Videlov <kiril@videlov.com>
2024-01-21 21:25:57 +0100

    this is a revert to a previous working version

--- gitbutler-ui/src/lib/analytics/posthog.ts
+++ gitbutler-ui/src/lib/analytics/posthog.ts
@@ -5,19 +5,36 @@ import { getVersion, getName } from '@tauri-apps/api/app';
 
 export async function initPostHog() {
 	const [appName, appVersion] = await Promise.all([getName(), getVersion()]);
-	posthog.register_for_session({
-		appName,
-		appVersion
-	});
-	posthog.init(PUBLIC_POSTHOG_API_KEY, {
-		api_host: 'https://eu.posthog.com',
-		disable_session_recording: appName !== 'GitButler', // only record sessions in production
-		capture_performance: false,
-		request_batching: true,
-		persistence: 'localStorage',
-		on_xhr_error: () => {
-			// noop
-		}
+	new Promise((resolve, _reject) => {
+		posthog.init(PUBLIC_POSTHOG_API_KEY, {
+			api_host: 'https://eu.posthog.com',
+			disable_session_recording: appName !== 'GitButler', // only record sessions in production
+			capture_performance: false,
+			request_batching: true,
+			persistence: 'localStorage',
+			on_xhr_error: () => {
+				// noop
+			},
+			loaded: (instance) => {
+				instance.register_for_session({
+					appName,
+					appVersion
+				});
+				resolve({
+					identify: (user: User | undefined) => {
+						if (user) {
+							instance.identify(`user_${user.id}`, {
+								email: user.email,
+								name: user.name
+							});
+						} else {
+							instance.capture('log-out');
+							instance.reset();
+						}
+					}
+				});
+			}
+		});
 	});
 }
 
