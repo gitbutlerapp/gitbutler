14cafcdc1e2ecee9a6b3a018bde3b358e78f4405
Mattias Granlund <mtsgrd@gmail.com>
2024-01-23 14:57:15 +0100

    Move Sentry initialization
    - makes sense to have it in sentry.ts and called from root layout.ts

--- gitbutler-ui/src/lib/analytics/sentry.ts
+++ gitbutler-ui/src/lib/analytics/sentry.ts
@@ -1,5 +1,17 @@
-import { startSpan, setUser, type Span } from '@sentry/sveltekit';
+import { startSpan, setUser, type Span, init } from '@sentry/sveltekit';
+
 import type { User } from '../backend/cloud';
+import { dev } from '$app/environment';
+import { PUBLIC_SENTRY_ENVIRONMENT } from '$env/static/public';
+
+export function initSentry() {
+	init({
+		enabled: !dev,
+		dsn: 'https://a35bbd6688a3a8f76e4956c6871f414a@o4504644069687296.ingest.sentry.io/4505976067129344',
+		environment: PUBLIC_SENTRY_ENVIRONMENT,
+		tracesSampleRate: 1.0
+	});
+}
 
 export function setSentryUser(user: User) {
 	setUser({
