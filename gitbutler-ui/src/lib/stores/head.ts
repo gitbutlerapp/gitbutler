97a65f1f8d87270d0d552786d873b390b8956e10
Mattias Granlund <mtsgrd@gmail.com>
2024-01-26 14:30:30 +0100

    feat: add distinctUntilChanged operator to prevent emitting duplicate values in getHeads function

--- gitbutler-ui/src/lib/stores/head.ts
+++ gitbutler-ui/src/lib/stores/head.ts
@@ -1,10 +1,10 @@
 import { getHead, subscribeToHead } from '$lib/backend/heads';
-import { Observable, concat, from, shareReplay } from 'rxjs';
+import { Observable, concat, distinctUntilChanged, from, shareReplay } from 'rxjs';
 
 export function getHeads(projectId: string): Observable<string> {
 	const head$ = from(getHead(projectId));
 	const stream$ = new Observable<string>((subscriber) =>
 		subscribeToHead(projectId, (head) => subscriber.next(head))
 	);
-	return concat(head$, stream$).pipe(shareReplay(1));
+	return concat(head$, stream$).pipe(distinctUntilChanged(), shareReplay(1));
 }
