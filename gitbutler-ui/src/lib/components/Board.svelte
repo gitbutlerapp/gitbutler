d484ef16c7141accf4eb38d2d8dbab2986bfe5f9
Kiril Videlov <kiril@videlov.com>
2024-01-29 19:34:08 +0100

    fix: dragging of lanes on Linux / Windows

--- gitbutler-ui/src/lib/components/Board.svelte
+++ gitbutler-ui/src/lib/components/Board.svelte
@@ -98,6 +98,9 @@
 					}
 					clone = cloneWithRotation(e.target);
 					document.body.appendChild(clone);
+					// Get chromium to fire dragover & drop events
+					// https://stackoverflow.com/questions/6481094/html5-drag-and-drop-ondragover-not-firing-in-chrome/6483205#6483205
+					e.dataTransfer?.setData('text/html', 'd'); // cannot be empty string
 					e.dataTransfer?.setDragImage(clone, e.offsetX + 30, e.offsetY + 30); // Adds the padding
 					dragged = e.currentTarget;
 					priorPosition = Array.from(dropZone.children).indexOf(dragged);
