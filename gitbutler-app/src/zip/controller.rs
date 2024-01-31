3e1a77e83ad2c72c61d76b91d8392677f7709033
Kiril Videlov <kiril@videlov.com>
2024-01-31 15:42:37 +0100

    Remove LogsDir in favor of just path::PathBuf

--- gitbutler-app/src/zip/controller.rs
+++ gitbutler-app/src/zip/controller.rs
@@ -2,16 +2,13 @@ use std::path;
 
 use tauri::AppHandle;
 
-use crate::{
-    paths::LogsDir,
-    projects::{self, ProjectId},
-};
+use crate::projects::{self, ProjectId};
 
 use super::Zipper;
 
 pub struct Controller {
     local_data_dir: path::PathBuf,
-    logs_dir: LogsDir,
+    logs_dir: path::PathBuf,
     zipper: Zipper,
     projects_controller: projects::Controller,
 }
@@ -20,19 +17,20 @@ impl TryFrom<&AppHandle> for Controller {
     type Error = anyhow::Error;
 
     fn try_from(value: &AppHandle) -> Result<Self, Self::Error> {
-        let local_data_dir = value.path_resolver().app_data_dir();
-        match local_data_dir {
-            Some(local_data_dir) => {
-                let logs_dir = LogsDir::try_from(value)?;
-                Ok(Self {
-                    local_data_dir,
-                    logs_dir,
-                    zipper: Zipper::try_from(value)?,
-                    projects_controller: projects::Controller::try_from(value)?,
-                })
-            }
-            None => Err(anyhow::anyhow!("failed to get app data dir")),
-        }
+        let local_data_dir = value
+            .path_resolver()
+            .app_data_dir()
+            .ok_or_else(|| anyhow::anyhow!("failed to get local data dir"))?;
+        let logs_dir = value
+            .path_resolver()
+            .app_log_dir()
+            .ok_or_else(|| anyhow::anyhow!("failed to get logs dir"))?;
+        Ok(Self {
+            local_data_dir,
+            logs_dir,
+            zipper: Zipper::try_from(value)?,
+            projects_controller: projects::Controller::try_from(value)?,
+        })
     }
 }
 
@@ -54,9 +52,7 @@ impl Controller {
     }
 
     pub fn logs_archive(&self) -> Result<path::PathBuf, LogsArchiveError> {
-        self.zipper
-            .zip(self.logs_dir.to_path_buf())
-            .map_err(Into::into)
+        self.zipper.zip(&self.logs_dir).map_err(Into::into)
     }
 }
 
