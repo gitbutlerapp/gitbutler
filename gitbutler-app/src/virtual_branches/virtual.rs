42302763d66b0296b0432ad22829af4ae20f39a6
Kiril Videlov <kiril@videlov.com>
2024-02-16 23:31:53 +0100

    fix: update regex pattern to allow periods in branch names

--- gitbutler-app/src/virtual_branches/virtual.rs
+++ gitbutler-app/src/virtual_branches/virtual.rs
@@ -155,8 +155,7 @@ impl From<git::Signature<'_>> for Author {
 }
 
 pub fn normalize_branch_name(name: &str) -> String {
-    // Technically this pattern should include ".", but it makes libgit2 throws an error
-    let pattern = Regex::new("[^A-Za-z0-9_/]+").unwrap();
+    let pattern = Regex::new("[^A-Za-z0-9_/.]+").unwrap();
     pattern.replace_all(name, "-").to_string()
 }
 
