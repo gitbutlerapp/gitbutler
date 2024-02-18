d133f21b305bf36660bd89a5910ca6dd415fb1ec
Meesayen <gvg.fede@gmail.com>
2024-02-18 20:53:35 +0100

    chore: fix linting issue

--- gitbutler-ui/src/lib/components/BranchCard.svelte
+++ gitbutler-ui/src/lib/components/BranchCard.svelte
@@ -218,7 +218,7 @@
 								branchId={branch.id}
 								files={branch.files}
 								{isUnapplied}
-                {branchController}
+								{branchController}
 								{selectedOwnership}
 								{selectedFiles}
 								showCheckboxes={$commitBoxOpen}
