599e96e9e40c0bcd7214e16d4bd60cb5fa81d7be
Meesayen <gvg.fede@gmail.com>
2024-02-19 14:29:04 +0100

    chore: consider "contextmenu" event as a click outside event too

--- gitbutler-ui/src/lib/clickOutside.ts
+++ gitbutler-ui/src/lib/clickOutside.ts
@@ -15,15 +15,19 @@ export function clickOutside(
 		}
 	}
 	document.addEventListener('click', onClick, true);
+	document.addEventListener('contextmenu', onClick, true);
 	return {
 		destroy() {
 			document.removeEventListener('click', onClick, true);
+			document.removeEventListener('contextmenu', onClick, true);
 		},
 		update(opts: ClickOpts) {
 			document.removeEventListener('click', onClick, true);
+			document.removeEventListener('contextmenu', onClick, true);
 			if (opts.enabled !== undefined && !opts.enabled) return;
 			trigger = opts.trigger;
 			document.addEventListener('click', onClick, true);
+			document.addEventListener('contextmenu', onClick, true);
 		}
 	};
 }
