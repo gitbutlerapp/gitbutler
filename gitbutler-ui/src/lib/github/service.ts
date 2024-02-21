dd1c8866c355f8c7a74f2b3d9994731fa5b14057
Mattias Granlund <mtsgrd@gmail.com>
2024-02-22 00:53:25 +0200

    Fix GitHub error mapping
    - correctly parses the error response

--- gitbutler-ui/src/lib/github/service.ts
+++ gitbutler-ui/src/lib/github/service.ts
@@ -358,11 +358,103 @@ function loadPrs(
 	});
 }
 
+/**
+ * Example error responses
+ * ```
+ * {
+ *   "name": "HttpError",
+ *   "request": {
+ *      "body": "{\"head\":\"Add-amplitude-account\",\"base\":\"main\",\"title\":\"Add amplitude account\",\"body\":\"\",\"draft\":false}",
+ *      "headers": {
+ *        "accept": "application/vnd.github.v3+json",
+ *        "authorization": "token [REDACTED]",
+ *        "content-type": "application/json; charset=utf-8",
+ *        "user-agent": "GitButler Client octokit-rest.js/20.0.2 octokit-core.js/5.0.1 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)"
+ *      },
+ *      "method": "POST",
+ *      "request": {
+ *        "hook": {}
+ *      "url": "https://api.github.com/repos/moises-ai/moises-data-platform-infra/pulls"
+ *     },
+ *     "response": {
+ *       "data": {
+ *         "documentation_url": "https://docs.github.com/rest/pulls/pulls#create-a-pull-request",
+ *         "message": "Although you appear to have the correct authorization credentials, the `moises-ai` organization has enabled OAuth App access restrictions, meaning that data access to third-parties is limited. For more information on these restrictions, including how to enable this app, visit https://docs.github.com/articles/restricting-access-to-your-organization-s-data/"
+ *       },
+ *       "headers": {
+ *         "content-type": "application/json; charset=utf-8",
+ *         "x-accepted-oauth-scopes": "",
+ *         "x-github-media-type": "github.v3; format=json",
+ *         "x-github-request-id": "F93F:2A6A69:1CEAF8:1D2E00:65D486FC",
+ *         "x-oauth-scopes": "repo",
+ *         "x-ratelimit-limit": "5000",
+ *         "x-ratelimit-remaining": "4968",
+ *         "x-ratelimit-reset": "1708427744",
+ *         "x-ratelimit-resource": "core",
+ *         "x-ratelimit-used": "32"
+ *       },
+ *       "status": 403,
+ *       "url": "https://api.github.com/repos/moises-ai/moises-data-platform-infra/pulls"
+ *     },
+ *   "status": 403
+ * }
+ *
+ * {
+ *   name: 'HttpError',
+ *   request: {
+ *     body: '{"head":"Update-message-query-order","base":"main","title":"Update message query order","body":"","draft":false}',
+ *     headers: {
+ *       accept: 'application/vnd.github.v3+json',
+ *       authorization: 'token [REDACTED]',
+ *       'content-type': 'application/json; charset=utf-8',
+ *       'user-agent': 'GitButler Client octokit-rest.js/20.0.2 octokit-core.js/5.0.1 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)'
+ *     },
+ *     method: 'POST',
+ *     request: {
+ *         hook: {}
+ *     },
+ *     url: 'https://api.github.com/repos/personal-applications/chat-api/pulls'
+ *   },
+ *   response: {
+ *     data: {
+ *       documentation_url:
+ *           'https://docs.github.com/rest/pulls/pulls#create-a-pull-request',
+ *       errors: [
+ *         {
+ *            code: 'custom',
+ *            message: 'A pull request already exists for personal-applications:Update-message-query-order.',
+ *            resource: 'PullRequest'
+ *         }
+ *       ],
+ *       message: 'Validation Failed'
+ *     },
+ *     headers: {
+ *       'content-length': '266',
+ *       'content-type': 'application/json; charset=utf-8',
+ *       'x-accepted-oauth-scopes': '',
+ *       'x-github-media-type': 'github.v3; format=json',
+ *       'x-github-request-id': 'C233:72D21:6493:6C61:65D366B1',
+ *       'x-oauth-scopes': 'repo',
+ *       'x-ratelimit-limit': '5000',
+ *       'x-ratelimit-remaining': '4994',
+ *       'x-ratelimit-reset': '1708356743',
+ *       'x-ratelimit-resource': 'core',
+ *       'x-ratelimit-used': '6'
+ *     },
+ *     status: 422,
+ *     url: 'https://api.github.com/repos/personal-applications/chat-api/pulls'
+ *   },
+ *   status: 422
+ * }
+ * ```
+ */
 function mapErrorToToast(err: any): ToastMessage | undefined {
 	// We expect an object to be thrown by octokit.
 	if (typeof err != 'object') return;
 
-	const { status, message } = err;
+	const { status, response } = err;
+	const { data } = response;
+	const { message, errors } = data;
 
 	// If this expectation isn't met we must be doing something wrong
 	if (status == undefined || message == undefined) return;
@@ -392,6 +484,26 @@ function mapErrorToToast(err: any): ToastMessage | undefined {
                 for additional help.
 
                 \`\`\`${message}\`\`\`
+            `,
+			style: 'error'
+		};
+	}
+
+	if (message.includes('Validation Failed')) {
+		let errorStrings = '';
+		if (errors instanceof Array) {
+			errorStrings = errors.map((err) => err.message).join('\n');
+		}
+		return {
+			title: 'GitHub validation failed',
+			message: `
+                It looks like OAuth access has been restricted by your organization.
+
+                Please see our [documentation](https://docs.gitbutler.com/)
+                for additional help.
+
+                \`\`\`${errorStrings}\`\`\`
+
             `,
 			style: 'error'
 		};
