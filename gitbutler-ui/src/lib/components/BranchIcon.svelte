ac376c1b03596dd920b3469f535ea4f21e70bd36
Pavel Laptev <pawellaptew@gmail.com>
2024-03-03 21:37:06 +0100

    document format to pass Lint checks

--- gitbutler-ui/src/lib/components/BranchIcon.svelte
+++ gitbutler-ui/src/lib/components/BranchIcon.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 	import { tooltip } from '$lib/utils/tooltip';
 
-	export let name:
-		| 'remote-branch'
-		| 'virtual-branch'
-		| 'pr'
-		| 'pr-draft'
-		| 'pr-closed'
-		| undefined;
+	export let name: 'remote-branch' | 'virtual-branch' | 'pr' | 'pr-draft' | 'pr-closed' | undefined;
 	export let help: string | undefined;
 
 	const getIconColor = (name: string | undefined) => {
@@ -23,13 +17,7 @@
 
 <div class="branch-icon {getIconColor(name)}" use:tooltip={help}>
 	{#if name == 'virtual-branch'}
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M5.75 8V4H4.25V8H5.75Z" />
 			<path
 				d="M11.75 7V4H10.25V7C10.25 8.24264 9.24264 9.25 8 9.25C5.92893 9.25 4.25 10.9289 4.25 13V16H5.75V13C5.75 11.7574 6.75736 10.75 8 10.75C10.0711 10.75 11.75 9.07107 11.75 7Z"
@@ -37,26 +25,14 @@
 		</svg>
 	{/if}
 	{#if name == 'remote-branch'}
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M5.75 9.99973V4H4.25V16H5.75V13C5.75 11.7574 6.75736 10.75 8 10.75C10.0711 10.75 11.75 9.07107 11.75 7V4H10.25V7C10.25 8.24264 9.24264 9.25 8 9.25C7.1558 9.25 6.37675 9.52896 5.75 9.99973Z"
 			/>
 		</svg>
 	{/if}
 	{#if name == 'pr'}
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M3 5L8 2V4.25H10C11.5188 4.25 12.75 5.48122 12.75 7V16H11.25V7C11.25 6.30964 10.6904 5.75 10 5.75H8V8L3 5Z"
 			/>
@@ -64,13 +40,7 @@
 		</svg>
 	{/if}
 	{#if name == 'pr-draft'}
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M11.75 7.88555C12.7643 7.56698 13.5 6.61941 13.5 5.5C13.5 4.11929 12.3807 3 11 3C9.61929 3 8.5 4.11929 8.5 5.5C8.5 6.61941 9.23572 7.56698 10.25 7.88555V9H11.75V7.88555Z"
 			/>
@@ -80,13 +50,7 @@
 		</svg>
 	{/if}
 	{#if name == 'pr-closed'}
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M9.99999 4.93934L12.4697 2.46967L13.5303 3.53033L11.0607 6L13.5303 8.46967L12.4697 9.53033L9.99999 7.06066L7.53033 9.53033L6.46967 8.46967L8.93933 6L6.46967 3.53033L7.53033 2.46967L9.99999 4.93934Z"
 			/>
