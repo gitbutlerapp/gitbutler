388b3393c0ebe90bd75444898ae53c67e505aac4
Mattias Granlund <mtsgrd@gmail.com>
2024-03-16 10:32:04 +0100

    feat: Added function to convert observable to Svelte store.

--- gitbutler-ui/src/lib/rxjs/store.ts
+++ gitbutler-ui/src/lib/rxjs/store.ts
@@ -1,8 +1,30 @@
-import { Observable } from 'rxjs';
-import type { Readable, Writable } from 'svelte/store';
+import { Observable, catchError, of } from 'rxjs';
+import { writable, type Readable, type Writable } from 'svelte/store';
 
 export function storeToObservable<T>(svelteStore: Writable<T> | Readable<T>): Observable<T> {
 	return new Observable<T>((subscriber) => {
 		return svelteStore.subscribe((val) => subscriber.next(val));
 	});
+}
+
+export function observableToStore<T>(
+	observable: Observable<T>
+): [Readable<T | undefined>, Readable<string | undefined>] {
+	let unsubscribe: any = undefined;
+
+	const store = writable<T | undefined>(undefined, () => unsubscribe);
+	const error = writable<string | undefined>();
+
+	const subscription = observable
+		.pipe(
+			catchError((e: any) => {
+				error.set(e.message);
+				return of(undefined);
+			})
+		)
+		.subscribe((item) => {
+			store.set(item);
+		});
+	unsubscribe = subscription.unsubscribe;
+	return [store, error];
 }
