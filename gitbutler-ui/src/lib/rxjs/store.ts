8987b106477a03d9c196fc5f3b02998015b5c5b2
Mattias Granlund <mtsgrd@gmail.com>
2024-03-19 14:46:01 +0100

    Fix occasional problem with unsubscribing observable
    - can happen when switching projects

--- gitbutler-ui/src/lib/rxjs/store.ts
+++ gitbutler-ui/src/lib/rxjs/store.ts
@@ -1,4 +1,4 @@
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, Subscription, catchError, of } from 'rxjs';
 import { writable, type Readable, type Writable } from 'svelte/store';
 
 export function storeToObservable<T>(svelteStore: Writable<T> | Readable<T>): Observable<T> {
@@ -11,17 +11,20 @@ export function observableToStore<T>(
 	observable: Observable<T>
 ): [Readable<T | undefined>, Readable<string | undefined>] {
 	let unsubscribe: any = undefined;
+	let subscription: Subscription | undefined = undefined;
 
 	const store = writable<T | undefined>(undefined, () => {
 		// This runs when the last subscriber unsubscribes
 		return () => {
 			// TODO: Investigate why project switching breaks without `setTimeout`
-			setTimeout(() => unsubscribe(), 0);
+			setTimeout(() => {
+				if (subscription?.closed) unsubscribe();
+			}, 0);
 		};
 	});
 	const error = writable<string>();
 
-	const subscription = observable
+	subscription = observable
 		.pipe(
 			catchError((e: any) => {
 				error.set(e.message);
