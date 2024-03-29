b35b24eb59f1797e6457be9b1fb0269f6ffb5721
Caleb Owens <hello@calebowens.com>
2024-03-29 17:02:32 +0000

    Simplify naming

--- gitbutler-ui/src/lib/components/ProjectSettingsMenuAction.svelte
+++ gitbutler-ui/src/lib/components/ProjectSettingsMenuAction.svelte
@@ -2,13 +2,12 @@
 	import { invoke, listen } from '$lib/backend/ipc';
 	import { Project } from '$lib/backend/projects';
 	import { getContextByClass } from '$lib/utils/context';
-	import { unsubscribe } from '$lib/utils/unsubscribe';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
 	const project = getContextByClass(Project);
 
-	function setProjectSettingsEnabled(enabled: boolean) {
+	function setEnabled(enabled: boolean) {
 		return invoke('menu_item_set_enabled', {
 			menuItemId: 'project/settings',
 			enabled
@@ -16,15 +15,15 @@
 	}
 
 	onMount(() => {
-		setProjectSettingsEnabled(true);
+		setEnabled(true);
 
-		const projectSettingsSubscription = listen<string>('menu://project/settings/clicked', () => {
+		const unsubscribe = listen<string>('menu://project/settings/clicked', () => {
 			goto(`/${project.id}/settings/`);
 		});
 
 		return () => {
-			unsubscribe(projectSettingsSubscription)();
-			setProjectSettingsEnabled(false);
+			unsubscribe();
+			setEnabled(false);
 		};
 	});
 </script>
