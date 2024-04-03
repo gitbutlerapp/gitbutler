a14bab37d85fe90724241dd7d232b3b5a60e878b
Mattias Granlund <mtsgrd@gmail.com>
2024-04-03 13:50:35 +0200

    Remove tree list view
    - not being used
    - reduced maintenance cost

--- gitbutler-ui/src/lib/components/BranchFiles.svelte
+++ gitbutler-ui/src/lib/components/BranchFiles.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import BranchFilesHeader from './BranchFilesHeader.svelte';
 	import BranchFilesList from './BranchFilesList.svelte';
-	import FileTree from './FileTree.svelte';
 	import { createCommitStore } from '$lib/vbranches/contexts';
-	import { filesToFileTree } from '$lib/vbranches/filetree';
 	import type { LocalFile, RemoteFile } from '$lib/vbranches/types';
 
 	export let files: LocalFile[] | RemoteFile[];
@@ -13,30 +10,13 @@
 	export let allowMultiple = false;
 	export let readonly = false;
 
-	let selectedListMode: string;
-
 	createCommitStore(undefined);
 </script>
 
 <div class="branch-files" class:isUnapplied>
-	<div class="branch-files__header">
-		<BranchFilesHeader {files} {showCheckboxes} bind:selectedListMode />
-	</div>
 	{#if files.length > 0}
 		<div class="files-padding">
-			{#if selectedListMode == 'list'}
-				<BranchFilesList {allowMultiple} {readonly} {files} {showCheckboxes} {isUnapplied} />
-			{:else}
-				<FileTree
-					{allowMultiple}
-					{readonly}
-					node={filesToFileTree(files)}
-					{showCheckboxes}
-					isRoot={true}
-					{isUnapplied}
-					{files}
-				/>
-			{/if}
+			<BranchFilesList {allowMultiple} {readonly} {files} {showCheckboxes} {isUnapplied} />
 		</div>
 	{/if}
 </div>
@@ -50,12 +30,6 @@
 		&.isUnapplied {
 			border-radius: var(--radius-m);
 		}
-	}
-	.branch-files__header {
-		padding-top: var(--size-14);
-		padding-bottom: var(--size-12);
-		padding-left: var(--size-14);
-		padding-right: var(--size-14);
 	}
 	.files-padding {
 		padding-top: 0;
