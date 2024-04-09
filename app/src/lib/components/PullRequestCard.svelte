0eaaaec3bea0ba57bcc27c88a55b994c7bbd3b55
Pavel Laptev <pawellaptew@gmail.com>
2024-04-09 14:10:32 +0200

    console.log updated

--- app/src/lib/components/PullRequestCard.svelte
+++ app/src/lib/components/PullRequestCard.svelte
@@ -194,6 +194,7 @@
 		console.log('mergeableState:', mergeableState);
 		console.log('checksStatus:', checksStatus);
 		console.log('isFetchingChecks:', isFetchingChecks);
+		console.log('-------------------');
 
 		if (mergeableState == 'blocked' && !checksStatus && !isFetchingChecks) {
 			return {
