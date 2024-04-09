6dc13de63123ff231186008cdb12ab30995d6470
Pavel Laptev <pawellaptew@gmail.com>
2024-04-09 13:48:25 +0200

    succeeded const updated

--- app/src/lib/components/PullRequestCard.svelte
+++ app/src/lib/components/PullRequestCard.svelte
@@ -273,7 +273,9 @@
 			<Tag
 				icon={prStatusInfo.icon}
 				style={prStatusInfo.color}
-				kind={prStatusInfo.label !== 'Open' && prStatusInfo.label !== 'Status' ? 'solid' : 'soft'}
+				kind={prStatusInfo.label !== 'Open' && prStatusInfo.label !== 'Status'
+					? 'solid'
+					: 'soft'}
 				verticalOrientation={isLaneCollapsed}
 			>
 				{prStatusInfo.label}
