11b6cc66be0a8a4ee479c28ab5fd31cb24548b27
Caleb Owens <hello@calebowens.com>
2024-04-10 22:35:50 +0100

    Introduce method specific methods

--- app/src/lib/backend/cloud.ts
+++ app/src/lib/backend/cloud.ts
@@ -81,10 +81,9 @@ export class CloudClient {
 		}
 	}
 
-	// TODO: consider renaming
-	async makeRequest<T>(params: {
+	async request<T>(params: {
 		path: string;
-		method?: RequestMethod;
+		method: RequestMethod;
 		token?: string;
 		body?: FormData | object;
 		headers?: Record<string, string | undefined>;
@@ -104,7 +103,7 @@ export class CloudClient {
 		if (params.token) butlerHeaders.set('X-Auth-Token', params.token);
 
 		const response = await this.fetch(getUrl(params.path), {
-			method: params.method || RequestMethod.GET,
+			method: params.method,
 			headers: butlerHeaders,
 			body: this.formatBody(params.body)
 		});
@@ -112,12 +111,48 @@ export class CloudClient {
 		return parseResponseJSON(response);
 	}
 
+	get<T>(params: { path: string; token?: string; headers?: Record<string, string | undefined> }) {
+		return this.request<T>({ ...params, method: RequestMethod.GET });
+	}
+
+	post<T>(params: {
+		path: string;
+		token?: string;
+		body?: FormData | object;
+		headers?: Record<string, string | undefined>;
+	}) {
+		return this.request<T>({ ...params, method: RequestMethod.POST });
+	}
+
+	put<T>(params: {
+		path: string;
+		token?: string;
+		body?: FormData | object;
+		headers?: Record<string, string | undefined>;
+	}) {
+		return this.request<T>({ ...params, method: RequestMethod.PUT });
+	}
+
+	patch<T>(params: {
+		path: string;
+		token?: string;
+		body?: FormData | object;
+		headers?: Record<string, string | undefined>;
+	}) {
+		return this.request<T>({ ...params, method: RequestMethod.PATCH });
+	}
+
+	delete<T>(params: {
+		path: string;
+		token?: string;
+		body?: FormData | object;
+		headers?: Record<string, string | undefined>;
+	}) {
+		return this.request<T>({ ...params, method: RequestMethod.DELETE });
+	}
+
 	async createLoginToken(): Promise<LoginToken> {
-		const token = await this.makeRequest<LoginToken>({
-			path: 'login/token.json',
-			method: RequestMethod.POST,
-			body: {}
-		});
+		const token = await this.post<LoginToken>({ path: 'login/token.json', body: {} });
 		const url = new URL(token.url);
 		url.host = apiUrl.host;
 		return {
@@ -127,7 +162,7 @@ export class CloudClient {
 	}
 
 	getLoginUser(token: string): Promise<User> {
-		return this.makeRequest({ path: `login/user/${token}.json` });
+		return this.get({ path: `login/user/${token}.json` });
 	}
 
 	createFeedback(
@@ -150,9 +185,8 @@ export class CloudClient {
 		if (params.data) formData.append('data', params.data);
 
 		// Content Type must be unset for the right form-data border to be set automatically
-		return this.makeRequest({
+		return this.put({
 			path: 'feedback',
-			method: RequestMethod.PUT,
 			body: formData,
 			headers: { 'Content-Type': undefined },
 			token
@@ -160,10 +194,7 @@ export class CloudClient {
 	}
 
 	getUser(token: string): Promise<User> {
-		return this.makeRequest({
-			path: 'user.json',
-			token
-		});
+		return this.get({ path: 'user.json', token });
 	}
 
 	updateUser(token: string, params: { name?: string; picture?: File }): Promise<any> {
@@ -172,9 +203,8 @@ export class CloudClient {
 		if (params.picture) formData.append('avatar', params.picture);
 
 		// Content Type must be unset for the right form-data border to be set automatically
-		return this.makeRequest({
+		return this.put({
 			path: 'user.json',
-			method: RequestMethod.PUT,
 			body: formData,
 			headers: { 'Content-Type': undefined },
 			token
@@ -189,12 +219,7 @@ export class CloudClient {
 			uid?: string;
 		}
 	): Promise<Project> {
-		return this.makeRequest({
-			path: 'projects.json',
-			method: RequestMethod.POST,
-			body: params,
-			token
-		});
+		return this.post({ path: 'projects.json', body: params, token });
 	}
 
 	updateProject(
@@ -205,34 +230,19 @@ export class CloudClient {
 			description?: string;
 		}
 	): Promise<Project> {
-		return this.makeRequest({
-			path: `projects/${repositoryId}.json`,
-			method: RequestMethod.PUT,
-			body: params,
-			token
-		});
+		return this.put({ path: `projects/${repositoryId}.json`, body: params, token });
 	}
 
 	listProjects(token: string): Promise<Project[]> {
-		return this.makeRequest({
-			path: 'projects.json',
-			token
-		});
+		return this.get({ path: 'projects.json', token });
 	}
 
 	getProject(token: string, repositoryId: string): Promise<Project> {
-		return this.makeRequest({
-			path: `projects/${repositoryId}.json`,
-			token
-		});
+		return this.get({ path: `projects/${repositoryId}.json`, token });
 	}
 
 	deleteProject(token: string, repositoryId: string): Promise<void> {
-		return this.makeRequest({
-			path: `projects/${repositoryId}.json`,
-			method: RequestMethod.DELETE,
-			token
-		});
+		return this.delete({ path: `projects/${repositoryId}.json`, token });
 	}
 }
 
