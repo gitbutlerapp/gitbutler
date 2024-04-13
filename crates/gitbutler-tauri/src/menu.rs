2a23b6bff744272cea14a799f4677a5f1c19b50b
Mattias Granlund <mtsgrd@gmail.com>
2024-04-13 16:58:39 +0200

    Add devtools to view menu

--- crates/gitbutler-tauri/src/menu.rs
+++ crates/gitbutler-tauri/src/menu.rs
@@ -31,10 +31,29 @@ pub async fn menu_item_set_enabled(
 }
 
 pub fn build(package_info: &PackageInfo) -> Menu {
-    Menu::os_default(&package_info.name).add_submenu(Submenu::new(
+    let mut menu = Menu::os_default(&package_info.name).add_submenu(Submenu::new(
         "Project",
         Menu::with_items([disabled_menu_item("project/settings", "Project Settings")]),
-    ))
+    ));
+
+    // Try to find the View menu and attach the dev tools item
+    let view_menu = menu.items.iter_mut().find(|item| match item {
+        MenuEntry::CustomItem(_) => false,
+        MenuEntry::Submenu(submenu) => submenu.title == "View",
+        MenuEntry::NativeItem(_) => false,
+    });
+
+    let devtools = CustomMenuItem::new("view/devtools", "Developer Tools");
+    if let Some(MenuEntry::Submenu(view_menu)) = view_menu {
+        view_menu.inner.items.push(devtools.into());
+    } else {
+        menu = menu.add_submenu(Submenu::new(
+            "Developer",
+            Menu::with_items([devtools.into()]),
+        ));
+    }
+
+    menu
 }
 
 fn disabled_menu_item(id: &str, title: &str) -> MenuEntry {
@@ -44,6 +63,9 @@ fn disabled_menu_item(id: &str, title: &str) -> MenuEntry {
 }
 
 pub fn handle_event<R: Runtime>(event: &WindowMenuEvent<R>) {
+    if event.menu_item_id() == "view/devtools" {
+        event.window().open_devtools();
+    }
     emit(
         event.window(),
         format!("menu://{}/clicked", event.menu_item_id()).as_str(),
