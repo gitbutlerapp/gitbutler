dada28b4816ee5a0593b1c404d8d5ca831120173
Caleb Owens <hello@calebowens.com>
2024-04-14 20:21:02 +0100

    Refactor unsubscribe

--- app/src/lib/utils/unsubscribe.ts
+++ app/src/lib/utils/unsubscribe.ts
@@ -2,7 +2,9 @@ type MaybePromise<T> = T | Promise<T> | undefined;
 
 export function unsubscribe(...unsubscribers: MaybePromise<() => any>[]) {
 	return async () => {
-		const promises = unsubscribers.map(async (unsubscriber) => (await unsubscriber)?.());
+		const awaitedUnsubscribers = await Promise.all(unsubscribers);
+
+		const promises = awaitedUnsubscribers.map((unsubscriber) => unsubscriber?.());
 
 		return Promise.all(promises);
 	};
