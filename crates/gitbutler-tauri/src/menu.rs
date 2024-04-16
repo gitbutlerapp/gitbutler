fa06586cdf41c37fc47ad760ae86d44964382938
Josh Junon <josh@gitbutler.com>
2024-04-16 16:43:22 +0200

    fix devtools on release builds

--- crates/gitbutler-tauri/src/menu.rs
+++ crates/gitbutler-tauri/src/menu.rs
@@ -31,26 +31,30 @@ pub async fn menu_item_set_enabled(
 }
 
 pub fn build(package_info: &PackageInfo) -> Menu {
+    #[allow(unused_mut)]
     let mut menu = Menu::os_default(&package_info.name).add_submenu(Submenu::new(
         "Project",
         Menu::with_items([disabled_menu_item("project/settings", "Project Settings")]),
     ));
 
-    // Try to find the View menu and attach the dev tools item
-    let view_menu = menu.items.iter_mut().find(|item| match item {
-        MenuEntry::CustomItem(_) => false,
-        MenuEntry::Submenu(submenu) => submenu.title == "View",
-        MenuEntry::NativeItem(_) => false,
-    });
+    #[cfg(any(debug_assertions, feature = "devtools"))]
+    {
+        // Try to find the View menu and attach the dev tools item
+        let view_menu = menu.items.iter_mut().find(|item| match item {
+            MenuEntry::CustomItem(_) => false,
+            MenuEntry::Submenu(submenu) => submenu.title == "View",
+            MenuEntry::NativeItem(_) => false,
+        });
 
-    let devtools = CustomMenuItem::new("view/devtools", "Developer Tools");
-    if let Some(MenuEntry::Submenu(view_menu)) = view_menu {
-        view_menu.inner.items.push(devtools.into());
-    } else {
-        menu = menu.add_submenu(Submenu::new(
-            "Developer",
-            Menu::with_items([devtools.into()]),
-        ));
+        let devtools = CustomMenuItem::new("view/devtools", "Developer Tools");
+        if let Some(MenuEntry::Submenu(view_menu)) = view_menu {
+            view_menu.inner.items.push(devtools.into());
+        } else {
+            menu = menu.add_submenu(Submenu::new(
+                "Developer",
+                Menu::with_items([devtools.into()]),
+            ));
+        }
     }
 
     menu
@@ -63,8 +67,11 @@ fn disabled_menu_item(id: &str, title: &str) -> MenuEntry {
 }
 
 pub fn handle_event<R: Runtime>(event: &WindowMenuEvent<R>) {
-    if event.menu_item_id() == "view/devtools" {
-        event.window().open_devtools();
+    #[cfg(any(debug_assertions, feature = "devtools"))]
+    {
+        if event.menu_item_id() == "view/devtools" {
+            event.window().open_devtools();
+        }
     }
     emit(
         event.window(),
