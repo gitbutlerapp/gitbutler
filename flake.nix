a5482592c659f4f8b0564d4af6d6f858d9395257
ndom91 <yo@ndo.dev>
2024-04-23 17:40:03 +0200

    fix: add GIO env var

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           common = with pkgs; [
             gtk3
             glib
+            glib-networking
             dbus
             openssl_3
             librsvg
@@ -70,6 +71,7 @@
               ''
                 export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
                 export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+                export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
               '';
           };
         }
