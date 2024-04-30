088b919a2a26ce33308c8eb874d69b42c64b8800
Mattias Granlund <mtsgrd@gmail.com>
2024-04-30 18:40:04 +0200

    Drop unused global.d.ts

--- app/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Item = import('svelte-dnd-action').Item;
-declare type DndEvent<ItemType = Item> = import('svelte-dnd-action/typings').DndEvent<ItemType>;
-declare namespace svelte.JSX {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	interface HTMLAttributes<T> {
-		onconsider?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-		onfinalize?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-	}
-}
