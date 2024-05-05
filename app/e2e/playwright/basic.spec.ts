25fea8e35a2b648e9b0029eebd7a72919075892c
ndom91 <yo@ndo.dev>
2024-05-05 18:33:40 +0200

    chore: cleanup unnecessary deps

--- app/e2e/playwright/basic.spec.ts
+++ app/e2e/playwright/basic.spec.ts
@@ -1,45 +1,8 @@
 import { test, expect } from '@playwright/test';
-// import { getUtils } from '../utils';
-// import { mockIPC } from '@tauri-apps/api/mocks';
-
-// let utils;
-
-// test.beforeEach(({ context, page }) => {
-// 	utils = getUtils(page);
-// });
-//
-// test.beforeEach(async ({ page, context }) => {
-// 	context.exposeFunction('__TAURI_IPC__', (data) => {
-// 		console.log('ipc', data);
-// 	});
-// });
 
 test('has title GitButler', async ({ page }) => {
-	// mockIPC((cmd, args) => {
-	// 	// simulated rust command called "add" that just adds two numbers
-	// 	if (cmd === 'add') {
-	// 		return (args.a as number) + (args.b as number);
-	// 	}
-	// });
-	await page.goto('http://localhost:1420');
-	await page.screenshot({ path: 'cdp.png' });
+    await page.goto('http://localhost:1420');
+    await page.screenshot({ path: 'cdp.png' });
 
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle('GitButler Dev');
+    await expect(page).toHaveTitle('GitButler Dev');
 });
-
-// import playwright from 'playwright';
-//
-// const browser = await playwright.chromium.connectOverCDP(
-// 	'ws://localhost:9222'
-// 	// 'ws://localhost:9222/devtools/page/localhost:1420'
-// 	// 'ws://localhost:9222/devtools/page'
-// );
-//
-// const context = await browser.newContext();
-// const page = await context.newPage();
-//
-// await page.goto('http://localhost:1420/');
-// await page.screenshot({ path: 'cdp.png' });
-//
-// await browser.close();
--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -6,10 +6,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './e2e/playwright',
     testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-    // reporter: 'html',
-    reporter: process.env.CI
-        ? [['dot'], ['json', { outputFile: 'test-results.json' }]]
-        : [['list'], ['json', { outputFile: 'test-results.json' }], ['html', { open: 'on-failure' }]],
+    reporter: process.env.CI ? 'list' : 'html',
+    // reporter: process.env.CI
+    //     ? [['dot'], ['json', { outputFile: 'test-results.json' }]]
+    //     : [['list'], ['json', { outputFile: 'test-results.json' }], ['html', { open: 'on-failure' }]],
     // globalSetup: './e2e/playwright/globalSetup.ts',
     use: {
         launchOptions: {
@@ -22,12 +22,12 @@ export default defineConfig({
     projects: [
         {
             name: 'Google Chrome',
-            use: { ...devices['Desktop Chrome'], channel: 'chrome' } // or 'chrome-beta'
+            use: { ...devices['Desktop Chrome'] } // or 'chrome-beta'
         }
     ],
 
     webServer: {
-        command: 'pnpm dev --mode testing',
+        command: 'pnpm test:e2e:run',
         url: 'http://localhost:1420',
         reuseExistingServer: !process.env.CI
     }
