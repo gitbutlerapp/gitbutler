440ac0145ad5ed0f489526f40022780d08b9535b
ndom91 <yo@ndo.dev>
2024-05-05 18:44:55 +0200

    feat: add test to check changed files

--- app/e2e/playwright/basic.spec.ts
+++ app/e2e/playwright/basic.spec.ts
@@ -4,5 +4,11 @@ test('has title GitButler', async ({ page }) => {
     await page.goto('http://localhost:1420');
     await page.screenshot({ path: 'cdp.png' });
 
-    await expect(page).toHaveTitle('GitButler Dev');
+    await expect(page).toHaveTitle('');
 });
+
+test('has text package.json', async ({ page }) => {
+    await page.goto('http://localhost:1420');
+    const listBox = page.getByRole('listbox').getByRole('button')
+    await expect(listBox).toHaveText('package.json');
+})
