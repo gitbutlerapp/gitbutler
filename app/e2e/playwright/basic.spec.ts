248f8df3a55fc125c4f68320c0d68f5390be9cb7
ndom91 <yo@ndo.dev>
2024-05-05 19:04:07 +0200

    chore(test): cleanup playwright initial test

--- app/e2e/playwright/basic.spec.ts
+++ app/e2e/playwright/basic.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title GitButler', async ({ page }) => {
+test('has empty title', async ({ page }) => {
     await page.goto('http://localhost:1420');
-    await page.screenshot({ path: 'cdp.png' });
 
     await expect(page).toHaveTitle('');
 });
 
 test('has text package.json', async ({ page }) => {
     await page.goto('http://localhost:1420');
+
     const listBox = page.getByRole('listbox').getByRole('button')
+
+    await page.screenshot({ path: 'board.png' });
+
     await expect(listBox).toHaveText('package.json');
 })
