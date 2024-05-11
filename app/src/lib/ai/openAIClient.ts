27583248d8997077fff20a5d57d3dceb351c0d13
Caleb Owens <hello@calebowens.com>
2024-05-11 13:36:05 +0100

    Remove no-longer-required ts-expect-error

--- app/src/lib/ai/openAIClient.ts
+++ app/src/lib/ai/openAIClient.ts
@@ -16,8 +16,6 @@ export class OpenAIClient implements AIClient {
 		const messages: PromptMessage[] = [{ role: MessageRole.User, content: prompt }];
 
 		const response = await this.openAI.chat.completions.create({
-			// @ts-expect-error There is a type mismatch where it seems to want a "name" paramater
-			// that isn't required https://github.com/openai/openai-openapi/issues/118#issuecomment-1847667988
 			messages,
 			model: this.modelName,
 			max_tokens: 400
