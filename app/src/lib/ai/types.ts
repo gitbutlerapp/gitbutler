25015edfb92b07afca0a456638ad0e5fec9b5e67
Vid Luther <vid@luther.io>
2024-05-16 09:02:37 -0500

    feat: update GPT4Turbo model name
    Change GPT4Turbo enum from 'gpt-4-turbo-preview' to 'gpt-4-turbo'
    to reflect the transition of the model from preview to general
    availability. This update ensures our application references the
    correct model version in line with OpenAI's latest offerings.

--- app/src/lib/ai/types.ts
+++ app/src/lib/ai/types.ts
@@ -7,7 +7,7 @@ export enum ModelKind {
 export enum OpenAIModelName {
 	GPT35Turbo = 'gpt-3.5-turbo',
 	GPT4 = 'gpt-4',
-	GPT4Turbo = 'gpt-4-turbo-preview',
+	GPT4Turbo = 'gpt-4-turbo',
 	GPT4o = 'gpt-4o'
 }
 
