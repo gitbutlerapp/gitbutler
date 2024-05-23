93508d7e7c2170bb419a733f9069c0ff96ccd16f
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-05-23 09:53:04 +0200

    fix: assure tests aren't affected by local configuration
    Otherwise, tests will pickup global configuration, which can affect them.
    For instance, if `core.gpgsign` is true, it will cause commits to be signed
    which can be slow, or hang entirely if gpg-agent requests a password.
    
    Note that now there is possibly no tests that validates signing,
    and doing so is difficult as it requires gpg and keys to be setup.

--- crates/gitbutler-testsupport/src/test_project.rs
+++ crates/gitbutler-testsupport/src/test_project.rs
@@ -30,6 +30,11 @@ impl Default for TestProject {
         let local_tmp = temp_dir();
         let local_repository = git::Repository::init_opts(local_tmp.path(), &init_opts())
             .expect("failed to init repository");
+        local_repository
+            .config()
+            .unwrap()
+            .set_local("commit.gpgsign", "false")
+            .unwrap();
         let mut index = local_repository.index().expect("failed to get index");
         let oid = index.write_tree().expect("failed to write tree");
         let signature = git::Signature::now("test", "test@email.com").unwrap();
@@ -55,6 +60,11 @@ impl Default for TestProject {
                 .external_template(false),
         )
         .expect("failed to init repository");
+        remote_repository
+            .config()
+            .unwrap()
+            .set_local("commit.gpgsign", "false")
+            .unwrap();
 
         {
             let mut remote = local_repository
