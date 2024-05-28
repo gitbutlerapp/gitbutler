b77f7af26c0936a8f6e94ced6011fec80e91e742
Caleb Owens <hello@calebowens.com>
2024-05-28 09:58:06 +0200

    Initial doodling

---
+++ app/src/lib/components/Commit/BranchCommit.svelte
@@ -0,0 +1,25 @@
+<script>
+	import BranchFilesList from '$lib/components/BranchFilesList.svelte';
+	import Commit from '$lib/components/Commit/Commit.svelte';
+	import Tag from '$lib/components/Tag.svelte';
+
+	// This is the point of integration for a commit under a branch
+</script>
+
+<Commit
+	local={true}
+	remote={false}
+	hash={'123FJE'}
+	createdAt={new Date()}
+	title={'This is a gud commit'}
+	description={'Made stuff hehehe'}
+	borderBottom={true}
+>
+	<svelte:fragment slot="actions">
+		<Tag clickable kind="solid" style="ghost">Undo</Tag>
+		<Tag clickable kind="solid" style="ghost">Ammend Message</Tag>
+	</svelte:fragment>
+	<svelte:fragment slot="files">
+		<BranchFilesList title="Files" {files} {isUnapplied} />
+	</svelte:fragment>
+</Commit>
---
+++ app/src/lib/components/Commit/Commit.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import type { Commit, RemoteCommit } from '$lib/vbranches/types';
+
+	export let acceptAmmend: (commit: Commit | RemoteCommit) => boolean;
+	export let acceptSquash: (commit: Commit | RemoteCommit) => boolean;
+	export let local: boolean;
+	export let remote: boolean;
+	export let hash: string;
+	export let createdAt: Date;
+	export let title: string;
+	export let description: string;
+	export let borderBottom: boolean;
+
+	interface Events {
+		amend: { commit: Commit | RemoteCommit };
+		squash: { commit: Commit | RemoteCommit };
+	}
+
+	const dispatcher = createEventDispatcher<Events>();
+</script>
+
+<slot name="actions"></slot>
+<slot name="files"></slot>
