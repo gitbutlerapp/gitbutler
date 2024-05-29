407ec1427b95c20c795e8fa072e14e9a165ded1c
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-05-29 06:48:49 +0200

    Get performance metrics for `get_wd_tree()`
    It's a tiny little function, but I think it can be slow
    as it could incur a lot of work, scaling up with the size
    of the repository, and the amount of untracked data in it.
    
    Let's gather some data.

--- crates/gitbutler-core/src/git/repository_ext.rs
+++ crates/gitbutler-core/src/git/repository_ext.rs
@@ -1,14 +1,17 @@
 use anyhow::Result;
 use git2::{Repository, Tree};
+use tracing::instrument;
 
 /// Extension trait for `git2::Repository`.
 ///
 /// For now, it collects useful methods from `gitbutler-core::git::Repository`
 pub trait RepositoryExt {
+    /// Based on the index, add all data similar to `git add .` and create a tree from it, which is returned.
     fn get_wd_tree(&self) -> Result<Tree>;
 }
 
 impl RepositoryExt for Repository {
+    #[instrument(level = tracing::Level::DEBUG, skip(self), err(Debug))]
     fn get_wd_tree(&self) -> Result<Tree> {
         let mut index = self.index()?;
         index.add_all(["*"], git2::IndexAddOption::DEFAULT, None)?;
