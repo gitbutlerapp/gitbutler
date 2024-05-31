a8a3b0c8ac5c7686252933bcb3398462b5c8eb0d
Pavel Laptev <pawellaptew@gmail.com>
2024-05-31 15:57:36 +0200

    remove Teilwind package

--- app/postcss.config.cjs
+++ app/postcss.config.cjs
@@ -1,13 +1,7 @@
-const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-const nested = require('tailwindcss/nesting');
 
 const config = {
 	plugins: [
-		//Makes it easier to define .dark theme classes
-		nested,
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
 		autoprefixer
 	]
