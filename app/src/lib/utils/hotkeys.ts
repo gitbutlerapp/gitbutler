91a66ffdf8fd1f8b2f4a474ec94c5a1b21e969ae
ndom91 <yo@ndo.dev>
2024-06-07 11:12:25 +0200

    fix: typescript

--- app/src/lib/utils/hotkeys.ts
+++ app/src/lib/utils/hotkeys.ts
@@ -4,26 +4,27 @@ interface KeybindDefinitions {
 	[combo: string]: (event: KeyboardEvent) => void;
 }
 
-export function createKeybind(keybinds: KeybindDefinitions[]) {
+export function createKeybind(keybinds: KeybindDefinitions) {
 	const keys: KeybindDefinitions = {
 		// Ignore backspace keydown events always
 		Backspace: () => {}
 	};
 
-	Object.entries(keybinds).forEach(([combo, callback]) => {
+	for (const [combo, callback] of Object.entries(keybinds)) {
 		keys[combo] = (event: KeyboardEvent) => {
 			if (
 				event.repeat ||
 				event.target instanceof HTMLInputElement ||
 				event.target instanceof HTMLTextAreaElement
-			)
+			) {
 				return;
+			}
 
 			event.preventDefault();
 
 			callback(event);
 		};
-	});
+	}
 
 	return createKeybindingsHandler(keys);
 }
