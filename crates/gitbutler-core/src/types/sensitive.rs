788958ee08a6c92efb6de8339c22e8580d29655b
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-06-09 07:18:44 +0200

    hide sensitive information from ever being debug-logged

---
+++ crates/gitbutler-core/src/types/sensitive.rs
@@ -0,0 +1,69 @@
+use crate::types::Sensitive;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::ops::{Deref, DerefMut};
+
+impl<T> Serialize for Sensitive<T>
+where
+    T: Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+impl<'de, T> Deserialize<'de> for Sensitive<T>
+where
+    T: Deserialize<'de>,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        T::deserialize(deserializer).map(Sensitive)
+    }
+}
+
+impl<T> std::fmt::Debug for Sensitive<T>
+where
+    T: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt("<redacted>", f)
+    }
+}
+
+impl<T> Default for Sensitive<T>
+where
+    T: Default,
+{
+    fn default() -> Self {
+        Self(T::default())
+    }
+}
+
+impl<T> Deref for Sensitive<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Sensitive<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> Clone for Sensitive<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<T> Copy for Sensitive<T> where T: Copy {}
