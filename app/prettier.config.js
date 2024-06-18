dc67ce01682f95f0b1e0d4fc59c31f21a3308038
ndom91 <yo@ndo.dev>
2024-06-18 11:47:51 +0200

    fix: migrate eslint and prettier configs to JS files

---
+++ app/prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+  useTabs: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  endOfLine: 'auto'
+};
