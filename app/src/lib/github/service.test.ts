e93fc544536218a7557759a00cc639405d950e61
ndom91 <yo@ndo.dev>
2024-06-21 05:15:42 +0200

    feat: add initial GitHubService test for parsing remote URLs

---
+++ app/src/lib/github/service.test.ts
@@ -0,0 +1,52 @@
+import { GitHubService } from './service';
+import { BehaviorSubject } from 'rxjs';
+import { expect, test, describe, vi } from 'vitest';
+
+describe.concurrent('GitHubService', () => {
+	describe.concurrent('parse GitHub remote URL', () => {
+		test('default url', async () => {
+			const accessToken$ = new BehaviorSubject<string | undefined>('token');
+			const remoteUrl$ = new BehaviorSubject<string | undefined>(
+				'git@github.com:gitbutlerapp/gitbutler'
+			);
+
+			const githubService = new GitHubService(accessToken$, remoteUrl$);
+
+			expect(githubService.owner).toBe('gitbutlerapp');
+			expect(githubService.repo).toBe('gitbutler');
+		});
+		test('ssh URL with .git suffix', async () => {
+			const accessToken$ = new BehaviorSubject<string | undefined>('token');
+			const remoteUrl$ = new BehaviorSubject<string | undefined>(
+				'git@github.com:gitbutlerapp/gitbutler.git'
+			);
+
+			const githubService = new GitHubService(accessToken$, remoteUrl$);
+
+			expect(githubService.owner).toBe('gitbutlerapp');
+			expect(githubService.repo).toBe('gitbutler');
+		});
+		test('http URL', async () => {
+			const accessToken$ = new BehaviorSubject<string | undefined>('token');
+			const remoteUrl$ = new BehaviorSubject<string | undefined>(
+				'https://github.com/gitbutlerapp/gitbutler'
+			);
+
+			const githubService = new GitHubService(accessToken$, remoteUrl$);
+
+			expect(githubService.owner).toBe('gitbutlerapp');
+			expect(githubService.repo).toBe('gitbutler');
+		});
+		test('http URL with .git suffix', async () => {
+			const accessToken$ = new BehaviorSubject<string | undefined>('token');
+			const remoteUrl$ = new BehaviorSubject<string | undefined>(
+				'https://github.com/gitbutlerapp/gitbutler.git'
+			);
+
+			const githubService = new GitHubService(accessToken$, remoteUrl$);
+
+			expect(githubService.owner).toBe('gitbutlerapp');
+			expect(githubService.repo).toBe('gitbutler');
+		});
+	});
+});
