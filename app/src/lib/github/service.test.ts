0d9b43f48d0d3331e578ec0654f2460a485b1a85
ndom91 <yo@ndo.dev>
2024-06-21 05:52:35 +0200

    fix: simplify GitHubService test file

--- app/src/lib/github/service.test.ts
+++ app/src/lib/github/service.test.ts
@@ -2,51 +2,41 @@ import { GitHubService } from './service';
 import { BehaviorSubject } from 'rxjs';
 import { expect, test, describe } from 'vitest';
 
+const exampleRemoteUrls = [
+	'ssh://user@host.xz:123/org/repo.git/',
+	'ssh://user@host.xz/org/repo.git/',
+	'ssh://host.xz:123/org/repo.git/',
+	'ssh://host.xz:123/org/repo',
+	'ssh://host.xz/org/repo.git/',
+	'ssh://host.xz/org/repo.git',
+	'ssh://host.xz/org/repo',
+	'ssh://user@host.xz/org/repo.git/',
+	'ssh://user@host.xz/org/repo.git',
+	'ssh://user@host.xz/org/repo',
+	'host.xz:org/repo.git/',
+	'host.xz:org/repo.git',
+	'host.xz:org/repo',
+	'user@host.xz:org/repo.git/',
+	'user@host.xz:org/repo.git',
+	'user@host.xz:org/repo',
+	'git@github.com:org/repo.git/',
+	'git@github.com:org/repo.git',
+	'git@github.com:org/repo',
+	'https://github.com/org/repo.git/',
+	'https://github.com/org/repo.git',
+	'https://github.com/org/repo'
+];
+
 describe.concurrent('GitHubService', () => {
 	describe.concurrent('parse GitHub remote URL', () => {
-		test('default url', async () => {
+		test.each(exampleRemoteUrls)('Remote %s', async (remoteUrl) => {
 			const accessToken$ = new BehaviorSubject<string | undefined>('token');
-			const remoteUrl$ = new BehaviorSubject<string | undefined>(
-				'git@github.com:gitbutlerapp/gitbutler'
-			);
+			const remoteUrl$ = new BehaviorSubject<string | undefined>(remoteUrl);
 
 			const githubService = new GitHubService(accessToken$, remoteUrl$);
 
-			expect(githubService.owner).toBe('gitbutlerapp');
-			expect(githubService.repo).toBe('gitbutler');
-		});
-		test('ssh URL with .git suffix', async () => {
-			const accessToken$ = new BehaviorSubject<string | undefined>('token');
-			const remoteUrl$ = new BehaviorSubject<string | undefined>(
-				'git@github.com:gitbutlerapp/gitbutler.git'
-			);
-
-			const githubService = new GitHubService(accessToken$, remoteUrl$);
-
-			expect(githubService.owner).toBe('gitbutlerapp');
-			expect(githubService.repo).toBe('gitbutler');
-		});
-		test('http URL', async () => {
-			const accessToken$ = new BehaviorSubject<string | undefined>('token');
-			const remoteUrl$ = new BehaviorSubject<string | undefined>(
-				'https://github.com/gitbutlerapp/gitbutler'
-			);
-
-			const githubService = new GitHubService(accessToken$, remoteUrl$);
-
-			expect(githubService.owner).toBe('gitbutlerapp');
-			expect(githubService.repo).toBe('gitbutler');
-		});
-		test('http URL with .git suffix', async () => {
-			const accessToken$ = new BehaviorSubject<string | undefined>('token');
-			const remoteUrl$ = new BehaviorSubject<string | undefined>(
-				'https://github.com/gitbutlerapp/gitbutler.git'
-			);
-
-			const githubService = new GitHubService(accessToken$, remoteUrl$);
-
-			expect(githubService.owner).toBe('gitbutlerapp');
-			expect(githubService.repo).toBe('gitbutler');
+			expect(githubService.owner).toBe('org');
+			expect(githubService.repo).toBe('repo');
 		});
 	});
 });
