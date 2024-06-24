e0f0fe1db8d9382b0a45a070fea8f96bcbc58351
ndom91 <yo@ndo.dev>
2024-06-24 10:56:51 +0200

    fix: add initial pages

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,25 +1,28 @@
-import { type BaseLayoutProps, type DocsLayoutProps } from 'fumadocs-ui/layout';
-import { pageTree } from './source';
-import Logo from '@/components/Logo';
+import { type BaseLayoutProps, type DocsLayoutProps } from "fumadocs-ui/layout"
+import { pageTree } from "./source"
+import Logo from "@/components/Logo"
 
 // shared configuration
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    component: <Logo />,
-    transparentMode: 'top',
+    // component: <Logo />,
+    transparentMode: "top"
   },
-  githubUrl: 'https://github.com/gitbutlerapp/gitbutler',
+  githubUrl: "https://github.com/gitbutlerapp/gitbutler",
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
-    },
-  ],
-};
+      text: "Documentation",
+      url: "/docs",
+      active: "nested-url"
+    }
+  ]
+}
 
 // docs layout configuration
 export const docsOptions: DocsLayoutProps = {
   ...baseOptions,
-  tree: pageTree,
-};
+  sidebar: {
+    banner: <Logo />
+  },
+  tree: pageTree
+}
