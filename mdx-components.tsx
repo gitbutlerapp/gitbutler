34421e5da9bb7237e5c4b2d39f9fd43965790ed8
ndom91 <yo@ndo.dev>
2024-06-25 23:31:32 +0200

    fix: cleanup pages and use fuma components where appropriate

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,6 @@
-import type { MDXComponents } from "mdx/types"
 import defaultComponents from "fumadocs-ui/mdx"
 import ImageSection from "@/components/ImageSection"
+import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
