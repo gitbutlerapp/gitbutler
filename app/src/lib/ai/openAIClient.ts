08c38ab86abc025a4a6c993ef42efd4b448332af
Caleb Owens <hello@calebowens.com>
2024-06-27 21:41:43 +0200

    Use andThen

--- app/src/lib/ai/openAIClient.ts
+++ app/src/lib/ai/openAIClient.ts
@@ -1,7 +1,8 @@
 import { SHORT_DEFAULT_BRANCH_TEMPLATE, SHORT_DEFAULT_COMMIT_TEMPLATE } from '$lib/ai/prompts';
-import { buildFailureFromAny, ok, type Result } from '$lib/result';
+import { andThen, buildFailureFromAny, ok, wrapAsync, type Result } from '$lib/result';
 import type { OpenAIModelName, Prompt, AIClient } from '$lib/ai/types';
 import type OpenAI from 'openai';
+import type { ChatCompletion } from 'openai/resources/index.mjs';
 
 export class OpenAIClient implements AIClient {
 	defaultCommitTemplate = SHORT_DEFAULT_COMMIT_TEMPLATE;
@@ -13,24 +14,20 @@ export class OpenAIClient implements AIClient {
 	) {}
 
 	async evaluate(prompt: Prompt): Promise<Result<string, Error>> {
-		try {
-			const response = await this.openAI.chat.completions.create({
+		const responseResult = await wrapAsync<ChatCompletion, Error>(async () => {
+			return await this.openAI.chat.completions.create({
 				messages: prompt,
 				model: this.modelName,
 				max_tokens: 400
 			});
+		});
 
+		return andThen(responseResult, (response) => {
 			if (response.choices[0]?.message.content) {
 				return ok(response.choices[0]?.message.content);
 			} else {
 				return buildFailureFromAny('Open AI generated an empty message');
 			}
-		} catch (e) {
-			if (e instanceof Error) {
-				return buildFailureFromAny(e.message);
-			} else {
-				return buildFailureFromAny('Failed to contact Open AI');
-			}
-		}
+		});
 	}
 }
