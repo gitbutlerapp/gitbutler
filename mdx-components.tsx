32d1f6e6f1aa1462b83cb9da00c4653a5f60780d
ndom91 <yo@ndo.dev>
2024-06-28 10:46:39 +0200

    fix: rm CodeEditor chrome

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,10 @@
 import defaultComponents from "fumadocs-ui/mdx"
 import ImageSection from "@/components/ImageSection"
-import CodeEditor from "@/components/CodeEditor"
 import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ImageSection,
-    CodeEditor,
     ...defaultComponents,
     ...components
   }
