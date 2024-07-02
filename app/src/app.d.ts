33b7a56c623d726f9dfcfbf1865eb0c146cfc04d
ndom91 <yo@ndo.dev>
2024-07-02 14:49:17 +0200

    fix: cleanup hooks.client.ts handleError fn

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -1,10 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {}
+declare global {
+	namespace App {
+		interface Error {
+			message: string;
+			errorId: string;
+		}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
 
 declare module 'tinykeys';
 
 interface HTMLElement {
 	scrollIntoViewIfNeeded: (centerIfNeeded?: boolean) => void;
 }
+
+export {};
--- app/src/hooks.client.ts
+++ app/src/hooks.client.ts
@@ -1,25 +1,17 @@
 import { handleErrorWithSentry } from '@sentry/sveltekit';
 import { error as logErrorToFile } from 'tauri-plugin-log-api';
-import type { NavigationEvent } from '@sveltejs/kit';
+import type { HandleClientError } from '@sveltejs/kit';
 
-function myErrorHandler({ error, event }: { error: any; event: NavigationEvent }) {
-	console.error(error.message + '\n' + error.stack);
-	logErrorToFile(error.toString());
-	console.error('An error occurred on the client side:', error, event);
-}
+const errorHandler: HandleClientError = ({ error, message }) => {
+	const errorId = crypto.randomUUID();
 
-export const handleError = handleErrorWithSentry(myErrorHandler);
+	logErrorToFile(message);
+	console.error(`${errorId}: ${message}\n${(error as Error)?.stack}`);
 
-/**
- * This is not an ideal way of handling unhandled errors, but it's what we have at the moment. The
- * main reason for adding this is that it gives us better stack traces when promises throw errors
- * in reactive statements.
- *
- * See: https://github.com/sveltejs/rfcs/pull/46
- */
-const originalUnhandledHandler = window.onunhandledrejection;
-window.onunhandledrejection = (event: PromiseRejectionEvent) => {
-	logErrorToFile('Unhandled exception: ' + event?.reason + ' ' + event?.reason?.sourceURL);
-	console.log('Unhandled exception', event.reason);
-	originalUnhandledHandler?.bind(window)(event);
+	return {
+		message,
+		errorId
+	};
 };
+
+export const handleError = handleErrorWithSentry(errorHandler);
