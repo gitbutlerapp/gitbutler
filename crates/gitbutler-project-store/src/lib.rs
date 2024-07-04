a3c4abf9b35332785ec7037deb37ff23bf859d4f
Caleb Owens <caleb@gitbutler.com>
2024-07-04 14:31:33 +0200

    Cleaned up an oil spill

--- crates/gitbutler-project-store/src/lib.rs
+++ crates/gitbutler-project-store/src/lib.rs
@@ -1,18 +1,17 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
+use rusqlite::Connection;
 use std::path::Path;
 
-use diesel::{Connection, SqliteConnection};
-use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-
 const DATABASE_NAME: &str = "project.sqlite";
-const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 /// ProjectStore provides a light wrapper around a sqlite database
 struct ProjectStore {
-    connection: SqliteConnection,
+    connection: Connection,
 }
 
 /// Database setup
+///
+/// Before touching any database related code, please read https://github.com/the-lean-crate/criner/discussions/5 first.
 impl ProjectStore {
     /// Creates an instance of ProjectStore and runs any pending sqlite migrations
     /// gitbutler_project_directory should be the `.git/gitbutler` path of a given
@@ -23,13 +22,46 @@ impl ProjectStore {
             "Failed to get database {}",
             gitbutler_project_directory.display()
         ))?;
-        let mut connection = SqliteConnection::establish(database_path)?;
 
-        // Run any pending migrations
-        connection
-            .run_pending_migrations(MIGRATIONS)
-            .map_err(|err| anyhow!("{}", err))?;
+        let connection = Connection::open(database_path)?;
 
-        Ok(ProjectStore { connection })
+        ProjectStore::configure_connection(&connection)?;
+
+        let project_store = ProjectStore { connection };
+
+        project_store.run_migrations()?;
+
+        Ok(project_store)
     }
+
+    /// Configures a sqlite connection to behave sensibly in a concurrent environemnt.
+    ///
+    /// Busy handler and pargma's have been taken from https://github.com/the-lean-crate/criner/discussions/5
+    /// and will help with concurrent reading and writing.
+    ///
+    /// This should be run before a project store is created and any other SQL is run.
+    fn configure_connection(connection: &Connection) -> Result<()> {
+        connection
+            .busy_handler(Some(sleeper))
+            .context("Failed to set connection's busy handler")?;
+
+        connection.execute_batch("
+            PRAGMA journal_mode = WAL;          -- better write-concurrency
+            PRAGMA synchronous = NORMAL;        -- fsync only in critical moments
+            PRAGMA wal_autocheckpoint = 1000;   -- write WAL changes back every 1000 pages, for an in average 1MB WAL file. May affect readers if number is increased
+            PRAGMA wal_checkpoint(TRUNCATE);    -- free some space by truncating possibly massive WAL files from the last run.
+        ").context("Failed to set PRAGMA's for connection")?;
+
+        Ok(())
+    }
+
+    fn run_migrations(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+fn sleeper(attempts: i32) -> bool {
+    println!("SQLITE_BUSY, retrying after 50ms (attempt {})", attempts);
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    true
 }
