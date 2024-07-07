7627a41c894cb2635fa8b25fb824eb80b1f4e857
Kiril Videlov <kiril@videlov.com>
2024-07-07 16:06:26 +0200

    remove unused method on the assets proxy impl

--- crates/gitbutler-core/src/assets.rs
+++ crates/gitbutler-core/src/assets.rs
@@ -7,9 +7,7 @@ use url::Url;
 
 use crate::{
     users,
-    virtual_branches::{
-        Author, BaseBranch, RemoteBranchData, RemoteCommit, VirtualBranch, VirtualBranchCommit,
-    },
+    virtual_branches::{Author, BaseBranch, RemoteBranchData, RemoteCommit},
 };
 
 #[derive(Clone)]
@@ -38,40 +36,6 @@ impl Proxy {
             );
         }
         user
-    }
-
-    async fn proxy_virtual_branch_commit(
-        &self,
-        commit: VirtualBranchCommit,
-    ) -> VirtualBranchCommit {
-        VirtualBranchCommit {
-            author: self.proxy_author(commit.author).await,
-            ..commit
-        }
-    }
-
-    pub async fn proxy_virtual_branch(&self, branch: VirtualBranch) -> VirtualBranch {
-        VirtualBranch {
-            commits: join_all(
-                branch
-                    .commits
-                    .iter()
-                    .map(|commit| self.proxy_virtual_branch_commit(commit.clone()))
-                    .collect::<Vec<_>>(),
-            )
-            .await,
-            ..branch
-        }
-    }
-
-    pub async fn proxy_virtual_branches(&self, branches: Vec<VirtualBranch>) -> Vec<VirtualBranch> {
-        join_all(
-            branches
-                .into_iter()
-                .map(|branch| self.proxy_virtual_branch(branch))
-                .collect::<Vec<_>>(),
-        )
-        .await
     }
 
     pub async fn proxy_remote_branch_data(&self, branch: RemoteBranchData) -> RemoteBranchData {
