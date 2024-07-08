9c2fc6d719baf059b7942efef13628e29cc145d3
ndom91 <yo@ndo.dev>
2024-07-08 15:59:29 +0200

    fix: cleanup app.d.ts

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -1,17 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-declare global {
-	namespace App {
-		interface Error {
-			message: string;
-			errorId?: string;
-		}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+declare namespace App {
+	interface Error {
+		message: string;
+		errorId?: string;
 	}
 }
-
-export {};
