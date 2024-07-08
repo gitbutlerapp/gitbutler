d67e0110f0411e57bae54ac4d3be6a7b1aa84c3b
ndom91 <yo@ndo.dev>
2024-07-08 15:24:55 +0200

    fix: tinykeys ts module declaration

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -1,5 +1,6 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
 declare global {
 	namespace App {
 		interface Error {
@@ -11,12 +12,6 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
-}
-
-declare module 'tinykeys';
-
-interface HTMLElement {
-	scrollIntoViewIfNeeded: (centerIfNeeded?: boolean) => void;
 }
 
 export {};
