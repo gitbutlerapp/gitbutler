f30bfcfce2151b63e61b0dfdd09723d07f53a679
ndom91 <yo@ndo.dev>
2024-07-24 11:48:12 +0200

    fix: highlight diffs

--- app/src/lib/hunk/HunkViewer.svelte
+++ app/src/lib/hunk/HunkViewer.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import { Project } from '$lib/backend/projects';
-	import { draggableElement } from '$lib/dragging/draggable';
-	import { DraggableHunk } from '$lib/dragging/draggables';
+	// import { draggableElement } from '$lib/dragging/draggable';
+	// import { DraggableHunk } from '$lib/dragging/draggables';
 	import HunkContextMenu from '$lib/hunk/HunkContextMenu.svelte';
-	import HunkLines from '$lib/hunk/HunkLines.svelte';
+	// import HunkLines from '$lib/hunk/HunkLines.svelte';
 	import { SETTINGS, type Settings } from '$lib/settings/userSettings';
 	import LargeDiffMessage from '$lib/shared/LargeDiffMessage.svelte';
-	import Scrollbar from '$lib/shared/Scrollbar.svelte';
+	// import Scrollbar from '$lib/shared/Scrollbar.svelte';
+	import { create } from '$lib/utils/codeHighlight';
 	import { getContext, getContextStoreBySymbol, maybeGetContextStore } from '$lib/utils/context';
 	import { SectionType } from '$lib/utils/fileSections';
+	import { type HunkSection, type ContentSection } from '$lib/utils/fileSections';
 	import { Ownership } from '$lib/vbranches/ownership';
 	import { VirtualBranch, type Hunk } from '$lib/vbranches/types';
-	import { type HunkSection, type ContentSection, type Line } from '$lib/utils/fileSections';
 	import type { Writable } from 'svelte/store';
-	import ListItem from '$lib/shared/ListItem.svelte';
+	// import ListItem from '$lib/shared/ListItem.svelte';
 
 	interface Props {
 		filePath: string;
@@ -29,35 +30,35 @@
 	let {
 		filePath,
 		section,
+    linesModified,
 		minWidth,
 		selectable = false,
 		isUnapplied,
 		isFileLocked,
 		readonly = false,
-		linesModified
 	}: Props = $props();
 
 	$inspect('section', section);
 
-	const selectedOwnership: Writable<Ownership> | undefined = maybeGetContextStore(Ownership);
-	const userSettings = getContextStoreBySymbol<Settings>(SETTINGS);
-	const branch = maybeGetContextStore(VirtualBranch);
-	const project = getContext(Project);
+	// const selectedOwnership: Writable<Ownership> | undefined = maybeGetContextStore(Ownership);
+	// const userSettings = getContextStoreBySymbol<Settings>(SETTINGS);
+	// const branch = maybeGetContextStore(VirtualBranch);
+	// const project = getContext(Project);
 
-	let contents = $state<HTMLDivElement>();
-	let viewport = $state<HTMLDivElement>();
-	let contextMenu = $state<HunkContextMenu>();
-	let alwaysShow = $state(false);
-	const draggingDisabled = $derived(readonly || isUnapplied);
+  let alwaysShow = $state(false);
+	// let contents = $state<HTMLDivElement>();
+	// let viewport = $state<HTMLDivElement>();
+	// let contextMenu = $state<HunkContextMenu>();
+	// const draggingDisabled = $derived(readonly || isUnapplied);
 
-	function onHunkSelected(hunk: Hunk, isSelected: boolean) {
-		if (!selectedOwnership) return;
-		if (isSelected) {
-			selectedOwnership.update((ownership) => ownership.add(hunk.filePath, hunk));
-		} else {
-			selectedOwnership.update((ownership) => ownership.remove(hunk.filePath, hunk.id));
-		}
-	}
+	// function onHunkSelected(hunk: Hunk, isSelected: boolean) {
+	// 	if (!selectedOwnership) return;
+	// 	if (isSelected) {
+	// 		selectedOwnership.update((ownership) => ownership.add(hunk.filePath, hunk));
+	// 	} else {
+	// 		selectedOwnership.update((ownership) => ownership.remove(hunk.filePath, hunk.id));
+	// 	}
+	// }
 
 	// const subsections = $derived(
 	// 	section.subSections.flatMap((subsection) => {
@@ -70,7 +71,28 @@
 
 	// $inspect(subsections);
 
-	function mapLines(subsections: ContentSection[]): ContentSection[] {
+	function toTokens(inputLine: string): string[] {
+		// debugger;
+		function sanitize(text: string) {
+			var element = document.createElement('div');
+			element.innerText = text;
+			return element.innerHTML;
+		}
+
+		let highlighter = create(inputLine, filePath);
+    console.log('HIGHLIGHTER', highlighter)
+		let tokens: string[] = [];
+		highlighter.highlight((text, classNames) => {
+			const token = classNames
+				? `<span class=${classNames}>${sanitize(text)}</span>`
+				: sanitize(text);
+
+			tokens.push(token);
+		});
+		return tokens;
+	}
+
+	function filterSections(subsections: ContentSection[]): ContentSection[] {
 		return subsections.map((nextSection, i) => {
 			const prevSection = subsections[i - 1];
 			if (!prevSection || nextSection.sectionType === SectionType.Context) return nextSection;
@@ -81,7 +103,7 @@
 		});
 	}
 
-	const subSections = $derived(mapLines(section.subSections));
+	const subSections = $derived(filterSections(section.subSections));
 
 	function calculateWordDiff(
 		prevSection: ContentSection,
@@ -89,27 +111,40 @@
 	): ContentSection {
 		// Skip sections which aren't the same length in lines changed
 		if (prevSection.lines.length !== nextSection.lines.length) return nextSection;
-
-		console.log({ prevSection, nextSection });
-
 		const numberOfLines = nextSection.lines.length;
 
+    // Loop through every line in the section
+    // We're only bothered with prev/next sections with equal # of lines changes
 		for (let i = 0; i < numberOfLines; i++) {
 			const oldLine = prevSection.lines[i];
 			const newLine = nextSection.lines[i];
-			let forwardDiff: number[] = [];
+			let forwardRmDiff: string[] = [];
+			let forwardAddDiff: string[] = [];
 
+      // Ignore whitespace changes
 			const wordRegExp = new RegExp(/\w/g);
-
 			const oldWordContent = oldLine.content.match(wordRegExp);
 			const newWordContent = newLine.content.match(wordRegExp);
 
+      // Brute force walk all "word" characters and calculate the 
+      // text difference between line0 and line1
+      // TODO: See if we can merge these loops; save on compute
 			oldWordContent?.forEach((char, i) => {
 				if (newWordContent?.[i] !== char) {
-					forwardDiff.push(char);
+					forwardRmDiff.push(char);
 				}
 			});
-			prevSection.lines[i].diff = forwardDiff.join('');
+			newWordContent?.forEach((char, i) => {
+				if (oldWordContent?.[i] !== char) {
+					forwardAddDiff.push(char);
+				}
+			});
+
+      // @ts-expect-error - TODO: Update data structure / types
+			prevSection.lines[i].diffs = {
+        removed: forwardRmDiff.join(''),
+        added: forwardAddDiff.join('')
+      }
 		}
 		console.log({ prevSection, nextSection });
 
@@ -118,8 +153,8 @@
 </script>
 
 <div class="scrollable">
-	<div bind:this={viewport} tabindex="0" role="cell">
-		<div bind:this={contents} class="hunk__bg-stretch">
+	<div tabindex="0" role="cell">
+		<div class="hunk__bg-stretch">
 			{#if linesModified > 2500 && !alwaysShow}
 				<LargeDiffMessage
 					on:show={() => {
@@ -144,7 +179,7 @@
 										class:diff-line-addition={section.sectionType === SectionType.AddedLines}
 									>
 										<span class="blob-code-content">
-											{@html line.content}
+											{@html toTokens(line.content)}
 										</span>
 									</td>
 								</tr>
@@ -199,11 +234,11 @@
 	}
 
 	.diff-line-deletion {
-		background-color: #cf8d8e40;
+		background-color: #cf8d8e20;
 	}
 
 	.diff-line-addition {
-		background-color: #94cf8d40;
+		background-color: #94cf8d20;
 	}
 
 	.blob-code-content {
