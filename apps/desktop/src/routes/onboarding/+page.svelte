43b86e37ace30efc1012f05d37341a8e89c98928
ndom91 <yo@ndo.dev>
2024-07-30 13:59:14 +0200

    feat: create /onboarding route

---
+++ apps/desktop/src/routes/onboarding/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import newProjectSvg from '$lib/assets/illustrations/new-project.svg?raw';
+	import DecorativeSplitView from '$lib/components/DecorativeSplitView.svelte';
+	import Welcome from '$lib/components/Welcome.svelte';
+</script>
+
+<DecorativeSplitView img={newProjectSvg} showLinks={false}>
+	<Welcome />
+</DecorativeSplitView>
