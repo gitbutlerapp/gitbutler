0c9904f23e1915bed7f6846e6d5cdef260aa0006
ndom91 <yo@ndo.dev>
2024-07-30 15:22:16 +0200

    fix: welcome onbaordin gpage

--- apps/desktop/src/routes/onboarding/+page.svelte
+++ apps/desktop/src/routes/onboarding/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import analyticsSvg from '$lib/assets/illustrations/analytics.svg?raw';
 	import newProjectSvg from '$lib/assets/illustrations/new-project.svg?raw';
 	import DecorativeSplitView from '$lib/components/DecorativeSplitView.svelte';
 	import Welcome from '$lib/components/Welcome.svelte';
+	import { appAnalyticsConfirmed } from '$lib/config/appSettings';
+	import AnalyticsConfirmation from '$lib/settings/AnalyticsConfirmation.svelte';
+
+	const analyticsConfirmed = appAnalyticsConfirmed();
 </script>
 
-<DecorativeSplitView img={newProjectSvg} showLinks={false}>
-	<Welcome />
+<DecorativeSplitView
+	img={$analyticsConfirmed ? newProjectSvg : analyticsSvg}
+	showLinks={$analyticsConfirmed ? false : true}
+>
+	{#if $analyticsConfirmed}
+		<Welcome />
+	{:else}
+		<AnalyticsConfirmation {analyticsConfirmed} />
+	{/if}
 </DecorativeSplitView>
