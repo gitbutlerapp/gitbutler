858df70f284757a93c437d139f01d4a291fcf075
ndom91 <yo@ndo.dev>
2024-07-31 21:50:47 +0200

    feat: add debugging output

--- apps/desktop/e2e/selenium.spec.cjs
+++ apps/desktop/e2e/selenium.spec.cjs
@@ -22,35 +22,63 @@ before(async function () {
 	// set timeout to 2 minutes
 	this.timeout(120000);
 
-	tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver')); //, [], {
-	// 	stdio: [null, process.stdout, process.stderr]
-	// });
+	// ensure the program has been built
+	// spawnSync("cargo", ["build", "--release"]);
 
-	tauriDriver?.stdout?.on('data', (data) => {
-		console.log(data.toString());
+	tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'), [], {
+		stdio: [null, process.stdout, process.stderr]
 	});
 
 	const capabilities = new Capabilities();
 	capabilities.set('tauri:options', { application });
-	capabilities.setBrowserName('wry');
+	capabilities.setBrowserName('');
 
 	driver = await new Builder()
 		.withCapabilities(capabilities)
-		.usingServer('http://localhost:4444')
+		.usingServer('http://localhost:4444/')
 		.build();
 });
 
-describe('Hello Tauri', () => {
-	it('should be cordial', async () => {
-		const text = await driver.findElement(By.css('body > h1')).getText();
-		expect(text).to.match(/^[hH]ello/);
-	});
-});
-
 after(async function () {
+	console.log('\n\nCALLING AFTER CLEANING FNs');
 	// stop the webdriver session
 	await driver?.quit();
 
 	// kill the tauri-driver process
-	tauriDriver.kill();
+	tauriDriver?.kill();
 });
+
+console.log('\n\nSTARTING TEST');
+describe('GitButler Startup', () => {
+	console.log('\n\nSTARTING DESCRIBE');
+	it('should have gray background', async () => {
+		console.log('\n\nIT_SHOULD_HAVE_GRAY_BACKGROUND');
+		const text = await driver.findElement(By.css('body')).getCssValue('background-color');
+
+		expect(text).to.match(/#000/);
+	});
+	console.log('\n\nFINISHING DSECRIBE');
+});
+console.log('\n\nFINISHING TEST');
+// describe('foo', (_) => {
+//
+// 	it('should be cordial', () => {
+// 		console.log('\n\nDRIVAH', driver);
+// 		// await driver.get('http://localhost:1420');
+// 		const text = driver.findElement(By.css('body > h1')).getText();
+// 		expect(text).to.match(/^[hH]ello/);
+// 		// done();
+// 	});
+//
+// });
+
+// describe('Hello Tauri', () => {
+// 	console.log('\n\nHELLO TAURI');
+//
+// 	it('should be cordial', async () => {
+// 		console.log('\n\nSHOULD BE CORDIAL');
+//
+// 		const text = await driver.findElement(By.css('body > h1')).getText();
+// 		expect(text).to.match(/^[hH]ello/);
+// 	});
+// });
