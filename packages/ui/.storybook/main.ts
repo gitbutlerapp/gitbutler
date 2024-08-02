6cec7d7b4e01724ae8134ecf6848499ed8730d7a
Pavel Laptev <pawellaptew@gmail.com>
2024-08-02 11:37:51 +0200

    formatting fix

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,22 +1,22 @@
-import { dirname, join } from "path";
+import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
 	stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		getAbsolutePath("@storybook/addon-links"),
-		getAbsolutePath("@storybook/addon-essentials"),
-		getAbsolutePath("@chromatic-com/storybook"),
-		getAbsolutePath("@storybook/addon-interactions"),
-		getAbsolutePath("storybook-dark-mode")
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@chromatic-com/storybook'),
+		getAbsolutePath('@storybook/addon-interactions'),
+		getAbsolutePath('storybook-dark-mode')
 	],
 	framework: {
-		name: getAbsolutePath("@storybook/sveltekit"),
+		name: getAbsolutePath('@storybook/sveltekit'),
 		options: {}
 	}
 };
 export default config;
 
 function getAbsolutePath(value: string): any {
-    return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, 'package.json')));
 }
