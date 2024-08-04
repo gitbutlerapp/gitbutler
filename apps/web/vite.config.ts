d559cffe6a766b6e0cbf18d8fb994568c9e70985
Nico Domino <yo@ndo.dev>
2024-08-04 13:39:41 +0200

    feat: add `@gitbutler/ui`, `userService`, `authService` and some minor cleanup (#4595)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,21 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'gitbutler',
+				project: 'gitbutler-web',
+				telemetry: false
+			}
+		}),
+		sveltekit()
+	],
+	server: {
+		fs: {
+			strict: false
+		}
+	}
 });
