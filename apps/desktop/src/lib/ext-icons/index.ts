5992d0b027a4d26d4fc902de289fc7174176ab88
ndom91 <yo@ndo.dev>
2024-08-05 14:07:38 +0200

    fix: always fallback to 'document' icon

--- apps/desktop/src/lib/ext-icons/index.ts
+++ apps/desktop/src/lib/ext-icons/index.ts
@@ -31,8 +31,12 @@ export function getFileIcon(fileName: string) {
 		splitName.shift();
 	}
 
-	if (iconName === '') iconName = 'document';
-	const icon = fileIcons[iconName];
-	// @ts-expect-error - we know fileIcons['document'] exists
+	if (iconName === '') {
+		iconName = 'document';
+	}
+	let icon = fileIcons[iconName];
+	if (!icon) {
+		icon = fileIcons['document'] as string;
+	}
 	return `data:image/svg+xml;base64,${convertToBase64(icon)}`;
 }
