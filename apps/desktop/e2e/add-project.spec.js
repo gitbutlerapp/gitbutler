979a6a775838f30da16f13cccbe8f8459c8f0ddd
ndom91 <yo@ndo.dev>
2024-08-06 16:12:13 +0200

    fix: remaining add-project steps

--- apps/desktop/e2e/add-project.spec.js
+++ apps/desktop/e2e/add-project.spec.js
@@ -1,26 +1,50 @@
 // import { handleTelemetryPage } from './utils';
+import { browser } from '@wdio/globals';
 
 describe('Project', () => {
 	it('should add a local project', async () => {
+		// 0. Accept Telemetry
 		// TODO: Fix broken import in wdio :shrug:
 		// await handleTelemetryPage();
-		const telemetryAgreement = await $('div=GitButler uses telemetry');
-		if (await telemetryAgreement.isExisting()) {
-			const acceptTelemetryBtn = await $('button=Continue');
-			await acceptTelemetryBtn.click();
-		}
+		const telemetryAgreement = await $('h1=Before we begin');
+		await telemetryAgreement.waitForDisplayed();
 
+		// if (await telemetryAgreement.isExisting()) {
+		const acceptTelemetryBtn = await $('button=Continue');
+		await acceptTelemetryBtn.click();
+		// }
+
+		// 1. Click "Add local project"
 		const addLocalProjectBtn = await $('div=Add local project');
 		expect(addLocalProjectBtn).toExist();
 
-		// Now a FileDialog pops up, maybe we can control it by sending in keyboard commands?
-		// await addLocalProjectBtn.click();
-
-		// For now, hacky workaround by setting a file path in a new hidden input
-		const filePathInput = await $('input#test-directory-path');
+		// For now, workaround by setting a file path in a new hidden input
+		const filePathInput = await $('input[data-testid="test-directory-path"]');
 		expect(filePathInput).toExist();
-		filePathInput.setValue('/opt/ndomino/home2021');
-
+		browser.execute((s) => {
+			s.value = '/opt/ndomino/home2021';
+		}, filePathInput);
 		await addLocalProjectBtn.click();
+
+		// 2. Set target base branch
+		const currentTargetBranchLabel = await $('h3=Target branch');
+
+		if (await currentTargetBranchLabel.isExisting()) {
+			// expect(currentTargetBranchLabel).toExist();
+
+			const currentTargetBranchContinueBtn = await $('button=Continue');
+			await currentTargetBranchContinueBtn.click();
+
+			// 3. Git Authentication
+			const gitAuthenticationLabel = await $('h3=Git authentication');
+			expect(gitAuthenticationLabel).toExist();
+
+			const gitAuthenticationContinueBtn = await $("button=Let's go!");
+			await gitAuthenticationContinueBtn.click();
+		}
+
+		// 4. Board
+		const boardWorkspaceBtn = await $('button.domain-button');
+		expect(boardWorkspaceBtn).toExist();
 	});
 });
