17e22e896bb15f44153a9372fad76c4e532f73bf
ndom91 <yo@ndo.dev>
2024-08-09 11:23:07 +0200

    feat: selenium 'add project' test

--- apps/desktop/e2e/selenium.spec.js
+++ apps/desktop/e2e/selenium.spec.js
@@ -50,14 +50,41 @@ describe('GitButler', function () {
 describe('On-Boarding', function () {
 	this.timeout(20000);
 	it('should add a local project', async function () {
-		// await driver.manage().setTimeouts({ explicit: 20000, implicit: 20000 });
-		await driver.sleep(2000);
+		await driver.sleep(1000);
 
+		// 0. Accept Telemetry Warnings
 		const telemetryAgreementShown = await driver.findElement(By.css('h1'));
 		await driver.wait(until.elementIsVisible(telemetryAgreementShown), 10000);
 
-		const acceptTelemetryBtn = await driver.findElement(By.css('button'));
+		const acceptTelemetryBtn = await driver.findElement(
+			By.xpath(
+				'.//button[normalize-space(text()) = "Continue"] | .//button[not(.//button[normalize-space(text()) = "Continue"]) and normalize-space() = "Continue"]'
+			)
+		);
 		// By.css('button[data-testid="analytics-continue"]')
 		await acceptTelemetryBtn.click();
+
+		// 1. Add Local Project
+		const addLocalProjectBtn = await driver.findElement(
+			By.xpath('.//div[normalize-space(text()) = "Add local project"]')
+		);
+
+		// 2. Set input value to repository path
+		// const targetRepositoryPath = './one-vbranch-on-integration';
+		const targetRepositoryPath = '/opt/ndomino/home2021';
+		const filePathInput = await driver.findElement(
+			By.css('input[data-testid="test-directory-path"]')
+		);
+		expect(filePathInput).to.exist;
+		driver.executeScript(
+			"arguments[0].setAttribute('value',arguments[1])",
+			filePathInput,
+			targetRepositoryPath
+		);
+
+		// After input has been modified, click!
+		await addLocalProjectBtn.click();
+
+		await driver.sleep(2000);
 	});
 });
