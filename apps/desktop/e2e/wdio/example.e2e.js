8e586886ac9c47d5eb27af119fe245316658413f
ndom91 <yo@ndo.dev>
2024-08-09 10:08:08 +0200

    fix: remove all wdio

--- apps/desktop/e2e/wdio/example.e2e.js
+++
@@ -1,6 +0,0 @@
-describe('GitButler', () => {
-	it('should have the root element', async () => {
-		const element = await $('body.text-base');
-		expect(element).toExist();
-	});
-});
--- apps/desktop/e2e/wdio/utils.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_TIMEOUT = 5_000;
-
-export async function findAndClick(selector, timeout) {
-	const button = await $(selector);
-	await button.waitForClickable({
-		timeout: timeout ?? DEFAULT_TIMEOUT
-	});
-	await browser.execute('arguments[0].click();', button);
-}
