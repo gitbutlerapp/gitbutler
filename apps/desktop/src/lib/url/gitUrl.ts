831a019cc0e55cab22fb546656b62f22f7565f1c
ndom91 <yo@ndo.dev>
2024-08-09 15:29:04 +0200

    fix: handle local paths as git remote URIs

--- apps/desktop/src/lib/url/gitUrl.ts
+++ apps/desktop/src/lib/url/gitUrl.ts
@@ -3,18 +3,27 @@ import gitUrlParse from 'git-url-parse';
 export type RepoInfo = {
 	source: string;
 	name: string;
-	owner: string;
+	owner?: string;
 	organization?: string;
 	protocol?: string;
 };
 
 export function parseRemoteUrl(url: string): RepoInfo {
-	const { protocol, source, name, owner, organization } = gitUrlParse(url);
-	return {
-		protocol,
-		source,
-		name,
-		owner,
-		organization
-	};
+	try {
+		const { protocol, source, name, owner, organization } = gitUrlParse(url);
+
+		return {
+			protocol,
+			source,
+			name,
+			owner,
+			organization
+		};
+	} catch (e) {
+		return {
+			protocol: 'path',
+			source: 'local',
+			name: url
+		};
+	}
 }
