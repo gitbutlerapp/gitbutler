ed94228326f350ef33bfdbf2f0762e6d6ef1cf79
ndom91 <yo@ndo.dev>
2024-08-09 10:05:00 +0200

    Revert "fix: remove selenium-webdriver"
    This reverts commit f83391c223cebdb6b9e01f907fba8d7486f1189f.

---
+++ apps/desktop/e2e/selenium.spec.js
@@ -0,0 +1,67 @@
+import os from 'node:os';
+import path from 'node:path';
+import { spawn } from 'node:child_process';
+import { Builder, By, Capabilities } from 'selenium-webdriver';
+
+// See: https://tauri.app/v1/guides/testing/webdriver/example/selenium/
+
+const application = path.resolve(
+	import.meta.dirname,
+	'..',
+	'..',
+	'..',
+	'target',
+	'release',
+	'git-butler-dev'
+);
+
+let driver;
+let tauriDriver;
+
+before(async function () {
+	// set timeout to 2 minutes to cover build
+	this.timeout(60000 * 2);
+
+	// For CI(?) - ensure the program has been built
+	// spawnSync("cargo", ["build", "--release"]);
+
+	tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'), [], {
+		stdio: [null, process.stdout, process.stderr]
+	});
+
+	const capabilities = new Capabilities();
+	capabilities.set('tauri:options', { application });
+	capabilities.setBrowserName(''); // Setting this to 'wry' triggers the "Capabilities do not match" error
+
+	driver = await new Builder()
+		.withCapabilities(capabilities)
+		.usingServer('http://localhost:4444/')
+		.build();
+});
+
+after(async function () {
+	console.log('\n\nCALLING AFTER CLEANING FNs');
+
+	// stop the webdriver session
+	await driver?.quit();
+
+	// kill the tauri-driver process
+	tauriDriver?.kill();
+});
+
+console.log('\n\nSTARTING TEST');
+
+describe('GitButler Startup', () => {
+	console.log('\n\nSTARTING DESCRIBE');
+
+	it('should have gray background', async () => {
+		console.log('\n\nIT_SHOULD_HAVE_GRAY_BACKGROUND');
+
+		const text = await driver.findElement(By.css('body')).getCssValue('background-color');
+		expect(text).to.match(/#000/);
+	});
+
+	console.log('\n\nFINISHING DSECRIBE');
+});
+
+console.log('\n\nFINISHING TEST');
