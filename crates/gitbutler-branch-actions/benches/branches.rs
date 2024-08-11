e26bf2f54a88d0deace57c4721b40ed70b62f5a1
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-08-11 11:36:07 +0200

    add benchmarks for branch details

--- crates/gitbutler-branch-actions/benches/branches.rs
+++ crates/gitbutler-branch-actions/benches/branches.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
-use gitbutler_branch_actions::list_branches;
+use gitbutler_branch_actions::{get_branch_listing_details, list_branches};
 use gitbutler_command_context::CommandContext;
 use gitbutler_project::Project;
 
@@ -50,5 +50,59 @@ pub fn benchmark_list_branches(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, benchmark_list_branches);
+pub fn benchmark_branch_details(c: &mut Criterion) {
+    for (bench_name, (repo_name, branch_name, files_to_diff, script_name)) in [
+        (
+            "branch-details [many branches no change]",
+            ("many-local", "virtual", 0, "branch-benches.sh"),
+        ),
+        (
+            "branch-details [tiny no change]",
+            (
+                "one-vbranch-on-integration-two-remotes",
+                "main",
+                0,
+                "for-listing.sh",
+            ),
+        ),
+        (
+            "branch-details [big repo no change]",
+            (
+                "big-repo-clone",
+                "no-change",
+                0,
+                "branch-details-benches.sh",
+            ),
+        ),
+        (
+            "branch-details [every-file-changed]",
+            (
+                "big-repo-clone-one-commit-ahead",
+                "change-with-new-content",
+                10_000,
+                "branch-details-benches.sh",
+            ),
+        ),
+    ] {
+        let mut group = c.benchmark_group(bench_name);
+        let project = fixture_project(repo_name, script_name);
+        if files_to_diff != 0 {
+            group.throughput(Throughput::Elements(files_to_diff));
+        }
+        group.bench_function("list details of known branch", |b| {
+            b.iter(|| {
+                let ctx = CommandContext::open(&project).unwrap();
+                let details =
+                    get_branch_listing_details(black_box(&ctx), Some(branch_name)).unwrap();
+                assert_eq!(details.len(), 1, "{script_name}:{repo_name}:{branch_name}");
+                assert_eq!(
+                    details[0].number_of_files, files_to_diff as usize,
+                    "currently it creates a new vbranch for changes in local-commits, something we leverage here"
+                );
+            })
+        });
+    }
+}
+
+criterion_group!(benches, benchmark_list_branches, benchmark_branch_details);
 criterion_main!(benches);
