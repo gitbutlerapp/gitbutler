b5a5be42b0d5b1054296ff06657280cf97807179
Comnir <comnir@gmail.com>
2024-08-14 19:51:29 +0300

    Revert BackButton changes

--- apps/desktop/src/lib/components/BackButton.svelte
+++ apps/desktop/src/lib/components/BackButton.svelte
@@ -1,30 +1,17 @@
-<script lang="ts">
+<script>
 	import Button from '@gitbutler/ui/inputs/Button.svelte';
 	import { goto } from '$app/navigation';
-	async function defaultPreMouseDown() {
-		Promise.resolve();
-	}
-
-	export let preMouseDown: () => Promise<void> = defaultPreMouseDown;
 </script>
 
 <Button
 	style="ghost"
 	outline
 	onmousedown={() => {
-		preMouseDown().then(
-			() => {
-				if (history.length > 0) {
-					history.back();
-				} else {
-					goto('/');
-				}
-			},
-			(err) => {
-				console.log('Failed to execute the pre-mouse-down action');
-				console.log(err);
-			}
-		);
+		if (history.length > 0) {
+			history.back();
+		} else {
+			goto('/');
+		}
 	}}
 >
 	<slot />
