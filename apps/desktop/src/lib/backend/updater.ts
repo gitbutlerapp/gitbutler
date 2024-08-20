4a2e947c4622fb0568319a0bcd22fee9e07f5611
Mattias Granlund <mtsgrd@gmail.com>
2024-08-20 12:13:11 +0100

    Simplify and test updater service
    - reverts to old UI
    - moves more business logic into .ts file
    - add tests for a few scenarios

--- apps/desktop/src/lib/backend/updater.ts
+++ apps/desktop/src/lib/backend/updater.ts
@@ -1,33 +1,29 @@
-import { listen } from './ipc';
+import { Tauri } from './tauri';
 import { showToast } from '$lib/notifications/toasts';
-import { getVersion } from '@tauri-apps/api/app';
 import { relaunch } from '@tauri-apps/api/process';
 import {
-	checkUpdate,
 	installUpdate,
-	onUpdaterEvent,
 	type UpdateResult,
 	type UpdateManifest,
 	type UpdateStatus
 } from '@tauri-apps/api/updater';
 import posthog from 'posthog-js';
-import { derived, writable, type Readable } from 'svelte/store';
+import { derived, readable, writable } from 'svelte/store';
 
-// TODO: Investigate why 'DOWNLOADED' is not in the type provided by Tauri.
-export type Update = {
-	version?: string;
-	status?: UpdateStatus | 'DOWNLOADED';
-	body?: string;
-	manual: boolean;
-};
+type Status = UpdateStatus | 'DOWNLOADED';
+const TIMEOUT_SECONDS = 30;
 
+/**
+ * Note that the Tauri API `checkUpdate` hangs indefinitely in dev mode, build
+ * a nightly if you want to test the updater manually.
+ *
+ * export TAURI_PRIVATE_KEY=doesnot
+ * export TAURI_KEY_PASSWORD=matter
+ * ./scripts/release.sh --channel nightly --version "0.5.678"
+ */
 export class UpdaterService {
-	// True if manually initiated check.
-	private manual = writable(false);
-
-	// An object rather than string to prevent unique deduplication.
-	private status = writable<{ status: UpdateStatus } | undefined>(undefined);
-
+	readonly loading = writable(false);
+	readonly status = writable<Status | undefined>();
 	private manifest = writable<UpdateManifest | undefined>(undefined, () => {
 		this.start();
 		return () => {
@@ -35,48 +31,52 @@ export class UpdaterService {
 		};
 	});
 
-	update: Readable<Update | undefined> = derived(
-		[this.status, this.manifest, this.manual],
-		([status, result, manual]) => {
-			// Do not emit when up-to-date unless manually initiated.
-			if (status?.status === 'UPTODATE' && result && !manual) {
-				return;
-			}
-			return { ...result, ...status, manual };
-		},
-		undefined
+	private currentVersion = readable<string | undefined>(undefined, (set) => {
+		this.tauri.currentVersion().then((version) => set(version));
+	});
+
+	readonly update = derived(
+		[this.manifest, this.status, this.currentVersion],
+		([manifest, status, currentVersion]) => {
+			return {
+				version: manifest?.version,
+				releaseNotes: manifest?.body,
+				status,
+				currentVersion
+			};
+		}
 	);
 
-	// Needed to reset dismissed modal when version changes.
-	currentVersion = writable<string | undefined>(undefined);
-	readonly version = derived(this.update, (update) => update?.version);
+	private intervalId: any;
+	private seenVersion: string | undefined;
 
-	intervalId: any;
-	unlistenStatusFn: any;
-	unlistenManualCheckFn: any;
+	unlistenStatus?: () => void;
+	unlistenMenu?: () => void;
 
-	constructor() {}
+	constructor(private tauri: Tauri) {}
 
 	private async start() {
-		this.currentVersion.set(await getVersion());
-		this.unlistenManualCheckFn = listen<string>('menu://global/update/clicked', () => {
+		this.unlistenMenu = this.tauri.listen<string>('menu://global/update/clicked', () => {
 			this.checkForUpdate(true);
 		});
-		this.unlistenStatusFn = await onUpdaterEvent((event) => {
+
+		this.unlistenStatus = await this.tauri.onUpdaterEvent((event) => {
 			const { error, status } = event;
+			this.status.set(status);
 			if (error) {
-				showErrorToast(error);
+				handleError(error, false);
 				posthog.capture('App Update Status Error', { error });
 			}
-			this.status.set({ status });
 		});
+
 		setInterval(async () => await this.checkForUpdate(), 3600000); // hourly
 		this.checkForUpdate();
 	}
 
 	private async stop() {
-		this.unlistenStatusFn?.();
-		this.unlistenManualCheckFn?.();
+		this.unlistenStatus?.();
+		this.unlistenMenu?.();
+
 		if (this.intervalId) {
 			clearInterval(this.intervalId);
 			this.intervalId = undefined;
@@ -84,41 +84,78 @@ export class UpdaterService {
 	}
 
 	async checkForUpdate(manual = false) {
-		const update = await Promise.race([
-			checkUpdate(), // In DEV mode this never returns.
-			new Promise<UpdateResult>((resolve) =>
-				setTimeout(() => resolve({ shouldUpdate: false }), 30000)
-			)
-		]);
-		this.manual.set(manual);
-		if (!update.shouldUpdate && manual) {
-			this.status.set({ status: 'UPTODATE' });
-		} else if (update.manifest) {
-			this.manifest.set(update.manifest);
+		this.loading.set(true);
+		try {
+			const update = await Promise.race([
+				this.tauri.checkUpdate(), // In DEV mode this never returns.
+				new Promise<UpdateResult>((_resolve, reject) =>
+					// For manual testing use resolve instead of reject here.
+					setTimeout(
+						() => reject(`Timed out after ${TIMEOUT_SECONDS} seconds.`),
+						TIMEOUT_SECONDS * 1000
+					)
+				)
+			]);
+			await this.processUpdate(update, manual);
+		} catch (err: unknown) {
+			// No toast unless manually invoked.
+			if (manual) {
+				handleError(err, true);
+			} else {
+				console.error(err);
+			}
+		} finally {
+			this.loading.set(false);
+		}
+	}
+
+	private async processUpdate(update: UpdateResult, manual: boolean) {
+		const { shouldUpdate, manifest } = update;
+		if (shouldUpdate === false && manual) {
+			this.status.set('UPTODATE');
+		}
+		if (manifest && manifest.version !== this.seenVersion) {
+			this.manifest.set(manifest);
+			this.seenVersion = manifest.version;
 		}
 	}
 
 	async installUpdate() {
+		this.loading.set(true);
 		try {
 			await installUpdate();
 			posthog.capture('App Update Successful');
 		} catch (err: any) {
 			// We expect toast to be shown by error handling in `onUpdaterEvent`
 			posthog.capture('App Update Install Error', { error: err });
+		} finally {
+			this.loading.set(false);
 		}
 	}
 
-	relaunchApp() {
-		relaunch();
+	async relaunchApp() {
+		try {
+			await relaunch();
+		} catch (err: unknown) {
+			handleError(err, true);
+		}
+	}
+
+	dismiss() {
+		this.manifest.set(undefined);
+		this.status.set(undefined);
 	}
 }
 
 function isOffline(err: any): boolean {
-	return typeof err === 'string' && err.includes('Could not fetch a valid release');
+	return (
+		typeof err === 'string' &&
+		(err.includes('Could not fetch a valid release') || err.includes('Network Error'))
+	);
 }
 
-function showErrorToast(err: any) {
-	if (isOffline(err)) return;
+function handleError(err: any, manual: boolean) {
+	if (!manual && isOffline(err)) return;
 	showToast({
 		title: 'App update failed',
 		message: `
