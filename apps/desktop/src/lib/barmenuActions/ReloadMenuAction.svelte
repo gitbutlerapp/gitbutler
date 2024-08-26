3721b1c1fbd1af079e452d5c55b71b6aea985737
Pavel Laptev <pawellaptew@gmail.com>
2024-08-26 15:24:44 +0200

    Add zoom functionality with keyboard shortcuts and update menu actions for the `View` item (#4753)
    * feat: Add zoom functionality with keyboard shortcuts and menu actions
    
    feat: Implement global settings menu action navigation to settings page
    
    feat: Implement theme switching functionality with keyboard shortcut and menu action
    
    * code review fixes

---
+++ apps/desktop/src/lib/barmenuActions/ReloadMenuAction.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { listen } from '$lib/backend/ipc';
+	import { createKeybind } from '$lib/utils/hotkeys';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const unsubscribe = listen<string>('menu://view/reload/clicked', () => {
+			location.reload();
+		});
+
+		return async () => {
+			unsubscribe();
+		};
+	});
+
+	const handleKeyDown = createKeybind({
+		'$mod+R': () => {
+			location.reload();
+		}
+	});
+</script>
+
+<svelte:window on:keydown={handleKeyDown} />
