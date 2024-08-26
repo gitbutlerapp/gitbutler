082c51a450dde2028c55bc0c104cd56ada29a029
ndom91 <yo@ndo.dev>
2024-08-26 18:43:00 +0200

    fix: add regex check back against

--- crates/gitbutler-tauri/src/open.rs
+++ crates/gitbutler-tauri/src/open.rs
@@ -1,24 +1,28 @@
 use crate::error::Error;
+use anyhow::anyhow;
+use regex::Regex;
 use tracing::instrument;
 
-pub fn open_that(path: String) {
-    // `open` required to run in separate thread, to avoid blocking on some
-    // platforms (eg Fedora38 blocks)
+pub fn open_that(path: String) -> Result<(), Error> {
+    let re = Regex::new(r"^((https://)|(http://)|(mailto:)|(vscode://)|(vscodium://)).+").unwrap();
+    if !re.is_match(&path) {
+        return Err(anyhow!("Invalid path format").into());
+    }
+
     std::thread::spawn(|| {
         for mut cmd in open::commands(path) {
-            // required to set path to good one to use `open` on Ubuntu 22
-            // (otherwise can be permission error)
             cmd.current_dir(std::env::temp_dir());
             if cmd.status().is_ok() {
                 break;
             };
         }
     });
+    Ok(())
 }
 
 #[tauri::command(async)]
 #[instrument(err(Debug))]
 pub fn open_url(url: String) -> Result<(), Error> {
-    open_that(url);
+    open_that(url).unwrap();
     Ok(())
 }
