3cc2edda7a023f6192c1d366d3d0e221b7c0c6a1
Filipe Louro <filipe.louro@giantmonkey.de>
2024-08-27 20:36:56 +0200

    Set gitHostFactory for gitlab self hosted instances

--- apps/desktop/src/lib/url/gitUrl.ts
+++ apps/desktop/src/lib/url/gitUrl.ts
@@ -4,20 +4,22 @@ export type RepoInfo = {
 	source: string;
 	name: string;
 	owner: string;
+	resource: string;
 	organization?: string;
 	protocol?: string;
 };
 
 export function parseRemoteUrl(url: string): RepoInfo | undefined {
 	try {
-		const { protocol, source, name, owner, organization } = gitUrlParse(url);
+		const { protocol, source, name, owner, organization, resource } = gitUrlParse(url);
 
 		return {
 			protocol,
 			source,
 			name,
 			owner,
-			organization
+			organization,
+			resource
 		};
 	} catch {
 		return undefined;
