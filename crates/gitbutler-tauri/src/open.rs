302dc22743eda168b05a896ca4fea83b4a969e09
ndom91 <yo@ndo.dev>
2024-08-28 18:03:04 +0200

    fix: cleanup open.rs changes

--- crates/gitbutler-tauri/src/open.rs
+++ crates/gitbutler-tauri/src/open.rs
@@ -1,21 +1,27 @@
 use crate::error::Error;
 use anyhow::anyhow;
 use regex::Regex;
-use tracing::instrument;
-use std::env;
 use std::collections::HashMap;
-
+use std::env;
+use tracing::instrument;
 
 pub fn open_that(path: &str) -> Result<(), Error> {
     let re = Regex::new(r"^((https://)|(http://)|(mailto:)|(vscode://)|(vscodium://)).+").unwrap();
     if !re.is_match(&path) {
         return Err(anyhow!("Invalid path format").into());
     }
-    let filtered_env : HashMap<String, String> =
-    env::vars().filter(|&(ref k, _)|
-        k == "BROWSER" || k == "NIXOS_XDG_OPEN_USE_PORTAL" || k == "LANG" || k == "PATH" || k == "DISPLAY" || k == "WAYLAND_DISPLAY" || k == "DBUS_SESSION_BUS_ADDRESS"
-    ).collect();
 
+    let filtered_env: HashMap<String, String> = env::vars()
+        .filter(|&(ref k, _)| {
+            k == "BROWSER"
+                || k == "NIXOS_XDG_OPEN_USE_PORTAL"
+                || k == "LANG"
+                || k == "PATH"
+                || k == "DISPLAY"
+                || k == "WAYLAND_DISPLAY"
+                || k == "DBUS_SESSION_BUS_ADDRESS"
+        })
+        .collect();
 
     std::thread::spawn({
         let path = path.to_string();
@@ -23,7 +29,7 @@ pub fn open_that(path: &str) -> Result<(), Error> {
             for mut cmd in open::commands(&path) {
                 cmd.env_clear();
                 cmd.envs(&filtered_env);
-                cmd.current_dir(std::env::temp_dir());
+                cmd.current_dir(env::temp_dir());
                 if cmd.status().is_ok() {
                     break;
                 };
