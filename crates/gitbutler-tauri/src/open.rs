301f7e033cad0d8344a51f028b30227993f2a774
ndom91 <yo@ndo.dev>
2024-08-29 17:13:48 +0200

    fix: only cleanup required env vars

--- crates/gitbutler-tauri/src/open.rs
+++ crates/gitbutler-tauri/src/open.rs
@@ -11,24 +11,51 @@ pub fn open_that(path: &str) -> Result<(), Error> {
         return Err(anyhow!("Invalid path format").into());
     }
 
-    let filtered_env: HashMap<String, String> = env::vars()
-        .filter(|&(ref k, _)| {
-            k == "BROWSER"
-                || k == "NIXOS_XDG_OPEN_USE_PORTAL"
-                || k == "LANG"
-                || k == "PATH"
-                || k == "DISPLAY"
-                || k == "WAYLAND_DISPLAY"
-                || k == "DBUS_SESSION_BUS_ADDRESS"
-        })
-        .collect();
+    fn clean_env_vars(var_names: &[&str]) {
+        for var_name in var_names {
+            if let Ok(var_value) = env::var(var_name) {
+                let cleaned_value = var_value
+                    .split(':')
+                    .filter(|path| !path.contains("appimage-run"))
+                    .collect::<Vec<_>>()
+                    .join(":");
+                env::set_var(var_name, cleaned_value);
+            }
+        }
+    }
 
     std::thread::spawn({
         let path = path.to_string();
         move || {
+            clean_env_vars(&[
+                "XDG_DATA_DIRS",
+                "GTK_PATH",
+                "GTK_EXE_PREFIX",
+                "APPDIR",
+                "LD_LIBRARY_PATH",
+                "GIO_EXTRA_MODULES",
+                "PATH",
+                "PYTHONHOME",
+                "PYTHONPATH",
+                "PERLLIB",
+                "QT_PLUGIN_PATH",
+                "GSETTINGS_SCHEMA_DIR",
+                "GST_PLUGIN_SYSTEM_PATH",
+                "GST_PLUGIN_SYSTEM_PATH_1_0",
+                "GTK_DATA_PREFIX",
+                "GDK_PIXBUF_MODULE_FILE",
+                "GTK_IM_MODULE_FILE",
+            ]);
+
             for mut cmd in open::commands(&path) {
-                cmd.env_clear();
-                cmd.envs(&filtered_env);
+                // println!("{:#?}", env::vars());
+                // cmd.env_clear();
+                // println!("{:#?}", filtered_out_env);
+                // cmd.envs(&filtered_out_env);
+
+                println!("{:#?}", env::vars());
+                cmd.envs(env::vars());
+
                 cmd.current_dir(env::temp_dir());
                 if cmd.status().is_ok() {
                     break;
