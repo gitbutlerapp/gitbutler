e67ed4b59a0d104124e598f1ad0c2d003808c009
ndom91 <yo@ndo.dev>
2024-08-30 20:06:11 +0200

    fix: only modify env vars of target proc

--- crates/gitbutler-tauri/src/open.rs
+++ crates/gitbutler-tauri/src/open.rs
@@ -10,7 +10,8 @@ pub fn open_that(path: &str) -> Result<(), Error> {
         return Err(anyhow!("Invalid path format").into());
     }
 
-    fn clean_env_vars(var_names: &[&str]) {
+    fn clean_env_vars(var_names: &[&str]) -> Vec<(String, String)> {
+        let mut cleaned_vars = Vec::new();
         for var_name in var_names {
             if let Ok(var_value) = env::var(var_name) {
                 let cleaned_value = var_value
@@ -18,40 +19,47 @@ pub fn open_that(path: &str) -> Result<(), Error> {
                     .filter(|path| !path.contains("appimage-run"))
                     .collect::<Vec<_>>()
                     .join(":");
-                env::set_var(var_name, cleaned_value);
+                cleaned_vars.push((var_name.to_string(), cleaned_value));
             }
         }
+        cleaned_vars
     }
 
     std::thread::spawn({
         let path = path.to_string();
         move || {
-            clean_env_vars(&[
-                "XDG_DATA_DIRS",
-                "GTK_PATH",
-                "GTK_EXE_PREFIX",
-                "APPDIR",
-                "LD_LIBRARY_PATH",
-                "GIO_EXTRA_MODULES",
-                "PATH",
-                "PYTHONHOME",
-                "PYTHONPATH",
-                "PERLLIB",
-                "QT_PLUGIN_PATH",
-                "GSETTINGS_SCHEMA_DIR",
-                "GST_PLUGIN_SYSTEM_PATH",
-                "GST_PLUGIN_SYSTEM_PATH_1_0",
-                "GTK_DATA_PREFIX",
-                "GDK_PIXBUF_MODULE_FILE",
-                "GTK_IM_MODULE_FILE",
-            ]);
-
             for mut cmd in open::commands(&path) {
-                cmd.envs(env::vars());
+                let cleaned_vars = clean_env_vars(&[
+                    "XDG_DATA_DIRS",
+                    "GTK_PATH",
+                    "GTK_EXE_PREFIX",
+                    "APPDIR",
+                    "LD_LIBRARY_PATH",
+                    "GIO_EXTRA_MODULES",
+                    "GIO_EXTRA_MODULES",
+                    "PATH",
+                    "PYTHONHOME",
+                    "PYTHONPATH",
+                    "PERLLIB",
+                    "QT_PLUGIN_PATH",
+                    "GSETTINGS_SCHEMA_DIR",
+                    "GST_PLUGIN_SYSTEM_PATH",
+                    "GST_PLUGIN_SYSTEM_PATH_1_0",
+                    "GTK_DATA_PREFIX",
+                    "GDK_PIXBUF_MODULE_FILE",
+                    "GTK_IM_MODULE_FILE",
+                ]);
+
+                cmd.envs(cleaned_vars);
                 cmd.current_dir(env::temp_dir());
                 if cmd.status().is_ok() {
                     break;
                 };
+
+                // for mut cmd in open::commands(&path) {
+                //     cmd.envs(env::vars());
+                //     cmd.current_dir(env::temp_dir());
+                // }
             }
         }
     });
