dab4eb22d894d427fbf48ece7568e270983dc499
ndom91 <yo@ndo.dev>
2024-09-03 11:28:39 +0200

    fix: use url crate and remove thread spawn in favor of tauri async macro

--- crates/gitbutler-tauri/src/open.rs
+++ crates/gitbutler-tauri/src/open.rs
@@ -1,13 +1,16 @@
 use crate::error::Error;
 use anyhow::anyhow;
-use regex::Regex;
 use std::env;
 use tracing::instrument;
+use url::Url;
 
 pub(crate) fn open_that(path: &str) -> Result<(), Error> {
-    let re = Regex::new(r"^((https://)|(http://)|(mailto:)|(vscode://)|(vscodium://)).+").unwrap();
-    if !re.is_match(path) {
-        return Err(anyhow!("Invalid path format").into());
+    let target_url = match Url::parse(path) {
+        Ok(url) => url,
+        Err(_) => return Err(anyhow!("Invalid path format").into()),
+    };
+    if !["http", "https", "mailto", "vscode", "vscodium"].contains(&target_url.scheme()) {
+        return Err(anyhow!("Invalid path scheme").into());
     }
 
     fn clean_env_vars<'a, 'b>(
@@ -30,44 +33,38 @@ pub(crate) fn open_that(path: &str) -> Result<(), Error> {
             })
     }
 
-    std::thread::spawn({
-        let path = path.to_string();
+    for mut cmd in open::commands(&path) {
+        let cleaned_vars = clean_env_vars(&[
+            "APPDIR",
+            "GDK_PIXBUF_MODULE_FILE",
+            "GIO_EXTRA_MODULES",
+            "GIO_EXTRA_MODULES",
+            "GSETTINGS_SCHEMA_DIR",
+            "GST_PLUGIN_SYSTEM_PATH",
+            "GST_PLUGIN_SYSTEM_PATH_1_0",
+            "GTK_DATA_PREFIX",
+            "GTK_EXE_PREFIX",
+            "GTK_IM_MODULE_FILE",
+            "GTK_PATH",
+            "LD_LIBRARY_PATH",
+            "PATH",
+            "PERLLIB",
+            "PYTHONHOME",
+            "PYTHONPATH",
+            "QT_PLUGIN_PATH",
+            "XDG_DATA_DIRS",
+        ]);
 
-        move || {
-            for mut cmd in open::commands(&path) {
-                let cleaned_vars = clean_env_vars(&[
-                    "APPDIR",
-                    "GDK_PIXBUF_MODULE_FILE",
-                    "GIO_EXTRA_MODULES",
-                    "GIO_EXTRA_MODULES",
-                    "GSETTINGS_SCHEMA_DIR",
-                    "GST_PLUGIN_SYSTEM_PATH",
-                    "GST_PLUGIN_SYSTEM_PATH_1_0",
-                    "GTK_DATA_PREFIX",
-                    "GTK_EXE_PREFIX",
-                    "GTK_IM_MODULE_FILE",
-                    "GTK_PATH",
-                    "LD_LIBRARY_PATH",
-                    "PATH",
-                    "PERLLIB",
-                    "PYTHONHOME",
-                    "PYTHONPATH",
-                    "QT_PLUGIN_PATH",
-                    "XDG_DATA_DIRS",
-                ]);
-
-                cmd.envs(cleaned_vars);
-                cmd.current_dir(env::temp_dir());
-                if cmd.status().is_ok() {
-                    break;
-                };
-            }
-        }
-    });
+        cmd.envs(cleaned_vars);
+        cmd.current_dir(env::temp_dir());
+        if cmd.status().is_ok() {
+            break;
+        };
+    }
     Ok(())
 }
 
-#[tauri::command()]
+#[tauri::command(async)]
 #[instrument(err(Debug))]
 pub fn open_url(url: &str) -> Result<(), Error> {
     open_that(url)
