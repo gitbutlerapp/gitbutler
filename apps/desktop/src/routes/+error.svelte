13be25f3e53565bf65642d3fa36f80f4cc40f9f9
Mattias Granlund <mtsgrd@gmail.com>
2024-09-05 14:20:55 +0300

    Fix broken error page
    - nested if condition left the page blank

--- apps/desktop/src/routes/+error.svelte
+++ apps/desktop/src/routes/+error.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-	import { Code, isUserErrorCode } from '$lib/backend/ipc';
+	import { Code } from '$lib/backend/ipc';
 	import ProjectNotFound from '$lib/components/ProjectNotFound.svelte';
 	import SomethingWentWrong from '$lib/components/SomethingWentWrong.svelte';
 	import { page } from '$app/stores';
 
-	$: message = $page.error
-		? $page.error.message
-		: $page.status === 404
-			? 'Page not found'
-			: 'Unknown error';
+	const code = $derived($page.error?.errorCode);
+	const status = $derived($page.status);
+	const message = $derived($page.error?.message);
+
+	const error = $derived(message ? message : status === 404 ? 'Page not found' : 'Unknown error');
 </script>
 
-{#if isUserErrorCode($page.error?.errorCode)}
-	{#if $page.error?.errorCode === Code.ProjectMissing}
-		<ProjectNotFound />
-	{/if}
+{#if code === Code.ProjectMissing}
+	<ProjectNotFound />
 {:else}
-	<SomethingWentWrong error={message} />
+	<SomethingWentWrong {error} />
 {/if}
