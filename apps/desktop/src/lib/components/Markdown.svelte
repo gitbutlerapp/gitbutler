95addd9bf04c45a06df5b46ae0e9f2716d282d35
ndom91 <yo@ndo.dev>
2024-09-06 14:03:57 +0200

    feat: add MarkdownContent to wrap to remove flex/flex-col

--- apps/desktop/src/lib/components/Markdown.svelte
+++ apps/desktop/src/lib/components/Markdown.svelte
@@ -1,39 +1,19 @@
 <script lang="ts">
-	import { renderers, options } from '$lib/utils/markdownRenderers';
-	import { Lexer, type Tokens } from 'marked';
+	import MarkdownContent from '$lib/components/MarkdownContent.svelte';
 
 	interface Props {
 		content?: string;
 	}
 
-	let { content, type, tokens, ...rest }: Props & Tokens.Generic = $props();
-
-	const lexer = new Lexer(options);
-	if (!tokens && content) {
-		tokens = lexer.lex(content);
-	}
-
-	$inspect('TOKENS', { tokens, rest });
+	let { content }: Props = $props();
 </script>
 
-{#if !type && tokens}
-	{#each tokens as token}
-		<svelte:self {...token} />
-	{/each}
-{:else if type && renderers[type]}
-	<svelte:component this={renderers[type]} {...rest} {options}>
-		{#if tokens}
-			<svelte:self {tokens} />
-		{:else}
-			{rest.raw}
-		{/if}
-	</svelte:component>
-{:else if tokens}
-	<svelte:self {tokens} />
-{:else if rest.items}
-	{#each rest.items as item}
-		{@html item.raw}<br />
-	{/each}
-{:else}
-	{rest.raw}
-{/if}
+<div class="markdown-content">
+	<MarkdownContent {content} />
+</div>
+
+<style>
+	.markdown-content {
+		display: inline;
+	}
+</style>
---
+++ apps/desktop/src/lib/components/MarkdownContent.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { renderers, options } from '$lib/utils/markdownRenderers';
+	import { Lexer, type Tokens } from 'marked';
+
+	interface Props {
+		content?: string;
+	}
+
+	let { content, type, tokens, ...rest }: Props & Partial<Tokens.Generic> = $props();
+
+	const lexer = new Lexer(options);
+	if (!tokens && content) {
+		tokens = lexer.lex(content);
+	}
+
+	$inspect('TOKENS', { tokens, rest });
+</script>
+
+{#if !type && tokens}
+	{#each tokens as token}
+		<svelte:self {...token} />
+	{/each}
+{:else if type && renderers[type]}
+	<svelte:component this={renderers[type]} {...rest} {options}>
+		{#if tokens}
+			<svelte:self {tokens} />
+		{:else}
+			{rest.raw}
+		{/if}
+	</svelte:component>
+{:else if tokens}
+	<svelte:self {tokens} />
+{:else}
+	{@html rest.raw.replaceAll('\n', '<br />')}
+{/if}
