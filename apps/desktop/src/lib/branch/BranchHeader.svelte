a1ff2079db15d952815420cd86bda50b9998ebc0
Nico Domino <yo@ndo.dev>
2024-09-11 13:43:26 +0200

    fix: `createPr` arguments order (#4878)

--- apps/desktop/src/lib/branch/BranchHeader.svelte
+++ apps/desktop/src/lib/branch/BranchHeader.svelte
@@ -126,7 +126,13 @@
 				return;
 			}
 
-			await $prService.createPr(title, body, opts.draft, upstreamBranchName, baseBranchName);
+			await $prService.createPr({
+				title,
+				body,
+				draft: opts.draft,
+				baseBranchName,
+				upstreamName: upstreamBranchName
+			});
 		} catch (err: any) {
 			console.error(err);
 			const toast = mapErrorToToast(err);
