33061acc7dde89b0a8b6c8412c767ca82b7397f0
ndom91 <yo@ndo.dev>
2024-09-11 10:26:13 +0200

    fix: rm unnecessary Space.svelte markdown renderer component

--- apps/desktop/src/lib/utils/markdownRenderers.ts
+++ apps/desktop/src/lib/utils/markdownRenderers.ts
@@ -4,14 +4,12 @@ import Codespan from '$lib/components/markdownRenderers/Codespan.svelte';
 import Heading from '$lib/components/markdownRenderers/Heading.svelte';
 import Image from '$lib/components/markdownRenderers/Image.svelte';
 import Paragraph from '$lib/components/markdownRenderers/Paragraph.svelte';
-import Space from '$lib/components/markdownRenderers/Space.svelte';
 import Text from '$lib/components/markdownRenderers/Text.svelte';
 import Link from '$lib/shared/Link.svelte';
 
 export const renderers = {
 	link: Link,
 	image: Image,
-	space: Space,
 	blockquote: Blockquote,
 	code: Code,
 	codespan: Codespan,
