3c4cda739fe687c4e88ee7b25c0b80e7dce2a808
Caleb Owens <hello@calebowens.com>
2024-09-26 00:24:21 +0200

    Update imports in relation to the 8.3 release

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -4,11 +4,11 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)'],
 	addons: [
-        getAbsolutePath('@storybook/addon-links'),
-        getAbsolutePath('@storybook/addon-essentials'),
-        getAbsolutePath('storybook-dark-mode'),
-        getAbsolutePath('@storybook/experimental-addon-vitest'),
-    ],
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('storybook-dark-mode'),
+		getAbsolutePath('@storybook/experimental-addon-test')
+	],
 	framework: {
 		name: getAbsolutePath('@storybook/sveltekit'),
 		options: {}
--- packages/ui/vitest.workspace.ts
+++ packages/ui/vitest.workspace.ts
@@ -1,24 +1,21 @@
 import { defineWorkspace } from 'vitest/config';
-import { storybookTest } from '@storybook/experimental-addon-vitest/plugin';
-import { storybookSveltekitPlugin } from '@storybook/sveltekit/vite'
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import { storybookSveltekitPlugin } from '@storybook/sveltekit/vite-plugin';
 
 export default defineWorkspace([
-  'vite.config.ts',
-  {
-    extends: 'vite.config.ts',
-    plugins: [
-      storybookTest({ storybookScript: 'pnpm storybook' }),
-      storybookSveltekitPlugin()
-    ],
-    test: {
-      browser: {
-        enabled: true,
-        headless: true,
-        name: 'chromium',
-        provider: 'playwright',
-      },
-      include: ['**/*.stories.?(m)[jt]s?(x)'],
-      setupFiles: ['./.storybook/vitest.setup.ts'],
-    },
-  },
+	'vite.config.ts',
+	{
+		extends: 'vite.config.ts',
+		plugins: [storybookTest({ storybookScript: 'pnpm storybook' }), storybookSveltekitPlugin()],
+		test: {
+			browser: {
+				enabled: true,
+				headless: true,
+				name: 'chromium',
+				provider: 'playwright'
+			},
+			include: ['**/*.stories.?(m)[jt]s?(x)'],
+			setupFiles: ['./.storybook/vitest.setup.ts']
+		}
+	}
 ]);
