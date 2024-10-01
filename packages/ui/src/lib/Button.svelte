d756cd1916c837fcc470678d85a6d133822d72ff
Pavel Laptev <pawellaptew@gmail.com>
2024-10-01 11:44:02 +0200

    increase button icons contrast

--- packages/ui/src/lib/Button.svelte
+++ packages/ui/src/lib/Button.svelte
@@ -213,7 +213,7 @@
 		--btn-bg: transparent;
 
 		&:not(.not-clickable, &:disabled):hover {
-			--icon-opacity: 0.8;
+			--icon-opacity: 0.6;
 			--btn-bg: var(--clr-bg-1-muted);
 		}
 
@@ -232,13 +232,13 @@
 
 	.neutral {
 		&.soft {
-			--icon-opacity: 0.6;
+			--icon-opacity: 0.5;
 			--btn-text-clr: var(--clr-theme-ntrl-on-soft);
 			--btn-bg: var(--clr-theme-ntrl-soft);
 
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.6;
 				--btn-bg: var(--clr-theme-ntrl-soft-hover);
 			}
 		}
@@ -262,17 +262,17 @@
 			--btn-bg: var(--clr-theme-pop-soft);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.9;
+				--icon-opacity: 0.7;
 				--btn-bg: var(--clr-theme-pop-soft-hover);
 			}
 		}
 		&.solid {
-			--icon-opacity: 0.7;
+			--icon-opacity: 0.8;
 			--btn-text-clr: var(--clr-theme-pop-on-element);
 			--btn-bg: var(--clr-theme-pop-element);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.9;
 				--btn-bg: var(--clr-theme-pop-element-hover);
 			}
 		}
@@ -285,17 +285,17 @@
 			--btn-bg: var(--clr-theme-succ-soft);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.7;
 				--btn-bg: var(--clr-theme-succ-soft-hover);
 			}
 		}
 		&.solid {
-			--icon-opacity: 0.7;
+			--icon-opacity: 0.8;
 			--btn-text-clr: var(--clr-theme-succ-on-element);
 			--btn-bg: var(--clr-theme-succ-element);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.9;
 				--btn-bg: var(--clr-theme-succ-element-hover);
 			}
 		}
@@ -308,17 +308,17 @@
 			--btn-bg: var(--clr-theme-err-soft);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.7;
 				--btn-bg: var(--clr-theme-err-soft-hover);
 			}
 		}
 		&.solid {
-			--icon-opacity: 0.7;
+			--icon-opacity: 0.8;
 			--btn-text-clr: var(--clr-theme-err-on-element);
 			--btn-bg: var(--clr-theme-err-element);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.9;
 				--btn-bg: var(--clr-theme-err-element-hover);
 			}
 		}
@@ -331,17 +331,17 @@
 			--btn-bg: var(--clr-theme-warn-soft);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.7;
 				--btn-bg: var(--clr-theme-warn-soft-hover);
 			}
 		}
 		&.solid {
-			--icon-opacity: 0.7;
+			--icon-opacity: 0.8;
 			--btn-text-clr: var(--clr-theme-warn-on-element);
 			--btn-bg: var(--clr-theme-warn-element);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.9;
 				--btn-bg: var(--clr-theme-warn-element-hover);
 			}
 		}
@@ -354,17 +354,17 @@
 			--btn-bg: var(--clr-theme-purp-soft);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.7;
 				--btn-bg: var(--clr-theme-purp-soft-hover);
 			}
 		}
 		&.solid {
-			--icon-opacity: 0.7;
+			--icon-opacity: 0.8;
 			--btn-text-clr: var(--clr-theme-purp-on-element);
 			--btn-bg: var(--clr-theme-purp-element);
 			/* if button */
 			&:not(.not-clickable, &:disabled):hover {
-				--icon-opacity: 0.8;
+				--icon-opacity: 0.9;
 				--btn-bg: var(--clr-theme-purp-element-hover);
 			}
 		}
