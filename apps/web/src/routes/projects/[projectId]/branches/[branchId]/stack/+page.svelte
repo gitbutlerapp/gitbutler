1aaf2029659170fb2538a393813a81de6abd9746
Scott Chacon <schacon@gmail.com>
2024-10-04 10:49:06 +0200

    new review_all data

--- apps/web/src/routes/projects/[projectId]/branches/[branchId]/stack/+page.svelte
+++ apps/web/src/routes/projects/[projectId]/branches/[branchId]/stack/+page.svelte
@@ -76,9 +76,9 @@
 	<h2>Patches</h2>
 	{#each stackData.patches as patch}
 		<div class="patch">
-			{#if patch.review.rejected.length > 0}
+			{#if patch.review_all.rejected.length > 0}
 				<div class="patchHeader rejected">X</div>
-			{:else if patch.review.signed_off.length > 0}
+			{:else if patch.review_all.signed_off.length > 0}
 				<div class="patchHeader signoff">✓</div>
 			{:else}
 				<div class="patchHeader unreviewed">?</div>
@@ -104,23 +104,49 @@
 							.statistics.deletions}, Files: {patch.statistics.file_count}
 					</div>
 					<hr />
-					<div>
-						Viewed:
-						{#each patch.review.viewed as email}
-							<Gravatar {email} size={20} />
-						{/each}
-					</div>
-					<div>
-						Signed Off:
-						{#each patch.review.signed_off as email}
-							<Gravatar {email} size={20} />
-						{/each}
-					</div>
-					<div>
-						Rejected:
-						{#each patch.review.rejected as email}
-							<Gravatar {email} size={20} />
-						{/each}
+					<div class="columns">
+						<div class="column">
+							<h3>This Version</h3>
+							<div>
+								<div class="title">Viewed:</div>
+								{#each patch.review.viewed as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+							<div>
+								<div class="title">Signed Off:</div>
+								{#each patch.review.signed_off as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+							<div>
+								<div class="title">Rejected:</div>
+								{#each patch.review.rejected as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+						</div>
+						<div class="column">
+							<h3>All Versions</h3>
+							<div>
+								<div class="title">Viewed:</div>
+								{#each patch.review_all.viewed as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+							<div>
+								<div class="title">Signed Off:</div>
+								{#each patch.review_all.signed_off as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+							<div>
+								<div class="title">Rejected:</div>
+								{#each patch.review_all.rejected as email}
+									<Gravatar {email} size={20} />
+								{/each}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -134,6 +160,10 @@
 	}
 	h2 {
 		font-size: 1.5rem;
+	}
+	h3 {
+		font-size: 1.1rem;
+		font-weight: bold;
 	}
 	.columns {
 		display: flex;
@@ -168,5 +198,10 @@
 	.unreviewed {
 		background-color: rgb(215, 215, 144);
 		color: black;
+	}
+	.title {
+		min-width: 100px;
+		display: inline-block;
+		border-right: 1px solid #eee;
 	}
 </style>
