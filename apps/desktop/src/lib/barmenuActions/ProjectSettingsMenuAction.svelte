28fa6720b3b2e85e4adb188b76318390ec092d42
Ali Houssain Sareini <alihsareini@live.com>
2024-10-14 19:43:38 -0400

    refactor: Update code editor paths to use editor from userSettings

--- apps/desktop/src/lib/barmenuActions/ProjectSettingsMenuAction.svelte
+++ apps/desktop/src/lib/barmenuActions/ProjectSettingsMenuAction.svelte
@@ -2,15 +2,18 @@
 	import { listen } from '$lib/backend/ipc';
 	import { Project } from '$lib/backend/projects';
 	import { showHistoryView } from '$lib/config/config';
-	import { editor } from '$lib/editorLink/editorLink';
+	import { getContextStoreBySymbol } from '@gitbutler/shared/context';
+	import { SETTINGS, type Settings } from '$lib/settings/userSettings';
 	import * as events from '$lib/utils/events';
 	import { unsubscribe } from '$lib/utils/unsubscribe';
 	import { openExternalUrl } from '$lib/utils/url';
 	import { getContext } from '@gitbutler/shared/context';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import type { Writable } from 'svelte/store';
 
 	const project = getContext(Project);
+	const userSettings = getContextStoreBySymbol<Settings, Writable<Settings>>(SETTINGS);
 
 	onMount(() => {
 		const unsubscribeSettings = listen<string>('menu://project/settings/clicked', () => {
@@ -20,7 +23,7 @@
 		const unsubscribeOpenInVSCode = listen<string>(
 			'menu://project/open-in-vscode/clicked',
 			async () => {
-				const path = `${$editor}://file${project.vscodePath}?windowId=_blank`;
+				const path = `${$userSettings.defaultCodeEditor}://file${project.vscodePath}?windowId=_blank`;
 				openExternalUrl(path);
 			}
 		);
--- apps/desktop/src/lib/components/BoardEmptyState.svelte
+++ apps/desktop/src/lib/components/BoardEmptyState.svelte
@@ -2,21 +2,29 @@
 	import zenSvg from '$lib/assets/dzen-pc.svg?raw';
 	import { Project } from '$lib/backend/projects';
 	import { BaseBranch } from '$lib/baseBranch/baseBranch';
-	import { editor } from '$lib/editorLink/editorLink';
+	import { SETTINGS, type Settings } from '$lib/settings/userSettings';
 	import { getGitHost } from '$lib/gitHost/interface/gitHost';
 	import { openExternalUrl } from '$lib/utils/url';
 	import { BranchController } from '$lib/vbranches/branchController';
-	import { getContext, getContextStore } from '@gitbutler/shared/context';
+	import {
+		getContext,
+		getContextStore,
+		getContextStoreBySymbol
+	} from '@gitbutler/shared/context';
 	import Icon from '@gitbutler/ui/Icon.svelte';
+	import type { Writable } from 'svelte/store';
 
 	const gitHost = getGitHost();
 	const baseBranch = getContextStore(BaseBranch);
 	const branchController = getContext(BranchController);
+	const userSettings = getContextStoreBySymbol<Settings, Writable<Settings>>(SETTINGS);
 
 	const project = getContext(Project);
 
 	async function openInVSCode() {
-		openExternalUrl(`${$editor}://file${project.vscodePath}/?windowId=_blank`);
+		openExternalUrl(
+			`${$userSettings.defaultCodeEditor}://file${project.vscodePath}/?windowId=_blank`
+		);
 	}
 </script>
 
@@ -50,7 +58,8 @@
 						</div>
 						<button
 							class="empty-board__suggestions__link"
-							on:click={async () => await openExternalUrl('https://docs.gitbutler.com')}
+							on:click={async () =>
+								await openExternalUrl('https://docs.gitbutler.com')}
 						>
 							<div class="empty-board__suggestions__link__icon">
 								<Icon name="docs" />
@@ -66,7 +75,7 @@
 							<div class="empty-board__suggestions__link__icon">
 								<Icon name="vscode" />
 							</div>
-							<span class="text-12">Open in VSCode</span>
+							<span class="text-12">Open in Editor</span>
 						</button>
 					</div>
 				</div>
