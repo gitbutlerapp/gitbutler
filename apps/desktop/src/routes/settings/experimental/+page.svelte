88611b6ab5d45cb96d78d3aed8a48a652f9d07fc
Nico Domino <yo@ndo.dev>
2024-10-14 15:33:25 +0200

    fix: layout updates

--- apps/desktop/src/routes/settings/experimental/+page.svelte
+++ apps/desktop/src/routes/settings/experimental/+page.svelte
@@ -18,67 +18,83 @@
 		This sections contains a list of feature flags for features that are still in development or in
 		an experimental stage.
 	</p>
-	<SectionCard labelFor="baseBranchSwitching" orientation="row">
-		<svelte:fragment slot="title">Switching the target branch</svelte:fragment>
-		<svelte:fragment slot="caption">
-			This allows changing of the target branch after the initial project setup from within the
-			project settings. Not fully tested yet, use with caution.
-		</svelte:fragment>
-		<svelte:fragment slot="actions">
-			<Toggle
-				id="baseBranchSwitching"
-				checked={$baseBranchSwitching}
-				on:click={() => ($baseBranchSwitching = !$baseBranchSwitching)}
-			/>
-		</svelte:fragment>
-	</SectionCard>
-	<SectionCard labelFor="stackingFeature" orientation="row">
-		<svelte:fragment slot="title">Branch stacking UI</svelte:fragment>
-		<svelte:fragment slot="caption">
-			Enables the new user interface for managing lanes / stacks of branches.
-		</svelte:fragment>
-		<svelte:fragment slot="actions">
-			<Toggle
-				id="stackingFeature"
-				checked={$stackingFeature}
-				on:click={() => ($stackingFeature = !$stackingFeature)}
-			/>
-		</svelte:fragment>
-	</SectionCard>
-	<SectionCard labelFor="stackingFeatureMultipleSeries" orientation="row">
-		<svelte:fragment slot="title">Branch stacking multiple series</svelte:fragment>
-		<svelte:fragment slot="caption">
-			Enables the user to use the new stacking user interface to create multiple series (branches)
-			per lane.
-		</svelte:fragment>
-		<svelte:fragment slot="actions">
-			<Toggle
-				id="stackingFeatureMultipleSeries"
+
+	<div class="experimental-settings__toggles">
+		<SectionCard labelFor="baseBranchSwitching" orientation="row">
+			<svelte:fragment slot="title">Switching the target branch</svelte:fragment>
+			<svelte:fragment slot="caption">
+				This allows changing of the target branch after the initial project setup from within the
+				project settings. Not fully tested yet, use with caution.
+			</svelte:fragment>
+			<svelte:fragment slot="actions">
+				<Toggle
+					id="baseBranchSwitching"
+					checked={$baseBranchSwitching}
+					on:click={() => ($baseBranchSwitching = !$baseBranchSwitching)}
+				/>
+			</svelte:fragment>
+		</SectionCard>
+
+		<div class="stack-v">
+			<SectionCard labelFor="stackingFeature" orientation="row" roundedBottom={false}>
+				<svelte:fragment slot="title">Branch stacking UI</svelte:fragment>
+				<svelte:fragment slot="caption">
+					Enables the new user interface for managing lanes / stacks of branches.
+				</svelte:fragment>
+				<svelte:fragment slot="actions">
+					<Toggle
+						id="stackingFeature"
+						checked={$stackingFeature}
+						on:click={() => ($stackingFeature = !$stackingFeature)}
+					/>
+				</svelte:fragment>
+			</SectionCard>
+			<SectionCard
+				labelFor="stackingFeatureMultipleSeries"
+				orientation="row"
+				roundedTop={false}
 				disabled={!$stackingFeature}
-				checked={$stackingFeatureMultipleSeries}
-				on:click={() => ($stackingFeatureMultipleSeries = !$stackingFeatureMultipleSeries)}
-			/>
-		</svelte:fragment>
-	</SectionCard>
-	<SectionCard labelFor="topics" orientation="row">
-		<svelte:fragment slot="title">Topics</svelte:fragment>
-		<svelte:fragment slot="caption">
-			A highly experimental form of note taking / conversation. The form & function may change
-			drastically, and may result in lost notes.
-		</svelte:fragment>
-		<svelte:fragment slot="actions">
-			<Toggle
-				id="topics"
-				checked={$topicsEnabled}
-				on:click={() => ($topicsEnabled = !$topicsEnabled)}
-			/>
-		</svelte:fragment>
-	</SectionCard>
+			>
+				<svelte:fragment slot="title">Branch stacking multiple series</svelte:fragment>
+				<svelte:fragment slot="caption">
+					Enables the user to use the new stacking user interface to create multiple series
+					(branches) per lane.
+				</svelte:fragment>
+				<svelte:fragment slot="actions">
+					<Toggle
+						id="stackingFeatureMultipleSeries"
+						checked={$stackingFeatureMultipleSeries}
+						on:click={() => ($stackingFeatureMultipleSeries = !$stackingFeatureMultipleSeries)}
+					/>
+				</svelte:fragment>
+			</SectionCard>
+		</div>
+		<SectionCard labelFor="topics" orientation="row">
+			<svelte:fragment slot="title">Topics</svelte:fragment>
+			<svelte:fragment slot="caption">
+				A highly experimental form of note taking / conversation. The form & function may change
+				drastically, and may result in lost notes.
+			</svelte:fragment>
+			<svelte:fragment slot="actions">
+				<Toggle
+					id="topics"
+					checked={$topicsEnabled}
+					on:click={() => ($topicsEnabled = !$topicsEnabled)}
+				/>
+			</svelte:fragment>
+		</SectionCard>
+	</div>
 </SettingsPage>
 
 <style>
 	.experimental-settings__text {
 		color: var(--clr-text-2);
-		margin-bottom: 12px;
+		margin-bottom: 10px;
+	}
+
+	.experimental-settings__toggles {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
 	}
 </style>
