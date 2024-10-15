6258ae9970b4825c8f135382c675d0436fc3c22b
ndom91 <yo@ndo.dev>
2024-10-15 15:19:13 +0200

    feat: first attempt at dropzoneManager for multiple series

--- apps/desktop/src/lib/stack/StackSeries.svelte
+++ apps/desktop/src/lib/stack/StackSeries.svelte
@@ -21,9 +21,10 @@
 		$localAndRemoteCommits.some((commit) => commit.conflicted)
 	);
 
+	console.log('stackSeries.commits', { local: branch.localCommits, remote: branch.remoteCommits });
 	const reorderDropzoneManagerFactory = getContext(ReorderDropzoneManagerFactory);
 	const reorderDropzoneManager = $derived(
-		reorderDropzoneManagerFactory.build(branch, [...branch.localCommits, ...branch.remoteCommits])
+		reorderDropzoneManagerFactory.build({ branch, series: branch.series })
 	);
 </script>
 
