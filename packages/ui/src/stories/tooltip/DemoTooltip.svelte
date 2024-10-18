146d73ba9c55fc50ed7359a5daebdb37ffa2d659
Pavel Laptev <pawellaptew@gmail.com>
2024-10-18 03:14:00 +0200

    tooltip fix

--- packages/ui/src/stories/tooltip/DemoTooltip.svelte
+++ packages/ui/src/stories/tooltip/DemoTooltip.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Tooltip from '$lib/Tooltip.svelte';
+	import Icon from '$lib/Icon.svelte';
 
 	const props = $props();
 </script>
@@ -10,6 +11,26 @@
 			<span class="tooltip-text">tooltip</span>
 		</Tooltip> for you.
 	</p>
+</div>
+
+<div class="menu">
+	<Tooltip text={props.text} align={props.align} position={props.position}>
+		<button class="menu-btn">
+			<Icon name="info" />
+		</button>
+	</Tooltip>
+
+	<Tooltip text={props.text} align={props.align} position={props.position}>
+		<button class="menu-btn">
+			<Icon name="plus" />
+		</button>
+	</Tooltip>
+
+	<Tooltip text={props.text} align={props.align} position={props.position}>
+		<button class="menu-btn">
+			<Icon name="ai" />
+		</button>
+	</Tooltip>
 </div>
 
 <style>
@@ -26,5 +47,17 @@
 	.tooltip-text {
 		text-decoration: underline;
 		text-decoration-style: dotted;
+	}
+
+	.menu {
+		transform: translateX(220px);
+		display: flex;
+		border: 1px solid var(--clr-border-2);
+		border-radius: var(--radius-m);
+		width: fit-content;
+	}
+
+	.menu-btn {
+		padding: 4px;
 	}
 </style>
