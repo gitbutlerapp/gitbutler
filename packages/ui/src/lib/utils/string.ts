3eed596cc8bb2e2193e3c1c8ac0d669ecc22a6f1
ndom91 <yo@ndo.dev>
2024-10-22 10:39:52 +0200

    fix: replace lookbehind regex with for loop

--- packages/ui/src/lib/utils/string.ts
+++ packages/ui/src/lib/utils/string.ts
@@ -1,8 +1,14 @@
 export function camelCaseToTitleCase(input: string) {
-	return input
-		.split(/(?<![A-Z])(?=[A-Z])/)
-		.map(function (word: string) {
-			return word.charAt(0).toUpperCase() + word.slice(1);
-		})
-		.join(' ');
+	let result = input.charAt(0).toUpperCase();
+	for (let i = 1; i < input.length; i++) {
+		if (
+			input.charAt(i) === input.charAt(i).toUpperCase() &&
+			input.charAt(i) !== input.charAt(i).toLowerCase()
+		) {
+			result += ' ' + input.charAt(i);
+		} else {
+			result += input.charAt(i);
+		}
+	}
+	return result;
 }
