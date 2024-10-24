7e1a5c54f25f1d9a3e575138c1adf31213a86aae
Kiril Videlov <kiril@videlov.com>
2024-10-24 16:27:35 +0200

    Implement new stackign api

--- crates/gitbutler-tauri/src/main.rs
+++ crates/gitbutler-tauri/src/main.rs
@@ -177,6 +177,7 @@ fn main() {
                     virtual_branches::commands::undo_commit,
                     virtual_branches::commands::insert_blank_commit,
                     virtual_branches::commands::reorder_commit,
+                    virtual_branches::commands::reorder_stack,
                     virtual_branches::commands::update_commit_message,
                     virtual_branches::commands::list_local_branches,
                     virtual_branches::commands::list_branches,
