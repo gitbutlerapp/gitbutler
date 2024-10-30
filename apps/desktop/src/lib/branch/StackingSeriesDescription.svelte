fb0fa7eb9a9cbf8d11c097456b77bc0a32b9532f
Pavel Laptev <pawellaptew@gmail.com>
2024-10-30 13:27:36 +0100

    fix styles and words wrap

--- apps/desktop/src/lib/branch/StackingSeriesDescription.svelte
+++ apps/desktop/src/lib/branch/StackingSeriesDescription.svelte
@@ -45,7 +45,7 @@
 		display: flex;
 		flex-direction: column;
 		color: var(--clr-text-2);
-		padding: 2px;
+		padding: 0 2px;
 		border: 1px solid transparent;
 		border-radius: var(--radius-s);
 		width: calc(100% + 4px);
