992a0a707ca0e0e947831ec81976f4214a7582b5
Pavel Laptev <pawellaptew@gmail.com>
2024-10-31 12:10:56 +0100

    Delete autoHeight.ts

--- packages/ui/src/lib/utils/autoHeight.ts
+++
@@ -1,9 +0,0 @@
-export function autoHeight(element: HTMLTextAreaElement) {
-	if (!element) return;
-
-	const elementBorder =
-		parseInt(getComputedStyle(element).borderTopWidth) +
-		parseInt(getComputedStyle(element).borderBottomWidth);
-	element.style.height = 'auto';
-	element.style.height = `${element.scrollHeight + elementBorder}px`;
-}
