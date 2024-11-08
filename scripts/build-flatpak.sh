1c72b41e99a97cb585f7b8b083474b79106e11f9
ndom91 <yo@ndo.dev>
2024-11-08 15:39:38 +0100

    chore: add flake deps and build-flatpak helper script

---
+++ scripts/build-flatpak.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+MANIFEST_PATH="crates/gitbutler-tauri/flatpak/manifest.flatpak.yml"
+FLATPAK_ID="com.gitbutler.app"
+
+# Add Flathub Repository
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install Gnome runtimei dependency
+flatpak install --user flathub org.gnome.Platform//46 org.gnome.Sdk//46
+
+# Build the manifest
+flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo builddir "$MANIFEST_PATH"
+
+# Export gitbutler-tauri.flatpak
+flatpak build-bundle repo "$FLATPAK_ID.flatpak" "$FLATPAK_ID"
+
+# Notes
+# - https://tauri.app/distribute/flatpak/
+# - https://docs.flatpak.org/en/latest/first-build.html
+# - https://github.com/madeofpendletonwool/PinePods/blob/5377b061357d1ba5e04e607b5f626bf349cf09f8/.github/workflows/build-flatpak.yml#L4
+# - https://github.com/Beanow/hi-flatpak
+# - https://github.com/flathub/com.vscodium.codium-insiders/blob/master/com.vscodium.codium-insiders.yaml
