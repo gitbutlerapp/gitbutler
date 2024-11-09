a4d4ec57a66f91d82781ff503dd7c51e74f6120f
Kiril Videlov <kiril@videlov.com>
2024-11-09 13:59:58 +0100

    refactor: remove unused struct Series

--- crates/gitbutler-stack/src/lib.rs
+++ crates/gitbutler-stack/src/lib.rs
@@ -12,8 +12,6 @@ pub use state::{VirtualBranches as VirtualBranchesState, VirtualBranchesHandle};
 pub use target::Target;
 
 mod heads;
-mod series;
-pub use series::Series;
 pub use stack::{commit_by_oid_or_change_id, CommitsForId, PatchReferenceUpdate, TargetUpdate};
 
 mod patch_reference;
