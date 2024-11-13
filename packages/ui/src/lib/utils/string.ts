2291e38a82fa94a3e79fe2801e65ac189521a07d
Mattias Granlund <mtsgrd@gmail.com>
2024-11-13 11:31:17 +0100

    Include repo domain and hash in posthog events
    - so we can understand and prioritise forge support

--- packages/ui/src/lib/utils/string.ts
+++ packages/ui/src/lib/utils/string.ts
@@ -13,18 +13,15 @@ export function camelCaseToTitleCase(input: string) {
 	return result;
 }
 
-export function hashCode(s: string) {
-	let hash = 0;
-	let chr;
-	let i;
+export function hashCode(value: string) {
+	let hash = 5381; // Starting with a prime number
 
-	if (s.length === 0) return hash.toString();
-	for (i = 0; i < s.length; i++) {
-		chr = s.charCodeAt(i);
-		hash = (hash << 5) - hash + chr;
-		hash |= 0; // Convert to 32bit integer
+	for (let i = 0; i < value.length; i++) {
+		hash = (hash * 33) ^ value.charCodeAt(i); // Bitwise XOR and multiplication
 	}
-	return hash.toString();
+
+	// Convert to an unsigned 32-bit integer, then to a hexadecimal string
+	return (hash >>> 0).toString(16);
 }
 
 export function isChar(char: string) {
