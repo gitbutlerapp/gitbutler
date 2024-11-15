55d6fde463d6991a8eed0bc42f432031a3ac8542
ndom91 <yo@ndo.dev>
2024-11-15 15:38:08 +0100

    feat: remove storybook test

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.(test|spec).?(m)[jt]s?(x)']
+	}
 });
