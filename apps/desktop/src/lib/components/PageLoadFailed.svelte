27b9d45a32c55befcf394ee6fcf475c6acd2b1e0
Pavel Laptev <pawellaptew@gmail.com>
2024-11-24 03:39:38 +0100

    UI: PageLoadFailed design update

--- apps/desktop/src/lib/components/PageLoadFailed.svelte
+++ apps/desktop/src/lib/components/PageLoadFailed.svelte
@@ -1,83 +1,67 @@
 <script lang="ts">
-	import loadErrorSvg from '$lib/assets/illustrations/load-error.svg?raw';
-	import InfoMessage from '$lib/shared/InfoMessage.svelte';
+	import loadErrorSvg from '$lib/assets/empty-state/load-error.svg?raw';
+	import Button from '@gitbutler/ui/Button.svelte';
 
-	export let error: any = undefined;
+	const { error } = $props();
 </script>
 
-<div class="decorative-split-view">
-	<div class="left-side">
-		<div class="left-side__content">
-			<h2 class="problem__title text-18 text-body text-bold">
-				There was a problem loading this page
-			</h2>
-
-			<InfoMessage filled outlined={false} style="error" icon="info">
-				<svelte:fragment slot="content">
-					{error ? error : 'An unknown error occurred'}
-				</svelte:fragment>
-			</InfoMessage>
+<div class="page-load-error">
+	<div class="wrapper">
+		<div class="image">
+			{@html loadErrorSvg}
 		</div>
-	</div>
-	<div class="right-side">
-		<div class="right-side-wrapper">
-			<div class="img-wrapper">
-				{@html loadErrorSvg}
-			</div>
+		<h2 class="title text-15 text-body text-bold">Failed to load page</h2>
+		<p class="error-message text-12 text-body">
+			{error ? error : 'An unknown error occurred'}
+		</p>
+		<div class="actions">
+			<Button
+				color="primary"
+				outline
+				style="ghost"
+				icon="update"
+				onclick={() => {
+					location.reload();
+				}}>Reload view</Button
+			>
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
-	.decorative-split-view {
-		cursor: default;
-		user-select: none;
+	.page-load-error {
 		display: flex;
-		flex-grow: 1;
-		background-color: var(--clr-bg-1);
-	}
-	.right-side {
-		display: flex;
-		flex-direction: column;
-		position: relative;
-	}
-
-	.left-side {
-		display: grid;
 		align-items: center;
-		padding: 40px 80px;
-		flex: 1.3;
-		background-color: var(--clr-bg-1);
-		overflow-y: auto;
-	}
-	.left-side__content {
-		display: flex;
-		flex-direction: column;
-		margin: 0 auto;
-		width: 100%;
-		max-width: 512px;
-	}
-
-	.problem__title {
-		color: var(--clr-scale-ntrl-30);
-	}
-	.img-wrapper {
-		flex: 1;
-		width: 100%;
-		max-width: 440px;
-		overflow: hidden;
-		padding: 0 24px;
-	}
-	.right-side-wrapper {
-		overflow: hidden;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+		justify-content: center;
 		height: 100%;
-		border-radius: 8px;
-	}
-	.problem__title {
-		color: var(--clr-scale-ntrl-30);
-		margin-bottom: 12px;
+		width: 100%;
+
+		& .wrapper {
+			display: flex;
+			flex-direction: column;
+			max-width: 320px;
+			margin-bottom: 20px;
+		}
+
+		& .image {
+			width: 120px;
+			height: 100px;
+			margin-bottom: 24px;
+		}
+
+		& .title {
+			margin-bottom: 8px;
+		}
+
+		& .error-message {
+			color: var(--clr-text-2);
+			white-space: pre-wrap;
+			/* text-wrap: balance; */
+			margin-bottom: 12px;
+		}
+
+		.actions {
+			display: flex;
+		}
 	}
 </style>
