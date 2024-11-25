0528a5540f86fe9067e767228658be1241b65c2b
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-11-25 15:47:14 +0100

    When watching for changes, do not ignore files that are also tracked. (#5662)

--- crates/gitbutler-watcher/src/file_monitor.rs
+++ crates/gitbutler-watcher/src/file_monitor.rs
@@ -157,11 +157,13 @@ pub fn spawn(
                                 ) {
                                     for (file_path, kind) in classified_file_paths.iter_mut() {
                                         if let Ok(relative_path) = file_path.strip_prefix(&worktree_path) {
-                                            if excludes
+                                            let is_excluded = excludes
                                                 .at_path(relative_path, None)
                                                 .map(|platform| platform.is_excluded())
-                                                .unwrap_or(false)
-                                            {
+                                                .unwrap_or(false);
+                                            let is_untracked =
+                                                || index.entry_by_path(&gix::path::to_unix_separators_on_windows(gix::path::into_bstr(relative_path))).is_none();
+                                            if is_excluded && is_untracked() {
                                                 *kind = FileKind::ProjectIgnored
                                             }
                                         }
