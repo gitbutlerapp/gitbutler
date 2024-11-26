b24a124b49cf96cf9ec9bf944a63fc29c03954e1
Pavel Laptev <pawellaptew@gmail.com>
2024-11-26 04:37:17 +0100

    modify image section

--- app/components/ImageSection.tsx
+++ app/components/ImageSection.tsx
@@ -9,13 +9,14 @@ interface Props {
   subtitle?: string
 }
 
-export default function ImageSection({ width, height, className, alt, src, subtitle }: Props) {
+export default function ImageSection({ alt, src, subtitle }: Props) {
   return (
-    <div className="mx-auto mb-4 flex max-w-2xl flex-col justify-start rounded-lg border border-neutral-200 bg-neutral-100 p-2 dark:border-neutral-800 dark:bg-neutral-900 [&>span]:w-fit [&_img]:m-0">
+    <div className="mx-auto mb-4 flex flex-col justify-start rounded-lg border border-neutral-200 bg-neutral-100 p-2 dark:border-neutral-800 dark:bg-neutral-900 [&>span]:w-fit [&_img]:m-0">
       <ImageZoom
-        width={width}
-        height={height}
-        className={`min-w-content mx-auto w-auto max-w-full rounded-md ${className ?? ""}`}
+        width="0"
+        height="0"
+        className="h-auto w-full rounded-md"
+        sizes="(min-width: 808px) 50vw, 100vw"
         alt={alt ?? ""}
         src={src}
       />
