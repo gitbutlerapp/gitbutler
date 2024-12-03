70a2f8f74aca4db2e06aa088dff375529779ebc9
Pavel Laptev <pawellaptew@gmail.com>
2024-12-03 13:15:18 +0100

    linkButton update

---
+++ packages/ui/src/stories/linkButton/LinkButton.stories.svelte
@@ -0,0 +1,24 @@
+<script module lang="ts">
+	import LinkButton from '$lib/LinkButton.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Inputs / Link Button',
+		component: LinkButton,
+		args: {
+			icon: 'copy-small',
+			onclick: () => {
+				console.log('LinkButton clicked');
+			}
+		},
+		argTypes: {
+			icon: {
+				control: {
+					type: 'select'
+				}
+			}
+		}
+	});
+</script>
+
+<Story name="Default">Link button</Story>
