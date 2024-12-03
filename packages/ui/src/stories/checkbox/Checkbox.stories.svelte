e096d68182e49fc68a0d4958a99abb322a0a49e3
Pavel Laptev <pawellaptew@gmail.com>
2024-12-03 13:15:05 +0100

    checkbox story

---
+++ packages/ui/src/stories/checkbox/Checkbox.stories.svelte
@@ -0,0 +1,18 @@
+<script module lang="ts">
+	import Checkbox from '$lib/Checkbox.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Inputs / Checkbox',
+		component: Checkbox,
+		args: {
+			style: 'default',
+			checked: false,
+			disabled: false,
+			indeterminate: false,
+			small: false
+		}
+	});
+</script>
+
+<Story name="Default" />
