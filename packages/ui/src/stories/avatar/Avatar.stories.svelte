37aa73ebba6d880a5fe0be0ba490f6efbe43903a
Pavel Laptev <pawellaptew@gmail.com>
2024-12-08 21:55:10 +0100

    update avatar stories

---
+++ packages/ui/src/stories/avatar/Avatar.stories.svelte
@@ -0,0 +1,24 @@
+<script module lang="ts">
+	import Avatar from '$lib/avatar/Avatar.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Avatar / Avatar',
+		component: Avatar,
+		args: {
+			srcUrl: 'https://gravatar.com/avatar/f43ef760d895a84ca7bb35ff6f4c6b7c',
+			tooltip: 'The avatar of bob',
+			size: 'medium'
+		},
+		argTypes: {
+			size: {
+				control: {
+					type: 'select',
+					options: ['small', 'medium', 'large']
+				}
+			}
+		}
+	});
+</script>
+
+<Story name="Default" />
