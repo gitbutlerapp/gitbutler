853b9fd953fd64178213686223568e348e6d21a3
Scott Chacon <schacon@gmail.com>
2024-12-08 12:38:21 +0100

    better downloads page
    show latest release at the top and more underneath

--- apps/web/src/routes/downloads/+page.svelte
+++ apps/web/src/routes/downloads/+page.svelte
@@ -5,18 +5,53 @@
 	let loading = $state(true);
 	let releases: any[] = $state([]);
 	let nightlies: any[] = $state([]);
+	let latest: any;
+	let build: any = {};
 
 	onMount(() => {
 		fetch('https://gitbutler.com/api/downloads?limit=4&channel=release')
 			.then(async (response) => await response.json())
 			.then((data) => {
 				releases = data.filter((release: any) => release.channel === 'release');
+				releases.forEach((release: any) => {
+					release.builds = release.builds.filter((build: any) => !build.url.endsWith('.zip'));
+					// and deduplicate by url
+					release.builds = release.builds.filter((build: any, index: number, self: any) => self.findIndex((b: any) => b.url === build.url) === index);
+					// and sort by platform, reverse order
+					release.builds.sort((a: any, b: any) => b.platform.localeCompare(a.platform));
+				});
+
+				latest = releases[0];
+				console.log(latest);
+
+				// find mac builds
+				build['darwin_x86_64'] = latest.builds.find((build: any) => build.os === 'darwin' && build.arch === 'x86_64');
+				build['darwin_aarch64'] = latest.builds.find((build: any) => build.os === 'darwin' && build.arch === 'aarch64');
+
+				// find windows builds
+				build['windows_x86_64'] = latest.builds.find((build: any) => build.os === 'windows' && build.arch === 'x86_64');
+
+				// find linux builds
+				build['linux_appimage'] = latest.builds.find((build: any) => build.os === 'linux' && build.file.includes('AppImage'));
+				build['linux_deb'] = latest.builds.find((build: any) => build.os === 'linux' && build.file.includes('deb'));
+				build['linux_rpm'] = latest.builds.find((build: any) => build.os === 'linux' && build.file.includes('rpm'));
+
 				loading = false;
 			});
+
 		fetch('https://gitbutler.com/api/downloads?limit=9&channel=nightly')
 			.then(async (response) => await response.json())
 			.then((data) => {
 				nightlies = data.filter((release: any) => release.channel === 'nightly');
+				// filter out any build urls that end in zip
+				nightlies.forEach((release: any) => {
+					release.builds = release.builds.filter((build: any) => !build.url.endsWith('.zip'));
+					// and deduplicate by url
+					release.builds = release.builds.filter((build: any, index: number, self: any) => self.findIndex((b: any) => b.url === build.url) === index);
+					// and sort by platform, reverse order
+					release.builds.sort((a: any, b: any) => b.platform.localeCompare(a.platform));
+				});
+
 				loading = false;
 			});
 	});
@@ -29,51 +64,139 @@
 {#if loading}
 	<p>Loading...</p>
 {:else}
-	<div class="releases">
-		<div class="release-lane">
-			<h2>Stable Releases</h2>
-			{#each releases as release}
-				<div class="release">
-					<div class="release__version">
-						Version: <b>{release.version}</b> <span class="release__sha">{release.sha.substring(0, 6)}</span>
+	<div class="downloads">
+		<h1>Latest Release</h1>
+		<div class="current-release">
+			<div class="current-release__group">
+				<div>
+					<img src="/images/icon.png" width="200px" alt="GitButler"/>
+				</div>
+				<div class="current-builds">
+					<div class="version">
+						<div class="current__version">{latest.version}</div> 
+						<div class="current__version-date">{latest.released_at.substring(0, 10)}</div>
 					</div>
-					<div>Released: {new Date(release.released_at).toLocaleString()}</div>
-					{#if release.notes}
-						<div class="release__notes dotted">{@html marked(release.notes)}</div>
-					{/if}
-					<div class="builds">
-						{#each release.builds as build}
-							<li><a class="linked" href={build.url}>{build.platform}</a></li>
-						{/each}
+					<div class="current-builds-group">
+						<div>
+							<div class="os windows">
+								<div class="os__name">
+								<img
+										class="os-select__section-os-icon"
+										src="/images/os-icons/windows-small-logo.svg"
+										alt=""
+									/>
+								Windows
+								</div>
+								<div class="os__downloads">
+									{#if build['windows_x86_64']}
+										<a href={build['windows_x86_64'].url}>Download Windows (MSI)</a>
+									{/if}
+								</div>
+							</div>
+							<div class="os apple">
+								<div class="os__name">
+									<img
+											class="os-select__section-os-icon"
+											src="/images/os-icons/apple-small-logo.svg"
+											alt=""
+										/>
+									macOS
+								</div>
+								<div class="os__downloads">
+									{#if build['darwin_x86_64']}
+										<a href={build['darwin_x86_64'].url}>Download Intel</a>
+									{/if}
+									{#if build['darwin_aarch64']}
+										<a href={build['darwin_aarch64'].url}>Download Apple Silicon</a>
+									{/if}
+								</div>
+							</div>
+						</div>
+			<div>
+				<div class="os linux">
+					<div class="os__name">
+						<img
+								class="os-select__section-os-icon"
+								src="/images/os-icons/linux-small-logo.svg"
+								alt=""
+							/>
+						Linux
+					</div>
+					<div class="os__downloads">
+						{#if build['linux_appimage']}
+							<a href={build['linux_appimage'].url}>Download AppImage</a>
+						{/if}
+						{#if build['linux_deb']}
+							<a href={build['linux_deb'].url}>Download Deb</a>
+						{/if}
+						{#if build['linux_rpm']}
+							<a href={build['linux_rpm'].url}>Download RPM</a>
+						{/if}
 					</div>
 				</div>
-				<hr />
-			{/each}
+			</div>
+					</div>
+				</div>
+			</div>
+
 		</div>
-		<div class="release-lane">
-			<h2>Nightly Releases</h2>
-			{#each nightlies as release}
-				<div class="release">
-					<div class="release__version">
-						Version: <b>{release.version}</b> <span class="release__sha">{release.sha.substring(0, 6)}</span>
+
+		<h1>All Recent Releases</h1>
+		<div class="releases">
+			<div class="release-lane">
+				<h2>Stable Releases</h2>
+				{#each releases as release}
+					<div class="release">
+						<div class="release__version">
+							Version: <b>{release.version}</b> <span class="release__sha">{release.sha.substring(0, 6)}</span>
+						</div>
+						<div>Released: {new Date(release.released_at).toLocaleString()}</div>
+						{#if release.notes}
+							<div class="release__notes dotted">{@html marked(release.notes)}</div>
+						{/if}
+						<div class="builds">
+							{#each release.builds as build}
+								<li><a class="linked" href={build.url}>{build.platform}</a></li>
+							{/each}
+						</div>
 					</div>
-					<div>Released: {new Date(release.released_at).toLocaleString()}</div>
-					{#if release.notes}
-						<div class="release__notes dotted">{@html marked(release.notes)}</div>
-					{/if}
-					<div class="builds">
-						{#each release.builds as build}
-							<li><a class="linked" href={build.url}>{build.platform}</a></li>
-						{/each}
-					</div>
+					<hr />
+				{/each}
+			</div>
+			<div class="release-lane">
+				<h2>Nightly Releases</h2>
+				<div class="nightly-warning">
+					These are nightly builds that are automatically built from the master
+					branch each night and may be unstable.
 				</div>
-				<hr />
-			{/each}
+				{#each nightlies as release}
+					<div class="release">
+						<div class="release__version">
+							Version: <b>{release.version}</b> <span class="release__sha">{release.sha.substring(0, 6)}</span>
+						</div>
+						<div>Released: {new Date(release.released_at).toLocaleString()}</div>
+						{#if release.notes}
+							<div class="release__notes dotted">{@html marked(release.notes)}</div>
+						{/if}
+						<div class="builds">
+							{#each release.builds as build}
+								<li><a class="linked" href={build.url}>{build.platform}</a></li>
+							{/each}
+						</div>
+					</div>
+					<hr />
+				{/each}
+			</div>
 		</div>
 	</div>
 {/if}
 
 <style>
+	h1 {
+		font-size: 2rem;
+		margin-bottom: 1rem;
+	}
+
 	h2 {
 		margin-bottom: 1rem;
 		font-size: 1.5rem;
@@ -82,6 +205,82 @@
 	hr {
 		margin-block: 1rem;
 		opacity: 0.25;
+	}
+
+	.current-release {
+		display: flex;
+		flex-direction: row;
+		gap: 32px;
+		margin-bottom: 60px;
+		padding-bottom: 60px;
+		border-bottom: 1px solid #ccc;
+		border-style: dashed;
+	}
+
+	.current-builds {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.current-builds-group {
+		display: flex;
+		flex-direction: row;
+		gap: 1.8rem;
+	}
+
+	.version {
+		display: flex;
+		margin-top: 18px;
+		flex-direction: row;
+		align-items: baseline;
+		gap: 12px;
+	}
+
+	.current__version {
+		font-size: 1.5rem;
+	}
+
+	.current__version-date {
+		font-size: 0.8rem;
+		color: #777;
+	}
+
+	.current-release__group {
+		display: flex;
+		flex-direction: row;
+		gap: 32px;
+	}
+
+	.downloads {
+		padding: 20px 30px;
+	}
+
+	.os {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		margin-bottom: 1.5rem;
+	}
+
+	.os__name {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 8px;
+		color: #777;
+	}
+
+	.os__downloads {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		padding: 8px;
+	}
+
+	.os__downloads a {
+		color: #44c;
+		text-decoration: underline;
 	}
 
 	.builds {
@@ -123,8 +322,17 @@
 		width: calc(50% - 3rem);
 		margin: 1rem;
 		padding: 1rem;
-		border: 1px solid #cccccca9;
+		border: 1px solid #ccc;
 		border-radius: 0.5rem;
+	}
+
+	.nightly-warning {
+		font-size: 0.8rem;
+		color: #777;
+		background-color: #fdd;
+		border-radius: 8px;
+		padding: 1rem;
+		margin-bottom: 20px;
 	}
 
 </style>
