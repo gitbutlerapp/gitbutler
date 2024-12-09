b70741f15c801c8d6e43680cd4232a35b3853cc4
Pavel Laptev <pawellaptew@gmail.com>
2024-12-09 12:49:05 +0100

    story update: icon stories

--- packages/ui/src/stories/icon/AllIcon.stories.svelte
+++ packages/ui/src/stories/icon/AllIcon.stories.svelte
@@ -1,20 +1,27 @@
-<script lang="ts">
+<script module lang="ts">
 	import Icon from '$lib/Icon.svelte';
 	import iconsJson from '$lib/data/icons.json';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Basic / Icon'
+	});
 
 	let icons = Object.keys(iconsJson) as (keyof typeof iconsJson)[];
 </script>
 
-<div class="icons">
-	{#each icons as icon}
-		<div class="icon-item">
-			<Icon name={icon} />
-			<span class="text-11 icon-label">
-				{icon}
-			</span>
-		</div>
-	{/each}
-</div>
+<Story name="All">
+	<div class="icons">
+		{#each icons as icon}
+			<div class="icon-item">
+				<Icon name={icon} />
+				<span class="text-11 icon-label">
+					{icon}
+				</span>
+			</div>
+		{/each}
+	</div>
+</Story>
 
 <style>
 	.icons {
