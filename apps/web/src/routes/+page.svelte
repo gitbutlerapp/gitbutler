24f884c83b59d8a52c3fc8c8d75f3adad198e906
ndom91 <yo@ndo.dev>
2024-12-10 17:33:23 +0100

    feat: move all marketing homepage stuff to own dir

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { AuthService } from '$lib/auth/authService';
-	import LandingPage from '$lib/components/LandingPage.svelte';
+	import LandingPage from './home/components/LandingPage.svelte';
 	import UserDashboard from '$lib/components/UserDashboard.svelte';
 	import { getContext } from '@gitbutler/shared/context';
 	import { page } from '$app/stores';
