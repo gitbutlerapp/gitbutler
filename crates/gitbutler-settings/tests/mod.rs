53b4df367f5716c7627d61d8f287e54ed5f5d9ab
Kiril Videlov <kiril@videlov.com>
2024-12-19 12:58:29 +0100

    Update app setting schema

--- crates/gitbutler-settings/tests/mod.rs
+++ crates/gitbutler-settings/tests/mod.rs
@@ -8,7 +8,7 @@ fn test_load_settings() {
     assert_eq!(settings.telemetry.app_metrics_enabled, false); // modified
     assert_eq!(settings.telemetry.app_error_reporting_enabled, true); // default
     assert_eq!(settings.telemetry.app_non_anon_metrics_enabled, false); // default
-    assert_eq!(settings.telemetry.app_analytics_confirmed, false); // default
+    assert_eq!(settings.onboarding_complete, false); // default
     assert_eq!(
         settings.github_oauth_app.oauth_client_id,
         "cd51880daa675d9e6452"
