33133582e484393b63b5be16ce956a7459a54a46
ndom91 <yo@ndo.dev>
2025-01-08 15:48:04 +0100

    fix: bump vitest

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
