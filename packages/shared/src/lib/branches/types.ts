5e232924295cb7b9c30a5b69cfa900318d7afa70
Caleb Owens <caleb@gitbutler.com>
2025-01-09 15:21:34 +0100

    Add types

---
+++ packages/shared/src/lib/branches/types.ts
@@ -0,0 +1,140 @@
+export type ApiPatchStatistics = {
+	file_count: number;
+	section_count: number;
+	lines: number;
+	deletions: number;
+	files: string[];
+};
+
+export type PatchStatistics = {
+	fileCount: number;
+	sectionCount: number;
+	lines: number;
+	deletions: number;
+	files: string[];
+};
+
+export function apiToPatchStatistics(api: ApiPatchStatistics): PatchStatistics {
+	return {
+		fileCount: api.file_count,
+		sectionCount: api.section_count,
+		lines: api.lines,
+		deletions: api.deletions,
+		files: api.files
+	};
+}
+
+export type ApiPatchReview = {
+	viewed: string[];
+	signed_off: string[];
+	rejected: string[];
+};
+
+export type PatchReview = {
+	viewed: string[];
+	signedOff: string[];
+	rejected: string[];
+};
+
+export function apiToPatchReview(api: ApiPatchReview): PatchReview {
+	return {
+		viewed: api.viewed,
+		signedOff: api.signed_off,
+		rejected: api.rejected
+	};
+}
+
+export type ApiPatch = {
+	change_id: string;
+	commit_sha: string;
+	// patch_sha: string; Not sure this is real
+	title?: string;
+	description?: string;
+	position?: number;
+	version?: number;
+	contributors: string[];
+	statistics: ApiPatchStatistics;
+	review: ApiPatchReview;
+	review_all: ApiPatchReview;
+};
+
+export type Patch = {
+	changeId: string;
+	commitSha: string;
+	// patch_sha: string; Not sure this is real
+	title?: string;
+	description?: string;
+	position?: number;
+	version?: number;
+	contributors: string[];
+	statistics: PatchStatistics;
+	review: PatchReview;
+	reviewAll: PatchReview;
+};
+
+export function apiToPatch(api: ApiPatch): Patch {
+	return {
+		changeId: api.change_id,
+		commitSha: api.commit_sha,
+		title: api.title,
+		description: api.description,
+		position: api.position,
+		version: api.version,
+		contributors: api.contributors,
+		statistics: apiToPatchStatistics(api.statistics),
+		review: apiToPatchReview(api.review),
+		reviewAll: apiToPatchReview(api.review_all)
+	};
+}
+
+export const enum BranchStatus {
+	Active = 'active',
+	Inactive = 'inactive',
+	Closed = 'closed',
+	Loading = 'loading',
+	All = 'all'
+}
+
+export type ApiBranch = {
+	branch_id: string;
+	oplog_sha?: string;
+	uuid: string;
+	title?: string;
+	description?: string;
+	status?: BranchStatus;
+	version?: number;
+	created_at: string;
+	stack_size?: number;
+	contributors: string[];
+	patches: ApiPatch[];
+};
+
+export type Branch = {
+	branchId: string;
+	oplogSha?: string;
+	uuid: string;
+	title?: string;
+	description?: string;
+	status?: BranchStatus;
+	version?: number;
+	created_at: string;
+	stackSize?: number;
+	contributors: string[];
+	patch_ids: string[];
+};
+
+export function apiToBranch(api: ApiBranch): Branch {
+	return {
+		branchId: api.branch_id,
+		oplogSha: api.oplog_sha,
+		uuid: api.uuid,
+		title: api.title,
+		description: api.description,
+		status: api.status,
+		version: api.version,
+		created_at: api.created_at,
+		stackSize: api.stack_size,
+		contributors: api.contributors,
+		patch_ids: api.patches.map((patch) => patch.change_id)
+	};
+}
