7a975b1645466c030a744f691d637105ba32c42a
estib <stron@me.com>
2025-01-13 17:56:19 +0100

    Add Skeletons for the project pages
    Add some empty pages for the owner and project pages

---
+++ apps/web/src/routes/[ownerSlug]/[projectSlug]/+page.ts
@@ -0,0 +1,10 @@
+import type { PageLoad } from './$types';
+import type { ProjectParameters } from '../types';
+
+// eslint-disable-next-line func-style
+export const load: PageLoad<ProjectParameters> = async ({ params }) => {
+	return {
+		ownerSlug: params.ownerSlug,
+		projectSlug: params.projectSlug
+	};
+};
