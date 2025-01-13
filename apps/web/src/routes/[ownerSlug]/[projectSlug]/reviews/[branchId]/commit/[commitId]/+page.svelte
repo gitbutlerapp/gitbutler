f16c93798a6258014916a08d80a74722d87528b5
estib <stron@me.com>
2025-01-13 18:45:44 +0100

    Add Skeletons for the review pages

---
+++ apps/web/src/routes/[ownerSlug]/[projectSlug]/reviews/[branchId]/commit/[commitId]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { ProjectReviewCommitParameters } from '../../../../../types';
+
+	interface Props {
+		data: ProjectReviewCommitParameters;
+	}
+
+	let { data }: Props = $props();
+</script>
+
+<h2>Review page: {data.ownerSlug}/{data.projectSlug} {data.branchId}/{data.commitId}</h2>
