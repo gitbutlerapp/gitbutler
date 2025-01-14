a95c757d76d013e56ff5071d874b301aa5762487
estib <stron@me.com>
2025-01-14 10:39:49 +0100

    Create dumb components for the chat
    - Chat input
    - Message

---
+++ apps/web/src/lib/components/chat/Message.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" module>
+	export interface MessageProps {
+		author: string;
+		content: string;
+	}
+</script>
+
+<script lang="ts">
+	const { author, content }: MessageProps = $props();
+</script>
+
+<div class="chat-message">
+	<div class="chat-message-author">{author}</div>
+	<div class="chat-message-content">{content}</div>
+</div>
+
+<style>
+	.chat-message {
+		display: flex;
+		padding: 10px;
+		border-bottom: 1px solid #ccc;
+		gap: 10px;
+	}
+
+	.chat-message-author {
+		font-weight: bold;
+	}
+</style>
