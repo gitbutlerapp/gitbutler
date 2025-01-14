762273d1fbfae1c6680199c4e521297cc82194bb
estib <stron@me.com>
2025-01-14 09:30:58 +0100

    More layout changes for the review page
    Simple CSS for the review page layout

--- apps/web/src/routes/[ownerSlug]/[projectSlug]/reviews/[branchId]/commit/[commitId]/+page.svelte
+++ apps/web/src/routes/[ownerSlug]/[projectSlug]/reviews/[branchId]/commit/[commitId]/+page.svelte
@@ -11,9 +11,25 @@
 
 <h2>Review page: {data.ownerSlug}/{data.projectSlug} {data.branchId}/{data.commitId}</h2>
 
-<div>
-	<div>the main area with the diffs</div>
-	<div>
+<div class="review-page">
+	<div class="review-main-content">the main area with the diffs</div>
+	<div class="review-chat">
 		<ChatComponent />
 	</div>
 </div>
+
+<style>
+	.review-page {
+		display: flex;
+		width: 100%;
+		flex-grow: 1;
+	}
+
+	.review-main-content {
+		width: 100%;
+	}
+
+	.review-chat {
+		width: 100%;
+	}
+</style>
