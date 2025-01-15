8368beef4f048c793904bf80d68d0dc15eba230f
Mattias Granlund <mtsgrd@gmail.com>
2025-01-15 16:44:50 +0100

    Delete unused git config migration function

--- apps/desktop/src/lib/secrets/secretsService.ts
+++ apps/desktop/src/lib/secrets/secretsService.ts
@@ -24,20 +24,4 @@ export class RustSecretService implements SecretsService {
 			secret
 		});
 	}
-
-	/**
-	 * Migrates a specific key from git config to secrets.
-	 *
-	 * TODO: Remove this code and the dependency on GitConfigService in the future.
-	 */
-	private async migrate(key: string, handle: string): Promise<string | undefined> {
-		const secretInConfig = await this.gitConfigService.get(key);
-		if (secretInConfig === undefined) return;
-
-		await this.set(handle, secretInConfig);
-		await this.gitConfigService.remove(key);
-
-		console.warn(`Migrated Git config "${key}" to secret store.`);
-		return secretInConfig;
-	}
 }
