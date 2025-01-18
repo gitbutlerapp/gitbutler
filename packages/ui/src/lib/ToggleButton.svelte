6592019bf63f66a7ef864f5002eabee2c67e096e
Pavel Laptev <pawellaptew@gmail.com>
2025-01-19 00:52:40 +0100

    Align `ToogleButton` with the `Button` component

--- packages/ui/src/lib/ToggleButton.svelte
+++ packages/ui/src/lib/ToggleButton.svelte
@@ -40,26 +40,47 @@
 
 <style lang="postcss">
 	.toggle-btn {
+		--label-clr: var(--clr-btn-ntrl-outline-text);
+		--btn-bg: var(--clr-btn-ntrl-outline-bg);
+		--opacity-btn-bg: 0;
+		--btn-border-clr: var(--clr-btn-ntrl-outline);
+		--icon-opacity: var(--opacity-btn-icon-outline);
+		--btn-border-opacity: var(--opacity-btn-outline);
+
 		width: fit-content;
 		display: flex;
 		align-items: center;
 		gap: 10px;
 		height: var(--size-button);
 		padding: 4px 8px;
-		border: 1px solid var(--clr-border-2);
 		border-radius: var(--radius-m);
 		transition: border-color 0.2s;
 
+		color: var(--label-clr);
+		background: color-mix(
+			in srgb,
+			var(--btn-bg, transparent),
+			transparent calc((1 - var(--opacity-btn-bg, 1)) * 100%)
+		);
+		border: 1px solid
+			color-mix(
+				in srgb,
+				var(--btn-border-clr, transparent),
+				transparent calc((1 - var(--btn-border-opacity, 1)) * 100%)
+			);
+
 		&:hover {
-			border-color: var(--clr-border-1);
+			--icon-opacity: var(--opacity-btn-icon-outline-hover);
+			--opacity-btn-bg: var(--opacity-btn-outline-bg-hover);
+			--btn-border-opacity: var(--opacity-btn-outline);
 		}
 	}
 
 	.toggle-icon {
 		display: flex;
-		opacity: 0.5;
 		margin-left: -2px;
 		margin-right: -2px;
+		opacity: var(--icon-opacity);
 	}
 
 	.toggle-btn__label {
