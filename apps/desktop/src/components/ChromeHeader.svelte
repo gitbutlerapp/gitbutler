f8628cb19d1aea08b520dbf9d13f4f6c8acd5464
ndom91 <yo@ndo.dev>
2025-01-20 18:17:50 +0100

    feat: add chrome header

---
+++ apps/desktop/src/components/ChromeHeader.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import SyncButton from '$components/SyncButton.svelte';
+	import { platformName } from '$lib/platform/platform';
+	import { Project } from '$lib/project/project';
+	import { getContext } from '@gitbutler/shared/context';
+	import { env } from '$env/dynamic/public';
+
+	const project = getContext(Project);
+</script>
+
+<nav>
+	<div class="left">
+		{#if platformName === 'macos' || env.PUBLIC_TESTING}
+			<div class="traffic-lights-placeholder" data-tauri-drag-region></div>
+		{/if}
+		<SyncButton size="button" />
+	</div>
+	<div class="center">
+		{project.title}
+	</div>
+	<div class="right"></div>
+</nav>
+
+<style>
+	nav {
+		margin: 16px 16px 0 16px;
+	}
+</style>
