229aa6fc0fa2c40c5fb121331f2982cdeb352b7d
ndom91 <yo@ndo.dev>
2025-01-20 18:18:04 +0100

    fix: primary chrome layout

--- apps/desktop/src/components/Chrome.svelte
+++ apps/desktop/src/components/Chrome.svelte
@@ -1,24 +1,43 @@
 <script lang="ts">
+	import Header from '$components/ChromeHeader.svelte';
 	import Sidebar from '$components/ChromeSidebar.svelte';
 	import type { Snippet } from 'svelte';
 
 	const { children, projectId }: { children: Snippet; projectId: string } = $props();
 </script>
 
-<Sidebar {projectId} />
-<div class="chrome">
-	{@render children()}
+<div class="container">
+	<Header />
+	<div class="content">
+		<Sidebar {projectId} />
+		<div class="chrome">
+			{@render children()}
+		</div>
+	</div>
 </div>
 
 <style>
-	.chrome {
+	.container {
 		display: flex;
 		flex-grow: 1;
-		height: calc(100% - 32px);
-		margin: 16px;
-		align-items: self-start;
-		user-select: none;
-		position: relative;
-		background-color: var(--clr-bg-2);
+		flex-direction: column;
+		height: 100%;
+
+		.content {
+			display: flex;
+			flex-grow: 1;
+			height: 100%;
+
+			.chrome {
+				display: flex;
+				flex-grow: 1;
+				height: calc(100% - 32px);
+				margin: 16px;
+				align-items: self-start;
+				user-select: none;
+				position: relative;
+				background-color: var(--clr-bg-2);
+			}
+		}
 	}
 </style>
