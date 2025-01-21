8642fa11dc8bdb91ead438482c2d7f8cf1f7917c
ndom91 <yo@ndo.dev>
2025-01-21 14:48:10 +0100

    feat: add active page indicator to ChromeSidebar

--- apps/desktop/src/components/ChromeSidebar.svelte
+++ apps/desktop/src/components/ChromeSidebar.svelte
@@ -6,6 +6,7 @@
 	import { getContext } from '@gitbutler/shared/context';
 	import Button from '@gitbutler/ui/Button.svelte';
 	import Icon from '@gitbutler/ui/Icon.svelte';
+	import { slide } from 'svelte/transition';
 	import { goto } from '$app/navigation';
 
 	const routes = getContext(DesktopRoutesService);
@@ -13,16 +14,24 @@
 	const user = getContextStore(User);
 </script>
 
-<nav class="wrapper">
+<nav class="sidebar">
 	<div class="top">
-		<Button
-			kind="outline"
-			onclick={() => goto(routes.workspacePath(project.id))}
-			width={34}
-			class="btn-square"
-		>
-			<svg viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<g opacity="0.7">
+		<div class="">
+			{#if routes.isWorkspacePath}
+				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
+			{/if}
+			<Button
+				kind="outline"
+				onclick={() => goto(routes.workspacePath(project.id))}
+				width={34}
+				class="btn-square"
+			>
+				<svg
+					viewBox="0 0 16 13"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					class={[routes.isWorkspacePath && 'stroke-orange']}
+				>
 					<path
 						d="M2 12L3.5 7.5M14 12L12.5 7.5M12.5 7.5L11 3H5L3.5 7.5M12.5 7.5H3.5"
 						stroke-width="1.5"
@@ -31,59 +40,121 @@
 						d="M1.24142 3H14.7586C14.8477 3 14.8923 2.89229 14.8293 2.82929L13.0293 1.02929C13.0105 1.01054 12.9851 1 12.9586 1H3.04142C3.0149 1 2.98946 1.01054 2.97071 1.02929L1.17071 2.82929C1.10771 2.89229 1.15233 3 1.24142 3Z"
 						stroke-width="1.5"
 					/>
-				</g>
-			</svg>
-		</Button>
-		<Button
-			kind="outline"
-			onclick={() => goto(routes.branchesPath(project.id))}
-			width={34}
-			class="btn-square"
-		>
-			<svg viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<g opacity="0.7">
+				</svg>
+			</Button>
+		</div>
+		<div class="">
+			{#if routes.isBranchesPath}
+				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
+			{/if}
+			<Button
+				kind="outline"
+				onclick={() => goto(routes.branchesPath(project.id))}
+				width={34}
+				class="btn-square"
+			>
+				<svg
+					viewBox="0 0 16 14"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					class={[routes.isBranchesPath && 'stroke-purple']}
+				>
 					<path d="M5 3L11 3" stroke-width="1.5" />
 					<path
 						d="M3 5L3 7.17157C3 7.70201 3.21071 8.21071 3.58579 8.58579L5.41421 10.4142C5.78929 10.7893 6.29799 11 6.82843 11L11.5 11"
 						stroke-width="1.5"
 					/>
-					<rect x="15" y="1" width="4" height="4" transform="rotate(90 15 1)" stroke-width="1.5" />
-					<rect x="15" y="9" width="4" height="4" transform="rotate(90 15 9)" stroke-width="1.5" />
-					<rect x="5" y="1" width="4" height="4" transform="rotate(90 5 1)" stroke-width="1.5" />
-				</g>
-			</svg>
-		</Button>
-		<Button
-			kind="outline"
-			onclick={() => goto(routes.targetPath(project.id))}
-			width={34}
-			class="btn-square"
-		>
-			<svg viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<g opacity="0.7">
+					<rect
+						x="15"
+						y="1"
+						width="4"
+						height="4"
+						transform="rotate(90 15 1)"
+						stroke-width="1.5"
+						class={[routes.isBranchesPath && 'fill-purple']}
+					/>
+					<rect
+						x="15"
+						y="9"
+						width="4"
+						height="4"
+						transform="rotate(90 15 9)"
+						stroke-width="1.5"
+						class={[routes.isBranchesPath && 'fill-purple']}
+					/>
+					<rect
+						x="5"
+						y="1"
+						width="4"
+						height="4"
+						transform="rotate(90 5 1)"
+						stroke-width="1.5"
+						class={[routes.isBranchesPath && 'fill-purple']}
+					/>
+				</svg>
+			</Button>
+		</div>
+		<div class="">
+			{#if routes.isTargetPath}
+				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
+			{/if}
+			<Button
+				kind="outline"
+				onclick={() => goto(routes.targetPath(project.id))}
+				width={34}
+				class="btn-square"
+			>
+				<svg
+					viewBox="0 0 18 16"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					class={[routes.isTargetPath && 'fill-orange stroke-orange']}
+				>
 					<path
 						d="M10.6906 1C12.1197 1 13.4402 1.7624 14.1547 3L15.8453 5.9282C16.5598 7.16581 16.5598 8.6906 15.8453 9.9282L14.1547 12.8564C13.4402 14.094 12.1197 14.8564 10.6906 14.8564H7.3094C5.88034 14.8564 4.55983 14.094 3.8453 12.8564L2.1547 9.9282C1.44017 8.6906 1.44017 7.16581 2.1547 5.9282L3.8453 3C4.55983 1.7624 5.88034 1 7.3094 1H10.6906Z"
 						stroke-width="1.5"
+						class={[routes.isTargetPath && 'stroke-orange']}
 					/>
-					<path d="M9 14.5V10.5M9 5V1" stroke-width="1.5" />
-					<path d="M2.25 7.75L6.25 7.75M11.75 7.75L15.75 7.75" stroke-width="1.5" />
-					<circle cx="9" cy="8" r="1" fill="#272321" />
-				</g>
-			</svg>
-		</Button>
-		<Button
-			kind="outline"
-			onclick={() => goto(routes.historyPath(project.id))}
-			width={34}
-			class="btn-square"
-		>
-			<svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<g opacity="0.7">
-					<path d="M8 5V10H13" stroke-width="1.5" />
-					<rect x="1" y="1" width="16" height="16" rx="6" stroke-width="1.5" />
-				</g>
-			</svg>
-		</Button>
+					<path
+						d="M9 14.5V10.5M9 5V1"
+						stroke-width="1.5"
+						class={[routes.isTargetPath && 'stroke-white']}
+					/>
+					<path
+						d="M2.25 7.75L6.25 7.75M11.75 7.75L15.75 7.75"
+						stroke-width="1.5"
+						class={[routes.isTargetPath && 'stroke-white']}
+					/>
+					<circle cx="9" cy="8" r="1" class={[routes.isTargetPath && 'stroke-white']} />
+				</svg>
+			</Button>
+		</div>
+		<div class="">
+			{#if routes.isHistoryPath}
+				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
+			{/if}
+			<Button
+				kind="outline"
+				onclick={() => goto(routes.historyPath(project.id))}
+				width={34}
+				class="btn-square"
+			>
+				<svg
+					viewBox="0 0 18 18"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					stroke-width="1.5"
+					style="padding: 1px;"
+				>
+					<rect width="18" height="18" rx="6" class={[routes.isHistoryPath && 'fill-yellow']} />
+					<path
+						d="M8 3V10H13"
+						stroke-width="1.5"
+						class={[routes.isHistoryPath && 'stroke-black']}
+					/>
+				</svg>
+			</Button>
+		</div>
 	</div>
 	<div class="bottom">
 		<Button
@@ -131,7 +202,7 @@
 </nav>
 
 <style>
-	.wrapper {
+	.sidebar {
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
@@ -156,10 +227,50 @@
 		width: 34px;
 	}
 
+	.active-page-indicator {
+		content: '';
+		position: absolute;
+		left: 0;
+		width: 12px;
+		height: 18px;
+		border-radius: var(--radius-m);
+		background-color: var(--clr-theme-pop-element);
+		transform: translateX(-50%) translateY(50%);
+	}
+
 	svg {
 		width: 16px;
 		height: 16px;
 		stroke: var(--clr-btn-ntrl-outline-text);
+	}
+
+	.stroke-white {
+		stroke: #fff;
+	}
+
+	.fill-orange {
+		fill: #ff9774;
+	}
+
+	.stroke-orange {
+		stroke: #ff9774;
+	}
+
+	.fill-purple {
+		fill: #a486c8;
+	}
+
+	.stroke-purple {
+		stroke: #a486c8;
+	}
+
+	.fill-yellow {
+		fill: #fbdb79;
+		stroke: #fbdb79;
+	}
+
+	.stroke-black {
+		stroke: #000 !important;
 	}
 
 	:global(.btn-height-auto) {
--- apps/desktop/src/lib/routes/routes.svelte.ts
+++ apps/desktop/src/lib/routes/routes.svelte.ts
@@ -1,22 +1,42 @@
+import { page } from '$app/state';
+
+function isUrl<T>(id: string): T | undefined {
+	if (page.route.id === id) {
+		return page.params as T;
+	}
+}
 export class DesktopRoutesService {
 	constructor() {}
 
 	projectPath(projectId: string) {
 		return `/${projectId}`;
 	}
+	isProjectPath = $derived(isUrl<{ projectId: string }>('/[projectId]'));
+
 	settingsPath(projectId: string) {
 		return `/${projectId}/settings`;
 	}
+	isSettingsPath = $derived(isUrl<{ projectId: string }>('/[projectId]/settings'));
+
 	workspacePath(projectId: string) {
 		return `/${projectId}/workspace`;
 	}
+	isWorkspacePath = $derived(
+		isUrl<{ projectId: string; branchId?: string }>('/[projectId]/workspace/[[branchId]]')
+	);
+
 	branchesPath(projectId: string) {
 		return `/${projectId}/branches`;
 	}
+	isBranchesPath = $derived(isUrl<{ projectId: string }>('/[projectId]/branches'));
+
 	targetPath(projectId: string) {
 		return `/${projectId}/target`;
 	}
+	isTargetPath = $derived(isUrl<{ projectId: string }>('/[projectId]/target'));
+
 	historyPath(projectId: string) {
 		return `/${projectId}/history`;
 	}
+	isHistoryPath = $derived(isUrl<{ projectId: string }>('/[projectId]/history'));
 }
