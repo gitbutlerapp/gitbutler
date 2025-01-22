21739960df905dc0b5faa2abfcfcd48b6c38aabb
Kiril Videlov <kiril@videlov.com>
2025-01-22 15:10:29 +0100

    refactor: remove obsolete GC configuration handling code

--- crates/gitbutler-command-context/src/lib.rs
+++ crates/gitbutler-command-context/src/lib.rs
@@ -13,42 +13,6 @@ impl CommandContext {
     /// Open the repository identified by `project` and perform some checks.
     pub fn open(project: &Project) -> Result<Self> {
         let repo = git2::Repository::open(&project.path)?;
-
-        // Previously the app used to set `gc.pruneExpire=never`, disabling GC in order to prevent GitButler trees from being pruned.
-        // With the introduciton of the operations log, the trees that the app needs are referenced through the reflog hack (`gitbutler-oplog` reflog.rs).
-        // This code will now look for `gitbutler.didSetPrune`, and if set, it will undo the change by removing the `gc.pruneExpire` and `gitbutler.didSetPrune` entries.
-        if let Ok(config) = repo.config().as_mut() {
-            let did_set_prune = match config.get_bool("gitbutler.didSetPrune") {
-                Ok(did_set_prune) => did_set_prune, // If `gitbutler.didSetPrune` was previously set, we want to undo the change
-                Err(err) => {
-                    tracing::trace!(
-                                "failed to get gitbutler.didSetPrune for repository at {}; cannot disable gc: {}",
-                                project.path.display(),
-                                err
-                            );
-                    false
-                }
-            };
-
-            if did_set_prune {
-                if let Err(error) = config
-                    .remove("gc.pruneExpire")
-                    .and_then(|()| config.remove("gitbutler.didSetPrune"))
-                {
-                    tracing::warn!(
-                        "failed to remove gc.pruneExpire and gitbutler.didSetPrune for repository at {}: {}",
-                        project.path.display(),
-                        error
-                    );
-                }
-            }
-        } else {
-            tracing::warn!(
-                "failed to get config for repository at {}; cannot disable gc",
-                project.path.display()
-            );
-        }
-
         Ok(Self {
             git_repository: repo,
             project: project.clone(),
