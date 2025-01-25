dadac1a1dde60a1aa6e513abf01e40c5f0f6501a
Pavel Laptev <pawellaptew@gmail.com>
2025-01-25 01:09:46 +0100

    use variables instead of classes to increase code redability and avoid css properties overrides

--- apps/desktop/src/components/ChromeSidebar.svelte
+++ apps/desktop/src/components/ChromeSidebar.svelte
@@ -24,7 +24,7 @@
 
 <nav class="sidebar">
 	<div class="top">
-		<div class="">
+		<div>
 			{#if routes.isWorkspacePath}
 				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
 			{/if}
@@ -34,24 +34,22 @@
 				width={34}
 				class={['btn-square', routes.isWorkspacePath && 'btn-active']}
 			>
-				<svg
-					viewBox="0 0 16 13"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-					class={[routes.isWorkspacePath && 'stroke-orange']}
-				>
+				<svg viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path
 						d="M2 12L3.5 7.5M14 12L12.5 7.5M12.5 7.5L11 3H5L3.5 7.5M12.5 7.5H3.5"
 						stroke-width="1.5"
+						stroke="var(--clr-workspace-legs)"
 					/>
 					<path
 						d="M1.24142 3H14.7586C14.8477 3 14.8923 2.89229 14.8293 2.82929L13.0293 1.02929C13.0105 1.01054 12.9851 1 12.9586 1H3.04142C3.0149 1 2.98946 1.01054 2.97071 1.02929L1.17071 2.82929C1.10771 2.89229 1.15233 3 1.24142 3Z"
 						stroke-width="1.5"
+						stroke="var(--clr-workspace-top)"
+						fill="var(--clr-workspace-top)"
 					/>
 				</svg>
 			</Button>
 		</div>
-		<div class="">
+		<div>
 			{#if routes.isBranchesPath}
 				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
 			{/if}
@@ -61,16 +59,12 @@
 				width={34}
 				class={['btn-square', routes.isBranchesPath && 'btn-active']}
 			>
-				<svg
-					viewBox="0 0 16 14"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-					class={[routes.isBranchesPath && 'stroke-purple']}
-				>
-					<path d="M5 3L11 3" stroke-width="1.5" />
+				<svg viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M5 3L11 3" stroke-width="1.5" stroke="var(--clr-branches)" />
 					<path
 						d="M3 5L3 7.17157C3 7.70201 3.21071 8.21071 3.58579 8.58579L5.41421 10.4142C5.78929 10.7893 6.29799 11 6.82843 11L11.5 11"
 						stroke-width="1.5"
+						stroke="var(--clr-branches)"
 					/>
 					<rect
 						x="15"
@@ -79,7 +73,8 @@
 						height="4"
 						transform="rotate(90 15 1)"
 						stroke-width="1.5"
-						class={[routes.isBranchesPath && 'fill-purple']}
+						fill="var(--clr-branches)"
+						stroke="var(--clr-branches)"
 					/>
 					<rect
 						x="15"
@@ -88,7 +83,8 @@
 						height="4"
 						transform="rotate(90 15 9)"
 						stroke-width="1.5"
-						class={[routes.isBranchesPath && 'fill-purple']}
+						fill="var(--clr-branches)"
+						stroke="var(--clr-branches)"
 					/>
 					<rect
 						x="5"
@@ -97,12 +93,13 @@
 						height="4"
 						transform="rotate(90 5 1)"
 						stroke-width="1.5"
-						class={[routes.isBranchesPath && 'fill-purple']}
+						fill="var(--clr-branches)"
+						stroke="var(--clr-branches)"
 					/>
 				</svg>
 			</Button>
 		</div>
-		<div class="">
+		<div>
 			{#if routes.isTargetPath}
 				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
 			{/if}
@@ -112,32 +109,24 @@
 				width={34}
 				class={['btn-square', routes.isTargetPath && 'btn-active']}
 			>
-				<svg
-					viewBox="0 0 18 16"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-					class={[routes.isTargetPath && 'fill-orange stroke-orange']}
-				>
+				<svg viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path
 						d="M10.6906 1C12.1197 1 13.4402 1.7624 14.1547 3L15.8453 5.9282C16.5598 7.16581 16.5598 8.6906 15.8453 9.9282L14.1547 12.8564C13.4402 14.094 12.1197 14.8564 10.6906 14.8564H7.3094C5.88034 14.8564 4.55983 14.094 3.8453 12.8564L2.1547 9.9282C1.44017 8.6906 1.44017 7.16581 2.1547 5.9282L3.8453 3C4.55983 1.7624 5.88034 1 7.3094 1H10.6906Z"
 						stroke-width="1.5"
-						class={[routes.isTargetPath && 'stroke-orange']}
+						stroke="var(--clr-target-bg)"
+						fill="var(--clr-target-bg)"
 					/>
-					<path
-						d="M9 14.5V10.5M9 5V1"
-						stroke-width="1.5"
-						class={[routes.isTargetPath && 'stroke-white']}
-					/>
+					<path d="M9 14.5V10.5M9 5V1" stroke-width="1.5" stroke="var(--clr-target-lines)" />
 					<path
 						d="M2.25 7.75L6.25 7.75M11.75 7.75L15.75 7.75"
 						stroke-width="1.5"
-						class={[routes.isTargetPath && 'stroke-white']}
+						stroke="var(--clr-target-lines)"
 					/>
-					<circle cx="9" cy="8" r="1" class={[routes.isTargetPath && 'stroke-white']} />
+					<circle cx="9" cy="8" r="1" stroke="var(--clr-target-lines)" />
 				</svg>
 			</Button>
 		</div>
-		<div class="">
+		<div>
 			{#if routes.isHistoryPath}
 				<div class="active-page-indicator" in:slide={{ axis: 'x', duration: 150 }}></div>
 			{/if}
@@ -154,12 +143,14 @@
 					stroke-width="1.5"
 					style="padding: 1px;"
 				>
-					<rect width="18" height="18" rx="6" class={[routes.isHistoryPath && 'fill-yellow']} />
-					<path
-						d="M8 3V10H13"
-						stroke-width="1.5"
-						class={[routes.isHistoryPath && 'stroke-black']}
+					<rect
+						width="18"
+						height="18"
+						rx="6"
+						stroke="var(--clr-history-bg)"
+						fill="var(--clr-history-bg)"
 					/>
+					<path d="M8 3V10H13" stroke-width="1.5" stroke="var(--clr-history-arrows)" />
 				</svg>
 			</Button>
 		</div>
@@ -212,7 +203,7 @@
 				tooltipPosition="top"
 				tooltipAlign="start"
 				width={34}
-				class="fill-text-2"
+				class="faded-btn"
 			/>
 			<Button
 				icon="mail"
@@ -221,7 +212,7 @@
 				tooltipPosition="top"
 				tooltipAlign="start"
 				width={34}
-				class="fill-text-2"
+				class="faded-btn"
 				onclick={() => {
 					shareIssueModal?.show();
 				}}
@@ -313,10 +304,6 @@
 		border-radius: var(--radius-m);
 	}
 
-	:global(.user-button svg) {
-		opacity: 0.7;
-	}
-
 	.profile-picture {
 		border-radius: var(--radius-m);
 	}
@@ -332,62 +319,59 @@
 		transform: translateX(-50%) translateY(50%);
 	}
 
-	svg {
-		width: 16px;
-		height: 16px;
-		stroke: var(--clr-btn-ntrl-outline-text);
-	}
-
-	.stroke-white {
-		stroke: #fff;
-	}
-
-	.fill-orange {
-		fill: #ff9774;
-	}
-
-	.stroke-orange {
-		stroke: #ff9774;
-	}
-
-	.fill-purple {
-		fill: #a486c8;
-	}
-
-	.stroke-purple {
-		stroke: #a486c8;
-	}
-
-	.fill-yellow {
-		fill: #fbdb79;
-		stroke: #fbdb79;
-	}
-
-	.stroke-black {
-		stroke: #000;
-	}
-
 	/* OVERRIDE BUTTON STYLES */
+	:global(.sidebar .btn-square) {
+		--label-clr: var(--clr-btn-ntrl-outline-text);
+		--icon-opacity: var(--opacity-btn-icon-outline);
 
-	:global(.btn.fill-text-2) {
-		fill: var(--clr-text-2);
-		opacity: 0.5;
+		& svg {
+			width: 16px;
+			height: 16px;
+			stroke: currentColor;
+			opacity: var(--icon-opacity);
+			transition: opacity var(--transition-fast);
+		}
+
+		&:hover {
+			--icon-opacity: var(--opacity-btn-icon-outline-hover);
+		}
 	}
-
-	:global(.btn.btn-height-auto) {
+	:global(.sidebar .btn-height-auto) {
 		height: auto;
 		border-radius: var(--radius-ml);
 		padding: 0;
 	}
-
-	:global(.btn.btn-square) {
+	:global(.sidebar .btn-square) {
 		aspect-ratio: 1 / 1;
 		height: unset;
-		opacity: 0.7;
 		border-radius: var(--radius-ml);
 	}
-	:global(.btn.btn-square.btn-active) {
-		opacity: 1;
-		background-color: var(--clr-scale-ntrl-100);
+	:global(.sidebar .btn-square.btn-active) {
+		--icon-opacity: 1;
+		--btn-bg: var(--clr-core-ntrl-100);
+		--opacity-btn-bg: 1;
+
+		/* workspace icon */
+		--clr-workspace-legs: #d96842;
+		--clr-workspace-top: #ff9774;
+
+		/* branches icon */
+		--clr-branches: #a486c8;
+
+		/* target icon */
+		--clr-target-bg: #ff9774;
+		--clr-target-lines: #fff;
+
+		/* history icon */
+		--clr-history-bg: #fbdb79;
+		--clr-history-arrows: #0a0a0a;
+	}
+
+	:global(.sidebar .faded-btn) {
+		--icon-opacity: 0.4;
+
+		&:hover {
+			--icon-opacity: 0.6;
+		}
 	}
 </style>
