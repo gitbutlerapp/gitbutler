d769a05b585d8760dea96846cf770dff69f91663
Mattias Granlund <mtsgrd@gmail.com>
2025-01-25 12:31:00 +0100

    Make redux result component operate on result type

--- apps/desktop/src/components/ReduxResult.svelte
+++ apps/desktop/src/components/ReduxResult.svelte
@@ -3,16 +3,21 @@
 	import type { QueryStatus } from '@reduxjs/toolkit/query';
 	import type { Snippet } from 'svelte';
 
-	type Props<A> = {
+	type Result<A> = {
+		data?: A;
 		status: QueryStatus;
 		error?: unknown;
-		data?: A;
+	};
+
+	type Props<A> = {
+		result: Result<A>;
 		children: Snippet<[A]>;
-		empty: Snippet;
+		empty?: Snippet;
 	};
 
 	// eslint-disable-next-line no-undef
-	const { status, data, error, children, empty }: Props<A> = $props();
+	const { result, children, empty }: Props<A> = $props();
+	const { data, status, error } = $derived(result);
 </script>
 
 {#if status === 'fulfilled'}
@@ -20,7 +25,7 @@
 	{#if data !== undefined && (!Array.isArray(data) || data.length > 0)}
 		{@render children(data)}
 	{:else}
-		{@render empty()}
+		{@render empty?.()}
 	{/if}
 {:else if status === 'pending'}
 	<Icon name="spinner" />
