aab9149db01b38dc9b5a51f404e115db959c8358
estib <stron@me.com>
2025-01-27 09:40:07 +0100

    Shared array utils: Dedup by property
    Add a utility to deduplicate items in an array based on the values of a property

--- packages/shared/src/lib/utils/array.ts
+++ packages/shared/src/lib/utils/array.ts
@@ -1,3 +1,18 @@
 export function deduplicate<T>(array: T[]): T[] {
 	return Array.from(new Set(array));
 }
+
+export function deduplicateBy<T, K extends keyof T>(array: T[], key: K): T[] {
+	const seen = new Set();
+	const result: T[] = [];
+
+	for (const item of array) {
+		const value = item[key];
+		if (!seen.has(value)) {
+			seen.add(value);
+			result.push(item);
+		}
+	}
+
+	return result;
+}
