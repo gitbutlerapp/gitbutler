79f85f108f408b53301b78b017629a29c05e7019
estib <stron@me.com>
2025-01-28 16:53:27 +0100

    Chat: Correctly update the channel
    The chat channel should be updated every time the patch properties change.

---
+++ apps/web/src/lib/chat/subscribe.ts
@@ -0,0 +1,32 @@
+import { type SubscriptionEvent, getActionCableEndpoint, isSubscriptionEvent } from './utils';
+import { createConsumer } from '@rails/actioncable';
+
+export interface ChatChannelSubscriptionParams {
+	token: string;
+	changeId: string;
+	projectId: string;
+	onEvent: (data: SubscriptionEvent) => void;
+}
+
+export function subscribeToChatChannel({
+	token,
+	changeId,
+	projectId,
+	onEvent: onData
+}: ChatChannelSubscriptionParams): () => void {
+	const actionCableEndpoint = getActionCableEndpoint(token);
+	const consumer = createConsumer(actionCableEndpoint);
+	consumer.subscriptions.create(
+		{ channel: 'ChatChannel', change_id: changeId, project_id: projectId },
+		{
+			received(data: unknown) {
+				if (!isSubscriptionEvent(data)) return;
+				onData(data);
+			}
+		}
+	);
+
+	return () => {
+		consumer.disconnect();
+	};
+}
