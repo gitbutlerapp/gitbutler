c5e55a17e1f3d7a8d4c026f624e4b437bd617cb6
estib <stron@me.com>
2025-01-29 15:14:17 +0100

    Chat: Timestamp messages
    Display the time at which the messages were sent

--- apps/web/src/lib/chat/utils.ts
+++ apps/web/src/lib/chat/utils.ts
@@ -1,3 +1,5 @@
+import { getTimeAgo } from '@gitbutler/ui/utils/timeAgo';
+import type { ChatMessage } from '@gitbutler/shared/chat/types';
 import { env } from '$env/dynamic/public';
 
 export type SubscriptionEvent = {
@@ -29,4 +31,21 @@ export function getActionCableEndpoint(token: string): string {
 	url.search = urlSearchParams.toString();
 
 	return url.toString();
+}
+
+export function messageTimeStamp(message: ChatMessage): string {
+	const creationDate = new Date(message.createdAt);
+	const hoursAgo = Math.round((Date.now() - creationDate.getTime()) / 1000 / 60 / 60);
+
+	if (hoursAgo < 24) {
+		return (
+			'Today at ' +
+			creationDate.toLocaleTimeString('en-US', {
+				hour: 'numeric',
+				minute: 'numeric'
+			})
+		);
+	}
+
+	return getTimeAgo(creationDate);
 }
--- apps/web/src/lib/components/chat/Message.svelte
+++ apps/web/src/lib/components/chat/Message.svelte
@@ -1,4 +1,5 @@
 <script lang="ts" module>
+	import { messageTimeStamp } from '$lib/chat/utils';
 	import type { ChatMessage } from '@gitbutler/shared/chat/types';
 
 	export interface MessageProps {
@@ -14,13 +15,21 @@
 	const authorName = $derived(
 		message.user.login ?? message.user.name ?? message.user.email ?? UNKNOWN_AUTHOR
 	);
+
+	const timestamp = $derived(messageTimeStamp(message));
 </script>
 
 <div class="chat-message">
 	<img class="chat-message__subtitle-avatar" src={message.user.avatarUrl} alt={authorName} />
 	<div class="chat-message__content">
-		<div class="chat-message__author-name">
-			{authorName}
+		<div class="chat-message__header">
+			<div class="chat-message__author-name">
+				{authorName}
+			</div>
+
+			<div class="chat-message__timestamp">
+				{timestamp}
+			</div>
 		</div>
 		<p class="chat-message__content-text">
 			{message.comment}
@@ -50,13 +59,33 @@
 		gap: 12px;
 	}
 
+	.chat-message__header {
+		display: flex;
+		gap: 7px;
+	}
+
+	.chat-message__timestamp {
+		overflow: hidden;
+		color: var(--clr-text-1, #1a1614);
+		text-overflow: ellipsis;
+
+		/* base/12 */
+		font-family: var(--text-fontfamily-default, Inter);
+		font-size: 12px;
+		font-style: normal;
+		font-weight: var(--text-weight-regular, 400);
+		line-height: 120%; /* 14.4px */
+
+		opacity: 0.4;
+	}
+
 	.chat-message__author-name {
 		overflow: hidden;
 		color: var(--text-1, #1a1614);
 		text-overflow: ellipsis;
 
 		/* base/13-bold */
-		font-family: var(--fontfamily-default, Inter);
+		font-family: var(--text-fontfamily-default, Inter);
 		font-size: 13px;
 		font-style: normal;
 		font-weight: 600;
@@ -67,10 +96,10 @@
 		color: var(--text-1, #1a1614);
 
 		/* base-body/13 */
-		font-family: var(--fontfamily-default, Inter);
+		font-family: var(--text-fontfamily-default, Inter);
 		font-size: 13px;
 		font-style: normal;
-		font-weight: var(--weight-regular, 400);
+		font-weight: var(--text-weight-regular, 400);
 		line-height: 160%; /* 20.8px */
 	}
 </style>
