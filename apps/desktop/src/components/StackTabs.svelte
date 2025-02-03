cfe4e6a3d36fa955fb107a4998049e2e5cb9dc73
Mattias Granlund <mtsgrd@gmail.com>
2025-02-03 22:58:02 +0100

    Add shadows to scrollable tabs and conditionally round content corner

--- apps/desktop/src/components/StackTabs.svelte
+++ apps/desktop/src/components/StackTabs.svelte
@@ -2,76 +2,125 @@
 	import ReduxResult from './ReduxResult.svelte';
 	import StackTab from './StackTab.svelte';
 	import StackTabNew from './StackTabNew.svelte';
-	import StackTabPreview from './StackTabPreview.svelte';
-	import { IdSelection } from '$lib/selection/idSelection.svelte';
 	import { StackService } from '$lib/stacks/stackService.svelte';
 	import { stacksToTabs } from '$lib/tabs/mapping';
 	import { getContext } from '@gitbutler/shared/context';
+	import { onMount } from 'svelte';
 
 	type Props = {
 		projectId: string;
 		selectedId: string | undefined;
 		previewing: boolean;
+		width: number | undefined;
 	};
-	const { projectId, selectedId, previewing }: Props = $props();
+	let { projectId, selectedId, width = $bindable() }: Props = $props();
 
 	const stackService = getContext(StackService);
 	const result = $derived(stackService.getStacks(projectId));
-	$inspect('getStacks', result);
 
-	const idSelection = getContext(IdSelection);
-	const selectedFiles = $derived(idSelection.values());
+	let inner = $state<HTMLDivElement>();
+	let scroller = $state<HTMLDivElement>();
 
-	// TODO: Get reactive max allowed space for tabs in order to
-	// pin "+" button on the right side of page and add shadow on left
-	// side to indicate more content, scrolling of tabs, etc. etc.
-	const tabOverflow = $derived((result.current.data?.length ?? 0) >= 3);
+	let scrollable = $state(false);
+	let scrolled = $state(false);
+	let scrolledEnd = $state(false);
+
+	function onscroll() {
+		scrolled = scroller && scroller.scrollLeft > 0 ? true : false;
+		scrolledEnd = scroller
+			? scroller.scrollLeft + scroller.offsetWidth >= scroller.scrollWidth
+			: false;
+	}
+
+	onMount(() => {
+		const observer = new ResizeObserver(() => {
+			scrollable = scroller ? scroller.scrollWidth > scroller.offsetWidth : false;
+			width = inner?.offsetWidth;
+		});
+		observer.observe(inner!);
+		return () => {
+			observer.disconnect();
+		};
+	});
 </script>
 
 <div class="tabs">
-	<div class="tabs__available" class:overflow={tabOverflow}>
-		{#if selectedFiles.length > 0}
-			<StackTabPreview
-				{projectId}
-				count={selectedFiles.length}
-				selected={previewing || selectedId === undefined}
-			/>
-		{/if}
-		<ReduxResult result={result.current}>
-			{#snippet children(result)}
-				{@const tabs = stacksToTabs(result)}
-				{#each tabs as tab, i (tab.name)}
-					{@const first = i === 0}
-					{@const last = i === tabs.length - 1}
-					{@const selected = tab.id === selectedId}
-					<StackTab {projectId} {tab} {first} {last} {selected} {tabOverflow} />
-				{/each}
-			{/snippet}
-			{#snippet empty()}
-				no stacks
-			{/snippet}
-		</ReduxResult>
+	<div class="inner" bind:this={inner}>
+		<div class="shadows">
+			<div class="scroller" bind:this={scroller} class:scrolled {onscroll}>
+				<ReduxResult result={result.current}>
+					{#snippet children(result)}
+						{@const tabs = stacksToTabs(result)}
+						{#each tabs as tab, i (tab.name)}
+							{@const first = i === 0}
+							{@const last = i === tabs.length - 1}
+							{@const selected = tab.id === selectedId}
+							<StackTab {projectId} {tab} {first} {last} {selected} />
+						{/each}
+					{/snippet}
+					{#snippet empty()}
+						no stacks
+					{/snippet}
+				</ReduxResult>
+			</div>
+			<div class="shadow shadow-left" class:scrolled></div>
+			<div class="shadow shadow-right" class:scrollable class:scrolled-end={scrolledEnd}></div>
+		</div>
+		<StackTabNew {projectId} />
 	</div>
-	<StackTabNew {projectId} {selectedId} {tabOverflow} />
 </div>
 
 <style lang="postcss">
 	.tabs {
 		display: flex;
 	}
-
-	.tabs__available {
+	.inner {
 		display: flex;
-		width: calc(100% - 62px);
+		max-width: 100%;
+	}
 
-		&.overflow {
-			width: 100%;
-			overflow-x: scroll;
+	.scroller {
+		display: flex;
+		overflow-x: scroll;
+	}
+
+	.scroller::-webkit-scrollbar {
+		display: none;
+	}
+
+	.shadows {
+		position: relative;
+		overflow-x: hidden;
+		border-radius: 10px 0 0 0;
+		border-left: 1px solid var(--clr-border-2);
+		border-top: 1px solid var(--clr-border-2);
+	}
+
+	.shadow {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		width: 8px;
+	}
+
+	.shadow-left {
+		display: none;
+		left: 0;
+		background: linear-gradient(to right, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0));
+		&.scrolled {
+			display: block;
 		}
 	}
 
-	/* TODO: Replace with <ScrollContainer /> */
-	.tabs__available::-webkit-scrollbar {
+	.shadow-right {
 		display: none;
+		right: 0;
+		background: linear-gradient(to left, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0));
+		&.scrollable {
+			display: initial;
+			&.scrolled-end {
+				display: none;
+			}
+		}
 	}
 </style>
