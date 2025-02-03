0fde237e4a903cb8d1eb81eca2044b9f75075725
ndom91 <yo@ndo.dev>
2025-02-03 17:30:45 +0100

    feat: stack branch component

---
+++ apps/desktop/src/components/v3/Branch.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import BranchHeader from '$components/v3/BranchHeader.svelte';
+	import type { WorkspaceBranch } from '$lib/branches/v3';
+	// import { getColorFromBranchType } from '@gitbutler/ui/utils/getColorFromBranchType';
+
+	interface Props {
+		branch: WorkspaceBranch;
+		first: boolean;
+		last: boolean;
+	}
+
+	const { branch, first, last }: Props = $props();
+
+	// - If any of the commits are integrated; whole branch is considered "integrated"
+	// - branch status comes from 1st commit (local only / local and remote / etc.)
+</script>
+
+<div class="branch-group" data-series-name={branch.name}>
+	<!-- style:--highlight-color={getColorFromBranchType(seriesType)} -->
+	<BranchHeader {branch} isTopBranch={first} />
+	<pre>
+		<!-- {JSON.stringify(branch, null, 2)} -->
+	</pre>
+</div>
+
+<style>
+	.branch-group {
+		border: 1px solid var(--clr-border-2);
+		border-radius: var(--radius-m);
+		background: var(--clr-bg-1);
+		scroll-margin-top: 120px;
+
+		&:last-child {
+			margin-bottom: 12px;
+		}
+	}
+</style>
---
+++ apps/desktop/src/components/v3/BranchHeader.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import type { WorkspaceBranch } from '$lib/branches/v3';
+
+	interface Props {
+		branch: WorkspaceBranch;
+		// branch: PatchSeries;
+		isTopBranch: boolean;
+		// lastPush: Date | undefined;
+	}
+
+	// const { branch, isTopBranch, lastPush }: Props = $props();
+	const { branch, isTopBranch }: Props = $props();
+</script>
+
+<div>{branch.name}</div>
+
+<style lang="postcss">
+	.branch-header {
+		position: relative;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+
+		&:not(:last-child) {
+			border-bottom: 1px solid var(--clr-border-2);
+		}
+
+		&:hover,
+		&:focus-within {
+			& :global(.branch-actions-menu) {
+				--show: true;
+			}
+		}
+	}
+
+	.branch-info {
+		width: 100%;
+		padding-right: 14px;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+
+		.remote-name {
+			min-width: max-content;
+			padding: 0 0 0 2px;
+			color: var(--clr-scale-ntrl-60);
+		}
+	}
+
+	.branch-info__name {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		min-width: 0;
+		flex-grow: 1;
+	}
+
+	.branch-info__content {
+		overflow: hidden;
+		flex: 1;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 6px;
+		padding: 14px 0;
+		margin-left: -2px;
+	}
+
+	.branch-action {
+		width: 100%;
+		display: flex;
+		justify-content: flex-start;
+		align-items: stretch;
+
+		.branch-action__body {
+			width: 100%;
+			padding: 0 14px 14px 0;
+			display: flex;
+			flex-direction: column;
+			gap: 14px;
+		}
+	}
+
+	.branch-action__line {
+		min-width: 2px;
+		margin: 0 22px 0 20px;
+		background-color: var(--bg-color, var(--clr-border-3));
+	}
+</style>
