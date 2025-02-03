de16698813c1bd5d0080e62bbcc80dca797f0869
ndom91 <yo@ndo.dev>
2025-02-03 16:04:49 +0100

    feat: stack area contents layout

--- apps/desktop/src/components/StackDetails.svelte
+++ apps/desktop/src/components/StackDetails.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import StackBranch from '$components/StackBranch.svelte';
+	import StackContentPlaceholder from '$components/StackContentPlaceholder.svelte';
 	import { StackService } from '$lib/stacks/stackService.svelte';
 	import { getContext } from '@gitbutler/shared/context';
 
@@ -11,24 +13,51 @@
 
 	const stackService = getContext(StackService);
 	const result = $derived(stackService.getStackBranches(projectId, stackId));
+	const branches = $derived(result.current.data);
 
 	$inspect('getStackBranches', result);
 </script>
 
 <div class="stack">
-	{#if stackId}
-		<pre>
-{JSON.stringify(result.current, null, 2)}
-		</pre>
+	{#if stackId && branches && branches?.length > 0}
+		<div class="stack__branches">
+			<div class="stack__branches--list">
+				{#each branches as branch}
+					<StackBranch {branch} />
+				{/each}
+			</div>
+		</div>
+
+		<div class="stack__branch-content">
+			<StackContentPlaceholder />
+		</div>
 	{/if}
 </div>
 
 <style>
 	.stack {
+		height: 100%;
 		flex: 1;
+		display: flex;
 		border-radius: 0 var(--radius-ml) var(--radius-ml);
 	}
-	.stack pre {
-		text-wrap: wrap;
+
+	.stack__branches {
+		flex: 0.5;
+		display: flex;
+		flex-direction: column;
+		padding: 16px;
+
+		background-color: transparent;
+		opacity: 1;
+		background-image: radial-gradient(var(--clr-border-2) 0.9px, #ffffff00 0.9px);
+		background-size: 12px 12px;
+		border-right: 1px solid var(--clr-border-2);
+	}
+
+	.stack__branch-content {
+		flex: 0.5;
+		display: flex;
+		flex-direction: column;
 	}
 </style>
