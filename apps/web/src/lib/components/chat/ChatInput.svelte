30f859b302a7bea050d159061f0896c3d5ca110b
estib <stron@me.com>
2025-02-03 14:45:30 +0100

    Chat Input: Use new Rich Text Editor
    Use the new Rich Text Editor component and Mention Suggestions components in order to have cool mentions while editing a message

--- apps/web/src/lib/components/chat/ChatInput.svelte
+++ apps/web/src/lib/components/chat/ChatInput.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import MentionSuggestions from './MentionSuggestions.svelte';
+	import RichText from '$lib/chat/richText.svelte';
+	import { UserService } from '$lib/user/userService';
 	import { PatchService } from '@gitbutler/shared/branches/patchService';
+	import { getChatChannelParticipants } from '@gitbutler/shared/chat/chatChannelsPreview.svelte';
 	import { ChatChannelsService } from '@gitbutler/shared/chat/chatChannelsService';
 	import { getContext } from '@gitbutler/shared/context';
+	import { AppState } from '@gitbutler/shared/redux/store.svelte';
 	import Button from '@gitbutler/ui/Button.svelte';
 	import ContextMenuItem from '@gitbutler/ui/ContextMenuItem.svelte';
 	import ContextMenuSection from '@gitbutler/ui/ContextMenuSection.svelte';
 	import DropDownButton from '@gitbutler/ui/DropDownButton.svelte';
-	import Textarea from '@gitbutler/ui/Textarea.svelte';
+	import RichTextEditor from '@gitbutler/ui/RichTextEditor.svelte';
+	import type { UserSimple } from '@gitbutler/shared/users/types';
 
 	interface Props {
 		projectId: string;
@@ -17,12 +23,32 @@
 
 	let { branchUuid, projectId, branchId, changeId }: Props = $props();
 
+	const userService = getContext(UserService);
+	const user = $derived(userService.user);
+
+	const appState = getContext(AppState);
 	const patchService = getContext(PatchService);
 	const chatChannelService = getContext(ChatChannelsService);
+	const chatParticipants = $derived(
+		getChatChannelParticipants(appState, chatChannelService, projectId, changeId)
+	);
 
 	let message = $state<string>();
 	let isSendingMessage = $state(false);
 	let isExecuting = $state(false);
+
+	// Rich text editor
+	const richText = new RichText();
+	$effect(() => {
+		if (changeId) {
+			// Just here to track the changeId
+		}
+
+		return () => {
+			// Cleanup once the change ID changes
+			richText.reset();
+		};
+	});
 
 	async function sendMessage(message: string | undefined, issue?: boolean) {
 		if (message === undefined || message.trim() === '') {
@@ -40,25 +66,39 @@
 
 	async function handleSendMessage(issue?: boolean) {
 		if (isSendingMessage) return;
+
 		isSendingMessage = true;
 		try {
 			await sendMessage(message, issue);
 		} finally {
-			message = undefined;
+			const editor = richText.richTextEditor?.getEditor();
+			editor?.commands.clearContent(true);
 			isSendingMessage = false;
 		}
 	}
 
-	async function handleKeyDown(
-		event: KeyboardEvent & { currentTarget: EventTarget & HTMLTextAreaElement }
-	) {
-		message = event.currentTarget.value;
-		if (event.key === 'Enter' && !event.shiftKey) {
+	function handleKeyDown(event: KeyboardEvent): boolean {
+		if (event.key === 'Enter' && !event.shiftKey && richText.suggestions === undefined) {
+			const editor = richText.richTextEditor?.getEditor();
+			editor?.commands.clearContent();
 			event.preventDefault();
 			event.stopPropagation();
-			await handleSendMessage();
-			return;
+			handleSendMessage();
+			return true;
 		}
+
+		const editor = richText.richTextEditor?.getEditor();
+		if (event.key === 'Enter' && event.shiftKey && editor) {
+			editor.commands.first(({ commands }) => [
+				() => commands.newlineInCode(),
+				() => commands.createParagraphNear(),
+				() => commands.liftEmptyBlock(),
+				() => commands.splitBlock()
+			]);
+			return true;
+		}
+
+		return false;
 	}
 
 	async function handleClickSend() {
@@ -77,7 +117,8 @@
 
 	async function approve() {
 		await patchService.updatePatch(branchUuid, changeId, { signOff: true, message });
-		message = undefined;
+		const editor = richText.richTextEditor?.getEditor();
+		editor?.commands.clearContent(true);
 	}
 
 	async function requestChanges() {
@@ -106,11 +147,54 @@
 		const suffix = message ? ' & Comment' : '';
 		return actionLabels[action] + suffix;
 	});
+
+	const userMap = $derived.by(() => {
+		const map = new Map<string, UserSimple>();
+		chatParticipants.current?.forEach((participant) => {
+			if (!participant.login) return;
+			map.set(participant.login, participant);
+		});
+		return map;
+	});
+
+	async function getSuggestionItems(query: string): Promise<string[]> {
+		return (
+			chatParticipants.current
+				?.map((participant) => participant.login)
+				.filter((username): username is string => !!username && username !== $user?.login)
+				.filter((item) => item.toLowerCase().startsWith(query.toLowerCase())) ?? []
+		);
+	}
 </script>
 
 <div class="chat-input">
+	<MentionSuggestions
+		bind:this={richText.mentionSuggestions}
+		suggestions={richText.suggestions}
+		selectSuggestion={richText.selectSuggestion}
+	>
+		{#snippet item(username)}
+			<div class="mention-suggestion">
+				{#if userMap.has(username)}
+					<img src={userMap.get(username)?.avatarUrl} alt={username} class="avatar" />
+				{/if}
+				<p class=" text-12 text-semibold name truncate">
+					{username}
+				</p>
+			</div>
+		{/snippet}
+	</MentionSuggestions>
 	<div class="chat-input__content-container">
-		<Textarea bind:value={message} unstyled autofocus onkeydown={handleKeyDown} />
+		<RichTextEditor
+			bind:this={richText.richTextEditor}
+			{getSuggestionItems}
+			onSuggestionStart={(p) => richText.onSuggestionStart(p)}
+			onSuggestionUpdate={(p) => richText.onSuggestionUpdate(p)}
+			onSuggestionExit={() => richText.onSuggestionExit()}
+			onSuggestionKeyDown={(event) => richText.onSuggestionKeyDown(event)}
+			onKeyDown={handleKeyDown}
+			onTextUpdate={(text) => (message = text)}
+		/>
 		<div class="chat-input__actions">
 			<div class="chat-input__secondary-actions">
 				<p>📋</p>
@@ -159,7 +243,7 @@
 	.chat-input {
 		flex-shrink: 0;
 		display: flex;
-		justify-content: space-between;
+		flex-direction: column;
 		padding: 16px;
 		border-top: 1px solid #ccc;
 	}
@@ -192,5 +276,17 @@
 	.chat-input__action-buttons {
 		display: flex;
 		gap: 4px;
+	}
+
+	.mention-suggestion {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+	}
+
+	.avatar {
+		width: 16px;
+		height: 16px;
+		border-radius: 50%;
 	}
 </style>
