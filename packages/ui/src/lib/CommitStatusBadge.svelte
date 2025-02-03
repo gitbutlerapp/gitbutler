d4573040164a136adf60f7697e544213a6e26434
Pavel Laptev <pawellaptew@gmail.com>
2025-02-03 01:22:45 +0100

    added: `icon` kind for `commit status badge`

--- packages/ui/src/lib/CommitStatusBadge.svelte
+++ packages/ui/src/lib/CommitStatusBadge.svelte
@@ -1,29 +1,49 @@
 <script lang="ts">
+	import Icon from '$lib/Icon.svelte';
+
 	type Props = {
-		status: 'approved' | 'changes-requested' | 'unreviewed' | 'in-discussion';
+		status: 'unreviewed' | 'in-discussion' | 'approved' | 'changes-requested';
+		kind?: 'icon' | 'text';
 	};
 
-	const { status = 'unreviewed' }: Props = $props();
+	const { status = 'unreviewed', kind = 'text' }: Props = $props();
+
+	function getIconName() {
+		if (status === 'approved') {
+			return 'tick-small';
+		} else if (status === 'changes-requested') {
+			return 'refresh-small';
+		} else if (status === 'in-discussion') {
+			return 'dialog-small';
+		} else {
+			return 'minus-small';
+		}
+	}
 </script>
 
 <div
 	class="status-badge"
+	class:status-badge_icon={kind === 'icon'}
 	class:status-badge_approved={status === 'approved'}
 	class:status-badge_changes-requested={status === 'changes-requested'}
 	class:status-badge_in-discussion={status === 'in-discussion'}
 	class:status-badge_unreviewed={status === 'unreviewed'}
 >
-	<span class="text-10 text-bold status-badge__text">
-		{#if status === 'changes-requested'}
-			Changes requested
-		{:else if status === 'approved'}
-			Approved
-		{:else if status === 'in-discussion'}
-			In discussion
-		{:else}
-			Unreviewed
-		{/if}
-	</span>
+	{#if kind === 'icon'}
+		<Icon name={getIconName()} />
+	{:else}
+		<span class="text-10 text-bold status-badge__text">
+			{#if status === 'changes-requested'}
+				Changes requested
+			{:else if status === 'approved'}
+				Approved
+			{:else if status === 'in-discussion'}
+				In discussion
+			{:else}
+				Unreviewed
+			{/if}
+		</span>
+	{/if}
 </div>
 
 <style lang="postcss">
@@ -56,5 +76,11 @@
 	.status-badge_unreviewed {
 		background-color: var(--clr-br-commit-unreviewed-bg);
 		color: var(--clr-br-commit-unreviewed-text);
+	}
+
+	.status-badge_icon {
+		flex-shrink: 0;
+		width: var(--size-icon);
+		max-width: var(--size-icon);
 	}
 </style>
