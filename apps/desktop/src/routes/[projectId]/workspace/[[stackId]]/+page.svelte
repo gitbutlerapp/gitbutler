9ad21a4fa84b776c2501747ad1b520ffd40594ae
Pavel Laptev <pawellaptew@gmail.com>
2025-02-05 15:29:02 +0100

    rename variable

--- apps/desktop/src/routes/[projectId]/workspace/[[stackId]]/+page.svelte
+++ apps/desktop/src/routes/[projectId]/workspace/[[stackId]]/+page.svelte
@@ -57,7 +57,7 @@
 	/** Offset width for tabs component. */
 	let width: number | undefined = $state();
 	/** Content area on the right for stack details. */
-	let right = $state<HTMLDivElement>();
+	let rightEl = $state<HTMLDivElement>();
 	/** Width of content area on the right. */
 	let rightWidth: number | undefined = $state();
 	/** True if content area should be rounded. */
@@ -68,8 +68,8 @@
 	});
 
 	onMount(() => {
-		const observer = new ResizeObserver(() => (rightWidth = right?.offsetWidth));
-		observer.observe(right!);
+		const observer = new ResizeObserver(() => (rightWidth = rightEl?.offsetWidth));
+		observer.observe(rightEl!);
 		return () => {
 			observer.disconnect();
 		};
@@ -90,7 +90,7 @@
 		/>
 		<WorktreeChanges {projectId} />
 	</div>
-	<div class="right" bind:this={right}>
+	<div class="right" bind:this={rightEl}>
 		<StackTabs {projectId} selectedId={stackId} previewing={$previewing} bind:width />
 		<div class="contents" class:rounded>
 			{#if stackId}
