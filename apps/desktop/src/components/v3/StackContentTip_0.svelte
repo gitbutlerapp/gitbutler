ab3e0a3aa4c2e1e0c00289a8f50232dc74d1f699
ndom91 <yo@ndo.dev>
2025-02-05 18:51:11 +0100

    feat: add placeholder area various "slides" components

---
+++ apps/desktop/src/components/v3/StackContentTip_0.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import WhatIsAStack from '$lib/assets/illustrations/what-is-a-stack-tip.svg?raw';
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="placeholder__svg" in:fade={{ duration: 250 }}>
+	{@html WhatIsAStack}
+</div>
+<div class="placeholder__text">
+	<div class="text-18 text-semibold text-clr1">What is a stack?</div>
+	<div class="text-13 text-clr2">
+		<p>
+			Stack is a workflow where branches are built sequentially, breaking large features into
+			smaller parts. Each branch depends on the previous one. You can also choose a single-branch
+			flow.
+		</p>
+	</div>
+</div>
+
+<style>
+	.placeholder__svg {
+		height: 200px;
+		width: 300px;
+	}
+
+	.placeholder__text {
+		text-align: left;
+		line-height: 1 !important;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+
+		& p {
+			line-height: 1.75;
+		}
+	}
+</style>
