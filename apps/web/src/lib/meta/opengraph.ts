d28b86629e984c31cda5df2bead9679d8545a637
Scott Chacon <schacon@gmail.com>
2025-02-05 16:21:30 +0100

    specify the image width and height

--- apps/web/src/lib/meta/opengraph.ts
+++ apps/web/src/lib/meta/opengraph.ts
@@ -7,6 +7,8 @@ export function fillMeta(html: string, url: string) {
         <meta property="og:title" content="GitButler" />
         <meta property="og:description" content="GitButler software development platform" />
         <meta property="og:image" content="%image%" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:url" content="${url}" />
         <meta property="og:site_name" content="GitButler" />
 
