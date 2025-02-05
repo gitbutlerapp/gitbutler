756dfb78eac1aa31dea2a1022c0c4d32d784d96a
ndom91 <yo@ndo.dev>
2025-02-05 14:14:43 +0100

    feat: add branch emptyState placeholder

--- apps/desktop/src/components/v3/Branch.svelte
+++ apps/desktop/src/components/v3/Branch.svelte
@@ -2,6 +2,7 @@
 	import BranchDividerLine from './BranchDividerLine.svelte';
 	import BranchCommitList from '$components/v3/BranchCommitList.svelte';
 	import BranchHeader from '$components/v3/BranchHeader.svelte';
+	import EmptyBranch from '$components/v3/EmptyBranch.svelte';
 	import { isStackedBranch } from '$components/v3/lib';
 	import type { WorkspaceBranch } from '$lib/branches/v3';
 
@@ -11,10 +12,13 @@
 		last: boolean;
 	}
 
-	const { branch, first }: Props = $props();
+	const { branch, first, last }: Props = $props();
 
 	const localAndRemoteCommits = $derived(
 		isStackedBranch(branch.state) ? branch.state.subject.localAndRemote : []
+	);
+	const upstreamOnlyCommits = $derived(
+		isStackedBranch(branch.state) ? branch.state.subject.upstreamOnly : []
 	);
 </script>
 
@@ -23,6 +27,9 @@
 {/if}
 <div class="branch" data-series-name={branch.name}>
 	<BranchHeader {branch} isTopBranch={first} />
+	{#if !localAndRemoteCommits.length && !upstreamOnlyCommits.length}
+		<EmptyBranch {last} />
+	{/if}
 	{#if isStackedBranch(branch.state)}
 		<BranchCommitList commits={branch.state.subject} />
 	{/if}
@@ -30,6 +37,7 @@
 
 <style>
 	.branch {
+		position: relative;
 		border: 1px solid var(--clr-border-2);
 		border-radius: var(--radius-m);
 		background: var(--clr-bg-1);
