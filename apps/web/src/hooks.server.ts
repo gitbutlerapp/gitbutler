d6a7885c6c804e3f3985807df169e79f4d591fb1
Caleb Owens <caleb@gitbutler.com>
2025-02-05 18:20:04 +0100

    Await server response for OG title
    As a note, I'm wondering if we ought to also be setting the bog-standard `<title>` element as well.

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -3,6 +3,6 @@ import { fillMeta } from '$lib/meta/opengraph';
 export async function handle({ event, resolve }) {
 	const currentUrl = event.url.href;
 	return resolve(event, {
-		transformPageChunk: ({ html }) => fillMeta(html, currentUrl)
+		transformPageChunk: async ({ html }) => await fillMeta(html, currentUrl)
 	});
 }
