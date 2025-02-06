71ce089b1b546c4f0328e258bfd73f2b46d205c3
estib <stron@me.com>
2025-02-06 10:28:01 +0100

    Message handler
    Extract the business logic of keeping track of the message content and sending it into a svelte class module.
    
    Also, keep track of the 'displayable' message content, without the mention embeddings

---
+++ apps/web/src/lib/chat/message.svelte.ts
@@ -0,0 +1,69 @@
+import { embedUserMention } from './mentions';
+import { ChatChannelsService } from '@gitbutler/shared/chat/chatChannelsService';
+import type { EditorInstance } from '@gitbutler/ui/RichTextEditor.svelte';
+
+export default class MessageHandler {
+	private _message = $state<string>();
+	private _displayMessage = $state<string>();
+
+	constructor(
+		private chatChannelService: ChatChannelsService,
+		private projectId: string,
+		private branchId: string,
+		private changeId: string
+	) {}
+
+	private updateMessage(editor: EditorInstance) {
+		this._message = editor?.getText({
+			textSerializers: {
+				mention: ({ node }) => {
+					const id = node.attrs.id;
+					const username = node.attrs.label;
+					if (!id) {
+						return '@' + username;
+					}
+
+					return embedUserMention(id);
+				}
+			}
+		});
+	}
+
+	private updateDisplayMessage(editor: EditorInstance) {
+		this._displayMessage = editor?.getText({
+			textSerializers: {
+				mention: ({ node }) => {
+					const username = node.attrs.label;
+					return '@' + username;
+				}
+			}
+		});
+	}
+
+	async send(issue?: boolean) {
+		if (this._message === undefined || this._message.trim() === '') {
+			return;
+		}
+
+		await this.chatChannelService.sendChatMessage({
+			projectId: this.projectId,
+			branchId: this.branchId,
+			changeId: this.changeId,
+			chat: this._message,
+			issue
+		});
+	}
+
+	update(editor: EditorInstance) {
+		this.updateMessage(editor);
+		this.updateDisplayMessage(editor);
+	}
+
+	get message() {
+		return this._message;
+	}
+
+	get displayMessage() {
+		return this._displayMessage;
+	}
+}
