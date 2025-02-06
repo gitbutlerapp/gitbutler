2461071bdfb56ff45bf9f21e03bfb651c8c25dd3
Mattias Granlund <mtsgrd@gmail.com>
2025-02-06 21:25:00 +0100

    Make v3 file dragging use svelte action as intended
    - we just nest one more div

--- apps/desktop/src/components/v3/FileListItemWrapper.svelte
+++ apps/desktop/src/components/v3/FileListItemWrapper.svelte
@@ -2,7 +2,7 @@
 <script lang="ts">
 	import FileContextMenu from '$components/v3/FileContextMenu.svelte';
 	import { BranchStack } from '$lib/branches/branch';
-	import { draggableChips, type DraggableConfig } from '$lib/dragging/draggable';
+	import { draggableChips } from '$lib/dragging/draggable';
 	import { ChangeDropData } from '$lib/dragging/draggables';
 	import { getFilename } from '$lib/files/utils';
 	import { IdSelection } from '$lib/selection/idSelection.svelte';
@@ -40,72 +40,49 @@
 	let draggableEl: HTMLDivElement | undefined = $state();
 	let indeterminate = $state(false);
 	let checked = $state(false);
-
-	// TODO: Refactor to use this as a Svelte action, e.g. `use:draggableChips()`.
-	let chips:
-		| {
-				update: (opts: DraggableConfig) => void;
-				destroy: () => void;
-		  }
-		| undefined;
-
-	// Manage the lifecycle of the draggable chips.
-	$effect(() => {
-		if (draggableEl) {
-			const dropData = new ChangeDropData(stackId || '', change, idSelection, commitId);
-			const config: DraggableConfig = {
-				label: getFilename(change.path),
-				filePath: change.path,
-				data: dropData,
-				viewportId: 'board-viewport',
-				selector: '.selected-draggable'
-			};
-			if (chips) {
-				chips.update(config);
-			} else {
-				chips = draggableChips(draggableEl, config);
-			}
-		} else {
-			chips?.destroy();
-		}
-
-		return () => {
-			chips?.destroy();
-		};
-	});
 </script>
 
-<FileContextMenu
-	bind:this={contextMenu}
-	trigger={draggableEl}
-	isUnapplied={false}
-	branchId={$stack?.id}
-	isBinary={false}
-/>
-
-<FileListItem
-	id={key(change.path, commitId)}
-	bind:ref={draggableEl}
-	filePath={change.path}
-	fileStatus={computeChangeStatus(change)}
-	{selected}
-	{showCheckbox}
-	{checked}
-	{indeterminate}
-	draggable={true}
-	{onclick}
-	{onkeydown}
-	locked={false}
-	conflicted={false}
-	oncontextmenu={(e) => {
-		const changes = idSelection.treeChanges(projectId);
-		if (idSelection.has(change.path, commitId)) {
-			contextMenu?.open(e, { files: changes });
-		} else {
-			contextMenu?.open(e, { files: [change] });
-		}
+<div
+	bind:this={draggableEl}
+	use:draggableChips={{
+		label: getFilename(change.path),
+		filePath: change.path,
+		data: new ChangeDropData(stackId || '', change, idSelection, commitId),
+		viewportId: 'board-viewport',
+		selector: '.selected-draggable'
 	}}
-/>
+>
+	<FileContextMenu
+		bind:this={contextMenu}
+		trigger={draggableEl}
+		isUnapplied={false}
+		branchId={$stack?.id}
+		isBinary={false}
+	/>
+
+	<FileListItem
+		id={key(change.path, commitId)}
+		filePath={change.path}
+		fileStatus={computeChangeStatus(change)}
+		{selected}
+		{showCheckbox}
+		{checked}
+		{indeterminate}
+		draggable={true}
+		{onclick}
+		{onkeydown}
+		locked={false}
+		conflicted={false}
+		oncontextmenu={(e) => {
+			const changes = idSelection.treeChanges(projectId);
+			if (idSelection.has(change.path, commitId)) {
+				contextMenu?.open(e, { files: changes });
+			} else {
+				contextMenu?.open(e, { files: [change] });
+			}
+		}}
+	/>
+</div>
 
 <style lang="postcss">
 	/* blah */
