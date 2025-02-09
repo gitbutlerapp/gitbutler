68ec4bbaa28bb75db25aec336fd02fc0354027ef
Mattias Granlund <mtsgrd@gmail.com>
2025-02-09 11:59:24 +0100

    Fix the return type of the redux base query
    - noticed some incorrectness when poking at `transformResponse`

--- apps/desktop/src/lib/state/backendQuery.ts
+++ apps/desktop/src/lib/state/backendQuery.ts
@@ -1,16 +1,16 @@
 import { Tauri } from '$lib/backend/tauri';
 import { isBackendError } from '$lib/error/typeguards';
-import { type BaseQueryApi, type BaseQueryFn } from '@reduxjs/toolkit/query';
+import { type BaseQueryApi, type QueryReturnValue } from '@reduxjs/toolkit/query';
 
-export function tauriBaseQuery<T>(
+export async function tauriBaseQuery(
 	args: ApiArgs,
 	api: BaseQueryApi
-): ReturnType<BaseQueryFn<ApiArgs, Promise<T>, TauriCommandError, object, object>> {
+): Promise<QueryReturnValue<unknown, TauriCommandError, undefined>> {
 	if (!hasTauriExtra(api.extra)) {
 		return { error: { message: 'Redux dependency Tauri not found!' } };
 	}
 	try {
-		return { data: api.extra.tauri.invoke(args.command, args.params) };
+		return { data: await api.extra.tauri.invoke(args.command, args.params) };
 	} catch (error: unknown) {
 		if (isBackendError(error)) {
 			return { error: { message: error.message, code: error.code } };
