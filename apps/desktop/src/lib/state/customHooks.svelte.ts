e66ae3ed873ef8549c069c52db9ade5255adef9a
Mattias Granlund <mtsgrd@gmail.com>
2025-02-10 21:58:59 +0100

    Move hook context into separate file

--- apps/desktop/src/lib/state/customHooks.svelte.ts
+++ apps/desktop/src/lib/state/customHooks.svelte.ts
@@ -10,19 +10,7 @@ import {
 	type QueryDefinition,
 	type RootState
 } from '@reduxjs/toolkit/query';
-
-/**
- *	The api is necessary to create the store, so we need to provide
- *	a way for them to access state and dispatch. In react it's possible
- *	to use the application context since it is available to events
- *	fired by components, while Svelte requires `getContext` only be
- *	used during component initialization.
- */
-export type HookContext = {
-	/** Without the nested function we get looping reactivity.  */
-	getState: () => () => RootState<any, any, any>;
-	getDispatch: () => ThunkDispatch<any, any, UnknownAction>;
-};
+import type { HookContext } from './context';
 
 /**
  * Returns implementations for custom endpoint methods defined in `ButlerModule`.
