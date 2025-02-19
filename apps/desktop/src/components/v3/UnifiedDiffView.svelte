e7ae7c54fa66d3e68c34c73a5cd6f12caea2954d
ndom91 <yo@ndo.dev>
2025-02-19 13:11:32 +0100

    feat: show UnifiedDiff in commit file contents and remove change querying from inside UnifiedDiffView

--- apps/desktop/src/components/v3/UnifiedDiffView.svelte
+++ apps/desktop/src/components/v3/UnifiedDiffView.svelte
@@ -2,34 +2,24 @@
 	import ReduxResult from '../ReduxResult.svelte';
 	import { DiffService } from '$lib/hunks/diffService.svelte';
 	import { ChangeSelectionService } from '$lib/selection/changeSelection.svelte';
-	import { WorktreeService } from '$lib/worktree/worktreeService.svelte';
 	import { inject } from '@gitbutler/shared/context';
 	import HunkDiff from '@gitbutler/ui/HunkDiff.svelte';
+	import type { TreeChange } from '$lib/hunks/change';
 	import type { DiffHunk } from '$lib/hunks/hunk';
 
 	type Props = {
-		path: string;
-		commitId?: string;
 		projectId: string;
 		selectable: boolean;
+		change: TreeChange;
 	};
 
-	const { projectId, path, selectable = false }: Props = $props();
-	const [worktreeService, diffService, changeSelection] = inject(
-		WorktreeService,
-		DiffService,
-		ChangeSelectionService
-	);
+	const { projectId, selectable = false, change }: Props = $props();
+	const [diffService, changeSelection] = inject(DiffService, ChangeSelectionService);
+	const diffQuery = $derived(diffService.getDiff(projectId, change).current);
 
-	const changeQuery = $derived(worktreeService.getChange(projectId, path).current);
-	const change = $derived(changeQuery.data);
-	const diffQuery = $derived(
-		changeQuery.andThen((change) => diffService.getDiff(projectId, change)).current
-	);
-
-	const selection = $derived(changeSelection.getById(path).current);
+	const selection = $derived(changeSelection.getById(change.path).current);
 	const pathData = $derived({
-		path,
+		path: change.path,
 		pathBytes: change!.pathBytes,
 		previousPathBytes: change!.previousPathBytes
 	});
@@ -73,7 +63,7 @@
 						return h.newStart !== hunk.newStart && h.newLines !== hunk.newLines;
 					});
 					if (hunks.length === 0) {
-						changeSelection.remove(path);
+						changeSelection.remove(change.path);
 					} else {
 						changeSelection.update({
 							type: 'partial',
@@ -99,22 +89,26 @@
 	<ReduxResult result={diffQuery}>
 		{#snippet children(diff)}
 			{#if diff.type === 'Patch'}
-				{#each diff.subject.hunks as hunk}
-					<HunkDiff
-						filePath={path}
-						hunkStr={hunk.diff}
-						selected={selectable
-							? selection &&
-								(selection.type === 'full' ||
-									selection.hunks.some((h) => h.newStart === hunk.newStart))
-								? true
-								: false
-							: undefined}
-						onchange={(selected) => {
-							onchange(hunk, selected, diff.subject.hunks);
-						}}
-					/>
-				{/each}
+				{#if diff.subject.hunks.length}
+					{#each diff.subject.hunks as hunk}
+						<HunkDiff
+							filePath={change.path}
+							hunkStr={hunk.diff}
+							selected={selectable
+								? selection &&
+									(selection.type === 'full' ||
+										selection.hunks.some((h) => h.newStart === hunk.newStart))
+									? true
+									: false
+								: undefined}
+							onchange={(selected) => {
+								onchange(hunk, selected, diff.subject.hunks);
+							}}
+						/>
+					{/each}
+				{:else}
+					<span class="text-14 hunk-content-warning">No content</span>
+				{/if}
 			{:else if diff.type === 'TooLarge'}
 				Too large!
 			{:else if diff.type === 'Binary'}
@@ -134,5 +128,8 @@
 		align-self: stretch;
 		overflow-x: hidden;
 		max-width: 100%;
+	}
+	.hunk-content-warning {
+		margin-left: 8px;
 	}
 </style>
