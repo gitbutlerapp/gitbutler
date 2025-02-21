6e9902ae746f2c49177566cbce652acafca89811
Pavel Laptev <pawellaptew@gmail.com>
2025-02-21 16:41:17 +0100

    added editor footer and header components

--- apps/desktop/src/components/v3/NewCommit.svelte
+++ apps/desktop/src/components/v3/NewCommit.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import EditorFooter from './editor/EditorFooter.svelte';
+	import EditorHeader from './editor/EditorHeader.svelte';
 	import { BaseBranchService } from '$lib/baseBranch/baseBranchService';
 	import { showError } from '$lib/notifications/toasts';
 	import { stackPath } from '$lib/routes/routes.svelte';
@@ -9,8 +11,6 @@
 	import { emojiTextNodeTransform } from '$lib/textEditor/plugins/emojiPlugin';
 	import { getContext } from '@gitbutler/shared/context';
 	import { persisted } from '@gitbutler/shared/persisted';
-	import Button from '@gitbutler/ui/Button.svelte';
-	import Toggle from '@gitbutler/ui/Toggle.svelte';
 	import {
 		$convertToMarkdownString as convertToMarkdownString,
 		$convertFromMarkdownString as convertFromMarkdownString
@@ -38,21 +38,7 @@
 		CodeHighlightPlugin,
 		CodeActionMenuPlugin,
 		MarkdownShortcutPlugin,
-		ALL_TRANSFORMERS,
-		Toolbar,
-		UnderlineButton,
-		FormatCodeButton,
-		InsertLink,
-		BlockFormatDropDown,
-		ParagraphDropDownItem,
-		HeadingDropDownItem,
-		BulletDropDrownItem,
-		NumberDropDrownItem,
-		CheckDropDrownItem,
-		QuoteDropDrownItem,
-		CodeDropDrownItem,
-		BoldButton,
-		ItalicButton
+		ALL_TRANSFORMERS
 	} from 'svelte-lexical';
 	import { goto } from '$app/navigation';
 
@@ -191,70 +177,38 @@
 </script>
 
 <div class="new-commit">
+	<EditorHeader title="New commit" onRichTextEditorSwitch={(bool) => ($useMarkdown = bool)} />
 	<Composer {initialConfig} bind:this={composer}>
-		<div class="editor-shell">
-			<Toolbar>
-				{#snippet children({ editor, activeEditor })}
-					<BoldButton />
-					<ItalicButton />
-					<UnderlineButton />
-					<FormatCodeButton />
-					<InsertLink />
-					{#if activeEditor === editor}
-						<BlockFormatDropDown>
-							<ParagraphDropDownItem />
-							<HeadingDropDownItem headingSize="h1" />
-							<HeadingDropDownItem headingSize="h2" />
-							<HeadingDropDownItem headingSize="h3" />
-							<BulletDropDrownItem />
-							<NumberDropDrownItem />
-							<CheckDropDrownItem />
-							<QuoteDropDrownItem />
-							<CodeDropDrownItem />
-						</BlockFormatDropDown>
-					{/if}
-					<div class="markdown-box">
-						md
-						<Toggle bind:checked={$useMarkdown} />
-					</div>
-				{/snippet}
-			</Toolbar>
-			<div class="editor-container" bind:this={editorDiv}>
-				<div class="editor-scroller">
-					<div class="editor">
-						<ContentEditable />
-						<PlaceHolder>{placeholder}</PlaceHolder>
-					</div>
+		<div class="editor-container" bind:this={editorDiv}>
+			<div class="editor-scroller">
+				<div class="editor">
+					<ContentEditable />
+					<PlaceHolder>{placeholder}</PlaceHolder>
 				</div>
-				{#if $useMarkdown}
-					<AutoFocusPlugin />
-					<AutoLinkPlugin />
-					<CheckListPlugin />
-					<CodeActionMenuPlugin anchorElem={editorDiv} />
-					<CodeHighlightPlugin />
-					<FloatingLinkEditorPlugin anchorElem={editorDiv} />
-					<HashtagPlugin />
-					<ListPlugin />
-					<MarkdownShortcutPlugin transformers={ALL_TRANSFORMERS} />
-					<RichTextPlugin />
-					<SharedHistoryPlugin />
-				{:else}
-					<PlainTextPlugin />
-				{/if}
 			</div>
+
+			{#if $useMarkdown}
+				<AutoFocusPlugin />
+				<AutoLinkPlugin />
+				<CheckListPlugin />
+				<CodeActionMenuPlugin anchorElem={editorDiv} />
+				<CodeHighlightPlugin />
+				<FloatingLinkEditorPlugin anchorElem={editorDiv} />
+				<HashtagPlugin />
+				<ListPlugin />
+				<MarkdownShortcutPlugin transformers={ALL_TRANSFORMERS} />
+				<RichTextPlugin />
+				<SharedHistoryPlugin />
+			{:else}
+				<PlainTextPlugin />
+			{/if}
 		</div>
 	</Composer>
-	<div class="actions">
-		<Button
-			kind="outline"
-			style="neutral"
-			width={96}
-			onclick={() => goto(stackPath(projectId, stackId))}
-		>
-			Cancel
-		</Button>
-		<Button style="pop" wide onclick={createCommit}>Create commit!</Button>
-	</div>
+	<EditorFooter
+		SubmitButtonLabel="Create commit!"
+		onSubmit={createCommit}
+		onCancel={() => goto(stackPath(projectId, stackId))}
+	/>
 </div>
 
 <style>
@@ -266,15 +220,6 @@
 		background: var(--clr-bg-1);
 	}
 
-	.editor-shell {
-		display: flex;
-		flex-direction: column;
-		flex-grow: 1;
-		color: var(--clr-text-1);
-		position: relative;
-		line-height: var(--text-lineheight-default);
-	}
-
 	.editor-container {
 		flex-grow: 1;
 		background-color: var(--clr-bg-1);
@@ -284,16 +229,5 @@
 
 	.editor-scroller {
 		height: 100%;
-	}
-
-	.markdown-box {
-		flex-grow: 1;
-		text-align: right;
-	}
-	.actions {
-		display: flex;
-		gap: 6px;
-		padding: 16px;
-		border-top: 1px solid var(--clr-border-2);
 	}
 </style>
---
+++ apps/desktop/src/components/v3/editor/EditorFooter.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Button from '@gitbutler/ui/Button.svelte';
+
+	interface Props {
+		SubmitButtonLabel: string;
+		CancelButtonLabel?: string;
+		onSubmit: () => void;
+		onCancel: () => void;
+	}
+
+	const { SubmitButtonLabel, CancelButtonLabel = 'Cancel', onSubmit, onCancel }: Props = $props();
+</script>
+
+<div class="editor-footer">
+	<Button kind="outline" style="neutral" onclick={onCancel} width={94}>{CancelButtonLabel}</Button>
+	<Button style="pop" onclick={onSubmit} wide>{SubmitButtonLabel}</Button>
+</div>
+
+<style lang="postcss">
+	.editor-footer {
+		display: flex;
+		align-items: center;
+		gap: 6px;
+		padding: 16px;
+		border-top: 1px solid var(--clr-border-2);
+	}
+</style>
