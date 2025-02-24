339dd14647768081aa3e7861b41ea3bd4b8e71da
Mattias Granlund <mtsgrd@gmail.com>
2025-02-24 23:31:08 +0100

    Add "commit goes here" section in commit view

--- .conflict-side-1/apps/desktop/src/components/v3/NewCommit.svelte
+++ .conflict-side-1/apps/desktop/src/components/v3/NewCommit.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import CommitGoesHere from './CommitGoesHere.svelte';
 	import CommitMessageEditor from './editor/CommitMessageEditor.svelte';
 	import EditorFooter from './editor/EditorFooter.svelte';
 	import EditorHeader from './editor/EditorHeader.svelte';
 	import { BaseBranchService } from '$lib/baseBranch/baseBranchService';
 	import { showError } from '$lib/notifications/toasts';
-	import { stackPath } from '$lib/routes/routes.svelte';
+	import { createCommitPath, stackPath } from '$lib/routes/routes.svelte';
 	import { ChangeSelectionService } from '$lib/selection/changeSelection.svelte';
 	import { StackService } from '$lib/stacks/stackService.svelte';
 	import { getContext } from '@gitbutler/shared/context';
@@ -12,8 +13,14 @@
 	import Button from '@gitbutler/ui/Button.svelte';
 	import { goto } from '$app/navigation';
 
-	type Props = { projectId: string; stackId: string; branchName: string };
-	const { projectId, stackId, branchName }: Props = $props();
+	type Props = {
+		projectId: string;
+		stackId: string;
+		branchName: string;
+		// The parent of the new commit.
+		commitId?: string;
+	};
+	const { projectId, stackId, branchName, commitId }: Props = $props();
 
 	const baseBranchService = getContext(BaseBranchService);
 	const stackService = getContext(StackService);
@@ -27,15 +34,18 @@
 	 */
 	let markdown = persisted(true, 'useMarkdown__' + projectId);
 
-	const commitsResult = $derived(stackService.commitAt(projectId, stackId, branchName, 0).current);
-	const topCommit = $derived(commitsResult.data);
+	const commitResult = $derived(stackService.commitAt(projectId, stackId, branchName, 0).current);
+	const commit = $derived(commitResult.data);
+
+	const baseSha = $derived($base?.baseSha);
+	const defaultParentId = $derived(commit ? commit.id : baseSha);
+	const parentId = $derived(commitId ? commitId : defaultParentId);
 
 	/**
 	 * At the moment this code can only commit to the tip of the stack.
 	 *
 	 * TODO: Implement according to design.
 	 */
-	const commitParent = $derived(topCommit ? topCommit.id : $base?.baseSha);
 	let composer: CommitMessageEditor | undefined = $state();
 
 	/**
@@ -52,9 +62,12 @@
 	}
 
 	async function _createCommit(message: string) {
+		if (!parentId) {
+			throw new Error('No parent id provided.');
+		}
 		await stackService.createCommit(projectId, {
 			stackId,
-			parentId: commitParent!,
+			parentId,
 			message: message,
 			worktreeChanges: selection.map((item) =>
 				item.type === 'full'
@@ -74,19 +87,39 @@
 </script>
 
 <div class="new-commit">
-	<EditorHeader title="New commit" bind:markdown={$markdown} />
-	<CommitMessageEditor bind:this={composer} bind:markdown={$markdown} />
-	<EditorFooter onCancel={() => goto(stackPath(projectId, stackId))}>
-		<Button style="pop" onclick={createCommit} wide>Create commit</Button>
-	</EditorFooter>
+	<div class="left">
+		<EditorHeader title="New commit" bind:markdown={$markdown} />
+		<CommitMessageEditor bind:this={composer} bind:markdown={$markdown} />
+		<EditorFooter onCancel={() => goto(stackPath(projectId, stackId))}>
+			<Button style="pop" onclick={createCommit} wide>Create commit</Button>
+		</EditorFooter>
+	</div>
+	{#if parentId}
+		<div class="right">
+			<CommitGoesHere {projectId} {stackId} {branchName} {parentId} />
+		</div>
+	{/if}
 </div>
 
 <style>
 	.new-commit {
 		display: flex;
+		flex-grow: 1;
+	}
+	.left {
+		display: flex;
 		flex-direction: column;
 		flex-grow: 1;
 		height: 100%;
 		background: var(--clr-bg-1);
+	}
+	.right {
+		width: 300px;
+		background-image: radial-gradient(
+			oklch(from var(--clr-scale-ntrl-50) l c h / 0.5) 0.6px,
+			#ffffff00 0.6px
+		);
+		background-size: 6px 6px;
+		border-left: 1px solid var(--clr-border-2);
 	}
 </style>
