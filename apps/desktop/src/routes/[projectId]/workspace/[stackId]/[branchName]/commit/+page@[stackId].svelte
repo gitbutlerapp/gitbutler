2d096a86714342432f2bea3a531f51ea9e496c1d
Mattias Granlund <mtsgrd@gmail.com>
2025-02-24 23:31:08 +0100

    Add "commit goes here" section in commit view

--- apps/desktop/src/routes/[projectId]/workspace/[stackId]/[branchName]/commit/+page@[stackId].svelte
+++ apps/desktop/src/routes/[projectId]/workspace/[stackId]/[branchName]/commit/+page@[stackId].svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import NewCommit from '$components/v3/NewCommit.svelte';
-	import type { PageData } from './$types';
 	import { page } from '$app/state';
 
-	const { data }: { data: PageData } = $props();
-
-	const projectId = $derived(data.projectId);
-	const stackId = $derived(page.params.stackId!);
-	const branchName = $derived(page.params.branchName!);
+	const projectId = $derived(page.params.projectId);
+	const stackId = $derived(page.params.stackId);
+	const branchName = $derived(page.params.branchName);
+	const commitId = $derived(page.url.searchParams.get('commitId') || undefined);
 </script>
 
-<NewCommit {projectId} {stackId} {branchName} />
+{#if projectId && stackId && branchName}
+	<NewCommit {projectId} {stackId} {branchName} {commitId} />
+{:else}
+	Incorrect parameters.
+{/if}
