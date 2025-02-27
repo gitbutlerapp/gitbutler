710fc722f405019a3be59276ad5ccefeaaa16de2
Nico Domino <yo@ndo.dev>
2025-02-27 01:26:52 +0100

    fix: v3 workspace design tweaks (#7427)
    * fix: branch card border radius
    
    * fix: rm branch card header border-bottom
    
    * fix: adjust branch listing padding
    
    * add top border to empty branch
    
    * added pointing arrow, fix styles
    
    * align commit lines
    
    ---------
    
    Co-authored-by: Pavel Laptev <pawellaptew@gmail.com>

--- .conflict-side-1/apps/desktop/src/lib/commits/commitService.svelte.ts
+++
@@ -1,9 +0,0 @@
-import { Commit } from './commit';
-import { invoke } from '$lib/backend/ipc';
-import { plainToInstance } from 'class-transformer';
-
-export class CommitService {
-	async find(projectId: string, commitOid: string) {
-		return plainToInstance(Commit, await invoke<Commit>('find_commit', { projectId, commitOid }));
-	}
-}
--- .conflict-side-1/apps/web/src/routes/(app)/projects/[projectId]/branches/[branchId]/versions/+page.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line func-style
-export const load = ({ params }) => {
-	return {
-		projectId: params.projectId,
-		branchId: params.branchId
-	};
-};
