984be92fbc57ede74996331ecbea76ba1736e71c
Pavel Laptev <pawellaptew@gmail.com>
2025-03-02 04:17:49 +0100

    Update minimized chat look and behaviour (#7470)
    * chat fullscreen mode
    
    * minimize chat when enter tablet mode
    
    * Update +page.svelte

--- .conflict-base-0/apps/web/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import { fillMeta } from '$lib/meta/opengraph';
-
-export async function handle({ event, resolve }) {
-	const currentUrl = event.url.href;
-	return resolve(event, {
-		transformPageChunk: async ({ html }) => await fillMeta(html, currentUrl)
-	});
-}
