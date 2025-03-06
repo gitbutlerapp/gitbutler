2fc0141e8bfe7449e62a7e4c622437e13fd5a3f2
Mattias Granlund <mtsgrd@gmail.com>
2025-03-06 11:19:39 +0100

    Bits and bobs

--- apps/desktop/src/routes/[projectId]/workspace/[stackId]/[branchName]/+page.svelte
+++ apps/desktop/src/routes/[projectId]/workspace/[stackId]/[branchName]/+page.svelte
@@ -2,7 +2,7 @@
 	import ReduxResult from '$components/ReduxResult.svelte';
 	import CommitView from '$components/v3/CommitView.svelte';
 	import StackContentPlaceholder from '$components/v3/StackContentPlaceholder.svelte';
-	import { branchPath, commitPath } from '$lib/routes/routes.svelte';
+	import { commitPath } from '$lib/routes/routes.svelte';
 	import { StackService } from '$lib/stacks/stackService.svelte';
 	import { combineResults } from '$lib/state/helpers';
 	import { getContext } from '@gitbutler/shared/context';
@@ -19,30 +19,26 @@
 	{@const firstLocalAndRemote = stackService.commitAt(projectId, stackId, branchName, 0).current}
 	{@const firstUpstream = stackService.upstreamCommitAt(projectId, stackId, branchName, 0).current}
 
-	<ReduxResult result={combineResults(firstLocalAndRemote, firstUpstream)}>
-		{#snippet children([firstLocalAndRemote, firstUpstream])}
-			{@const firstCommitId = firstLocalAndRemote ? firstLocalAndRemote.id : firstUpstream?.id}
-			{@const upstream = !firstLocalAndRemote && !!firstUpstream}
-			{#if !commitId && firstCommitId}
-				{goto(
-					commitPath(projectId, {
-						stackId,
-						branchName,
-						commitId: firstCommitId,
-						upstream
-					})
-				)}
-			{/if}
-		{/snippet}
-	</ReduxResult>
-
 	{#if commitId}
-		<CommitView
-			{projectId}
-			commitKey={{ stackId, branchName, commitId, upstream }}
-			onClose={() => goto(branchPath(projectId, stackId, branchName))}
-		/>
+		<CommitView {projectId} commitKey={{ stackId, branchName, commitId, upstream }} />
 	{:else}
-		<StackContentPlaceholder isNewStack={true} />
+		<ReduxResult result={combineResults(firstLocalAndRemote, firstUpstream)}>
+			{#snippet children([firstLocalAndRemote, firstUpstream])}
+				{@const firstCommitId = firstLocalAndRemote ? firstLocalAndRemote.id : firstUpstream?.id}
+				{@const upstream = !firstLocalAndRemote && !!firstUpstream}
+				{#if firstCommitId}
+					{goto(
+						commitPath(projectId, {
+							stackId,
+							branchName,
+							commitId: firstCommitId,
+							upstream
+						})
+					)}
+				{:else}
+					<StackContentPlaceholder isNewStack={true} />
+				{/if}
+			{/snippet}
+		</ReduxResult>
 	{/if}
 {/if}
