1d5792c5e292882a1a59da996d0d461ecd47e5af
estib <stron@me.com>
2025-03-07 16:38:41 +0100

    Let the sun shine on the new Commit Context Menu
    The commit context menu for v3 is now correctly displayed on right click or kebab-menu click

--- apps/desktop/src/components/v3/CommitRow.svelte
+++ apps/desktop/src/components/v3/CommitRow.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+	import CommitContextMenu from '$components/v3/CommitContextMenu.svelte';
 	import CommitLine from '$components/v3/CommitLine.svelte';
+	import { BaseBranch } from '$lib/baseBranch/baseBranch';
+	import { BranchController } from '$lib/branches/branchController';
+	import { ModeService } from '$lib/mode/modeService';
+	import { getContext, getContextStore, maybeGetContext } from '@gitbutler/shared/context';
+	import Button from '@gitbutler/ui/Button.svelte';
+	import ContextMenu from '@gitbutler/ui/ContextMenu.svelte';
 	import Icon from '@gitbutler/ui/Icon.svelte';
+	import Modal from '@gitbutler/ui/Modal.svelte';
+	import PopoverActionsContainer from '@gitbutler/ui/popoverActions/PopoverActionsContainer.svelte';
+	import PopoverActionsItem from '@gitbutler/ui/popoverActions/PopoverActionsItem.svelte';
 	import type { Commit, UpstreamCommit } from '$lib/branches/v3';
 	import type { CommitKey } from '$lib/commits/commit';
 
-	interface Props {
+	type Props = {
 		projectId: string;
+		branchName: string;
+		stackId: string;
 		commitKey: CommitKey;
 		commit: Commit | UpstreamCommit;
 		first?: boolean;
@@ -16,9 +28,12 @@
 		opacity?: number;
 		borderTop?: boolean;
 		href?: string;
-	}
+		disableCommitActions?: boolean;
+	};
 
 	const {
+		stackId,
+		branchName,
 		commit,
 		first,
 		lastCommit,
@@ -27,36 +42,153 @@
 		lineColor,
 		opacity,
 		borderTop,
-		href
+		href,
+		disableCommitActions = false
 	}: Props = $props();
 
+	const baseBranch = getContextStore(BaseBranch);
+	const branchController = getContext(BranchController);
+	const modeService = maybeGetContext(ModeService);
+
 	const commitTitle = $derived(commit.message.split('\n')[0]);
+	const commitUrl = undefined;
+	const conflicted = false; // TODO
+	const isAncestorMostConflicted = false; // TODO
+	const isUnapplied = false; // TODO
+	const branchRefName = undefined;
+
+	let commitRowElement = $state<HTMLAnchorElement>();
+	let kebabMenuTrigger = $state<HTMLButtonElement>();
+	let contextMenu = $state<ReturnType<typeof ContextMenu>>();
+	let conflictResolutionConfirmationModal = $state<ReturnType<typeof Modal>>();
+
+	let isOpenedByKebabButton = $state(false);
+
+	function handleUncommit() {
+		if (!$baseBranch) {
+			console.error('Unable to undo commit');
+			return;
+		}
+		branchController.undoCommit(stackId, branchName, commit.id);
+	}
+
+	function openCommitMessageModal() {
+		// TODO: Implement openCommitMessageModal
+	}
+
+	function canEdit() {
+		if (isUnapplied) return false;
+		if (!modeService) return false;
+
+		return true;
+	}
+
+	async function editPatch() {
+		if (!canEdit() || !branchRefName) return;
+		modeService!.enterEditMode(commit.id, branchRefName);
+	}
+
+	async function handleEditPatch() {
+		if (conflicted && !isAncestorMostConflicted) {
+			conflictResolutionConfirmationModal?.show();
+			return;
+		}
+		await editPatch();
+	}
 </script>
 
 <a
+	bind:this={commitRowElement}
 	type="button"
 	class="commit"
-	class:first
-	class:lastCommit
-	class:selected
-	style:opacity
-	class:border-top={borderTop || first}
 	{href}
+	oncontextmenu={(e) => {
+		e.preventDefault();
+		isOpenedByKebabButton = false;
+		contextMenu?.open(e);
+	}}
 >
-	<CommitLine {commit} {lastCommit} {lastBranch} {lineColor} />
-	<div class="commit-content">
-		<span class="commit-title text-13 text-semibold">
-			{commitTitle}
-		</span>
+	<div
+		class="commit-row__main"
+		class:first
+		class:lastCommit
+		class:selected
+		style:opacity
+		class:border-top={borderTop || first}
+	>
+		<CommitLine {commit} {lastCommit} {lastBranch} {lineColor} />
 
-		<div class="commit-arrow">
-			<Icon name="chevron-right" />
+		<div class="commit-content">
+			<span class="commit-title text-13 text-semibold">
+				{commitTitle}
+			</span>
+
+			<div class="commit-arrow">
+				<Icon name="chevron-right" />
+			</div>
 		</div>
 	</div>
+
+	{#if !disableCommitActions}
+		<PopoverActionsContainer class="commit-row-actions-menu" thin stayOpen={isOpenedByKebabButton}>
+			<PopoverActionsItem
+				bind:el={kebabMenuTrigger}
+				activated={isOpenedByKebabButton}
+				icon="kebab"
+				tooltip="More options"
+				thin
+				onclick={() => {
+					contextMenu?.toggle();
+				}}
+			/>
+		</PopoverActionsContainer>
+	{/if}
 </a>
+
+<Modal bind:this={conflictResolutionConfirmationModal} width="small" onSubmit={editPatch}>
+	{#snippet children()}
+		<div>
+			<p>It's generally better to start resolving conflicts from the bottom up.</p>
+			<br />
+			<p>Are you sure you want to resolve conflicts for this commit?</p>
+		</div>
+	{/snippet}
+	{#snippet controls(close)}
+		<Button kind="outline" type="reset" onclick={close}>Cancel</Button>
+		<Button style="pop" type="submit">Yes</Button>
+	{/snippet}
+</Modal>
+
+<CommitContextMenu
+	leftClickTrigger={kebabMenuTrigger}
+	rightClickTrigger={commitRowElement}
+	onToggle={(isOpen, isLeftClick) => {
+		if (isLeftClick) {
+			isOpenedByKebabButton = isOpen;
+		}
+	}}
+	bind:menu={contextMenu}
+	baseBranch={$baseBranch}
+	{stackId}
+	{commit}
+	{commitUrl}
+	onUncommitClick={handleUncommit}
+	onEditMessageClick={openCommitMessageModal}
+	onPatchEditClick={handleEditPatch}
+/>
 
 <style lang="postcss">
 	.commit {
+		position: relative;
+		display: flex;
+		align-items: center;
+		width: 100%;
+
+		&:hover :global(.commit-row-actions-menu) {
+			--show: true;
+		}
+	}
+	.commit-row__main {
 		position: relative;
 		display: flex;
 		align-items: center;
