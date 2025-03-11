0d08efabd34b955f4a9e0717fdb75dc2951553e9
estib <stron@me.com>
2025-03-11 18:44:21 +0100

    rich text: File upload plugin
    Listen for dropped files, upload and embed them

---
+++ packages/ui/src/lib/richText/plugins/FileUpload.svelte
@@ -0,0 +1,58 @@
+<script lang="ts" module>
+	export type DropFileResult = {
+		name: string;
+		url: string;
+		isImage: boolean;
+	};
+</script>
+
+<script lang="ts">
+	import { getEditor } from '../context';
+	import { insertTextAtCaret } from '../selection';
+	import { COMMAND_PRIORITY_CRITICAL, DROP_COMMAND } from 'lexical';
+
+	type Props = {
+		onDrop: (files: FileList | undefined) => Promise<DropFileResult[]>;
+	};
+
+	const { onDrop }: Props = $props();
+
+	const editor = getEditor();
+
+	function embedLinkMD(url: string, text: string) {
+		return `[${text}](${url})`;
+	}
+
+	function embedImageMD(url: string, alt: string) {
+		return `![${alt}](${url})`;
+	}
+
+	function embedDroppedFile(file: DropFileResult) {
+		return file.isImage ? embedImageMD(file.url, file.name) : embedLinkMD(file.url, file.name);
+	}
+
+	async function handleDrop(files: FileList | undefined) {
+		if (!files) return;
+
+		const results = await onDrop(files);
+		results.forEach((result) => {
+			const embed = embedDroppedFile(result);
+			insertTextAtCaret(editor, `${embed}\n`);
+		});
+	}
+
+	$effect(() => {
+		return editor.registerCommand(
+			DROP_COMMAND,
+			(e) => {
+				e.preventDefault();
+				e.stopPropagation();
+
+				const files = e.dataTransfer?.files;
+				handleDrop(files);
+				return true;
+			},
+			COMMAND_PRIORITY_CRITICAL
+		);
+	});
+</script>
