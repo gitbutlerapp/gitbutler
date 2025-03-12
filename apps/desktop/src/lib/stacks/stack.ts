e993a1361ef78b528f2b661f952c88565c2b19d6
estib <stron@me.com>
2025-03-12 16:48:41 +0100

    Stack: get name
    Emulate the way that the rust side determines the stack name

--- apps/desktop/src/lib/stacks/stack.ts
+++ apps/desktop/src/lib/stacks/stack.ts
@@ -5,3 +5,8 @@ export type Stack = {
 	id: string;
 	branchNames: string[];
 };
+
+export function getStackName(stack: Stack): string | undefined {
+	const lastBranch = stack.branchNames[stack.branchNames.length - 1];
+	return lastBranch;
+}
