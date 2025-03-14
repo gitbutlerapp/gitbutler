7c6ce2b71db847cce524d7070b061de1dcd85aaf
estib <stron@me.com>
2025-03-14 13:10:10 +0100

    web: Use the vercel adapter

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
