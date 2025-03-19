1d8837a82361e00dbd6202c8a64249b5f763c12c
Pavel Laptev <pawellaptew@gmail.com>
2025-03-19 16:07:17 +0100

    Update RichTextEditor.stories.svelte

--- packages/ui/src/stories/richTextEditor/RichTextEditor.stories.svelte
+++ packages/ui/src/stories/richTextEditor/RichTextEditor.stories.svelte
@@ -1,5 +1,6 @@
 <script module lang="ts">
 	import RichTextEditor from '$lib/RichTextEditor.svelte';
+	import FormattingPopup from '$lib/richText/tools/FormattingPopup.svelte';
 	import {
 		type Args,
 		defineMeta,
@@ -26,13 +27,19 @@
 
 {#snippet template({ ...args }: Args<typeof Story>, _context: StoryContext<typeof Story>)}
 	<div class="wrap">
-		<RichTextEditor
-			namespace={args.namespace || 'commit-message'}
-			markdown={args.markdown || false}
-			onError={args.onError || console.error}
-			styleContext={args.styleContext || 'client-editor'}
-			placeholder={args.placeholder || 'Type your message here…'}
-		/>
+		<div class="editor-wrap">
+			<RichTextEditor
+				namespace={args.namespace || 'commit-message'}
+				markdown={args.markdown || false}
+				onError={args.onError || console.error}
+				styleContext={args.styleContext || 'client-editor'}
+				placeholder={args.placeholder || 'Type your message here…'}
+			>
+				{#snippet toolBar()}
+					<FormattingPopup />
+				{/snippet}
+			</RichTextEditor>
+		</div>
 	</div>
 {/snippet}
 
@@ -41,5 +48,14 @@
 <style>
 	.wrap {
 		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		max-width: 600px;
+	}
+	.editor-wrap {
+		display: flex;
+		flex-direction: column;
+		border: 1px solid var(--clr-border-2);
+		flex: 1;
 	}
 </style>
