125737091c033bcdfd1151d711c8ba3eb1b862c2
Mattias Granlund <mtsgrd@gmail.com>
2025-03-19 16:05:09 +0100

    Use `projectId` in github queries
    - becomes part of the key
    - otherwise data is incorrectly cached across project

--- .conflict-side-0/apps/desktop/src/routes/[projectId]/history/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>History Page Placeholder</div>
--- .conflict-side-1/apps/desktop/src/routes/settings/telemetry/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import AnalyticsSettings from '$components/AnalyticsSettings.svelte';
-	import SettingsPage from '$components/SettingsPage.svelte';
-</script>
-
-<SettingsPage title="Telemetry">
-	<AnalyticsSettings />
-</SettingsPage>
