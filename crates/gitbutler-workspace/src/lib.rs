e6f67e8b5b69e0605c492954a8ff0c02522b77d2
Caleb Owens <caleb@gitbutler.com>
2025-03-20 23:44:47 +0100

    Handle branch removal in v3
    This adds forked logic to handle branch unapplying in gitbutler v3.
    
    The main change here is to move away from tree based manipulations.
    
    We need to handle the uncommited  changes (cwdt) as just one tree. We want to somehow remove the contents of one branch from this tree. As it turns out, this is pretty much analogus to what the `git revert` command does.
    
    TBH, I've not read how `git revert` is implemented, but I've chosen to achieve the behaviour using a 3 way merge.
    
    There are three relevant trees to think about when performing this operation. The `cwdt`,  the head of the branch to remove (`rh`), and the workspace base (`wb`).
    
    If we diff `cwdt` against `rh`, we get a diff that introduces all the changes that the other branches introduce. If we  diff `wb` against `rh`, we get a diff that removes all of the contents of the branch to remove. Combining those together in a three way merge results in `cwdt` without the branch we want to remove

--- crates/gitbutler-workspace/src/lib.rs
+++ crates/gitbutler-workspace/src/lib.rs
@@ -4,3 +4,32 @@ pub use branch_trees::{
     checkout_branch_trees, compute_updated_branch_head, compute_updated_branch_head_for_commits,
     BranchHeadAndTree,
 };
+
+use anyhow::Result;
+use gitbutler_command_context::CommandContext;
+use gitbutler_oxidize::OidExt;
+use gitbutler_project::access::WorktreeReadPermission;
+use gitbutler_repo::RepositoryExt;
+use gitbutler_stack::VirtualBranchesHandle;
+
+pub fn workspace_base(
+    ctx: &CommandContext,
+    _perm: &WorktreeReadPermission,
+) -> Result<gix::ObjectId> {
+    let gix_repo = ctx.gix_repository_for_merging()?;
+    let repo = ctx.repo();
+    let vb_state = VirtualBranchesHandle::new(ctx.project().gb_dir());
+    let default_target = vb_state.get_default_target()?;
+    let target_branch = repo.find_branch_by_refname(&default_target.branch.into())?;
+    let target_branch_commit = target_branch.get().peel_to_commit()?.id().to_gix();
+
+    let stacks = vb_state.list_stacks_in_workspace()?;
+    let stack_heads = stacks.iter().map(|b| b.head().to_gix()).collect::<Vec<_>>();
+    let merge_base_tree_id = gix_repo
+        .merge_base_octopus([stack_heads, vec![target_branch_commit]].concat())?
+        .object()?
+        .id()
+        .detach();
+
+    Ok(merge_base_tree_id)
+}
