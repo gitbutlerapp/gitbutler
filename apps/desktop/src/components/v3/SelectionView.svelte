0cd75863a3fab31db346a67e07ba476e756151a2
Mattias Granlund <mtsgrd@gmail.com>
2025-03-22 22:31:35 +0100

    Show illustration when no changes selected

--- apps/desktop/src/components/v3/SelectionView.svelte
+++ apps/desktop/src/components/v3/SelectionView.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import ScrollableContainer from '$components/ConfigurableScrollableContainer.svelte';
 	import SelectedChange from '$components/v3/SelectedChange.svelte';
+	import SelectACommitSVG from '$lib/assets/illustrations/select-a-commit-preview.svg?raw';
 	import { IdSelection } from '$lib/selection/idSelection.svelte';
 	import { inject } from '@gitbutler/shared/context';
 
@@ -15,21 +16,45 @@
 </script>
 
 <div class="selection-view">
-	<ScrollableContainer wide>
-		{#each selection as selectedFile, index ('selection-view-' + selectedFile.path + index)}
-			<SelectedChange {projectId} {selectedFile} {index} />
-		{/each}
-	</ScrollableContainer>
+	{#if selection.length === 0}
+		<div class="no-content">
+			<div>
+				{@html SelectACommitSVG}
+			</div>
+			<div class="text text-13">Select a file to preview</div>
+		</div>
+	{:else}
+		<ScrollableContainer wide>
+			{#each selection as selectedFile, index ('selection-view-' + selectedFile.path + index)}
+				<SelectedChange {projectId} {selectedFile} {index} />
+			{/each}
+		</ScrollableContainer>
+	{/if}
 </div>
 
 <style>
 	.selection-view {
+		display: flex;
 		flex-grow: 1;
 		height: 100%;
 		overflow: hidden;
 		background-size: 6px 6px;
+		width: 100%;
 
 		border-radius: var(--radius-ml);
 		border: 1px solid var(--clr-border-2);
+		align-items: center;
+		justify-content: center;
+	}
+
+	.no-content {
+		display: flex;
+		flex-direction: column;
+		gap: 28px;
+	}
+
+	.text {
+		text-align: center;
+		color: var(--clr-text-2);
 	}
 </style>
