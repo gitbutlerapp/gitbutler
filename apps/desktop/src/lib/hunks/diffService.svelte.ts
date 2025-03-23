39a23b182356d82ff78e62539dacdb1c60a7b8dd
estib <stron@me.com>
2025-03-23 12:02:24 +0100

    diff service: get multiple diffs
    Create a hook function that fetches multiple tree diffs

--- apps/desktop/src/lib/hunks/diffService.svelte.ts
+++ apps/desktop/src/lib/hunks/diffService.svelte.ts
@@ -3,6 +3,11 @@ import type { TreeChange } from '$lib/hunks/change';
 import type { UnifiedDiff } from '$lib/hunks/diff';
 import type { ClientState } from '$lib/state/clientState.svelte';
 
+export type ChangeDiff = {
+	path: string;
+	diff: UnifiedDiff;
+};
+
 export class DiffService {
 	private api: ReturnType<typeof injectEndpoints>;
 
@@ -15,8 +20,15 @@ export class DiffService {
 		const result = $derived(getDiff.useQuery({ projectId, change }));
 		return result;
 	}
-}
 
+	getChanges(projectId: string, changes: TreeChange[]) {
+		const args = changes.map((change) => ({ projectId, change }));
+		const { getDiff } = this.api.endpoints;
+		return getDiff.useQueries(args, {
+			transform: (data, args): ChangeDiff => ({ path: args.change.path, diff: data })
+		});
+	}
+}
 function injectEndpoints(api: ClientState['backendApi']) {
 	return api.injectEndpoints({
 		endpoints: (build) => ({
