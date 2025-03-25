1d439756bd391e06d1216111f1477108d9c74c1d
Mattias Granlund <mtsgrd@gmail.com>
2025-03-25 18:35:06 +0100

    Fix `useMutation` side effects

--- .conflict-side-1/apps/desktop/src/lib/config/uiFeatureFlags.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file contains functions for managing ui-specific feature flags.
- * The values are persisted in local storage. Entries are prefixed with 'feature'.
- *
- * @module appSettings
- */
-import { persisted } from '@gitbutler/shared/persisted';
-
-export const autoSelectBranchNameFeature = persisted(false, 'autoSelectBranchLaneContentsFeature');
