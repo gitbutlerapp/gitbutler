64b807b29f578715549b9bc649956f71f65f97a1
Pavel Laptev <pawellaptew@gmail.com>
2025-04-02 22:25:07 +0200

    commit header: text weight update

--- apps/desktop/src/components/v3/CommitView.svelte
+++ apps/desktop/src/components/v3/CommitView.svelte
@@ -122,7 +122,7 @@
 		{:else}
 			<Drawer {projectId} {stackId} splitView>
 				{#snippet header()}
-					<h3 class="text-13 text-bold commit-view__header">
+					<h3 class="text-13 text-semibold commit-view__header">
 						Commit
 						<Tooltip text="Copy commit SHA">
 							<button
