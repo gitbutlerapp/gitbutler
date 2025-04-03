6263e39a2870c80fea7279a8c818c5fb216f50a5
Caleb Owens <hello@calebowens.com>
2025-04-03 07:45:14 +0200

    Added a little script

---
+++ apps/butler-bot/scripts/production.sh
@@ -0,0 +1,20 @@
+cd /home/ec2-user
+source .env
+
+while true; do
+    cd /home/ec2-user/gitbutler
+
+    git pull
+
+    pnpm install
+    pnpm turbo package --filter=@gitbutler/butler-bot...
+
+    cd /home/ec2-user/gitbutler/apps/butler-bot
+
+    nvm install
+
+    corepack enable
+    corepack install
+
+    pnpm run run
+end
