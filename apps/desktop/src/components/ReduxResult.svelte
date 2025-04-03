71378a5c5520c33dbeb249575eb01cc4d567149e
estib <stron@me.com>
2025-04-03 11:44:24 +0200

    Redux Resut: cache the stack ID and project ID as well

--- apps/desktop/src/components/ReduxResult.svelte
+++ apps/desktop/src/components/ReduxResult.svelte
@@ -1,8 +1,9 @@
 <script lang="ts" module>
 	type A = unknown;
+	type B = string | undefined;
 </script>
 
-<script lang="ts" generics="A">
+<script lang="ts" generics="A, B extends string | undefined">
 	import Icon from '@gitbutler/ui/Icon.svelte';
 	import { isErrorlike } from '@gitbutler/ui/utils/typeguards';
 	import { QueryStatus } from '@reduxjs/toolkit/query';
@@ -14,39 +15,135 @@
 		error?: unknown;
 	};
 
-	type Props<A> = {
+	type BaseProps<A> = {
+		type: 'stack' | 'project' | 'optional-stack';
 		result: Result<A> | undefined;
-		children: Snippet<[A]>;
 		empty?: Snippet;
+		projectId: string;
 	};
 
-	const { result, children }: Props<A> = $props();
+	/**
+	 * Children depend on a the result of a RTK query, project ID and stack ID.
+	 */
+	type StackProps<A> = BaseProps<A> & {
+		type: 'stack';
+		stackId: string;
+		children: Snippet<[A, { stackId: string; projectId: string }]>;
+	};
 
-	let dataCopy: undefined | Result<A>['data'] = $state(result?.data);
+	/**
+	 * Children depend on a the result of a RTK query, project ID and an optional stack ID.
+	 *
+	 * @note Only use this if the stack ID is optional. If the stack ID is required, use the props of type `stack`.
+	 */
+	type OptionalStackProps<A> = BaseProps<A> & {
+		type: 'optional-stack';
+		stackId: string | undefined;
+		children: Snippet<[A, { stackId: string | undefined; projectId: string }]>;
+	};
+
+	/**
+	 * Children depend on a the result of a RTK query and project ID.
+	 */
+	type ProjectProps<A> = BaseProps<A> & {
+		type: 'project';
+		children: Snippet<[A, { projectId: string }]>;
+	};
+
+	type Props<A> = StackProps<A> | ProjectProps<A> | OptionalStackProps<A>;
+
+	let props: Props<A> = $props();
+
+	type CachedData =
+		| {
+				type: 'stack';
+				data: A;
+				stackId: string;
+				projectId: string;
+		  }
+		| {
+				type: 'project';
+				data: A;
+				projectId: string;
+		  }
+		| {
+				type: 'optional-stack';
+				data: A;
+				stackId: string | undefined;
+				projectId: string;
+		  };
 
 	/**
 	 * To prevent flickering when the input data changes for an already
 	 * rendered component we render the previous result until the next
 	 * result is ready.
 	 */
+	let cachedData = $state<CachedData | undefined>(undefined);
+
+	function setCachedData(data: A): CachedData {
+		switch (props.type) {
+			case 'stack': {
+				return {
+					type: 'stack',
+					data: data,
+					stackId: props.stackId,
+					projectId: props.projectId
+				};
+			}
+
+			case 'project': {
+				return {
+					type: 'project',
+					data: data,
+					projectId: props.projectId
+				};
+			}
+
+			case 'optional-stack': {
+				return {
+					type: 'optional-stack',
+					data: data,
+					stackId: props.stackId,
+					projectId: props.projectId
+				};
+			}
+		}
+	}
+
 	$effect(() => {
-		if (result?.data) dataCopy = result.data;
+		if (props.result?.data !== undefined) {
+			cachedData = setCachedData(props.result.data);
+		}
 	});
 </script>
 
-{#if dataCopy !== undefined}
-	{@render children(dataCopy)}
-{:else if result?.status === 'pending'}
+{#if cachedData !== undefined}
+	{#if cachedData.type === 'stack' && props.type === 'stack'}
+		{@render props.children(cachedData.data, {
+			stackId: cachedData.stackId,
+			projectId: cachedData.projectId
+		})}
+	{:else if cachedData.type === 'project' && props.type === 'project'}
+		{@render props.children(cachedData.data, {
+			projectId: cachedData.projectId
+		})}
+	{:else if cachedData.type === 'optional-stack' && props.type === 'optional-stack'}
+		{@render props.children(cachedData.data, {
+			stackId: cachedData.stackId,
+			projectId: cachedData.projectId
+		})}
+	{/if}
+{:else if props.result?.status === 'pending'}
 	<div class="loading-spinner">
 		<Icon name="spinner" />
 	</div>
-{:else if result?.status === 'rejected'}
-	{#if isErrorlike(result.error)}
-		{result.error.message}
+{:else if props.result?.status === 'rejected'}
+	{#if isErrorlike(props.result.error)}
+		{props.result.error.message}
 	{:else}
-		{String(result.error)}
+		{JSON.stringify(props.result.error)}
 	{/if}
-{:else if status === 'uninitialized'}
+{:else if props.result?.status === 'uninitialized'}
 	Uninitialized...
 {/if}
 
