90520d61866682b4f3d9c0e3fba150a5962708ab
Mattias Granlund <mtsgrd@gmail.com>
2025-04-08 11:01:42 +0200

    Fix irc branch chat toggle check

--- apps/desktop/src/components/v3/SelectionView.svelte
+++ apps/desktop/src/components/v3/SelectionView.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import ScrollableContainer from '$components/ConfigurableScrollableContainer.svelte';
 	import FileViewPlaceholder from '$components/v3/FileViewPlaceholder.svelte';
-	import IrcChannel from '$components/v3/IrcChannel.svelte';
 	import SelectedChange from '$components/v3/SelectedChange.svelte';
-	import { ircEnabled } from '$lib/config/uiFeatureFlags';
 	import { IdSelection } from '$lib/selection/idSelection.svelte';
 	import { UiState } from '$lib/state/uiState.svelte';
 	import { inject } from '@gitbutler/shared/context';
@@ -18,22 +16,13 @@
 	const [idSelection, uiState] = inject(IdSelection, UiState);
 
 	const stackState = $derived(stackId ? uiState.stack(stackId) : undefined);
-	const branchName = $derived(
-		stackState?.activeSelectionId.current.type === 'branch'
-			? stackState.activeSelectionId.current.branchName
-			: undefined
-	);
 	const selectionId = $derived(stackState?.activeSelectionId.get());
 	const selection = $derived(selectionId?.current ? idSelection.values(selectionId.current) : []);
 </script>
 
 <div class="selection-view">
 	{#if selection.length === 0}
-		{#if ircEnabled && branchName}
-			<IrcChannel type="group" channel={'#' + branchName} autojoin />
-		{:else}
-			<FileViewPlaceholder />
-		{/if}
+		<FileViewPlaceholder />
 	{:else}
 		<ScrollableContainer wide>
 			{#each selection as selectedFile}
