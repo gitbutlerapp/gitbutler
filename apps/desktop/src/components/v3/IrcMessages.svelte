a4dbe5e001d500f3cf5c9f6e2ee4e9d40cef8d9c
Mattias Granlund <mtsgrd@gmail.com>
2025-04-09 11:59:42 +0200

    Make irc messages selectable

--- apps/desktop/src/components/v3/IrcMessages.svelte
+++ apps/desktop/src/components/v3/IrcMessages.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ConfigurableScrollableContainer from '$components/ConfigurableScrollableContainer.svelte';
 	import type { IrcLog } from '$lib/irc/types';
 
 	type Props = {
@@ -8,27 +9,32 @@
 	const { logs }: Props = $props();
 </script>
 
-{#each logs || [] as log}
-	<div class="message" class:error={log.type === 'outgoing' && log.error}>
-		{#if log.type === 'incoming'}
-			[{new Date(log.timestamp).toLocaleTimeString()}] {log.from}: {log.message}
-		{:else if log.type === 'outgoing'}
-			[{new Date(log.timestamp).toLocaleTimeString()}] {log.from}: {log.message}
-		{:else if log.type === 'server'}
-			[{new Date(log.timestamp).toLocaleTimeString()}] {log.message}
-		{:else if log.type === 'command'}
-			[{new Date(log.timestamp).toLocaleTimeString()}] {log.raw}
-		{/if}
-	</div>
-	{#if log.type === 'outgoing' && log.error}
-		{log.error}
-	{/if}
-{/each}
+<div class="messages">
+	<ConfigurableScrollableContainer>
+		{#each logs || [] as log}
+			<div class="message" class:error={log.type === 'outgoing' && log.error}>
+				{#if log.type === 'incoming'}
+					[{new Date(log.timestamp).toLocaleTimeString()}] {log.from}: {log.message}
+				{:else if log.type === 'outgoing'}
+					[{new Date(log.timestamp).toLocaleTimeString()}] {log.from}: {log.message}
+				{:else if log.type === 'server'}
+					[{new Date(log.timestamp).toLocaleTimeString()}] {log.message}
+				{:else if log.type === 'command'}
+					[{new Date(log.timestamp).toLocaleTimeString()}] {log.raw}
+				{/if}
+			</div>
+			{#if log.type === 'outgoing' && log.error}
+				{log.error}
+			{/if}
+		{/each}
+	</ConfigurableScrollableContainer>
+</div>
 
 <style lang="postcss">
 	.message {
 		font-family: monospace;
 		white-space: pre-wrap;
+		user-select: text;
 	}
 	.error {
 		background-color: var(--clr-scale-err-90);
