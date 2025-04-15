b20a398a9d85ba49d1cd5ee34b5a50664ea147eb
estib <stron@me.com>
2025-04-15 11:40:12 +0200

    Branch List: Add a button for review creation
    Add a button next to the push button that allows the user to quickly open a review request based of off the “next reviewable branch"

--- apps/desktop/src/components/v3/PushButton.svelte
+++ apps/desktop/src/components/v3/PushButton.svelte
@@ -8,7 +8,6 @@
 	import { UserService } from '$lib/user/userService';
 	import { getContext } from '@gitbutler/shared/context';
 	import Button from '@gitbutler/ui/Button.svelte';
-	import { stickyHeader } from '@gitbutler/ui/utils/stickyHeader';
 
 	type Props = {
 		projectId: string;
@@ -27,7 +26,6 @@
 	const branches = $derived(branchesResult.current.data || []);
 	const [pushStack, pushResult] = stackService.pushStack;
 	const [publishBranch, publishResult] = stackService.publishBranch;
-	let isSticked = $state(true);
 
 	const requiresForce = $derived(stackInfo && stackRequiresForcePush(stackInfo));
 	const hasThingsToPush = $derived(stackInfo && stackHasUnpushedCommits(stackInfo));
@@ -51,74 +49,24 @@
 	);
 </script>
 
-<div
-	class="push-button"
-	class:is-sticked={isSticked}
-	use:stickyHeader={{
-		align: 'bottom',
-		onStick: (sticky) => {
-			isSticked = sticky;
-		},
-		unstyled: true
-	}}
->
-	<div class="push-button__inner">
-		<Button
-			style="neutral"
-			wide
-			{loading}
-			disabled={!hasThingsToPush || hasConflicts}
-			tooltip={hasConflicts
-				? 'In order to push, please resolve any conflicted commits.'
-				: undefined}
-			onclick={push}
-		>
-			{requiresForce ? 'Force push' : multipleBranches ? 'Push All' : 'Push'}
-		</Button>
-	</div>
+<div class="push-button">
+	<Button
+		style="neutral"
+		wide
+		{loading}
+		disabled={!hasThingsToPush || hasConflicts}
+		tooltip={hasConflicts ? 'In order to push, please resolve any conflicted commits.' : undefined}
+		onclick={push}
+	>
+		{requiresForce ? 'Force push' : multipleBranches ? 'Push All' : 'Push'}
+	</Button>
 </div>
 
 <style lang="postcss">
 	.push-button {
-		z-index: var(--z-lifted);
-		position: sticky;
-		padding: 8px 0 8px;
-		margin-bottom: -9px;
-		bottom: -1px;
-		transition: padding var(--transition-medium);
-
-		&:after {
-			content: '';
-			display: block;
-			position: absolute;
-			bottom: 0;
-			left: -14px;
-			height: calc(100% + 8px);
-			width: calc(100% + 28px);
-			z-index: -1;
-			background-color: var(--clr-bg-1);
-			border-top: 1px solid var(--clr-border-2);
-
-			transform: translateY(10%);
-			opacity: 0;
-			transition:
-				opacity var(--transition-fast),
-				transform var(--transition-medium);
-		}
-
-		&.is-sticked {
-			padding-bottom: 14px;
-		}
-
-		&.is-sticked:after {
-			opacity: 1;
-			transform: translateY(0);
-		}
-	}
-
-	.push-button__inner {
 		/* This is just here so that the disabled button is still opaque */
 		border-radius: var(--radius-m);
 		background-color: var(--clr-bg-1);
+		flex: 1;
 	}
 </style>
