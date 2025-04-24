84f8bf36a4947ab583d27bc37cb5737939a9427e
Mattias Granlund <mtsgrd@gmail.com>
2025-04-24 19:36:21 +0200

    Move some seleced state logic

--- apps/desktop/src/components/v3/PullRequestSidebarEntry.svelte
+++ apps/desktop/src/components/v3/PullRequestSidebarEntry.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import SidebarEntry from '$components/v3/SidebarEntry.svelte';
-	import { UiState } from '$lib/state/uiState.svelte';
 	import { UserService } from '$lib/user/userService';
 	import { parseDate } from '$lib/utils/time';
 	import { inject } from '@gitbutler/shared/context';
@@ -9,22 +8,20 @@
 	interface Props {
 		projectId: string;
 		pullRequest: PullRequest;
+		selected: boolean;
 		onclick: (listing: PullRequest) => void;
 	}
 
-	const { projectId, pullRequest, onclick }: Props = $props();
+	const { pullRequest, selected, onclick }: Props = $props();
 
-	const [userService, uiState] = inject(UserService, UiState);
+	const [userService] = inject(UserService);
 	const user = userService.user;
-	const explorerState = $derived(uiState.project(projectId).branchesSelection);
 
 	const authorImgUrl = $derived.by(() => {
 		return pullRequest.author?.email?.toLowerCase() === $user?.email?.toLowerCase()
 			? $user?.picture
 			: pullRequest.author?.gravatarUrl;
 	});
-
-	const selected = $derived(explorerState.current.prNumber === pullRequest.number);
 </script>
 
 <SidebarEntry
