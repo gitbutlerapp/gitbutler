a9e65921840fe91465f48cb6552e440b9542f4e1
Pavel Laptev <pawellaptew@gmail.com>
2025-04-29 23:23:01 +0200

    feat(BranchDetails): improve branch details UI
    - wrap the row
    - shorten copy

--- apps/desktop/src/components/v3/BranchDetails.svelte
+++ apps/desktop/src/components/v3/BranchDetails.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import BranchBadge from '$components/v3/BranchBadge.svelte';
+	import Icon from '@gitbutler/ui/Icon.svelte';
 	import AvatarGroup from '@gitbutler/ui/avatar/AvatarGroup.svelte';
 	import { getTimeAgo } from '@gitbutler/ui/utils/timeAgo';
 	import type { BranchDetails } from '$lib/stacks/stack';
@@ -14,30 +15,30 @@
 </script>
 
 <div class="branch-view">
-	<div class="branch-view__header-container">
-		<div class="text-12 branch-view__header-details-row">
-			<BranchBadge pushStatus={branch.pushStatus} />
-			<span class="branch-view__details-divider">•</span>
+	<div class="text-12 branch-view__header-container">
+		<BranchBadge pushStatus={branch.pushStatus} />
+		<span class="branch-view__details-divider">•</span>
 
-			{#if branch.isConflicted}
-				<span class="branch-view__header-details-row-conflict">Has conflicts</span>
-				<span class="branch-view__details-divider">•</span>
-			{/if}
-
-			<span>Contribs:</span>
-			<AvatarGroup
-				maxAvatars={2}
-				avatars={branch.authors.map((a) => ({
-					name: a.name,
-					srcUrl: a.gravatarUrl
-				}))}
-			/>
-
-			{#if branch.lastUpdatedAt}
-				<span class="branch-view__details-divider">•</span>
-				<span class="truncate">{getTimeAgo(branch.lastUpdatedAt)}</span>
-			{/if}
+		<!-- {#if branch.isConflicted} -->
+		<div class="branch-view__header-details-row-conflict">
+			<Icon name="warning-small" /> <span>Conflicts</span>
 		</div>
+		<span class="branch-view__details-divider">•</span>
+		<!-- {/if} -->
+
+		<span>Contribs:</span>
+		<AvatarGroup
+			maxAvatars={2}
+			avatars={branch.authors.map((a) => ({
+				name: a.name,
+				srcUrl: a.gravatarUrl
+			}))}
+		/>
+
+		{#if branch.lastUpdatedAt}
+			<span class="branch-view__details-divider">•</span>
+			<span class="truncate">{getTimeAgo(branch.lastUpdatedAt)}</span>
+		{/if}
 	</div>
 
 	{@render children?.()}
@@ -53,21 +54,17 @@
 
 	.branch-view__header-container {
 		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		gap: 16px;
-		overflow: hidden;
-	}
-
-	.branch-view__header-details-row {
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 6px;
 		width: 100%;
 		color: var(--clr-text-2);
-		display: flex;
-		align-items: center;
-		gap: 6px;
 	}
 
 	.branch-view__header-details-row-conflict {
+		display: flex;
+		align-items: center;
+		gap: 4px;
 		color: var(--clr-theme-err-element);
 	}
 
