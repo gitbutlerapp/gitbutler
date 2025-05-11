8113c8c8fe31ace8effa27c6ee662887562f774c
Pavel Laptev <pawellaptew@gmail.com>
2025-05-12 01:11:31 +0200

    Add tooltips for branches view modes

--- packages/ui/src/lib/segmentControl/Segment.svelte
+++ packages/ui/src/lib/segmentControl/Segment.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Icon from '$lib/Icon.svelte';
+	import Tooltip from '$lib/Tooltip.svelte';
 	import { getContext, onMount } from 'svelte';
+	import type iconsJson from '$lib/data/icons.json';
 	import type { SegmentContext } from '$lib/segmentControl/segmentTypes';
-	import type iconsJson from '@gitbutler/ui/data/icons.json';
 	import type { Snippet } from 'svelte';
 
 	interface SegmentProps {
@@ -11,10 +12,13 @@
 		onselect?: (id: string) => void;
 		disabled?: boolean;
 		children?: Snippet;
+		tooltip?: string;
+		tooltipPosition?: 'top' | 'bottom';
 		icon?: keyof typeof iconsJson;
 	}
 
-	const { id, onselect, children, disabled, icon, testId }: SegmentProps = $props();
+	const { id, onselect, children, disabled, icon, tooltip, tooltipPosition, testId }: SegmentProps =
+		$props();
 
 	const context = getContext<SegmentContext>('SegmentControl');
 	const index = context.setIndex();
@@ -39,29 +43,18 @@
 	});
 </script>
 
-<button
-	data-testid={testId}
-	type="button"
-	bind:this={elRef}
-	{id}
-	class="segment-control-item"
-	role="tab"
-	{disabled}
-	tabindex={isSelected || disabled ? -1 : 0}
-	aria-selected={isSelected}
-	onclick={() => {
-		if (index !== $selectedSegmentIndex) {
-			context.setSelected({
-				index,
-				id
-			});
-			if (onselect) {
-				onselect(id);
-			}
-		}
-	}}
-	onkeydown={({ key }) => {
-		if (key === 'Enter' || key === ' ') {
+<Tooltip text={tooltip} position={tooltipPosition}>
+	<button
+		data-testid={testId}
+		type="button"
+		bind:this={elRef}
+		{id}
+		class="segment-control-item"
+		role="tab"
+		{disabled}
+		tabindex={isSelected || disabled ? -1 : 0}
+		aria-selected={isSelected}
+		onclick={() => {
 			if (index !== $selectedSegmentIndex) {
 				context.setSelected({
 					index,
@@ -71,17 +64,30 @@
 					onselect(id);
 				}
 			}
-		}
-	}}
->
-	{#if children}
-		<span class="text-12 text-semibold segment-control-item__label">
-			{@render children()}
-		</span>
-	{/if}
-	{#if icon}
-		<span class="segment-control-item__icon" aria-hidden="true">
-			<Icon name={icon} />
-		</span>
-	{/if}
-</button>
+		}}
+		onkeydown={({ key }) => {
+			if (key === 'Enter' || key === ' ') {
+				if (index !== $selectedSegmentIndex) {
+					context.setSelected({
+						index,
+						id
+					});
+					if (onselect) {
+						onselect(id);
+					}
+				}
+			}
+		}}
+	>
+		{#if children}
+			<span class="text-12 text-semibold segment-control-item__label">
+				{@render children()}
+			</span>
+		{/if}
+		{#if icon}
+			<span class="segment-control-item__icon" aria-hidden="true">
+				<Icon name={icon} />
+			</span>
+		{/if}
+	</button>
+</Tooltip>
