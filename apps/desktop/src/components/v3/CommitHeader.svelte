2d6969f86f6f985afa5834a502f6261b29334146
Pavel Laptev <pawellaptew@gmail.com>
2025-05-12 11:46:46 +0200

    refactor: "truncate" prop

--- apps/desktop/src/components/v3/CommitHeader.svelte
+++ apps/desktop/src/components/v3/CommitHeader.svelte
@@ -4,27 +4,18 @@
 	import Tooltip from '@gitbutler/ui/Tooltip.svelte';
 
 	type Props = {
-		row?: boolean;
+		truncate?: boolean;
 		commitMessage: string;
 		className?: string;
 	};
 
-	const { commitMessage, row, className }: Props = $props();
+	const { commitMessage, truncate, className }: Props = $props();
 
 	const title = $derived(splitMessage(commitMessage).title);
 </script>
 
 <Tooltip text={title}>
-	<h3 data-testid={TestId.CommitDrawerTitle} class="{className} commit-title" class:row>
+	<h3 data-testid={TestId.CommitDrawerTitle} class="{className} commit-title" class:truncate>
 		{title}
 	</h3>
 </Tooltip>
-
-<style>
-	.row {
-		text-align: left;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-	}
-</style>
