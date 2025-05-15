59ebd37ce012e8e7de91f5b51276362adb4da3ca
estib <stron@me.com>
2025-05-15 10:24:12 +0200

    Branch header: Style component
    Factor out the duplicated style logic into a pure style component

--- apps/desktop/src/components/v3/BranchCard.svelte
+++ apps/desktop/src/components/v3/BranchCard.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-	import BranchLabel from '$components/BranchLabel.svelte';
 	import CardOverlay from '$components/CardOverlay.svelte';
 	import Dropzone from '$components/Dropzone.svelte';
 	import BranchBadge from '$components/v3/BranchBadge.svelte';
 	import BranchDividerLine from '$components/v3/BranchDividerLine.svelte';
+	import BranchHeader from '$components/v3/BranchHeader.svelte';
 	import BranchHeaderContextMenu from '$components/v3/BranchHeaderContextMenu.svelte';
-	import BranchHeaderIcon from '$components/v3/BranchHeaderIcon.svelte';
 	import { MoveCommitDzHandler, StartCommitDzHandler } from '$lib/commits/dropHandler';
 	import { ChangeSelectionService } from '$lib/selection/changeSelection.svelte';
 	import { StackService } from '$lib/stacks/stackService.svelte';
 	import { UiState } from '$lib/state/uiState.svelte';
-	import { TestId } from '$lib/testing/testIds';
 	import { inject } from '@gitbutler/shared/context';
 	import ReviewBadge from '@gitbutler/ui/ReviewBadge.svelte';
 	import { getTimeAgo } from '@gitbutler/ui/utils/timeAgo';
-	import { slide } from 'svelte/transition';
 	import type { PushStatus } from '$lib/stacks/stack';
 	import type iconsJson from '@gitbutler/ui/data/icons.json';
 	import type { Snippet } from 'svelte';
@@ -77,8 +74,6 @@
 
 	const [updateName, nameUpdate] = stackService.updateBranchName;
 
-	let rightClickTrigger = $state<HTMLDivElement>();
-
 	const stackState = $derived(
 		args.type === 'stack-branch' ? uiState.stack(args.stackId) : undefined
 	);
@@ -129,155 +124,109 @@
 				{@const label = handler instanceof MoveCommitDzHandler ? 'Move here' : 'Start commit'}
 				<CardOverlay {hovered} {activated} {label} />
 			{/snippet}
-			<div
-				data-testid={TestId.BranchHeader}
-				data-testid-branch-header={branchName}
-				bind:this={rightClickTrigger}
-				role="button"
-				class="branch-header"
-				class:new-branch={args.isNewBranch}
-				class:selected={args.selected}
+
+			<BranchHeader
+				{branchName}
+				isEmpty={args.isNewBranch}
+				selected={args.selected}
+				selectIndicator
+				draft={false}
+				{lineColor}
+				{iconName}
+				{updateBranchName}
+				isUpdatingName={nameUpdate.current.isLoading}
+				{active}
+				{readonly}
+				{isPushed}
 				onclick={args.onclick}
-				onkeypress={args.onclick}
-				tabindex="0"
-				class:active
+				menu={args.menu}
 			>
-				{#if args.selected}
-					<div
-						class="branch-header__select-indicator"
-						in:slide={{ axis: 'x', duration: 150 }}
-						class:active
-					></div>
-				{/if}
+				{#snippet emptyState()}
+					<span class="branch-header__empty-state-span">This is an empty branch.</span>
+					<span class="branch-header__empty-state-span">Click for details.</span>
+					<br />
+					Create or drag & drop commits here.
+				{/snippet}
+				{#snippet content()}
+					<span class="branch-header__item">
+						<BranchBadge pushStatus={args.pushStatus} unstyled />
+					</span>
+					<span class="branch-header__divider">•</span>
 
-				<div class="branch-header__content">
-					<div class="branch-header__title text-14 text-bold">
-						<div class="flex gap-6">
-							<BranchHeaderIcon color={lineColor} {iconName} />
-							<BranchLabel
-								name={branchName}
-								fontSize="15"
-								disabled={nameUpdate.current.isLoading}
-								readonly={readonly || isPushed}
-								onChange={(name) => updateBranchName(name)}
-							/>
-						</div>
+					{#if args.isConflicted}
+						<span class="branch-header__item branch-header__item--conflict"> Conflicts </span>
+						<span class="branch-header__divider">•</span>
+					{/if}
 
-						{#if args.menu}
-							{@render args.menu({ rightClickTrigger })}
-						{/if}
-					</div>
+					{#if args.lastUpdatedAt}
+						<span class="branch-header__item">
+							{getTimeAgo(new Date(args.lastUpdatedAt))}
+						</span>
+					{/if}
 
-					{#if args.isNewBranch}
-						<p class="text-12 text-body branch-header__empty-state">
-							<span>This is an empty branch.</span> <span>Click for details.</span>
-							<br />
-							Create or drag & drop commits here.
-						</p>
-					{:else}
-						<div class="text-12 branch-header__details">
-							<span class="branch-header__item">
-								<BranchBadge pushStatus={args.pushStatus} unstyled />
-							</span>
-							<span class="branch-header__divider">•</span>
-
-							{#if args.isConflicted}
-								<span class="branch-header__item branch-header__item--conflict"> Conflicts </span>
-								<span class="branch-header__divider">•</span>
+					{#if args.reviewId || args.prNumber}
+						<span class="branch-header__divider">•</span>
+						<div class="branch-header__review-badges">
+							{#if args.reviewId}
+								<ReviewBadge brId={args.reviewId} brStatus="unknown" />
 							{/if}
-
-							{#if args.lastUpdatedAt}
-								<span class="branch-header__item">
-									{getTimeAgo(new Date(args.lastUpdatedAt))}
-								</span>
-							{/if}
-
-							{#if args.reviewId || args.prNumber}
-								<span class="branch-header__divider">•</span>
-								<div class="branch-header__review-badges">
-									{#if args.reviewId}
-										<ReviewBadge brId={args.reviewId} brStatus="unknown" />
-									{/if}
-									{#if args.prNumber}
-										<ReviewBadge prNumber={args.prNumber} prStatus="unknown" />
-									{/if}
-								</div>
+							{#if args.prNumber}
+								<ReviewBadge prNumber={args.prNumber} prStatus="unknown" />
 							{/if}
 						</div>
 					{/if}
-				</div>
-			</div>
+				{/snippet}
+			</BranchHeader>
 		</Dropzone>
 	{:else if args.type === 'normal-branch'}
-		<div
-			data-testid={TestId.BranchHeader}
-			bind:this={rightClickTrigger}
-			role="button"
-			class="branch-header"
-			class:selected={args.selected}
-			class:new-branch={args.isNewBranch}
+		<BranchHeader
+			{branchName}
+			isEmpty={args.isNewBranch}
+			selected={args.selected}
+			selectIndicator
+			draft={false}
+			{lineColor}
+			{iconName}
+			{updateBranchName}
+			isUpdatingName={nameUpdate.current.isLoading}
+			{active}
+			readonly
+			{isPushed}
 			onclick={args.onclick}
-			onkeypress={args.onclick}
-			tabindex="0"
 		>
-			{#if args.selected}
-				<div
-					class="branch-header__select-indicator"
-					in:slide={{ axis: 'x', duration: 150 }}
-					class:active
-				></div>
-			{/if}
-
-			<div class="branch-header__content">
-				<div class="branch-header__title text-14 text-bold">
-					<div class="flex gap-6">
-						<BranchHeaderIcon color={lineColor} {iconName} />
-						<BranchLabel name={branchName} fontSize="15" readonly={true} />
-					</div>
-				</div>
-
-				{#if args.isNewBranch}
-					<p class="text-12 text-body branch-header__empty-state">
-						<span>There are no commits yet on this branch.</span>
-					</p>
-				{:else}
-					<div class="text-12 branch-header__details">
-						{#if args.lastUpdatedAt}
-							<span class="branch-header__item">
-								{getTimeAgo(new Date(args.lastUpdatedAt))}
-							</span>
-						{/if}
-					</div>
+			{#snippet emptyState()}
+				<span class="branch-header__empty-state-span">There are no commits yet on this branch.</span
+				>
+			{/snippet}
+			{#snippet content()}
+				{#if args.lastUpdatedAt}
+					<span class="branch-header__item">
+						{getTimeAgo(new Date(args.lastUpdatedAt))}
+					</span>
 				{/if}
-			</div>
-		</div>
+			{/snippet}
+		</BranchHeader>
 	{:else}
-		<div
-			data-testid={TestId.BranchHeader}
-			bind:this={rightClickTrigger}
-			role="button"
-			class="branch-header new-branch draft selected"
-			tabindex="0"
+		<BranchHeader
+			{branchName}
+			isEmpty
+			selected
+			selectIndicator={false}
+			draft
+			{lineColor}
+			{iconName}
+			{updateBranchName}
+			isUpdatingName={nameUpdate.current.isLoading}
+			{active}
+			readonly={false}
+			isPushed={false}
 		>
-			<div class="branch-header__content">
-				<div class="branch-header__title text-14 text-bold">
-					<div class="flex gap-6">
-						<BranchHeaderIcon color={lineColor} {iconName} />
-						<BranchLabel
-							allowClear
-							name={branchName}
-							fontSize="15"
-							onChange={(name) => updateBranchName(name)}
-						/>
-					</div>
-				</div>
-				<p class="text-12 text-body branch-header__empty-state">
-					A new branch will be created for your commit.
-					<br />
-					Click the name to rename it now or later.
-				</p>
-			</div>
-		</div>
+			{#snippet emptyState()}
+				A new branch will be created for your commit.
+				<br />
+				Click the name to rename it now or later.
+			{/snippet}
+		</BranchHeader>
 	{/if}
 
 	{#if args.type !== 'draft-branch'}
@@ -302,103 +251,6 @@
 		height: 100%;
 	}
 
-	.branch-header {
-		--branch-selected-bg: var(--clr-bg-1);
-		--branch-selected-element-bg: var(--clr-selected-not-in-focus-element);
-
-		position: relative;
-		display: flex;
-		justify-content: flex-start;
-		align-items: center;
-		padding-left: 15px;
-		padding-right: 10px;
-		border-top-right-radius: var(--radius-ml);
-		border-top-left-radius: var(--radius-ml);
-		border-bottom: 1px solid var(--clr-border-2);
-		overflow: hidden;
-		background-color: var(--branch-selected-bg);
-
-		/* Selected but NOT in focus */
-		&:hover {
-			--branch-selected-bg: var(--clr-bg-1-muted);
-		}
-		&:focus-within,
-		&.selected {
-			--branch-selected-bg: var(--clr-selected-not-in-focus-bg);
-		}
-
-		/* Selected in focus */
-		&:focus-within.selected {
-			--branch-selected-bg: var(--clr-selected-in-focus-bg);
-			--branch-selected-element-bg: var(--clr-selected-in-focus-element);
-		}
-
-		/* MODIFIERS */
-		&.new-branch {
-			border-bottom: none;
-			border-radius: var(--radius-ml);
-		}
-	}
-
-	.branch-header__details {
-		display: flex;
-		align-items: center;
-		flex-wrap: wrap;
-		gap: 6px;
-		color: var(--clr-text-2);
-
-		&:empty {
-			display: none;
-		}
-	}
-
-	.branch-header__select-indicator {
-		position: absolute;
-		top: 14px;
-		left: 0;
-		width: 4px;
-		height: calc(100% - 28px);
-		border-radius: 0 var(--radius-ml) var(--radius-ml) 0;
-		background-color: var(--branch-selected-element-bg);
-		transition: transform var(--transition-fast);
-		&.active {
-			background-color: var(--clr-selected-in-focus-element);
-		}
-	}
-
-	.branch-header__title {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		min-width: 0;
-		flex-grow: 1;
-	}
-
-	.branch-header__content {
-		overflow: hidden;
-		flex: 1;
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		gap: 8px;
-		padding: 14px 0;
-		margin-left: -2px;
-		text-overflow: ellipsis;
-	}
-
-	.branch-header__empty-state {
-		opacity: 0.8;
-		color: var(--clr-text-2);
-
-		& span {
-			text-wrap: nowrap;
-		}
-	}
-	.branch-header__review-badges {
-		display: flex;
-		gap: 3px;
-	}
-
 	.branch-header__item {
 		white-space: nowrap;
 		color: var(--clr-text-2);
@@ -410,5 +262,9 @@
 
 	.branch-header__divider {
 		color: var(--clr-text-3);
+	}
+
+	.branch-header__empty-state-span {
+		text-wrap: nowrap;
 	}
 </style>
---
+++ apps/desktop/src/components/v3/BranchHeader.svelte
@@ -0,0 +1,197 @@
+<!-- BRANCH HEADER PURE COMPONENT -->
+<!-- ONLY CONCERNED ABOUT STYLE 😎 -->
+<script lang="ts">
+	import BranchLabel from '$components/BranchLabel.svelte';
+	import BranchHeaderIcon from '$components/v3/BranchHeaderIcon.svelte';
+	import { TestId } from '$lib/testing/testIds';
+	import { slide } from 'svelte/transition';
+	import type iconsJson from '@gitbutler/ui/data/icons.json';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		branchName: string;
+		isEmpty: boolean | undefined;
+		selected: boolean;
+		selectIndicator: boolean;
+		active: boolean | undefined;
+		readonly: boolean;
+		draft: boolean;
+
+		isPushed: boolean;
+
+		lineColor: string;
+		iconName: keyof typeof iconsJson;
+
+		onclick?: () => void;
+		updateBranchName: (name: string) => void;
+		isUpdatingName: boolean;
+
+		emptyState: Snippet;
+		content?: Snippet;
+		menu?: Snippet<[{ rightClickTrigger: HTMLElement }]>;
+	};
+
+	const {
+		branchName,
+		isEmpty = false,
+		selected,
+		selectIndicator,
+		draft,
+		active,
+		isUpdatingName,
+		readonly,
+		isPushed,
+		lineColor,
+		iconName,
+		onclick,
+		updateBranchName,
+		emptyState,
+		content,
+		menu
+	}: Props = $props();
+
+	let rightClickTrigger = $state<HTMLDivElement>();
+</script>
+
+<div
+	data-testid={TestId.BranchHeader}
+	data-testid-branch-header={branchName}
+	bind:this={rightClickTrigger}
+	role="button"
+	class="branch-header"
+	class:new-branch={isEmpty}
+	class:selected
+	class:draft
+	{onclick}
+	onkeypress={onclick}
+	tabindex="0"
+	class:active
+>
+	{#if selected && selectIndicator}
+		<div
+			class="branch-header__select-indicator"
+			in:slide={{ axis: 'x', duration: 150 }}
+			class:active
+		></div>
+	{/if}
+
+	<div class="branch-header__content">
+		<div class="branch-header__title text-14 text-bold">
+			<div class="flex gap-6">
+				<BranchHeaderIcon color={lineColor} {iconName} />
+				<BranchLabel
+					name={branchName}
+					fontSize="15"
+					disabled={isUpdatingName}
+					readonly={readonly || isPushed}
+					onChange={(name) => updateBranchName(name)}
+				/>
+			</div>
+
+			{#if menu}
+				{@render menu({ rightClickTrigger })}
+			{/if}
+		</div>
+
+		{#if isEmpty}
+			<p class="text-12 text-body branch-header__empty-state">
+				{@render emptyState()}
+			</p>
+		{:else if content}
+			<div class="text-12 branch-header__details">
+				{@render content()}
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style lang="postcss">
+	.branch-header {
+		--branch-selected-bg: var(--clr-bg-1);
+		--branch-selected-element-bg: var(--clr-selected-not-in-focus-element);
+
+		position: relative;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		padding-left: 15px;
+		padding-right: 10px;
+		border-top-right-radius: var(--radius-ml);
+		border-top-left-radius: var(--radius-ml);
+		border-bottom: 1px solid var(--clr-border-2);
+		overflow: hidden;
+		background-color: var(--branch-selected-bg);
+
+		/* Selected but NOT in focus */
+		&:hover {
+			--branch-selected-bg: var(--clr-bg-1-muted);
+		}
+		&:focus-within,
+		&.selected {
+			--branch-selected-bg: var(--clr-selected-not-in-focus-bg);
+		}
+
+		/* Selected in focus */
+		&:focus-within.selected {
+			--branch-selected-bg: var(--clr-selected-in-focus-bg);
+			--branch-selected-element-bg: var(--clr-selected-in-focus-element);
+		}
+
+		/* MODIFIERS */
+		&.new-branch {
+			border-bottom: none;
+			border-radius: var(--radius-ml);
+		}
+	}
+
+	.branch-header__details {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 6px;
+		color: var(--clr-text-2);
+
+		&:empty {
+			display: none;
+		}
+	}
+
+	.branch-header__select-indicator {
+		position: absolute;
+		top: 14px;
+		left: 0;
+		width: 4px;
+		height: calc(100% - 28px);
+		border-radius: 0 var(--radius-ml) var(--radius-ml) 0;
+		background-color: var(--branch-selected-element-bg);
+		transition: transform var(--transition-fast);
+		&.active {
+			background-color: var(--clr-selected-in-focus-element);
+		}
+	}
+
+	.branch-header__title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		min-width: 0;
+		flex-grow: 1;
+	}
+
+	.branch-header__content {
+		overflow: hidden;
+		flex: 1;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		padding: 14px 0;
+		margin-left: -2px;
+		text-overflow: ellipsis;
+	}
+
+	.branch-header__empty-state {
+		opacity: 0.8;
+		color: var(--clr-text-2);
+	}
+</style>
