cd779455c4137875808bc8c9037bae67209e7479
estib <stron@me.com>
2025-05-20 17:19:37 +0200

    Use Ollama SDK
    Don’t use the fetch hook for Ollamaing

--- apps/desktop/src/lib/ai/ollamaClient.ts
+++ apps/desktop/src/lib/ai/ollamaClient.ts
@@ -3,18 +3,12 @@ import {
 	LONG_DEFAULT_COMMIT_TEMPLATE,
 	SHORT_DEFAULT_PR_TEMPLATE
 } from '$lib/ai/prompts';
-import { MessageRole, type PromptMessage, type AIClient, type Prompt } from '$lib/ai/types';
+import { type PromptMessage, type AIClient, type Prompt } from '$lib/ai/types';
 import { isNonEmptyObject } from '@gitbutler/ui/utils/typeguards';
-import { fetch } from '@tauri-apps/plugin-http';
+import { Ollama } from 'ollama/browser';
 
 export const DEFAULT_OLLAMA_ENDPOINT = 'http://127.0.0.1:11434';
 export const DEFAULT_OLLAMA_MODEL_NAME = 'llama3';
-
-enum OllamaAPEndpoint {
-	Generate = 'api/generate',
-	Chat = 'api/chat',
-	Embed = 'api/embeddings'
-}
 
 interface OllamaRequestOptions {
 	/**
@@ -22,14 +16,6 @@ interface OllamaRequestOptions {
 	 * Increasing the temperature will make the model answer more creatively. (Default: 0.8)
 	 */
 	temperature: number;
-}
-
-interface OllamaChatRequest {
-	model: string;
-	messages: Prompt;
-	stream: boolean;
-	format?: 'json';
-	options?: OllamaRequestOptions;
 }
 
 interface BaseOllamaMResponse {
@@ -41,27 +27,6 @@ interface BaseOllamaMResponse {
 interface OllamaChatResponse extends BaseOllamaMResponse {
 	message: PromptMessage;
 	done: true;
-}
-
-interface OllamaChatMessageFormat {
-	result: string;
-}
-
-const OLLAMA_CHAT_MESSAGE_FORMAT_SCHEMA = {
-	type: 'object',
-	properties: {
-		result: { type: 'string' }
-	},
-	required: ['result'],
-	additionalProperties: false
-};
-
-function isOllamaChatMessageFormat(message: unknown): message is OllamaChatMessageFormat {
-	if (!isNonEmptyObject(message)) {
-		return false;
-	}
-
-	return typeof message.result === 'string';
 }
 
 function isOllamaChatResponse(response: unknown): response is OllamaChatResponse {
@@ -81,68 +46,20 @@ export class OllamaClient implements AIClient {
 	defaultBranchTemplate = LONG_DEFAULT_BRANCH_TEMPLATE;
 	defaultPRTemplate = SHORT_DEFAULT_PR_TEMPLATE;
 
+	readonly ollama: Ollama;
+
 	constructor(
 		private endpoint: string,
 		private modelName: string
-	) {}
+	) {
+		this.ollama = new Ollama({
+			host: this.endpoint
+		});
+	}
 
 	async evaluate(prompt: Prompt): Promise<string> {
-		const messages = this.formatPrompt(prompt);
-
-		const response = await this.chat(messages);
-
-		const rawResponse = JSON.parse(response.message.content);
-
-		if (!isOllamaChatMessageFormat(rawResponse)) {
-			throw new Error('Invalid response: ' + response.message.content);
-		}
-
-		return rawResponse.result;
-	}
-
-	/**
-	 * Appends a system message which instructs the model to respond using a particular JSON schema
-	 * Modifies the prompt's Assistant messages to make use of the correct schema
-	 */
-	private formatPrompt(prompt: Prompt) {
-		const withFormattedResponses = prompt.map((promptMessage) => {
-			if (promptMessage.role === MessageRole.Assistant) {
-				return {
-					role: MessageRole.Assistant,
-					content: JSON.stringify({ result: promptMessage.content })
-				};
-			} else {
-				return promptMessage;
-			}
-		});
-
-		return [
-			{
-				role: MessageRole.System,
-				content: `You are an expert in software development. Answer the given user prompts following the specified instructions.
-Return your response in JSON and only use the following JSON schema:
-${JSON.stringify(OLLAMA_CHAT_MESSAGE_FORMAT_SCHEMA, null, 2)}`
-			},
-			...withFormattedResponses
-		];
-	}
-
-	/**
-	 * Fetches the chat using the specified request.
-	 * @param request - The OllamaChatRequest object containing the request details.
-	 * @returns A Promise that resolves to the Response object.
-	 */
-	private async fetchChat(request: OllamaChatRequest): Promise<unknown> {
-		const url = new URL(OllamaAPEndpoint.Chat, this.endpoint);
-		const body = JSON.stringify(request);
-
-		return await fetch(url.toString(), {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body
-		}).then(async (response) => await response.json());
+		const response = await this.chat(prompt);
+		return response.message.content;
 	}
 
 	/**
@@ -156,16 +73,15 @@ ${JSON.stringify(OLLAMA_CHAT_MESSAGE_FORMAT_SCHEMA, null, 2)}`
 		messages: Prompt,
 		options?: OllamaRequestOptions
 	): Promise<OllamaChatResponse> {
-		const result = await this.fetchChat({
+		const result = await this.ollama.chat({
 			model: this.modelName,
-			stream: false,
 			messages,
-			options,
-			format: 'json'
+			stream: false,
+			options
 		});
 
 		if (!isOllamaChatResponse(result)) {
-			throw new Error('Invalid response\n' + JSON.stringify(result));
+			throw new Error('Invalid response: ' + JSON.stringify(result));
 		}
 
 		return result;
