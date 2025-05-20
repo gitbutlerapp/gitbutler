8e4f79021f5a4b7b55e3c9a1e28843db153f2686
Caleb Owens <hello@calebowens.com>
2025-05-20 09:25:19 +0200

    Added test cases to show asynchronous behaviours

---
+++ apps/desktop/src/components/test/change-during-async-sees-updates/Component.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { ExternallyResolvedPromise } from '$lib/utils/resolveExternally';
+
+	type Props = {
+		promise: ExternallyResolvedPromise<undefined>;
+		log: (value: string) => void;
+	};
+
+	const { promise, log }: Props = $props();
+
+	let value = $state('hello');
+
+	async function logfn() {
+		log(value);
+		await promise.promise;
+		log(value);
+	}
+</script>
+
+<button onclick={logfn} type="button">log</button>
+<button onclick={() => (value = 'world')} type="button">update-state</button>
---
+++ apps/desktop/src/components/test/readme.md
@@ -0,0 +1,8 @@
+# Confirming behavior around TS/Svelte
+
+It's often excessive to test functionality of a language, framework, or library,
+but in cases where documentation is lacking or is poorly defined, it can be
+reasonable to ensure that assumptions do indeed hold.
+
+Test cases defined here likly should be upstreamed to define the behaviors
+perminently.
