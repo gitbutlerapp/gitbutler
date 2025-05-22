06acf53d0035fbc4899db95ad1b6e5ffa1d7b7ce
Mattias Granlund <mtsgrd@gmail.com>
2025-05-22 09:34:32 +0200

    Dismiss v3 popup in e2e test

--- apps/desktop/e2e/tests/add-project.spec.ts
+++ apps/desktop/e2e/tests/add-project.spec.ts
@@ -15,6 +15,8 @@ describe('Project', () => {
 		setElementValue(dirInput, '/tmp/gb-e2e-repos/one-vbranch-on-integration');
 
 		await findAndClick('button[data-testid="add-local-project"]');
+		// TODO: Remove next click when v3 is default!
+		await findAndClick('button[data-testid="v3-not-now"]');
 		await findAndClick('button[data-testid="set-base-branch"]');
 		await findAndClick('button[data-testid="accept-git-auth"]');
 
