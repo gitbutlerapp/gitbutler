aa98913b69828cc7c0626ccc42234b9d032e73e1
Mattias Granlund <mtsgrd@gmail.com>
2025-05-23 23:05:34 +0200

    Make e2e test click utility wait longer
    Will hopefully fix the flakiness issue.

--- apps/desktop/e2e/tests/add-project.spec.ts
+++ apps/desktop/e2e/tests/add-project.spec.ts
@@ -12,13 +12,12 @@ describe('Project', () => {
 		await findAndClick('button[data-testid="analytics-continue"]');
 
 		const dirInput = await $('input[data-testid="test-directory-path"]');
-		setElementValue(dirInput, '/tmp/gb-e2e-repos/one-vbranch-on-integration');
+		await setElementValue(dirInput, '/tmp/gb-e2e-repos/one-vbranch-on-integration');
 
 		await findAndClick('button[data-testid="add-local-project"]');
 		// TODO: Remove next click when v3 is default!
 		await findAndClick('button[data-testid="set-base-branch"]');
 		await findAndClick('button[data-testid="accept-git-auth"]');
-		await findAndClick('button[data-testid="v3-not-now"]');
 
 		const workspaceButton = await $('button=Workspace');
 		await expect(workspaceButton).toExist();
