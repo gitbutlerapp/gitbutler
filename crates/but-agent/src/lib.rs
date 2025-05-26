16b0e7b44aaae26bca58b87e18ffcf561195614e
Caleb Owens <hello@calebowens.com>
2025-05-26 17:24:44 +0100

    De-thread agent code

--- crates/but-agent/src/lib.rs
+++ crates/but-agent/src/lib.rs
@@ -9,9 +9,6 @@
 //! Some way of getting an output.
 //!
 //! Store message logs, because this might be used in a CLI.
-
-use std::{collections::HashMap, ops::Deref};
-
 use serde::{Deserialize, Serialize};
 
 fn get_token() -> Option<gitbutler_secret::Sensitive<String>> {
@@ -55,7 +52,6 @@ pub struct Message {
 }
 
 pub enum Action {
-    Exit,
     /// Starts a new thread that the user can send messages to.
     StartNewThread,
     SendMessage {
@@ -79,9 +75,14 @@ pub enum Response {
     },
 }
 
-pub struct Agent {
-    thread: std::thread::JoinHandle<()>,
-    actions_tx: std::sync::mpsc::Sender<Action>,
+impl Response {
+    pub fn id(&self) -> ConversationId {
+        match self {
+            Response::ThreadCreated { id } => *id,
+            Response::MessageRecieved { id } => *id,
+            Response::ReplyReceived { id } => *id,
+        }
+    }
 }
 
 pub enum LLMParams {
@@ -156,10 +157,12 @@ impl LLM for OpenRouter {
     }
 }
 
+#[cfg(test)]
 struct MockLLM<CB: Fn(String) -> String> {
     callback: CB,
 }
 
+#[cfg(test)]
 impl<CB: Fn(String) -> String> LLM for MockLLM<CB> {
     fn perform(&self, params: LLMParams) -> LLMResponse {
         match params {
@@ -183,7 +186,7 @@ impl ConversationId {
     }
 }
 
-impl Deref for ConversationId {
+impl std::ops::Deref for ConversationId {
     type Target = uuid::Uuid;
 
     fn deref(&self) -> &Self::Target {
@@ -202,19 +205,22 @@ pub trait ConversationStore {
     fn write(&mut self, id: ConversationId, messages: &[Message]);
 }
 
+#[cfg(test)]
 struct InMemoryConversationStore {
-    map: HashMap<ConversationId, Vec<Message>>,
+    map: std::collections::HashMap<ConversationId, Vec<Message>>,
 }
 
 // Construction
+#[cfg(test)]
 impl InMemoryConversationStore {
     fn new() -> Self {
         Self {
-            map: HashMap::new(),
+            map: std::collections::HashMap::new(),
         }
     }
 }
 
+#[cfg(test)]
 impl ConversationStore for InMemoryConversationStore {
     fn read(&self, id: ConversationId) -> Result<Vec<Message>, ConversationStoreReadError> {
         self.map
@@ -229,88 +235,65 @@ impl ConversationStore for InMemoryConversationStore {
 }
 
 pub struct AgentConfig<CB: Fn(Response) + Send + 'static> {
-    pub llm: Box<dyn LLM + Sync + Send>,
-    pub conversation_store: std::sync::Arc<std::sync::Mutex<dyn ConversationStore + Sync + Send>>,
+    pub llm: Box<dyn LLM>,
+    pub conversation_store: std::cell::RefCell<Box<dyn ConversationStore>>,
     pub callback: CB,
     pub system_prompt: String,
 }
 
-impl Agent {
-    pub fn start<CB: Fn(Response) + Send + 'static>(
-        AgentConfig {
-            llm,
-            conversation_store,
-            callback,
-            system_prompt,
-        }: AgentConfig<CB>,
-    ) -> Agent {
-        let (actions_tx, actions_rx) = std::sync::mpsc::channel::<Action>();
+pub fn agent_perform<CB: Fn(Response) + Send + 'static>(
+    AgentConfig {
+        llm,
+        conversation_store,
+        callback,
+        system_prompt,
+    }: &AgentConfig<CB>,
+    action: Action,
+) {
+    #[allow(clippy::never_loop)]
+    loop {
+        match &action {
+            Action::StartNewThread => {
+                let id = ConversationId::generate();
+                conversation_store.borrow_mut().write(
+                    id,
+                    &[Message {
+                        role: MessageRole::System,
+                        content: system_prompt.clone(),
+                    }],
+                );
+                callback(Response::ThreadCreated { id });
+                break;
+            }
+            Action::SendMessage { id, message } => {
+                // Persist and acknowledge the message
+                let messages = {
+                    let mut messages = conversation_store.borrow().read(*id).unwrap();
+                    messages.push(Message {
+                        role: MessageRole::User,
+                        content: message.to_string(),
+                    });
+                    conversation_store.borrow_mut().write(*id, &messages);
+                    callback(Response::MessageRecieved { id: *id });
+                    messages
+                };
 
-        let thread = std::thread::spawn(move || {
-            'thread_loop: loop {
-                let action = actions_rx.recv().unwrap();
+                let response = llm.perform(LLMParams::Message { messages });
 
-                match action {
-                    Action::StartNewThread => {
-                        let id = ConversationId::generate();
-                        let mut conversation_store = conversation_store.lock().unwrap();
-                        conversation_store.write(
-                            id,
-                            &[Message {
-                                role: MessageRole::System,
-                                content: system_prompt.clone(),
-                            }],
-                        );
-                        core::mem::drop(conversation_store);
-                        callback(Response::ThreadCreated { id });
+                match response {
+                    LLMResponse::Message { message } => {
+                        let mut messages = conversation_store.borrow().read(*id).unwrap();
+                        messages.push(Message {
+                            role: MessageRole::Assistant,
+                            content: message,
+                        });
+                        conversation_store.borrow_mut().write(*id, &messages);
+                        callback(Response::ReplyReceived { id: *id });
+                        break;
                     }
-                    Action::SendMessage { id, message } => {
-                        // Persist and acknowledge the message
-                        let messages = {
-                            let mut conversation_store = conversation_store.lock().unwrap();
-                            let mut messages = conversation_store.read(id).unwrap();
-                            messages.push(Message {
-                                role: MessageRole::User,
-                                content: message,
-                            });
-                            conversation_store.write(id, &messages);
-                            core::mem::drop(conversation_store);
-                            callback(Response::MessageRecieved { id });
-                            messages
-                        };
-
-                        let response = llm.perform(LLMParams::Message { messages });
-
-                        match response {
-                            LLMResponse::Message { message } => {
-                                let mut conversation_store = conversation_store.lock().unwrap();
-                                let mut messages = conversation_store.read(id).unwrap();
-                                messages.push(Message {
-                                    role: MessageRole::Assistant,
-                                    content: message,
-                                });
-                                conversation_store.write(id, &messages);
-                                core::mem::drop(conversation_store);
-                                callback(Response::ReplyReceived { id });
-                            }
-                        }
-                    }
-                    Action::Exit => break 'thread_loop,
                 }
             }
-        });
-
-        Self { thread, actions_tx }
-    }
-
-    pub fn perform_action(&self, action: Action) {
-        self.actions_tx.send(action).unwrap();
-    }
-
-    pub fn stop(self) {
-        // It is VERY important that we ask the thread to exit, otherwise it will just hang forever.
-        self.perform_action(Action::Exit);
-        self.thread.join().unwrap();
+        }
     }
 }
 
@@ -323,200 +306,101 @@ mod test {
     }
 
     #[test]
-    fn playgroud() {
-        let (tx, rx) = std::sync::mpsc::channel::<Response>();
-
-        let callback = move |response| {
-            tx.send(response).unwrap();
-        };
-
-        let llm = OpenRouter {
-            token: get_token().unwrap(),
-            model: "qwen/qwen3-32b".into(),
-            provider: "Cerebras".into(),
-        };
-
-        let conversation_store =
-            std::sync::Arc::new(std::sync::Mutex::new(InMemoryConversationStore::new()));
-
-        let start = std::time::SystemTime::now();
-
-        let agent = Agent::start(AgentConfig {
-            llm: Box::new(llm),
-            conversation_store: conversation_store.clone(),
-            callback,
-            system_prompt: system_prompt(),
-        });
-
-        let handle = std::thread::spawn(move || {
-            agent.perform_action(Action::StartNewThread);
-            loop {
-                let message = rx.recv().unwrap();
-
-                match message {
-                    Response::ThreadCreated { id } => {
-                        agent.perform_action(Action::SendMessage {
-                            id,
-                            message: "Generate a 1000 word poem about rust programming.".into(),
-                        });
-                    }
-                    Response::ReplyReceived { id } => {
-                        let conversation_store = conversation_store.lock().unwrap();
-                        let conversation = conversation_store.read(id).unwrap();
-
-                        println!("{}", conversation.last().unwrap().content);
-
-                        agent.stop();
-                        break;
-                    }
-                    _ => {}
-                };
-            }
-        });
-
-        handle.join().unwrap();
-
-        let end = std::time::SystemTime::now();
-        println!("Took: {}", end.duration_since(start).unwrap().as_millis());
-    }
-
-    /// Basic debug thingy to exiserice basic behaviours :D
-    #[test]
-    fn test_start() {
-        let callback = |response| {
-            if let Response::ThreadCreated { id } = response {
-                println!("Created conversation: {:?}", id)
-            };
-        };
-
+    fn send_message() {
         let llm = MockLLM {
-            callback: |string| string,
+            callback: |message| format!("Mocked response: {}", message),
+        };
+        let conversation_store =
+            std::cell::RefCell::new(Box::new(InMemoryConversationStore::new()));
+
+        let responses = std::sync::Arc::new(std::sync::Mutex::new(Vec::new()));
+        let moved_responses = responses.clone();
+        let callback = move |response| {
+            moved_responses.lock().unwrap().push(response);
         };
 
-        let conversation_store =
-            std::sync::Arc::new(std::sync::Mutex::new(InMemoryConversationStore::new()));
-
-        let agent = Agent::start(AgentConfig {
+        let config = AgentConfig {
             llm: Box::new(llm),
             conversation_store,
             callback,
             system_prompt: system_prompt(),
-        });
-        agent.perform_action(Action::StartNewThread);
-        agent.stop()
+        };
+
+        agent_perform(&config, Action::StartNewThread);
+
+        let id = responses.lock().unwrap().last().unwrap().id();
+
+        agent_perform(
+            &config,
+            Action::SendMessage {
+                id,
+                message: "Hello!".into(),
+            },
+        );
+
+        assert_eq!(responses.lock().unwrap().len(), 3);
+        assert!(matches!(
+            responses.lock().unwrap().last().unwrap(),
+            Response::ReplyReceived { .. }
+        ));
+
+        assert_eq!(
+            config.conversation_store.borrow().read(id).unwrap(),
+            vec![
+                Message {
+                    role: MessageRole::System,
+                    content: system_prompt(),
+                },
+                Message {
+                    role: MessageRole::User,
+                    content: "Hello!".into(),
+                },
+                Message {
+                    role: MessageRole::Assistant,
+                    content: "Mocked response: Hello!".into(),
+                },
+            ],
+        );
     }
 
     #[test]
-    fn test_read_conversation_store_after_event() {
-        let (tx, rx) = std::sync::mpsc::channel::<Response>();
-
-        let callback = move |response| {
-            tx.send(response).unwrap();
-        };
-
+    fn create_thread() {
         let llm = MockLLM {
-            callback: |string| string,
+            callback: |message| format!("Mocked response: {}", message),
+        };
+        let conversation_store =
+            std::cell::RefCell::new(Box::new(InMemoryConversationStore::new()));
+
+        let responses = std::sync::Arc::new(std::sync::Mutex::new(Vec::new()));
+        let moved_responses = responses.clone();
+        let callback = move |response| {
+            moved_responses.lock().unwrap().push(response);
         };
 
-        let conversation_store =
-            std::sync::Arc::new(std::sync::Mutex::new(InMemoryConversationStore::new()));
-
-        let agent = Agent::start(AgentConfig {
+        let config = AgentConfig {
             llm: Box::new(llm),
-            conversation_store: conversation_store.clone(),
+            conversation_store,
             callback,
             system_prompt: system_prompt(),
-        });
-
-        let handle = std::thread::spawn(move || {
-            agent.perform_action(Action::StartNewThread);
-            loop {
-                let message = rx.recv().unwrap();
-
-                #[allow(irrefutable_let_patterns)]
-                if let Response::ThreadCreated { id } = message {
-                    {
-                        let conversation_store = conversation_store.lock().unwrap();
-                        let conversation = conversation_store.read(id).unwrap();
-                        assert_eq!(
-                            conversation,
-                            vec![Message {
-                                role: MessageRole::System,
-                                content: system_prompt()
-                            }]
-                        )
-                    }
-                    agent.stop();
-                    break;
-                };
-            }
-        });
-        handle.join().unwrap();
-    }
-
-    #[test]
-    fn test_llm_message_response() {
-        let (tx, rx) = std::sync::mpsc::channel::<Response>();
-
-        let callback = move |response| {
-            tx.send(response).unwrap();
         };
 
-        let llm = MockLLM {
-            callback: |string| format!("response: {}", string),
-        };
+        agent_perform(&config, Action::StartNewThread);
 
-        let conversation_store =
-            std::sync::Arc::new(std::sync::Mutex::new(InMemoryConversationStore::new()));
+        assert_eq!(responses.lock().unwrap().len(), 1);
+        assert!(matches!(
+            responses.lock().unwrap().last().unwrap(),
+            Response::ThreadCreated { .. }
+        ));
 
-        let agent = Agent::start(AgentConfig {
-            llm: Box::new(llm),
-            conversation_store: conversation_store.clone(),
-            callback,
-            system_prompt: system_prompt(),
-        });
+        let id = responses.lock().unwrap().last().unwrap().id();
 
-        let handle = std::thread::spawn(move || {
-            agent.perform_action(Action::StartNewThread);
-            loop {
-                let message = rx.recv().unwrap();
-
-                match message {
-                    Response::ThreadCreated { id } => {
-                        agent.perform_action(Action::SendMessage {
-                            id,
-                            message: "Hello world!".into(),
-                        });
-                    }
-                    Response::ReplyReceived { id } => {
-                        let conversation_store = conversation_store.lock().unwrap();
-                        let conversation = conversation_store.read(id).unwrap();
-                        assert_eq!(
-                            conversation,
-                            vec![
-                                Message {
-                                    role: MessageRole::System,
-                                    content: system_prompt()
-                                },
-                                Message {
-                                    role: MessageRole::User,
-                                    content: "Hello world!".into()
-                                },
-                                Message {
-                                    role: MessageRole::Assistant,
-                                    content: "response: Hello world!".into()
-                                }
-                            ]
-                        );
-
-                        agent.stop();
-                        break;
-                    }
-                    _ => {}
-                };
-            }
-        });
-        handle.join().unwrap();
+        assert_eq!(
+            config.conversation_store.borrow().read(id).unwrap(),
+            vec![Message {
+                role: MessageRole::System,
+                content: system_prompt(),
+            }],
+        );
     }
 
     #[test]
