ff770cce0234ce9a346488c17595ee19094381f1
Caleb Owens <hello@calebowens.com>
2025-05-26 19:06:23 +0100

    Split code into folders and added "tool" type

---
+++ crates/but-agent/src/store.rs
@@ -0,0 +1,49 @@
+use crate::types::{ConversationId, Message};
+
+#[derive(Debug)]
+pub enum ConversationStoreReadError {
+    NotFound,
+    FailedToRead,
+}
+
+pub trait ConversationStore {
+    fn read(&self, id: ConversationId) -> Result<Vec<Message>, ConversationStoreReadError>;
+    fn write(&mut self, id: ConversationId, messages: &[Message]);
+}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use super::*;
+
+    pub struct InMemoryConversationStore {
+        map: std::collections::HashMap<ConversationId, Vec<Message>>,
+    }
+
+    // Construction
+    impl InMemoryConversationStore {
+        pub fn new() -> Self {
+            Self {
+                map: std::collections::HashMap::new(),
+            }
+        }
+    }
+
+    impl Default for InMemoryConversationStore {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl ConversationStore for InMemoryConversationStore {
+        fn read(&self, id: ConversationId) -> Result<Vec<Message>, ConversationStoreReadError> {
+            self.map
+                .get(&id)
+                .cloned()
+                .ok_or(ConversationStoreReadError::NotFound)
+        }
+
+        fn write(&mut self, id: ConversationId, messages: &[Message]) {
+            self.map.insert(id, messages.to_owned());
+        }
+    }
+}
