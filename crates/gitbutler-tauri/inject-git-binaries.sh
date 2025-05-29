1a7bc5b26bdbfd3fc5c73d8f39202ecd8540bfc0
Kiril Videlov <kiril@videlov.com>
2025-05-29 16:57:18 +0200

    Distribute the but binary

--- crates/gitbutler-tauri/inject-git-binaries.sh
+++ crates/gitbutler-tauri/inject-git-binaries.sh
@@ -15,6 +15,7 @@ CRATE_ROOT="$ROOT/crates/gitbutler-tauri"
 ASKPASS="gitbutler-git-askpass"
 SETSID="gitbutler-git-setsid"
 CLI="but-testing"
+BUT="but"
 
 
 if [ -f "$TARGET_ROOT/$ASKPASS" ] && [ -f "$TARGET_ROOT/$SETSID" ] && [ -f "$TARGET_ROOT/$CLI" ]; then
@@ -22,11 +23,13 @@ if [ -f "$TARGET_ROOT/$ASKPASS" ] && [ -f "$TARGET_ROOT/$SETSID" ] && [ -f "$TAR
     cp -v "$TARGET_ROOT/$ASKPASS" "$CRATE_ROOT/$ASKPASS-${TRIPLE}"
     cp -v "$TARGET_ROOT/$SETSID" "$CRATE_ROOT/$SETSID-${TRIPLE}"
     cp -v "$TARGET_ROOT/$CLI" "$CRATE_ROOT/$CLI-${TRIPLE}"
+    cp -v "$TARGET_ROOT/$BUT" "$CRATE_ROOT/$BUT-${TRIPLE}"
 elif [ -f "$TARGET_ROOT/$ASKPASS.exe" ] && [ -f "$TARGET_ROOT/$SETSID.exe" ] && [ -f "$TARGET_ROOT/$CLI.exe" ]; then
     log injecting gitbutler-git binaries into crates/gitbutler-tauri "(TRIPLE=${TRIPLE})"
     cp -v "$TARGET_ROOT/$ASKPASS.exe" "$CRATE_ROOT/$ASKPASS-${TRIPLE}.exe"
     cp -v "$TARGET_ROOT/$SETSID.exe" "$CRATE_ROOT/$SETSID-${TRIPLE}.exe"
     cp -v "$TARGET_ROOT/$CLI.exe" "$CRATE_ROOT/$CLI-${TRIPLE}.exe"
+    cp -v "$TARGET_ROOT/$BUT.exe" "$CRATE_ROOT/$BUT-${TRIPLE}.exe"
 else
     log gitbutler-git binaries are not built
     exit 1
