7f5a6b3b8af3e809ecc72a4dba18fcbfd6c18bd8
Pavel Laptev <pawellaptew@gmail.com>
2025-06-02 16:14:45 +0200

    ui-fixes-76 (#8877)
    * remove unused
    
    * fix: add bottom margin
    
    * ui: update design for the "Forge" section
    
    * feat(modal): disalow select text

--- apps/desktop/src/components/ForgeForm.svelte
+++ apps/desktop/src/components/ForgeForm.svelte
@@ -53,6 +53,7 @@
 		if (!$project) return;
 
 		const mutableProject: Project & { unset_forge_override?: boolean } = structuredClone($project);
+
 		if (selectedOption === 'default') {
 			mutableProject.unset_forge_override = true;
 		} else {
@@ -62,75 +63,89 @@
 	}
 </script>
 
-<Spacer />
-<SectionCard>
-	<h3 class="text-bold text-15">Forge override</h3>
-	{#if $determinedForgeType === 'default'}
-		<p class="text-13">
-			We were unable to determine what forge you were using. In order to make use of a forge
-			integration, select your forge of it from the dropdown below.
-		</p>
-		<p class="text-13">
-			Please note that only the GitHub and GitLab support PR creation at the moment.
-		</p>
-		<Select
-			value={selectedOption}
-			options={forgeOptions}
-			wide
-			onselect={(value) => {
-				selectedOption = value as ForgeName;
-				handleSelectionChange(selectedOption);
-			}}
-		>
-			{#snippet itemSnippet({ item, highlighted })}
-				<SelectItem selected={item.value === selectedOption} {highlighted}>
-					{item.label}
-				</SelectItem>
-			{/snippet}
-		</Select>
-	{:else}
-		<p class="text-13">
-			We have determined that you are currently using <code>{$determinedForgeType}</code>. We
-			currently do not support overriding an automatically determined forge type.
-		</p>
-	{/if}
-</SectionCard>
+<div>
+	<SectionCard roundedBottom={forge.current.name !== 'gitlab'}>
+		{#snippet title()}
+			Forge override
+		{/snippet}
 
-{#if forge.current.name === 'gitlab'}
-	<Spacer />
-	<SectionCard>
-		<h3 class="text-bold text-15">Configure GitLab Integration</h3>
-		<p class="text-13">
-			Learn how find your GitLab Personal Token and Project ID in our <Link
-				href="https://docs.gitbutler.com/features/gitlab-integration">documentation</Link
-			>
-		</p>
-		<p class="text-13">
-			The Fork Project ID should be the project that your branches get pushed to, and the Upstream
-			Project ID should be project where you want the Merge Requests to be created.
-		</p>
+		{#snippet caption()}
+			{#if $determinedForgeType === 'default'}
+				We couldn't detect which Forge you're using.
+				<br />
+				To enable Forge integration, please select your Forge from the dropdown below.
+				<br />
+				<span class="text-bold">Note:</span> Currently, only GitHub and GitLab support pull request creation.
+			{:else}
+				We’ve detected that you’re using <span class="text-bold"
+					>{$determinedForgeType.toUpperCase()}</span
+				>.
+				<br />
+				At the moment, it’s not possible to manually override the detected forge type.
+			{/if}
+		{/snippet}
 
-		<Textbox label="Personal Token" value={$token} oninput={(value) => ($token = value)} />
-		<Textbox
-			label="Your Fork's Project ID"
-			value={$forkProjectId}
-			oninput={(value) => ($forkProjectId = value)}
-		/>
-		<Textbox
-			label="Upstream Project ID"
-			value={$upstreamProjectId}
-			oninput={(value) => ($upstreamProjectId = value)}
-		/>
-		<Textbox
-			label="Instance URL"
-			value={$instanceUrl}
-			oninput={(value) => ($instanceUrl = value)}
-		/>
-		<p class="text-13">
-			If you use a custom GitLab instance (not gitlab.com), you will need to add it as a custom CSP
-			entry so that GitButler trusts connecting to that host. Read more in the <Link
-				href="https://docs.gitbutler.com/troubleshooting/custom-csp">docs</Link
+		{#if $determinedForgeType === 'default'}
+			<Select
+				value={selectedOption}
+				options={forgeOptions}
+				wide
+				onselect={(value) => {
+					selectedOption = value as ForgeName;
+					handleSelectionChange(selectedOption);
+				}}
 			>
-		</p>
+				{#snippet itemSnippet({ item, highlighted })}
+					<SelectItem selected={item.value === selectedOption} {highlighted}>
+						{item.label}
+					</SelectItem>
+				{/snippet}
+			</Select>
+		{:else}{/if}
 	</SectionCard>
-{/if}
+
+	{#if forge.current.name === 'gitlab'}
+		<SectionCard roundedTop={false} roundedBottom={false}>
+			{#snippet title()}
+				Configure GitLab integration
+			{/snippet}
+
+			{#snippet caption()}
+				Learn how find your GitLab Personal Token and Project ID in our <Link
+					href="https://docs.gitbutler.com/features/gitlab-integration">docs</Link
+				>
+				<br />
+				The Fork Project ID is where your branches will be pushed, and the Upstream Project ID is where
+				you want merge requests to be created.
+				<br />
+			{/snippet}
+
+			<Textbox label="Personal token" value={$token} oninput={(value) => ($token = value)} />
+			<Textbox
+				label="Your fork's project ID"
+				value={$forkProjectId}
+				oninput={(value) => ($forkProjectId = value)}
+			/>
+			<Textbox
+				label="Upstream project ID"
+				value={$upstreamProjectId}
+				oninput={(value) => ($upstreamProjectId = value)}
+			/>
+			<Textbox
+				label="Instance URL"
+				value={$instanceUrl}
+				oninput={(value) => ($instanceUrl = value)}
+			/>
+		</SectionCard>
+
+		<SectionCard roundedTop={false}>
+			{#snippet caption()}
+				If you use a custom GitLab instance (not gitlab.com), you will need to add it as a custom
+				CSP entry so that GitButler trusts connecting to that host. Read more in the <Link
+					href="https://docs.gitbutler.com/troubleshooting/custom-csp">docs</Link
+				>
+			{/snippet}
+		</SectionCard>
+	{/if}
+</div>
+<Spacer />
