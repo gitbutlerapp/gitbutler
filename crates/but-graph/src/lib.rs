22598369b49d07f5e1a4c2a4185b9b0a5f747c61
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-06-22 06:50:54 +0200

    Add more documentation for the `but-graph` crate

--- crates/but-graph/src/lib.rs
+++ crates/but-graph/src/lib.rs
@@ -1,4 +1,39 @@
 //! A graph data structure for seeing the Git commit graph as segments.
+//!
+//! This crate is part of the Correctness and Stability initiative.
+//!
+//! ### Before the Graph
+//!
+//! The application traditionally displays commits in lanes while allowing them to be segmented. Today a lane is a
+//! stack of segments. This is great for users as it's easy to understand, but there is a problem with it: it's a complete lie.
+//!
+//! To generate stacks, one performed a graph traversal, following only the first parent, down to the merge-base of the workspace
+//! commit with the target branch. It's clear how this degenerates information especially around merges that the application
+//! helps to create by allowing to merge with the target branch.
+//!
+//! When trying to rebase the workspace onto updated target branches though, it still exclusively operates in stacks, ignoring
+//! the complexities of the underlying graph, and the illusion starts to break down.
+//!
+//! Besides that, there is an inherent mental issue when programming with data structures that degenerate information, as the
+//! resulting program will inherently be unfit for the task as it's based on over-simplified assumptions baked into its very core.
+//!
+//! With the current data structures, achieving correctness simply isn't possible.
+//!
+//! ### The Graph - the Solution
+//!
+//! The graph solves this problem by simplifying the commit-graph and optimizing it for traversal, without oversimplifying the
+//! underlying commit-graph. That way, the notion of stacks with segments is merely a view of the graph specifically prepared
+//! for presentation.
+//!
+//! When operations are supposed to be performed, we can now do things like this `git rebase --preserve-merges origin/main`,
+//! creating a correctly ordered list of operations to do the transplantation correctly. Thanks to the additional metadata
+//! collected into the Graph I'd expect us to be able to whatever it takes without limitation.
+//!
+//! Besides that, operating on a graph, despite its own complexities, is finally aligning the mental model of the programmer
+//! with what's actually there, for algorithms suitable to perform the job correctly.
+//!
+//! All this makes the Graph the **new core data-structure** that is the world of GitButler and upon which visualisations and
+//! mutation operations are based.
 #![forbid(unsafe_code)]
 #![deny(missing_docs, rust_2018_idioms)]
 
