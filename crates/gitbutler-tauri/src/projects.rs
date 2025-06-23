416805fa46a9b5e5a0264cfa4be781302e3f617c
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-06-23 14:45:12 +0200

    Make the database error show up in the userinterface

--- crates/gitbutler-tauri/src/projects.rs
+++ crates/gitbutler-tauri/src/projects.rs
@@ -71,31 +71,45 @@ pub mod commands {
             })
     }
 
+    /// Additional information to help the user interface communicate what happened with the project.
+    #[derive(Debug, serde::Serialize)]
+    pub struct ProjectInfo {
+        /// `true` if the window is the first one to open the project.
+        is_exclusive: bool,
+        /// The display version of the error that communicates what went wrong while opening the database.
+        db_error: Option<String>,
+    }
+
     /// This trigger is the GUI telling us that the project with `id` is now displayed.
     /// Return `true` if the project is opened exclusively, i.e. there is no other Window looking at it.
     ///
     /// We use it to start watching for filesystem events.
     #[tauri::command(async)]
-    #[instrument(skip(projects, window_state, window, app_settings), err(Debug))]
+    #[instrument(skip(projects, window_state, window, app_settings), err(Debug), ret)]
     pub fn set_project_active(
         projects: State<'_, Controller>,
         window_state: State<'_, WindowState>,
         app_settings: State<'_, AppSettingsWithDiskSync>,
         window: Window,
         id: ProjectId,
-    ) -> Result<bool, Error> {
+    ) -> Result<ProjectInfo, Error> {
         let project = projects.get_validated(id).context("project not found")?;
         let mode = window_state.set_project_to_window(
             window.label(),
             &project,
             app_settings.inner().clone(),
         )?;
-        if let Some(err) = assure_database_valid(project.gb_dir())? {
-            tracing::error!("{err}");
+        let db_error = assure_database_valid(project.gb_dir())?.map(|err| err.to_string());
+        if let Some(db_error) = &db_error {
+            tracing::error!("{db_error}");
         }
-        Ok(match mode {
+        let is_exclusive = match mode {
             ProjectAccessMode::First => true,
             ProjectAccessMode::Shared => false,
+        };
+        Ok(ProjectInfo {
+            is_exclusive,
+            db_error,
         })
     }
 
