e40d8f67307c8c5058c11f929988754ae326169a
Mattias Granlund <mtsgrd@gmail.com>
2025-07-08 23:23:05 +0200

    Give default width to panel 1 in stack view
    Prevents changing width as visible diff line lengths vary.

--- .conflict-base-0/apps/desktop/src/lib/files/utils.ts
+++
@@ -1,9 +0,0 @@
-export function getFilename(path: string) {
-	return path.split(/[/\\]/).pop();
-}
-
-export function getDir(path: string) {
-	const parts = path.split(/[/\\]/);
-	parts.pop();
-	return parts.join(path.includes('\\') ? '\\' : '/');
-}
