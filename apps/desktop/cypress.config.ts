0df8f3f4efd8944617a6d076f5e6f57b485bac93
Mattias Granlund <mtsgrd@gmail.com>
2025-07-11 19:31:11 +0200

    Add a Cypress component test for commit message editor

--- apps/desktop/cypress.config.ts
+++ apps/desktop/cypress.config.ts
@@ -1,4 +1,6 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'cypress';
+import path from 'path';
 
 export default defineConfig({
 	retries: {
@@ -7,9 +9,29 @@ export default defineConfig({
 		// Configure retry attempts for `cypress open`
 		openMode: 0
 	},
+
 	e2e: {
 		baseUrl: 'http://localhost:1420',
 		supportFile: 'cypress/e2e/support/index.ts'
 	},
-	experimentalWebKitSupport: true
+
+	experimentalWebKitSupport: true,
+
+	component: {
+		devServer: {
+			framework: 'svelte',
+			bundler: 'vite',
+			viteConfig: {
+				plugins: [svelte()],
+				resolve: {
+					alias: {
+						$components: path.resolve('src/components'),
+						$lib: path.resolve('src/lib')
+					}
+				}
+			}
+		},
+		// 👇 And this line if Cypress still fails to resolve the iframe mount file
+		indexHtmlFile: 'cypress/support/index.html'
+	}
 });
