7260e28d532934209df190e167cc3a9e6293bd7c
Mattias Granlund <mtsgrd@gmail.com>
2025-07-14 13:17:23 +0200

    Drop unused sveltekit routes
    These settings routes have not actually been in use for a very long 
    time.

--- apps/desktop/src/routes/settings/appearance/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import AppearanceSettings from '$components/v3/projectSettings/AppearanceSettings.svelte';
-</script>
-
-<h1 class="text-head-20">Appearance</h1>
-<AppearanceSettings />
--- apps/desktop/src/routes/settings/telemetry/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import TelemetrySettings from '$components/v3/profileSettings/TelemetrySettings.svelte';
-</script>
-
-<h1 class="text-head-20">Telemetry</h1>
-<TelemetrySettings />
