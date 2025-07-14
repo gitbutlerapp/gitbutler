b77ceca82fd18a9c6ad914dd2648f8c77dc1bbe2
Mattias Granlund <mtsgrd@gmail.com>
2025-07-14 11:28:15 +0200

    Drop more unused v2 components

--- apps/desktop/src/routes/settings/organizations/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import OrganisationSettings from '$components/v3/profileSettings/OrganisationSettings.svelte';
-</script>
-
-<h1 class="text-head-20">Organisations</h1>
-<OrganisationSettings />
