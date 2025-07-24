6403e6f6976d9f34605666b6f26917ba18f25127
Mattias Granlund <mtsgrd@gmail.com>
2025-07-24 17:52:31 +0200

    Convert shortcut services from .svelte.ts to just .ts
    Because $effects are difficult to work with in unit tests.

--- apps/desktop/src/components/GlobalSettingsMenuAction.svelte
+++ apps/desktop/src/components/GlobalSettingsMenuAction.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { newSettingsPath } from '$lib/routes/routes.svelte';
-	import { SHORTCUT_SERVICE } from '$lib/shortcuts/shortcutService.svelte';
+	import { SHORTCUT_SERVICE } from '$lib/shortcuts/shortcutService';
 	import { inject } from '@gitbutler/shared/context';
 
 	const shortcutService = inject(SHORTCUT_SERVICE);
-	shortcutService.on('global-settings', () => {
-		goto(newSettingsPath());
-	});
+	$effect(() =>
+		shortcutService.on('global-settings', () => {
+			goto(newSettingsPath());
+		})
+	);
 </script>
