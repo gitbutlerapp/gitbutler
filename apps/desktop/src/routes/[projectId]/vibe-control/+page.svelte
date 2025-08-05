43ba29a6c9771dd6b815a51231faf7108c3c2207
Caleb Owens <hello@calebowens.com>
2025-08-05 11:19:15 +0200

    Basic UI for testing the bridge

---
+++ apps/desktop/src/routes/[projectId]/vibe-control/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import VibeControlPage from '$components/vibeControl/VibeControlPage.svelte';
+
+	// TODO: Refactor so we don't need non-null assertion.
+	const projectId = $derived(page.params.projectId!);
+</script>
+
+<VibeControlPage {projectId} />
