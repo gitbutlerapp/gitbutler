bcc34d9e7ad01145f895b8ee7d821fe4d8d8eee4
Pavel Laptev <pawellaptew@gmail.com>
2025-08-14 16:02:46 +0200

    refactor(ui): simplify AsyncButton props and loading flow for clarity

--- packages/ui/src/lib/components/AsyncButton.svelte
+++ packages/ui/src/lib/components/AsyncButton.svelte
@@ -1,49 +1,14 @@
 <script lang="ts">
-	import Button from '$components/Button.svelte';
-	import type { TooltipPosition, TooltipAlign } from '$components/Tooltip.svelte';
-	import type iconsJson from '$lib/data/icons.json';
-	import type { ComponentColorType, ComponentKindType } from '$lib/utils/colorTypes';
-	import type { Snippet } from 'svelte';
+	import Button, { type Props as ButtonProps } from '$components/Button.svelte';
 
-	type ButtonPropsSubset = {
-		id?: string | undefined;
-		el?: HTMLElement;
-		// Interaction props
-		disabled?: boolean;
-		activated?: boolean;
-		tabindex?: number | undefined;
-		type?: 'submit' | 'reset' | 'button' | undefined;
-		// Layout props
-		shrinkable?: boolean;
-		reversedDirection?: boolean;
-		width?: number | undefined;
-		maxWidth?: number | undefined;
-		size?: 'tag' | 'button' | 'cta';
-		wide?: boolean;
-		grow?: boolean;
-		align?: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline' | 'auto';
-		dropdownChild?: boolean;
-		// Style props
-		style?: ComponentColorType;
-		kind?: ComponentKindType;
-		solidBackground?: boolean;
-		// Additional elements
-		icon?: keyof typeof iconsJson | undefined;
-		tooltip?: string;
-		tooltipPosition?: TooltipPosition;
-		tooltipAlign?: TooltipAlign;
-		tooltipDelay?: number;
-		testId?: string;
-		loading?: boolean;
-		class?: string;
-		// Snippets
-		children?: Snippet;
+	type Props = Omit<ButtonProps, 'onclick'> & {
+		action: () => Promise<void>;
+		stopPropagation?: boolean;
 	};
 
-	type Props = ButtonPropsSubset & { action: () => Promise<void>; stopPropagation?: boolean };
-	let { action, loading = $bindable(), stopPropagation = false, ...rest }: Props = $props();
+	let { action, stopPropagation = false, ...rest }: Props = $props();
 
-	let state = $state<'inert' | 'loading' | 'complete'>('inert');
+	let loading = $state(false);
 
 	async function performAction(event: Event) {
 		if (stopPropagation) {
@@ -58,8 +23,6 @@
 			loading = false;
 		}
 	}
-
-	const isLoading = $derived(loading ?? state === 'loading');
 </script>
 
-<Button onclick={performAction} loading={isLoading} {...rest}></Button>
+<Button onclick={performAction} {loading} {...rest}></Button>
