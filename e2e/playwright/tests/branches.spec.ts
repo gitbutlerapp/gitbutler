ce3326a472ea8f1906c01932ecf003141786a311
estib <stron@me.com>
2025-09-05 18:00:24 +0200

    Add a test for simple integration of remote changes in branch

--- e2e/playwright/tests/branches.spec.ts
+++ e2e/playwright/tests/branches.spec.ts
@@ -1,5 +1,5 @@
 import { getBaseURL, type GitButler, startGitButler } from '../src/setup.ts';
-import { clickByTestId, getByTestId, waitForTestId } from '../src/util.ts';
+import { clickByTestId, getByTestId, sleep, waitForTestId } from '../src/util.ts';
 import { expect, test } from '@playwright/test';
 
 let gitbutler: GitButler;
@@ -56,4 +56,62 @@ test('should be able to apply a remote branch', async ({ page, context }, testIn
 	// The stack should have two commits
 	const commits = getByTestId(page, 'commit-row');
 	await expect(commits).toHaveCount(2);
+});
+
+test('should be able to apply a remote branch and integrate the remote changes - simple', async ({
+	page,
+	context
+}, testInfo) => {
+	const workdir = testInfo.outputPath('workdir');
+	const configdir = testInfo.outputPath('config');
+	gitbutler = await startGitButler(workdir, configdir, context);
+
+	await gitbutler.runScript('project-with-remote-branches.sh');
+
+	await page.goto('/');
+
+	// Should load the workspace
+	await waitForTestId(page, 'workspace-view');
+
+	// Should navigate to the branches page when clicking the branches button
+	await clickByTestId(page, 'navigation-branches-button');
+	const header = await waitForTestId(page, 'target-commit-list-header');
+
+	await expect(header).toContainText('origin/master');
+
+	const branchListCards = getByTestId(page, 'branch-list-card');
+	await expect(branchListCards).toHaveCount(2);
+
+	const firstBranchCard = branchListCards.filter({ hasText: 'branch1' });
+	await expect(firstBranchCard).toBeVisible();
+	await firstBranchCard.click();
+
+	// The delete branch should be visible
+	await waitForTestId(page, 'branches-view-delete-local-branch-button');
+
+	// Apply the branch
+	await clickByTestId(page, 'branches-view-apply-branch-button');
+	// Should be redirected to the workspace
+	await waitForTestId(page, 'workspace-view');
+
+	// There should be only one stack
+	const stacks = getByTestId(page, 'stack');
+	await expect(stacks).toHaveCount(1);
+	const stack = stacks.first();
+	await expect(stack).toContainText('branch1');
+
+	// The stack should have two commits
+	const commits = getByTestId(page, 'commit-row');
+	await expect(commits).toHaveCount(2);
+
+	await gitbutler.runScript('project-with-remote-branches__add-commit-to-remote-branch.sh');
+
+	// Click the sync button
+	await clickByTestId(page, 'sync-button');
+
+	// Integrate upstream commits
+	await clickByTestId(page, 'upstream-commits-integrate-button');
+
+	const commitsAfterIntegration = getByTestId(page, 'commit-row');
+	await expect(commitsAfterIntegration).toHaveCount(3);
 });
