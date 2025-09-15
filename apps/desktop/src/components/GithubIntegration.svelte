496abd6de458411b2da47691b0aceb2d41751014
Pavel Laptev <pawellaptew@gmail.com>
2025-09-16 00:26:11 +0200

    Update GithubIntegration.svelte

--- apps/desktop/src/components/GithubIntegration.svelte
+++ apps/desktop/src/components/GithubIntegration.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { OnboardingEvent, POSTHOG_WRAPPER } from '$lib/analytics/posthog';
+	import githubLogoSvg from '$lib/assets/unsized-logos/github.svg?raw';
 	import { CLIPBOARD_SERVICE } from '$lib/backend/clipboard';
 	import { GITHUB_USER_SERVICE } from '$lib/forge/github/githubUserService.svelte';
 	import { USER_SERVICE } from '$lib/user/userService';
@@ -98,19 +99,9 @@
 							<Icon name="success" color="success" size={18} />
 						</div>
 					{/if}
-					<svg
-						width="28"
-						height="28"
-						viewBox="0 0 28 28"
-						fill="var(--clr-scale-ntrl-0)"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M14.0116 0C6.26354 0 0 6.41664 0 14.3549C0 20.7004 4.01327 26.0717 9.58073 27.9728C10.2768 28.1157 10.5318 27.6639 10.5318 27.2838C10.5318 26.9511 10.5088 25.8104 10.5088 24.6218C6.61115 25.4776 5.79949 22.9106 5.79949 22.9106C5.17311 21.247 4.245 20.8194 4.245 20.8194C2.96929 19.94 4.33793 19.94 4.33793 19.94C5.75303 20.0351 6.49557 21.4135 6.49557 21.4135C7.74804 23.5998 9.76629 22.9821 10.5782 22.6017C10.6941 21.6748 11.0655 21.0332 11.4599 20.6767C8.3512 20.344 5.08047 19.1082 5.08047 13.5942C5.08047 12.0257 5.63687 10.7423 6.51851 9.74425C6.37941 9.38784 5.89213 7.91405 6.6579 5.94152C6.6579 5.94152 7.84097 5.56119 10.5085 7.41501C11.6506 7.10079 12.8284 6.94094 14.0116 6.9396C15.1947 6.9396 16.4007 7.10614 17.5143 7.41501C20.1822 5.56119 21.3653 5.94152 21.3653 5.94152C22.131 7.91405 21.6435 9.38784 21.5044 9.74425C22.4092 10.7423 22.9427 12.0257 22.9427 13.5942C22.9427 19.1082 19.672 20.32 16.5401 20.6767C17.0506 21.1282 17.4911 21.9837 17.4911 23.3385C17.4911 25.2635 17.4682 26.8084 17.4682 27.2836C17.4682 27.6639 17.7234 28.1157 18.4192 27.9731C23.9867 26.0714 27.9999 20.7004 27.9999 14.3549C28.0229 6.41664 21.7364 0 14.0116 0Z"
-						/>
-					</svg>
+					<div class="icon-wrapper__logo">
+						{@html githubLogoSvg}
+					</div>
 				</div>
 			{/snippet}
 			{#snippet title()}
@@ -212,6 +203,11 @@
 		flex-direction: column;
 		width: 100%;
 		margin-bottom: 8px;
+	}
+
+	.icon-wrapper__logo {
+		width: 28px;
+		height: 28px;
 	}
 
 	.step-section {
