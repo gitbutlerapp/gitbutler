870f54d2437de2de5edd5a85dd2deed5b5da71ae
Mattias Granlund <mtsgrd@gmail.com>
2025-09-16 00:56:25 +0200

    Hide tooltips when dragging
    If you know you know.

--- .conflict-base-0/packages/shared/src/lib/branches/latestBranchLookupSlice.ts
+++
@@ -1,4 +0,0 @@
-import { buildLoadableTable } from '$lib/redux/defaultSlices';
-import type { LoadableBranchUuid } from '$lib/branches/types';
-
-export const latestBranchLookupTable = buildLoadableTable<LoadableBranchUuid>('latestBranchLookup');
