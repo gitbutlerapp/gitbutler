61cbaeae257a1bb2e8f524dd99e8552c8ed88f30
Kiril Videlov <kiril@videlov.com>
2025-09-24 18:12:36 +0200

    but status use but api

--- crates/but/src/status/mod.rs
+++ crates/but/src/status/mod.rs
@@ -2,7 +2,7 @@ use assignment::FileAssignment;
 use bstr::BString;
 use but_core::ui::{TreeChange, TreeStatus};
 use but_hunk_assignment::HunkAssignment;
-use but_settings::AppSettings;
+use but_workspace::ui::StackDetails;
 use colored::Colorize;
 use gitbutler_command_context::CommandContext;
 use gitbutler_project::Project;
@@ -13,26 +13,13 @@ pub(crate) mod assignment;
 use crate::id::CliId;
 
 pub(crate) fn worktree(repo_path: &Path, _json: bool) -> anyhow::Result<()> {
-    let project = Project::from_path(repo_path).expect("Failed to create project from path");
-    let ctx = &mut CommandContext::open(&project, AppSettings::load_from_default_path_creating()?)?;
+    let project = Project::find_by_path(repo_path).expect("Failed to create project from path");
 
-    let stack_id_to_branch = crate::log::stacks(ctx)?
-        .iter()
-        .filter_map(|s| {
-            s.heads.first().and_then(|head| {
-                let id = s.id?;
-                let x = head.name.to_string();
-                Some((id, x))
-            })
-        })
-        .collect::<BTreeMap<but_workspace::StackId, String>>();
-
-    let changes = but_core::diff::ui::worktree_changes_by_worktree_dir(project.path)?.changes;
-    let (assignments, _assignments_error) =
-        but_hunk_assignment::assignments_with_fallback(ctx, false, Some(changes.clone()), None)?;
+    let stacks = but_api::workspace::stacks(project.id, None)?;
+    let worktree_changes = but_api::diff::changes_in_worktree(project.id)?;
 
     let mut by_file: BTreeMap<BString, Vec<HunkAssignment>> = BTreeMap::new();
-    for assignment in &assignments {
+    for assignment in worktree_changes.assignments {
         by_file
             .entry(assignment.path_bytes.clone())
             .or_default()
@@ -45,39 +32,46 @@ pub(crate) fn worktree(repo_path: &Path, _json: bool) -> anyhow::Result<()> {
             FileAssignment::from_assignments(path, assignments),
         );
     }
-    if stack_id_to_branch.is_empty() {
-        println!("No branches found. ¯\\_(ツ)_/¯");
-        return Ok(());
-    }
+    let mut stack_details = vec![];
 
     let unassigned = assignment::filter_by_stack_id(assignments_by_file.values(), &None);
-    print_group(None, unassigned, &changes)?;
+    stack_details.push((None, (None, unassigned)));
+    for stack in stacks {
+        let details = but_api::workspace::stack_details(project.id, stack.id)?;
+        let assignments = assignment::filter_by_stack_id(assignments_by_file.values(), &stack.id);
+        stack_details.push((stack.id, (Some(details), assignments)));
+    }
 
-    for (stack_id, branch) in &stack_id_to_branch {
-        let filtered =
-            assignment::filter_by_stack_id(assignments_by_file.values(), &Some(*stack_id));
-        print_group(Some(branch.as_str()), filtered, &changes)?;
+    for (_stack_id, (details, assignments)) in stack_details {
+        print_group(
+            details,
+            assignments,
+            &worktree_changes.worktree_changes.changes,
+        )?;
     }
     Ok(())
 }
 
 pub fn print_group(
-    group: Option<&str>,
+    group: Option<StackDetails>,
     assignments: Vec<FileAssignment>,
     changes: &[TreeChange],
 ) -> anyhow::Result<()> {
-    let id = if let Some(group) = group {
-        CliId::branch(group)
+    let binding = group
+        .clone()
+        .map(|g| g.derived_name)
+        .unwrap_or("UNASSIGNED".to_string());
+    let name = binding.as_str();
+
+    let id = if let Some(_group) = group {
+        CliId::branch(name)
     } else {
         CliId::unassigned()
     }
     .to_string()
     .underline()
     .blue();
-    let group = &group
-        .map(|s| format!("[{s}]"))
-        .unwrap_or("<UNASSIGNED>".to_string());
-    println!("{}    {}", id, group.green().bold());
+    println!("{}    {}", id, name.green().bold());
     for fa in assignments {
         let state = status_from_changes(changes, fa.path.clone());
         let path = match state {
