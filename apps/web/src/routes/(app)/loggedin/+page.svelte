5f10436aef86ca063fc430e505cb486fb9810c44
estib <stron@me.com>
2025-10-01 11:12:23 +0200

    Logged in page
    Add a page that displays the logged in state for the client login flow

---
+++ apps/web/src/routes/(app)/loggedin/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import FullscreenIllustrationCard from '$lib/components/service/FullscreenIllustrationCard.svelte';
+</script>
+
+<svelte:head>
+	<title>GitButler | Logged in</title>
+</svelte:head>
+
+<FullscreenIllustrationCard>
+	{#snippet title()}
+		You are succesfully logged in!
+	{/snippet}
+
+	<div>You can now close this window and return to your client.</div>
+</FullscreenIllustrationCard>
