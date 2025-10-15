bde9586d88d30645eb199cf17fbb2803dbadceda
estib <stron@me.com>
2025-10-15 16:46:40 +0200

    GitHub User Service: Add the retrieve and forget user methods
    Add methods that call the APIs to get infromation about the user and the mutation to forget that user

--- apps/desktop/src/lib/forge/github/githubUserService.svelte.ts
+++ apps/desktop/src/lib/forge/github/githubUserService.svelte.ts
@@ -1,8 +1,8 @@
 import { ghQuery } from '$lib/forge/github/ghQuery';
-import { providesList, ReduxTag } from '$lib/state/tags';
+import { providesItem, providesList, ReduxTag } from '$lib/state/tags';
 import { InjectionToken } from '@gitbutler/core/context';
 import type { IBackend } from '$lib/backend';
-import type { GitHubApi } from '$lib/state/clientState.svelte';
+import type { BackendApi, GitHubApi } from '$lib/state/clientState.svelte';
 import type { RestEndpointMethodTypes } from '@octokit/rest';
 
 export const GITHUB_USER_SERVICE = new InjectionToken<GitHubUserService>('GitHubUserService');
@@ -21,18 +21,25 @@ type AuthStatusResponse = {
 	email: string | null;
 };
 
+type AuthenticatedUser = {
+	accessToken: string;
+	login: string;
+	name: string | null;
+	email: string | null;
+	avatarUrl: string | null;
+};
+
 export class GitHubUserService {
 	private api: ReturnType<typeof injectEndpoints>;
+	private backendApi: ReturnType<typeof injectBackendEndpoints>;
 
 	constructor(
 		private backend: IBackend,
+		backendApi: BackendApi,
 		gitHubApi: GitHubApi
 	) {
 		this.api = injectEndpoints(gitHubApi);
-	}
-
-	async fetchGitHubLogin() {
-		return await this.api.endpoints.getAuthenticated.fetch();
+		this.backendApi = injectBackendEndpoints(backendApi);
 	}
 
 	async initDeviceOauth() {
@@ -41,6 +48,14 @@ export class GitHubUserService {
 
 	async checkAuthStatus(params: { deviceCode: string }) {
 		return await this.backend.invoke<AuthStatusResponse>('check_auth_status', params);
+	}
+
+	get forgetGitHubUsername() {
+		return this.backendApi.endpoints.forgetGitHubUsername.useMutation();
+	}
+
+	authenticatedUser(username: string) {
+		return this.backendApi.endpoints.getAccessToken.useQuery(username);
 	}
 }
 
@@ -55,6 +70,33 @@ function injectEndpoints(api: GitHubApi) {
 						extra: api.extra
 					}),
 				providesTags: [providesList(ReduxTag.ForgeUser)]
+			})
+		})
+	});
+}
+
+function injectBackendEndpoints(api: BackendApi) {
+	return api.injectEndpoints({
+		endpoints: (build) => ({
+			forgetGitHubUsername: build.mutation<void, string>({
+				extraOptions: {
+					command: 'forget_github_username',
+					actionName: 'Forget GitHub Username'
+				},
+				query: (username) => ({
+					username
+				})
+			}),
+			getAccessToken: build.query<AuthenticatedUser | null, string>({
+				extraOptions: {
+					command: 'get_gh_user'
+				},
+				query: (username) => ({
+					username
+				}),
+				providesTags: (_result, _error, username) => [
+					...providesItem(ReduxTag.ForgeUser, `github:${username}`)
+				]
 			})
 		})
 	});
