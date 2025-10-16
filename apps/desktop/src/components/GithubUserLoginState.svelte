eea8810dbe873f443beca8743375edee9cc76d70
estib <stron@me.com>
2025-10-16 17:57:00 +0200

    Display the state in which the user is not correctly authenticated

--- apps/desktop/src/components/GithubUserLoginState.svelte
+++ apps/desktop/src/components/GithubUserLoginState.svelte
@@ -3,7 +3,7 @@
 	import githubLogoSvg from '$lib/assets/unsized-logos/github.svg?raw';
 	import { GITHUB_USER_SERVICE } from '$lib/forge/github/githubUserService.svelte';
 	import { inject } from '@gitbutler/core/context';
-	import { Avatar, Button, SectionCard } from '@gitbutler/ui';
+	import { Avatar, Button, Icon, SectionCard } from '@gitbutler/ui';
 
 	type Props = {
 		username: string;
@@ -19,6 +19,35 @@
 </script>
 
 <ReduxResult result={ghUser.result}>
+	{#snippet error()}
+		<SectionCard orientation="row">
+			{#snippet iconSide()}
+				<div class="icon-wrapper">
+					<div class="icon-wrapper__tick">
+						<Icon name="error" color="error" size={18} />
+					</div>
+					<div class="icon-wrapper__logo">
+						{@html githubLogoSvg}
+					</div>
+				</div>
+			{/snippet}
+			{#snippet title()}
+				<p>{username}</p>
+			{/snippet}
+			{#snippet caption()}
+				<p>Error loading GitHub user</p>
+			{/snippet}
+
+			<Button
+				kind="outline"
+				icon="bin-small"
+				{disabled}
+				onclick={() => forget(username)}
+				loading={forgetting.current.isLoading}>Forget</Button
+			>
+		</SectionCard>
+	{/snippet}
+
 	{#snippet loading()}
 		<SectionCard orientation="row">
 			{#snippet iconSide()}
@@ -35,16 +64,28 @@
 	{#snippet children(user)}
 		<SectionCard orientation="row">
 			{#snippet iconSide()}
-				{#if user?.avatarUrl}
-					<Avatar size="large" tooltip={username} srcUrl={user?.avatarUrl} />
-				{:else}
-					<div class="icon-wrapper__logo">
-						{@html githubLogoSvg}
-					</div>
-				{/if}
+				<div class="icon-wrapper">
+					{#if !user}
+						<div class="icon-wrapper__tick">
+							<Icon name="error" color="error" size={18} />
+						</div>
+					{/if}
+					{#if user?.avatarUrl}
+						<Avatar size="large" tooltip={username} srcUrl={user?.avatarUrl} />
+					{:else}
+						<div class="icon-wrapper__logo">
+							{@html githubLogoSvg}
+						</div>
+					{/if}
+				</div>
 			{/snippet}
 			{#snippet title()}
 				<p>{username}</p>
+			{/snippet}
+			{#snippet caption()}
+				{#if !user}
+					<p>GitHub user not found</p>
+				{/if}
 			{/snippet}
 
 			<Button
@@ -59,6 +100,23 @@
 </ReduxResult>
 
 <style>
+	.icon-wrapper {
+		position: relative;
+		align-self: flex-start;
+		height: fit-content;
+	}
+
+	.icon-wrapper__tick {
+		display: flex;
+		position: absolute;
+		right: -4px;
+		bottom: -4px;
+		align-items: center;
+		justify-content: center;
+		border-radius: 50px;
+		background-color: var(--clr-scale-ntrl-100);
+	}
+
 	.icon-wrapper__logo {
 		width: 28px;
 		height: 28px;
