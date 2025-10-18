abcdad2547541732692652a3a879ef2f23cc009b
Pavel Laptev <pawellaptew@gmail.com>
2025-10-18 18:38:42 +0200

    refactor(ui/link): simplify Link component, adjust stories and text references

--- packages/ui/src/lib/components/Link.svelte
+++ packages/ui/src/lib/components/Link.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import Icon from '$components/Icon.svelte';
-	import { EXTERNAL_LINK_SERVICE } from '$lib/utils/externalLinkService';
-	import { inject } from '@gitbutler/core/context';
 	import { onMount, type Snippet } from 'svelte';
 
 	interface Props {
@@ -10,29 +7,11 @@
 		class?: string;
 		target?: '_blank' | '_self' | '_parent' | '_top' | undefined;
 		rel?: string | undefined;
-		role?: 'basic' | 'primary' | 'error';
-		underline?: boolean;
-		externalIcon?: boolean;
-		disabled?: boolean;
-		bypassExternalLinkService?: boolean;
 	}
 
-	const {
-		href,
-		target = undefined,
-		class: classes,
-		rel = undefined,
-		role = 'basic',
-		underline = true,
-		externalIcon = true,
-		disabled = false,
-		bypassExternalLinkService,
-		children
-	}: Props = $props();
+	const { href, target = undefined, class: classes, rel = undefined, children }: Props = $props();
 
-	let element = $state<HTMLAnchorElement | HTMLButtonElement>();
-
-	const externalLinkService = inject(EXTERNAL_LINK_SERVICE);
+	let element = $state<HTMLAnchorElement>();
 
 	onMount(() => {
 		if (element) {
@@ -43,53 +22,35 @@
 	const isExternal = $derived(href?.startsWith('http'));
 </script>
 
-<a
-	{href}
-	{target}
-	{rel}
-	class="link {role} {classes}"
-	bind:this={element}
-	class:disabled
-	class:underline
-	onclick={(e) => {
-		if (href && isExternal && !bypassExternalLinkService) {
-			e.preventDefault();
-			e.stopPropagation();
-			externalLinkService.open(href);
-		}
-	}}
->
-	{@render children()}
-	{#if isExternal && externalIcon}
-		<div class="link-icon">
-			<Icon name="open-link" />
-		</div>
+<a {href} {target} {rel} class="link {classes}" bind:this={element}>
+	<span class="underline">
+		{@render children()}
+	</span>
+
+	{#if isExternal}
+		<span class="link-icon">↗</span>
 	{/if}
 </a>
 
 <style lang="postcss">
 	.link {
-		display: inline-flex;
+		display: inline;
 		align-items: center;
-		gap: 2px;
-		border-radius: var(--radius-m);
 		text-decoration: none;
 		cursor: pointer;
 		transition: background-color var(--transition-fast);
-		user-select: text;
 
-		&:hover {
-			text-decoration: none;
+		&:hover .link-icon {
+			opacity: 1;
 		}
 	}
 
-	.link.underline {
-		text-decoration: underline;
+	.underline:hover {
+		text-decoration: none;
 	}
 
 	.link-icon {
-		display: flex;
-		flex-shrink: 0;
 		opacity: 0.7;
+		transition: opacity var(--transition-fast);
 	}
 </style>
