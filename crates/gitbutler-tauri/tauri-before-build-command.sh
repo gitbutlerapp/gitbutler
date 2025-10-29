bb1e23408367983780331a01a2fc17dd60a8827e
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-10-29 06:30:43 +0100

    Prevent windows from building the frontend again despite having downloaded it
    This gives another 2minutes speedup by avoiding duplicate work.

---
+++ crates/gitbutler-tauri/tauri-before-build-command.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$CI" = "true" ]; then
+  echo "CI environment detected, expecting frontend build to be downloaded."
+else
+  fe_mode=${1:?First argument must be the mode to build the frontend with}
+  echo "Assuming local invocation, building frontend in $fe_mode mode"
+  pnpm build:desktop -- --mode "$fe_mode"
+fi
+
+cargo build --release -p gitbutler-git
+bash ./crates/gitbutler-tauri/inject-git-binaries.sh
