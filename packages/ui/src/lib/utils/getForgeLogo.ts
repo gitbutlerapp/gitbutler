e472b55e653d7e753e66917b762d1bd8a7103855
Pavel Laptev <pawellaptew@gmail.com>
2025-10-31 00:32:44 +0100

    update gh logo

--- packages/ui/src/lib/utils/getForgeLogo.ts
+++ packages/ui/src/lib/utils/getForgeLogo.ts
@@ -8,10 +8,10 @@ export function getForgeLogo(forgeName: string, small = false): keyof typeof ico
 		return 'gitlab';
 	} else if (forgeName === 'github') {
 		if (small) {
-			return 'github-small';
+			return 'github-outline-small';
 		}
 
-		return 'github';
+		return 'github-outline';
 	}
 
 	return 'question-mark-small';
