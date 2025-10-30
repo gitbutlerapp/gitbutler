78329732b0c435e56b90a2dbff76313fd5821226
Pavel Laptev <pawellaptew@gmail.com>
2025-10-30 16:39:27 -0700

    chore(html): remove redundant text-base class from body element to avoid (#10915)
    * chore(html): remove redundant text-base class from body element to avoid
    
    * update gh logo

--- .conflict-side-1/apps/desktop/src/lib/irc/logs.ts
+++
@@ -1,8 +0,0 @@
-import { createEntityAdapter } from '@reduxjs/toolkit';
-import type { IrcLog } from '$lib/irc/types';
-
-export const logsAdapter = createEntityAdapter<IrcLog, string>({
-	selectId: (model) => model.msgid || String(model.timestamp)
-});
-
-export const logSelectors = logsAdapter.getSelectors();
