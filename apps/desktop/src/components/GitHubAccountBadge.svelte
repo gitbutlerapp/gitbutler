3c8632133c0871673944e190ded7fe60245a86bf
estib <stron@me.com>
2025-10-31 13:44:34 +0100

    GitHub integration: Improve the badges
    Add badges to the different account types available to integrate

--- apps/desktop/src/components/GitHubAccountBadge.svelte
+++ apps/desktop/src/components/GitHubAccountBadge.svelte
@@ -9,10 +9,33 @@
 
 	const { account, class: className }: Props = $props();
 
-	const tooltipText = $derived(account.type === 'enterprise' ? 'GitHub Enterprise' : account.type);
-	const badgeText = $derived(account.type === 'enterprise' ? account.info.host : account.type);
+	// const tooltipText = $derived(account.type === 'enterprise' ? 'GitHub Enterprise' : account.type);
+	// const badgeText = $derived(account.type === 'enterprise' ? account.info.host : account.type);
+	export function badgeText(account: GitHubAccountIdentifier): string | null {
+		switch (account.type) {
+			case 'oAuthUsername':
+				return null;
+			case 'enterprise':
+				return account.info.host;
+			case 'patUsername':
+				return 'PAT';
+		}
+	}
+
+	export function tooltipText(account: GitHubAccountIdentifier): string {
+		switch (account.type) {
+			case 'oAuthUsername':
+				return '';
+			case 'enterprise':
+				return 'GitHub Enterprise';
+			case 'patUsername':
+				return 'Personal Access Token';
+		}
+	}
 </script>
 
-<Badge class={className} tooltip={tooltipText}>
-	{badgeText}
-</Badge>
+{#if account.type !== 'oAuthUsername'}
+	<Badge class={className} tooltip={tooltipText(account)}>
+		{badgeText(account)}
+	</Badge>
+{/if}
