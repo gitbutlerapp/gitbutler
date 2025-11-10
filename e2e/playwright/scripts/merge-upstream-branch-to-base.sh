0cad73a1cee2e2b2953906d3bb016b5302f6ad06
estib <stron@me.com>
2025-11-10 14:03:59 +0100

    e2e: Merging a branch upstream deletes the remote branch
    Fix the shell script for merging a branch in the ‘forge'

--- e2e/playwright/scripts/merge-upstream-branch-to-base.sh
+++ e2e/playwright/scripts/merge-upstream-branch-to-base.sh
@@ -3,9 +3,11 @@
 echo "GIT CONFIG $GIT_CONFIG_GLOBAL"
 echo "DATA DIR $GITBUTLER_CLI_DATA_DIR"
 echo "BUT_TESTING $BUT_TESTING"
-echo "BRANCH TO APPLY: $1"
-# Apply remote branch to the workspace.
+echo "BRANCH TO MERGE: $1"
+
+# Merge the upstream branch into master and delete the upstream branch
 pushd remote-project
   git checkout master
   git merge --no-ff -m "Merging upstream branch $1 into base" "$1"
+  git branch -d "$1"
 popd
