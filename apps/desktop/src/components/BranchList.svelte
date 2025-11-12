8440768a4383075c40d4fb37012ec8a21b7d8934
Mattias Granlund <mtsgrd@gmail.com>
2025-11-12 21:46:09 +0100

    Auto-focus codegen input
    This is a hacky way, but we need the job done until we can figure out a 
    good way of autofocusing text inputs, without too many of them firing
    at the wrong times.

--- apps/desktop/src/components/BranchList.svelte
+++ apps/desktop/src/components/BranchList.svelte
@@ -330,6 +330,17 @@
 								onclick={async () => {
 									if (!stackId) return;
 									laneState?.selection.set({ branchName, codegen: true, previewOpen: true });
+									setTimeout(() => {
+										// This is a hacky way, but we need the job done until we
+										// can figure out a good way of autofocusing text inputs,
+										// without too many of them firing at the wrong times.
+										const element = document.querySelector(
+											`[data-id="${stackId}"] .ContentEditable__root`
+										);
+										if (element instanceof HTMLElement) {
+											element?.focus();
+										}
+									}, 100);
 								}}
 							/>
 						{/if}
