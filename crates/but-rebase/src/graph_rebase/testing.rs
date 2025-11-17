a783baf438fc8c7db33376674d254dd7e5d930ef
Caleb Owens <hello@calebowens.com>
2025-11-17 15:24:01 +0100

    Creating steps

---
+++ crates/but-rebase/src/graph_rebase/testing.rs
@@ -0,0 +1,29 @@
+#![deny(missing_docs)]
+//! Testing utilities
+
+use petgraph::dot::{Config, Dot};
+
+use crate::graph_rebase::{Editor, Step};
+
+impl Editor {
+    /// Creates a dot graph with labels
+    pub fn steps_dot(&self) -> String {
+        format!(
+            "{:?}",
+            Dot::with_attr_getters(
+                &self.graph,
+                &[Config::EdgeNoLabel, Config::NodeNoLabel],
+                &|_, v| format!("label=\"order: {}\"", v.weight().order),
+                &|_, (_, step)| {
+                    match step {
+                        Step::Pick { id } => format!("label=\"pick: {}\"", id),
+                        Step::Reference { refname } => {
+                            format!("label=\"reference: {}\"", refname.as_bstr())
+                        }
+                        Step::None => "label=\"none\"".into(),
+                    }
+                },
+            )
+        )
+    }
+}
