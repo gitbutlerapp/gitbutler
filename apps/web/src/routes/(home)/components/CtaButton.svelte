366e6f89e2718baf92e67fe1d25c9e82c3e969b2
Pavel Laptev <pawellaptew@gmail.com>
2025-11-24 22:46:13 +0100

    remove unused class

--- apps/web/src/routes/(home)/components/CtaButton.svelte
+++ apps/web/src/routes/(home)/components/CtaButton.svelte
@@ -196,14 +196,6 @@
 		pointer-events: none;
 	}
 
-	.canvas-container {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
-
 	.download-btn__canvas-cover {
 		z-index: 1;
 		position: absolute;
