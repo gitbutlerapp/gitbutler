6547c66cdbb37353108e597e970df85c1999a5c7
Pavel Laptev <pawellaptew@gmail.com>
2025-12-01 05:06:35 -0800

    some-marketin-page-ui-updates (#11418)
    * refactor: remove redundant /nightlies redirect page
    
    * feat(icon): refine nightly app icon to use stroked glyph
    
    * remove SVG filters because Safari implementation sucks

--- apps/web/src/routes/downloads/+page.svelte
+++ apps/web/src/routes/downloads/+page.svelte
@@ -162,8 +162,8 @@
 			</p>
 		</div>
 
-		<div class="latest-release-background__noisy noisy-1"></div>
-		<div class="latest-release-background__noisy noisy-2"></div>
+		<div class="grainy-bg grainy-1"></div>
+		<div class="grainy-bg grainy-2"></div>
 	</div>
 </section>
 
@@ -347,29 +347,35 @@
 		}
 	}
 
-	.latest-release-background__noisy {
+	/* GRAINY BG */
+	.grainy-bg {
 		position: absolute;
-		width: 100%;
-		height: 1240px;
-		transform: rotate(-65deg);
-		border-radius: 50%;
-		background:
-			radial-gradient(ellipse at 50% 50%, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)),
-			url("data:image/svg+xml,%3Csvg viewBox='0 0 600 600' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+		width: 1160px;
+		height: 950px;
+		transform: rotate(30deg);
+		background: url('/images/grainy-gr.png');
+		background-size: 100%;
+		background-repeat: no-repeat;
 		mix-blend-mode: multiply;
-		filter: contrast(145%) brightness(1100%);
+
 		opacity: 0.06;
 		pointer-events: none;
+	}
 
-		&.noisy-1 {
-			top: -30%;
-			right: -40%;
-		}
+	:global(.dark) .grainy-bg {
+		mix-blend-mode: screen;
+		filter: invert(100%);
+		opacity: 0.08;
+	}
 
-		&.noisy-2 {
-			bottom: -34%;
-			left: -35%;
-		}
+	.grainy-1 {
+		top: -30%;
+		right: -50%;
+	}
+
+	.grainy-2 {
+		bottom: -70%;
+		left: -60%;
 	}
 
 	.releases {
