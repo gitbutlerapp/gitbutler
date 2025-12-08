2f6c21a097b8118b00f370fe2b052f005b8d1c8e
Kiril Videlov <kiril@videlov.com>
2025-12-08 23:36:28 +0100

    feat(rules): Filter workspace rules by active stacks

--- crates/but-api/src/legacy/rules.rs
+++ crates/but-api/src/legacy/rules.rs
@@ -1,12 +1,16 @@
 //! In place of commands.rs
+use std::str::FromStr;
+
 use anyhow::Result;
 use but_api_macros::but_api;
 use but_ctx::Context;
+use but_meta::VirtualBranchesTomlMetadata;
 use but_rules::{
     CreateRuleRequest, UpdateRuleRequest, WorkspaceRule, create_rule, delete_rule, list_rules,
     update_rule,
 };
 use gitbutler_project::ProjectId;
+use gitbutler_stack::StackId;
 use tracing::instrument;
 
 #[but_api]
@@ -40,5 +44,44 @@ pub fn update_workspace_rule(
 #[instrument(err(Debug))]
 pub fn list_workspace_rules(project_id: ProjectId) -> Result<Vec<WorkspaceRule>> {
     let ctx = &mut Context::new_from_legacy_project_id(project_id)?;
-    list_rules(ctx)
+    let repo = ctx.open_repo_for_merging_non_persisting()?;
+
+    let in_workspace = if ctx.settings().feature_flags.ws3 {
+        let meta = VirtualBranchesTomlMetadata::from_path(
+            ctx.legacy_project.gb_dir().join("virtual_branches.toml"),
+        )?;
+        but_workspace::legacy::stacks_v3(
+            &repo,
+            &meta,
+            but_workspace::legacy::StacksFilter::InWorkspace,
+            None,
+        )
+    } else {
+        but_workspace::legacy::stacks(
+            ctx,
+            &ctx.project_data_dir(),
+            &repo,
+            but_workspace::legacy::StacksFilter::InWorkspace,
+        )
+    }?
+    .iter()
+    .filter_map(|s| s.id)
+    .collect::<Vec<StackId>>();
+
+    // Filter out specifically Codegen related rules that are refering to a stack that is not in the workspace.
+    let rules = list_rules(ctx)?
+        .into_iter()
+        .filter(|rule| {
+            if let (Some(_), Some(stack_id)) = (
+                rule.session_id(),
+                rule.target_stack_id()
+                    .and_then(|id| StackId::from_str(&id).ok()),
+            ) {
+                return in_workspace.contains(&stack_id);
+            }
+            true
+        })
+        .collect();
+
+    Ok(rules)
 }
