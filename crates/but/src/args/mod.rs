3088a0157521813dfe94475329492a11853d3a66
Kiril Videlov <kiril@videlov.com>
2025-12-26 19:41:36 +0200

    Initial stub of but diff

--- crates/but/src/args/mod.rs
+++ crates/but/src/args/mod.rs
@@ -159,6 +159,20 @@ pub enum Subcommands {
         target: String,
     },
 
+    /// Displays the diff of changes in the repo.
+    ///
+    /// Without any arguments, it shows the diff of all uncommitted changes.
+    /// Optionally, a CLI ID argument can be provided, which chan show the diff specific to
+    /// - an uncomitted file
+    /// - a branch
+    /// - an entiere stack
+    /// - a commit
+    /// - a file change within a commit
+    Diff {
+        /// The CLI ID of the entity to show the diff for
+        target: Option<String>,
+    },
+
     /// Initializes a GitButler project from a git repository in the current directory.
     ///
     /// If you have an existing Git repository and want to start using GitButler
---
+++ crates/but/src/command/diff/mod.rs
@@ -0,0 +1,31 @@
+use but_ctx::Context;
+
+use crate::{CliId, IdMap, command::diff::show::Filter, utils::OutputChannel};
+mod show;
+
+pub fn handle(
+    ctx: &mut Context,
+    out: &mut OutputChannel,
+    target_str: Option<&str>,
+) -> anyhow::Result<()> {
+    if let Some(entity) = target_str {
+        let id_map = IdMap::new_from_context(ctx)?;
+        let id = id_map
+            .resolve_entity_to_ids(entity)? // TODO: look up plain names
+            .first() // TODO: handle ambiguity
+            .cloned()
+            .ok_or_else(|| anyhow::anyhow!("No ID found for entity"))?;
+
+        match id {
+            CliId::Uncommitted(id) => show::worktree(ctx, out, Some(Filter::Uncommitted(id))),
+            CliId::Unassigned { .. } => show::worktree(ctx, out, Some(Filter::Unassigned)),
+            CliId::CommittedFile {
+                commit_id, path, ..
+            } => show::commit(ctx, out, commit_id, Some(path)),
+            CliId::Branch { name, .. } => show::branch(ctx, out, name),
+            CliId::Commit(id) => show::commit(ctx, out, id, None),
+        }
+    } else {
+        show::worktree(ctx, out, None)
+    }
+}
