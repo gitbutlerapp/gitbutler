7d8d465ebf9d610f6c635f239f9a26feadfde4b3
Pavel Laptev <pawellaptew@gmail.com>
2026-01-12 15:25:03 +0100

    refactor(ui/file-icons): use more specific namespace CSS vars

--- packages/ui/src/lib/components/file/FileIcon.svelte
+++ packages/ui/src/lib/components/file/FileIcon.svelte
@@ -20,16 +20,16 @@
 		width: var(--file-icon-size);
 		height: var(--file-icon-size);
 
-		--gray: #807976;
-		--green: #16a34a;
-		--teal: #14b8a6;
-		--blue: #0ea5e9;
-		--dark-blue: #0e7ce9;
-		--yellow: #eab308;
-		--orange: #f5700b;
-		--red: #ef4444;
-		--pink: #f472b6;
-		--purple: #a855f7;
-		--violet: #5249f8;
+		--file-icon-gray: #807976;
+		--file-icon-green: #16a34a;
+		--file-icon-teal: #14b8a6;
+		--file-icon-blue: #0ea5e9;
+		--file-icon-dark-blue: #0e7ce9;
+		--file-icon-yellow: #eab308;
+		--file-icon-orange: #f5700b;
+		--file-icon-red: #ef4444;
+		--file-icon-pink: #f472b6;
+		--file-icon-purple: #a855f7;
+		--file-icon-violet: #5249f8;
 	}
 </style>
