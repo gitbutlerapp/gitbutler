6dc215fec19ad7865e196791ed667823f5809e62
godnight10061 <177652969+godnight10061@users.noreply.github.com>
2026-01-24 14:46:46 +0800

    CI: install gtk3 dev deps

--- scripts/install-minimal-debian-dependencies.sh
+++ scripts/install-minimal-debian-dependencies.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 
 # Install the dependencies needed to build the but-server and the CLI.
 apt update
-apt install -y libdbus-1-dev libglib2.0-dev pkg-config
+apt install -y libdbus-1-dev libglib2.0-dev libgtk-3-dev pkg-config
