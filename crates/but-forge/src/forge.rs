466d99fb3a46436a8f7174a3e0f27cdb66ad4e2d
estib <stron@me.com>
2026-02-06 13:30:10 +0100

    rs ts: Generate types for forge user and use them
    Generate the TS types for the ForgeUser and use them in the Project type

--- crates/but-forge/src/forge.rs
+++ crates/but-forge/src/forge.rs
@@ -26,7 +26,9 @@ impl PartialEq for ForgeRepoInfo {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "export-ts", derive(ts_rs::TS))]
 #[serde(tag = "provider", rename_all = "lowercase", content = "details")]
+#[cfg_attr(feature = "export-ts", ts(export, export_to = "./forge/user.ts"))]
 pub enum ForgeUser {
     GitHub(but_github::GithubAccountIdentifier),
     GitLab(but_gitlab::GitlabAccountIdentifier),
