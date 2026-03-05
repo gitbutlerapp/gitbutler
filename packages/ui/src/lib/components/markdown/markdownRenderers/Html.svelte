5376db33fa7ed77943e90a59b9fd3e3f92f7c81f
Mattias Granlund <mtsgrd@gmail.com>
2026-03-05 13:12:04 +0100

    Sanitize HTML in Markdown renderer

--- packages/ui/src/lib/components/markdown/markdownRenderers/Html.svelte
+++ packages/ui/src/lib/components/markdown/markdownRenderers/Html.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import DOMPurify from "isomorphic-dompurify";
+
 	interface Props {
 		raw: string;
 	}
 
 	const { raw }: Props = $props();
+
+	const sanitized = $derived(DOMPurify.sanitize(raw));
 </script>
 
-{@html raw}
+{@html sanitized}
