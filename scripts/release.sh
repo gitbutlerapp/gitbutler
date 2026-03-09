8dcc9cd3761d9ec1311490939708bb93cd5a7a1f
Simon Larsén <slarse@slar.se>
2026-03-09 21:32:10 +0100

    fix(gitbutler-tauri,linux): disable "Install CLI" and `but update install`
    Builds gitbutler-tauri for Linux with the `packaged-but-distribution`
    flag propagated to the `but` crate s.t. the "Install But CLI" button
    in the GUI and the `but update install` command in the CLI are
    disabled. The packaged Linux distributions should _not_ "self-manage".
    
    This also disables the "Install But CLI" button for the AppImage, but
    that's a good thing as it's gibberish trying to symlink to the
    temporary filesystem the AppImage runtime mounts. Looks something like
    this:
    
    ```
    sudo ln -sf '/tmp/.mount_GitButnhBkLa/usr/bin/gitbutler-tauri' /usr/local/bin/but
    ```

--- scripts/release.sh
+++ scripts/release.sh
@@ -181,18 +181,19 @@ trap 'rm -rf "$TMP_DIR"' exit
 CONFIG_PATH=$(readlink -f "$PWD/../crates/gitbutler-tauri/tauri.conf.$CHANNEL.json")
 
 if [ "$OS" = "windows" ]; then
-  # WARNING: `builtin-but` doesn't work on Windows, see https://github.com/gitbutlerapp/gitbutler/issues/11461.
-  #          Should it be re-added, please ensure that `but` is built
-  #          as part of the 'beforeBuildCommand' in tauri.conf AND it must be injected
-  #          via 'inject-git-binaries.sh'.
-  EXTERNAL_BIN='["gitbutler-git-askpass", "gitbutler-git-setsid", "but"]'
+	# WARNING: `builtin-but` doesn't work on Windows, see https://github.com/gitbutlerapp/gitbutler/issues/11461.
+	#          Should it be re-added, please ensure that `but` is built
+	#          as part of the 'beforeBuildCommand' in tauri.conf AND it must be injected
+	#          via 'inject-git-binaries.sh'.
+	EXTERNAL_BIN='["gitbutler-git-askpass", "gitbutler-git-setsid", "but"]'
 	FEATURES="windows"
-else
-  # we need to include the gitbutler-git-setsid dummy binary as
-  # installers bundled with version <=0.19.3 will validate that it's there for macOS.
-  #
-  # As it doesn't hurt on Linux we'll just let it sit there.
-  EXTERNAL_BIN='["gitbutler-git-askpass", "gitbutler-git-setsid"]'
+elif [ "$OS" = "linux" ]; then
+	EXTERNAL_BIN='["gitbutler-git-askpass"]'
+	FEATURES="builtin-but packaged-but-distribution"
+elif [ "$OS" = "macos" ]; then
+	# we need to include the gitbutler-git-setsid dummy binary as
+	# installers bundled with version <=0.19.3 will validate that it's there for macOS.
+	EXTERNAL_BIN='["gitbutler-git-askpass", "gitbutler-git-setsid"]'
 	FEATURES="builtin-but"
 fi
 
