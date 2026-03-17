9f1772bf4b247d42668667c993df207cfae88c09
Oliver Joseph Ash <oliverjash@gmail.com>
2026-03-17 08:43:55 +0000

    Format

--- apps/lite/ui/src/main.tsx
+++ apps/lite/ui/src/main.tsx
@@ -1,6 +1,7 @@
 import { App } from "#ui/App.tsx";
 import { createRoot } from "react-dom/client";
 import "./global.css";
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
