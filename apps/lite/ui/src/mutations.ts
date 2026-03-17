2a9c4e118b733fd3176917a74975d2c7fce5e857
Oliver Joseph Ash <oliverjash@gmail.com>
2026-03-17 00:07:41 +0000

    Invalidate all queries after mutations
    For now. We may want to come back to this.

--- apps/lite/ui/src/mutations.ts
+++ apps/lite/ui/src/mutations.ts
@@ -1,105 +1,58 @@
 import { mutationOptions } from "@tanstack/react-query";
-import {
-	changesInWorktreeQueryOptions,
-	headInfoQueryOptions,
-	listBranchesQueryOptions,
-} from "#ui/queries.ts";
 import { rub } from "#ui/rub.ts";
 
 export const applyBranchMutationOptions = mutationOptions({
 	mutationFn: window.lite.apply,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const commitInsertBlankMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitInsertBlank,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries({ queryKey: ["branchDetails"] }),
-			client.invalidateQueries({ queryKey: ["branchDiff"] }),
-			client.invalidateQueries({ queryKey: ["commitDetailsWithLineStats"] }),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const commitMoveMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitMove,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries({ queryKey: ["branchDetails"] }),
-			client.invalidateQueries({ queryKey: ["branchDiff"] }),
-			client.invalidateQueries({ queryKey: ["commitDetailsWithLineStats"] }),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const commitMoveToBranchMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitMoveToBranch,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries({ queryKey: ["branchDetails"] }),
-			client.invalidateQueries({ queryKey: ["branchDiff"] }),
-			client.invalidateQueries({ queryKey: ["commitDetailsWithLineStats"] }),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const commitMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitCreate,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const commitRewordMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitReword,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries({ queryKey: ["branchDetails"] }),
-			client.invalidateQueries({ queryKey: ["branchDiff"] }),
-			client.invalidateQueries({ queryKey: ["commitDetailsWithLineStats"] }),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const rubMutationOptions = mutationOptions({
 	mutationFn: rub,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
 
 export const unapplyStackMutationOptions = mutationOptions({
 	mutationFn: window.lite.unapplyStack,
-	onSuccess: async (_data, { projectId }, _ctx, { client }) => {
-		await Promise.all([
-			client.invalidateQueries(listBranchesQueryOptions(projectId)),
-			client.invalidateQueries(headInfoQueryOptions(projectId)),
-			client.invalidateQueries(changesInWorktreeQueryOptions(projectId)),
-		]);
+	onSuccess: async (_data, _input, _ctx, { client }) => {
+		await client.invalidateQueries();
 	},
 });
