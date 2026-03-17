c61bec6ed953eb37e3aa1b03f6820304d6f8f5c3
Oliver Joseph Ash <oliverjash@gmail.com>
2026-03-17 17:13:10 +0000

    Use library types

--- apps/lite/ui/src/hooks/useDraggable.tsx
+++ apps/lite/ui/src/hooks/useDraggable.tsx
@@ -1,7 +1,4 @@
-import {
-	draggable,
-	type ElementGetFeedbackArgs,
-} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { centerUnderPointer } from "@atlaskit/pragmatic-drag-and-drop/element/center-under-pointer";
 import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
 import {
@@ -14,19 +11,21 @@ import {
 } from "react";
 import { createRoot } from "react-dom/client";
 
+type LibParams = Parameters<typeof draggable>[0];
+
 export const useDraggable = ({
 	getInitialData: getInitialDataProp,
 	canDrag: canDragProp,
 	preview,
-}: {
-	getInitialData: () => Record<string, unknown>;
-	canDrag?: (args: ElementGetFeedbackArgs) => boolean;
+}: Pick<LibParams, "canDrag" | "getInitialData"> & {
 	preview: ReactNode;
 }): [boolean, RefCallback<HTMLElement>] => {
 	const ref = useRef<HTMLElement>(null);
 	const [isDragging, setIsDragging] = useState(false);
-	const getInitialData = useEffectEvent(() => getInitialDataProp());
-	const canDrag = useEffectEvent((args: ElementGetFeedbackArgs) => canDragProp?.(args) ?? true);
+	const getInitialData: LibParams["getInitialData"] = useEffectEvent(
+		(args) => getInitialDataProp?.(args) ?? {},
+	);
+	const canDrag: LibParams["canDrag"] = useEffectEvent((args) => canDragProp?.(args) ?? true);
 	const onGenerateDragPreview = useEffectEvent(
 		({ nativeSetDragImage }: { nativeSetDragImage: DataTransfer["setDragImage"] | null }) => {
 			setCustomNativeDragPreview({
