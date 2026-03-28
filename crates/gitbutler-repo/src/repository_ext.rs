1cb2ebee5d4c3e7a313754b58f628ba2e354e46d
Sebastian Thiel <sebastian.thiel@icloud.com>
2026-03-28 19:31:19 +0800

    repo: drop dead git2 repo extension methods

--- crates/gitbutler-repo/src/repository_ext.rs
+++ crates/gitbutler-repo/src/repository_ext.rs
@@ -1,19 +1,18 @@
 use std::str;
 
-use anyhow::{Context as _, Result, anyhow, bail};
-use bstr::{BStr, BString};
+use anyhow::{Context as _, Result, bail};
+use bstr::BStr;
 use but_core::{
     RepositoryExt as RepositoryExtGix,
     commit::{Headers, SignCommit},
 };
 use but_oxidize::{ObjectIdExt as _, OidExt, git2_signature_to_gix_signature};
-use gitbutler_reference::{Refname, RemoteRefname};
+use gitbutler_reference::Refname;
 
 /// Extension trait for `git2::Repository`.
 ///
 /// For now, it collects useful methods from `gitbutler-core::git::Repository`
 pub trait RepositoryExt {
-    fn find_branch_by_refname(&self, name: &Refname) -> Result<git2::Branch<'_>>;
     /// Returns the common ancestor of the given commit Oids.
     ///
     /// This is like `git merge-base --octopus`.
@@ -22,20 +21,8 @@ pub trait RepositoryExt {
     /// conflict with the libgit2 binding I upstreamed when it eventually
     /// gets merged.
     fn merge_base_octopussy(&self, ids: &[git2::Oid]) -> Result<git2::Oid>;
-
-    fn remote_branches(&self) -> Result<Vec<RemoteRefname>>;
-    fn remotes_as_string(&self) -> Result<Vec<String>>;
-    /// `buffer` is the commit object to sign, but in theory could be anything to compute the signature for.
-    /// Returns the computed signature.
-    fn sign_buffer(&self, buffer: &[u8]) -> Result<BString>;
     fn checkout_tree_builder<'a>(&'a self, tree: &'a git2::Tree<'a>) -> CheckoutTreeBuidler<'a>;
     fn maybe_find_branch_by_refname(&self, name: &Refname) -> Result<Option<git2::Branch<'_>>>;
-    /// Returns the `gitbutler/workspace` branch if the head currently points to it, or fail otherwise.
-    /// Use it before any modification to the repository, or extra defensively each time the
-    /// workspace is needed.
-    ///
-    /// This is for safety to assure the repository actually is in 'gitbutler mode'.
-    fn workspace_ref_from_head(&self) -> Result<git2::Reference<'_>>;
 
     #[expect(clippy::too_many_arguments)]
     fn commit_with_signature(
@@ -164,31 +151,6 @@ impl RepositoryExt for git2::Repository {
         }
     }
 
-    fn find_branch_by_refname(&self, name: &Refname) -> Result<git2::Branch<'_>> {
-        let branch = self.find_branch(
-            &name.simple_name(),
-            match name {
-                Refname::Virtual(_) | Refname::Local(_) | Refname::Other(_) => {
-                    git2::BranchType::Local
-                }
-                Refname::Remote(_) => git2::BranchType::Remote,
-            },
-        )?;
-
-        Ok(branch)
-    }
-
-    fn workspace_ref_from_head(&self) -> Result<git2::Reference<'_>> {
-        let head_ref = self.head().context("BUG: head must point to a reference")?;
-        if head_ref.name_bytes() == b"refs/heads/gitbutler/workspace" {
-            Ok(head_ref)
-        } else {
-            Err(anyhow!(
-                "Unexpected state: cannot perform operation on non-workspace branch"
-            ))
-        }
-    }
-
     fn commit_with_signature(
         &self,
         update_ref: Option<&Refname>,
@@ -214,39 +176,6 @@ impl RepositoryExt for git2::Repository {
             commit_headers,
         )
         .map(|oid| oid.to_git2())
-    }
-
-    fn sign_buffer(&self, buffer: &[u8]) -> Result<BString> {
-        but_core::commit::sign_buffer(&gix::open(self.path())?, buffer)
-    }
-
-    fn remotes_as_string(&self) -> Result<Vec<String>> {
-        Ok(gix::open(self.path())?
-            .remote_names()
-            .iter()
-            .map(|name| name.to_string())
-            .collect())
-    }
-
-    fn remote_branches(&self) -> Result<Vec<RemoteRefname>> {
-        use bstr::ByteSlice;
-
-        let repo = gix::open_opts(self.path(), gix::open::Options::isolated())?;
-        repo.references()?
-            .remote_branches()?
-            .filter_map(Result::ok)
-            // TODO: the question is if we really need this? Probably not, but it's part
-            // of the `gix` migration and we'd rather play it safe. Goal is for `gitbutler-` crates
-            // to not exist anyway.
-            .filter(|reference| !reference.name().as_bstr().ends_with_str("/HEAD"))
-            .map(|reference| {
-                reference
-                    .name()
-                    .to_string()
-                    .parse()
-                    .context("failed to convert branch to remote name")
-            })
-            .collect()
     }
 
     fn merge_base_octopussy(&self, ids: &[git2::Oid]) -> Result<git2::Oid> {
