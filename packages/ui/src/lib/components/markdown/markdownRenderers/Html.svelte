4f1bbc2f27a3cf92b1f0b3b935049f817c30eb4e
Simon Larsén <slarse@slar.se>
2026-03-30 09:21:29 +0200

    set purify config

--- packages/ui/src/lib/components/markdown/markdownRenderers/Html.svelte
+++ packages/ui/src/lib/components/markdown/markdownRenderers/Html.svelte
@@ -7,7 +7,52 @@
 
 	const { raw }: Props = $props();
 
-	const sanitized = $derived(DOMPurify.sanitize(raw));
+	const sanitized = $derived(
+		DOMPurify.sanitize(raw, {
+			ALLOWED_TAGS: [
+				"b",
+				"strong",
+				"i",
+				"em",
+				"s",
+				"del",
+				"u",
+				"mark",
+				"sub",
+				"sup",
+				"small",
+				"code",
+				"pre",
+				"h1",
+				"h2",
+				"h3",
+				"h4",
+				"h5",
+				"h6",
+				"p",
+				"br",
+				"hr",
+				"blockquote",
+				"ul",
+				"ol",
+				"li",
+				"table",
+				"thead",
+				"tbody",
+				"tr",
+				"th",
+				"td",
+				"details",
+				"summary",
+			],
+			ALLOWED_ATTR: [],
+			ALLOWED_NAMESPACES: [],
+			ALLOW_ARIA_ATTR: false,
+			ALLOW_SELF_CLOSE_IN_ATTR: false,
+			ALLOW_DATA_ATTR: false,
+			ALLOW_UNKNOWN_PROTOCOLS: false,
+		}),
+	);
 </script>
 
 {@html sanitized}
