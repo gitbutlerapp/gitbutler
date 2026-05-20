2971d58a377337c81d0afd4c5ac59c80540c6ba0
Pavel Laptev <pawellaptew@gmail.com>
2026-05-20 22:41:45 +0200

    remove docs addon
    It doesn’t work properly with theme switching and is somewhat redundant, as it merges all stories into one—which isn't always desirable.

--- apps/lite/.storybook/main.ts
+++ apps/lite/.storybook/main.ts
@@ -3,7 +3,7 @@ import { defineMain } from "@storybook/react-vite/node";
 export default defineMain({
 	stories: ["../ui/src/**/*.stories.tsx"],
 	framework: "@storybook/react-vite",
-	addons: ["@storybook/addon-designs", "@storybook/addon-docs"],
+	addons: ["@storybook/addon-designs"],
 	typescript: {
 		// Better props inference.
 		reactDocgen: "react-docgen-typescript",
