71605d9b8f4807bb5d6dec95c92900b015d422d3
Pavel Laptev <pawellaptew@gmail.com>
2026-05-22 14:23:29 +0200

    add button component

--- apps/lite/.storybook/main.ts
+++ apps/lite/.storybook/main.ts
@@ -3,7 +3,7 @@ import { defineMain } from "@storybook/react-vite/node";
 export default defineMain({
 	stories: ["../ui/src/**/*.stories.tsx"],
 	framework: "@storybook/react-vite",
-	addons: ["@storybook/addon-designs"],
+	addons: ["@storybook/addon-designs", "@storybook/addon-docs"],
 	typescript: {
 		// Better props inference.
 		reactDocgen: "react-docgen-typescript",
