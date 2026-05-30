086fd3b44b4c79a6d90cd9032ab259ed2e4c5bb9
Oliver Joseph Ash <oliverjash@gmail.com>
2026-05-30 16:51:51 +0100

    Consistent functions

--- apps/lite/ui/src/components/ToggleGroup.tsx
+++ apps/lite/ui/src/components/ToggleGroup.tsx
@@ -1,13 +1,11 @@
 import { classes } from "#ui/components/classes.ts";
 import styles from "./ToggleGroup.module.css";
-import { ComponentProps } from "react";
+import { ComponentProps, FC } from "react";
 
-export function ToggleGroupStyles(props: ComponentProps<"div">) {
-	return <div {...props} className={classes(props.className, styles.group)} />;
-}
+export const ToggleGroupStyles: FC<ComponentProps<"div">> = (props) => (
+	<div {...props} className={classes(props.className, styles.group)} />
+);
 
-export function ToggleStyles(props: ComponentProps<"button">) {
-	return (
-		<button {...props} type="button" className={classes(props.className, "text-13", styles.item)} />
-	);
-}
+export const ToggleStyles: FC<ComponentProps<"button">> = (props) => (
+	<button {...props} type="button" className={classes(props.className, "text-13", styles.item)} />
+);
