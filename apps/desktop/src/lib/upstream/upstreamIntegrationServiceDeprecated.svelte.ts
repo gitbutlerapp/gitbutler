b5eea504deb95098130896f78af9eb5c3a914671
estib <stron@me.com>
2026-06-09 10:06:22 +0200

    upstream integration: upstream integration modal

---
+++ apps/desktop/src/lib/upstream/upstreamIntegrationServiceDeprecated.svelte.ts
@@ -0,0 +1,58 @@
+import { InjectionToken } from "@gitbutler/core/context";
+import { isDefined } from "@gitbutler/ui/utils/typeguards";
+import type { StackService } from "$lib/stacks/stackService.svelte";
+import type { BackendApi } from "$lib/state/backendApi";
+import type { StackStatusesWithBranchesV3 } from "$lib/upstream/typesDeprecated";
+
+export const UPSTREAM_INTEGRATION_SERVICE_DEPRECATED =
+	new InjectionToken<UpstreamIntegrationServiceDeprecated>("UpstreamIntegrationServiceDeprecated");
+
+export class UpstreamIntegrationServiceDeprecated {
+	constructor(
+		private backendApi: BackendApi,
+		private stackService: StackService,
+	) {}
+
+	async upstreamStatuses(
+		projectId: string,
+		targetCommitOid: string | undefined,
+	): Promise<StackStatusesWithBranchesV3 | undefined> {
+		const stacks = await this.stackService.fetchStacks(projectId);
+		const branchStatuses = await this.backendApi.endpoints.upstreamIntegrationStatuses.fetch({
+			projectId,
+			targetCommitOid,
+		});
+
+		if (branchStatuses.type === "upToDate") return branchStatuses;
+
+		const stackStatusesWithBranches: StackStatusesWithBranchesV3 = {
+			type: "updatesRequired",
+			worktreeConflicts: branchStatuses.subject.worktreeConflicts,
+			subject: branchStatuses.subject.statuses
+				.map((status) => {
+					const stack = stacks.find((appliedBranch) => appliedBranch.id === status[0]);
+
+					if (!stack) return;
+					return {
+						stack,
+						status: status[1],
+					};
+				})
+				.filter(isDefined),
+		};
+
+		return stackStatusesWithBranches;
+	}
+
+	resolveUpstreamIntegration() {
+		return this.backendApi.endpoints.resolveUpstreamIntegration.useMutation();
+	}
+
+	get resolveUpstreamIntegrationMutation() {
+		return this.backendApi.endpoints.resolveUpstreamIntegration.mutate;
+	}
+
+	integrateUpstream() {
+		return this.backendApi.endpoints.integrateUpstream.useMutation();
+	}
+}
