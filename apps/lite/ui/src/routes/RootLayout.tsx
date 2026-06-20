db236ba09419ebe5ff589d7715dce7ba811106e0
Pavel Laptev <pawellaptew@gmail.com>
2026-06-21 01:09:06 +0200

    Add folder animation and update styles
    some updates

--- apps/lite/ui/src/routes/RootLayout.tsx
+++ apps/lite/ui/src/routes/RootLayout.tsx
@@ -108,10 +108,13 @@ const Projects: FC = () => {
 							// the tooltip. Other props should be passed above.
 							render={<Button focusableWhenDisabled disabled={isSelected} />}
 						>
-							<div className={styles.folderFront}>
-								<span className={classes("text-bold", styles.folderFrontText)}>
-									{project.title.slice(0, 2)}
-								</span>
+							<div className={styles.folder}>
+								<div className={styles.folderFront}>
+									<span className={classes("text-bold", styles.folderFrontText)}>
+										{project.title.slice(0, 2)}
+									</span>
+								</div>
+								<div className={styles.folderBack} />
 							</div>
 							{shortcutSlot !== undefined && (
 								<span
