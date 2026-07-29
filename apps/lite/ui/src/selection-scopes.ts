a0d1216e312492ec4db899809197991a14242bf0
Sam A. Horvath-Hunt <hello@samhh.com>
2026-07-29 09:31:34 +0100

    Typo

--- apps/lite/ui/src/selection-scopes.ts
+++ apps/lite/ui/src/selection-scopes.ts
@@ -16,7 +16,7 @@ const allSelectionScopes: Set<string> = new Set([
 	"pr",
 ] satisfies Array<SelectionScope>);
 
-// Supports arbitarily nested scopes. Must share that ancestral relationship in the DOM. Tries from
+// Supports arbitrarily nested scopes. Must share that ancestral relationship in the DOM. Tries from
 // left to right.
 const selectionScopeChildren: Partial<Record<SelectionScope, Set<SelectionScope>>> = {
 	details: new Set(["diff", "pr", "files"]),
