8ebc8d6556e71230dbfc9be3668b4d3392aaa7b4
Sam A. Horvath-Hunt <hello@samhh.com>
2026-07-30 14:54:58 +0100

    Temporarily undo broken changes
    These broke the item metrics we provide to Pierre as per __devOnlyValidateItemHeights, harming performance.

--- apps/lite/ui/src/routes/project/$id/workspace/Details.tsx
+++ apps/lite/ui/src/routes/project/$id/workspace/Details.tsx
@@ -782,7 +782,7 @@ const DiffContents: FC<{
           [data-diff] {
             border-width: 0 1px 1px 1px;
             border-style: solid;
-            border: none;
+            /*border: none;*
           }
 
           [data-column-number] {
