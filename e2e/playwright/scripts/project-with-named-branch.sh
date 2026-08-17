46950cf1370e38239467cd3797692ad50ab0dcd9
estib <stron@me.com>
2026-08-17 15:32:39 +0200

    Fix Lite E2E package setup

--- e2e/playwright/scripts/project-with-named-branch.sh
+++ e2e/playwright/scripts/project-with-named-branch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "GIT CONFIG $GIT_CONFIG_GLOBAL"
+echo "DATA DIR $E2E_TEST_APP_DATA_DIR"
+echo "BUT $BUT"
+
 repository_name="${1:-onboarding-repository}"
 branch_name="${2:-onboarding-test}"
 
