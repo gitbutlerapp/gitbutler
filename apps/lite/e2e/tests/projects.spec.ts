9262319b52e9b2bd76eb127e4684e853c97342be
estib <stron@me.com>
2026-08-20 10:13:37 +0200

    Remove unused Lite test ID import

--- apps/lite/e2e/tests/projects.spec.ts
+++ apps/lite/e2e/tests/projects.spec.ts
@@ -1,5 +1,4 @@
 import path from "node:path";
-import { LiteTestId } from "../../ui/src/testIds.ts";
 import { expect, test } from "../test.ts";
 
 test.use({ scenario: "project-with-additional-repository.sh" });
