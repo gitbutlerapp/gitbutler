7651c032a62eb4ac95a73063ef9ea9e96c0d5ba6
Pavel Laptev <pawellaptew@gmail.com>
2026-08-27 14:00:03 +0200

    feat(badge): add pop badge variant

--- apps/lite/ui/src/components/Badge.tsx
+++ apps/lite/ui/src/components/Badge.tsx
@@ -11,6 +11,7 @@ export type BadgeVariant =
 	| "danger"
 	| "purple"
 	| "blue"
+	| "pop"
 	| "integrated";
 
 /** @public */
@@ -57,6 +58,7 @@ export const Badge: FC<{ variant: BadgeVariant; size?: BadgeSize } & ComponentPr
 				Match.when("danger", () => styles.danger),
 				Match.when("purple", () => styles.purple),
 				Match.when("blue", () => styles.blue),
+				Match.when("pop", () => styles.pop),
 				Match.when("integrated", () => styles.integrated),
 				Match.exhaustive,
 			),
